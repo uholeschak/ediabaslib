363d8ffae650a53b590df5922122c4efc63afb53
uholeschak <ulrich@holeschak.de>
2014-09-19 11:03:20 +0200

    Fixed symlinks

--- CreateLinks.bat
+++ CreateLinks.bat
@@ -1,3 +1,4 @@
 mklink /j D:\Projects\EdiabasLibUH\CarControl D:\Projects\EdiabasLib\CarControl
 mklink /j D:\Projects\EdiabasLibUH\Tools\CarSimulator D:\Projects\EdiabasLib\Tools\CarSimulator
 mklink /j D:\Projects\EdiabasLibUH\ToolsSetup D:\Projects\EdiabasLib\ToolsSetup
+mklink /j D:\Projects\EdiabasLibUH\EdiabasLib D:\Projects\EdiabasLib\EdiabasLib
