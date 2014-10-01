c9705a45178567a69408b7f45fdd0272d8e2f882
uholeschak <ulrich@holeschak.de>
2014-10-01 13:41:35 +0200

    Don't change consol encoding

--- EdiabasLib/Test/EdiabasCall/Program.cs
+++ EdiabasLib/Test/EdiabasCall/Program.cs
@@ -68,7 +68,6 @@ namespace EdiabasCall
 
             if (outFile == null)
             {
-                Console.OutputEncoding = encoding;
                 outputWriter = Console.Out;
             }
             else
