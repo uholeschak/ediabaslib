d7957dd4738e3b291820cc1215661b99564b8705
uholeschak <ulrich@holeschak.de>
2014-11-23 16:32:47 +0100

    Hide compiler message

--- EdiabasLib/Api32/CompileNetmodule.bat
+++ EdiabasLib/Api32/CompileNetmodule.bat
@@ -1,6 +1,7 @@
 @echo off
+echo Compile netmodule
 SETLOCAL EnableDelayedExpansion
 
 set BATPATH=%~dp0
 
-"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\csc.exe" %* /target:module "!BATPATH!..\ApiInternal\ApiInternal.cs" "!BATPATH!..\EdiabasLib\EdiabasNet.cs" "!BATPATH!..\EdiabasLib\EdOperations.cs" "!BATPATH!..\EdiabasLib\MemoryStreamReader.cs" "!BATPATH!..\EdiabasLib\EdInterfaceBase.cs" "!BATPATH!..\EdiabasLib\EdInterfaceObd.cs"
+"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\csc.exe" %* /nologo /target:module "!BATPATH!..\ApiInternal\ApiInternal.cs" "!BATPATH!..\EdiabasLib\EdiabasNet.cs" "!BATPATH!..\EdiabasLib\EdOperations.cs" "!BATPATH!..\EdiabasLib\MemoryStreamReader.cs" "!BATPATH!..\EdiabasLib\EdInterfaceBase.cs" "!BATPATH!..\EdiabasLib\EdInterfaceObd.cs"
