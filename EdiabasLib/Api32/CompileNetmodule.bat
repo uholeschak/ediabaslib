d53974c7a6740a976312ce58921a819829bb77a2
uholeschak <ulrich@holeschak.de>
2014-12-14 18:32:20 +0100

    Using seperate class for ADS interface

--- EdiabasLib/Api32/CompileNetmodule.bat
+++ EdiabasLib/Api32/CompileNetmodule.bat
@@ -3,4 +3,4 @@ SETLOCAL EnableDelayedExpansion
 
 set BATPATH=%~dp0
 
-"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\csc.exe" %* /nologo /target:module "!BATPATH!..\ApiInternal\ApiInternal.cs" "!BATPATH!..\EdiabasLib\EdiabasNet.cs" "!BATPATH!..\EdiabasLib\EdOperations.cs" "!BATPATH!..\EdiabasLib\MemoryStreamReader.cs" "!BATPATH!..\EdiabasLib\EdInterfaceBase.cs" "!BATPATH!..\EdiabasLib\EdInterfaceObd.cs"
+"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\csc.exe" %* /nologo /target:module "!BATPATH!..\ApiInternal\ApiInternal.cs" "!BATPATH!..\EdiabasLib\EdiabasNet.cs" "!BATPATH!..\EdiabasLib\EdOperations.cs" "!BATPATH!..\EdiabasLib\MemoryStreamReader.cs" "!BATPATH!..\EdiabasLib\EdInterfaceBase.cs" "!BATPATH!..\EdiabasLib\EdInterfaceObd.cs" "!BATPATH!..\EdiabasLib\EdInterfaceAds.cs"
