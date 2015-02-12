8acd841bd44912069f7a4d9fb779967ccdc6e0e2
uholeschak <ulrich@holeschak.de>
2015-02-13 00:23:16 +0100

    Added axis handling

--- CarControl/CarControlAndroid/MainActivity.cs
+++ CarControl/CarControlAndroid/MainActivity.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Android.App;
@@ -9,6 +9,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using EdiabasLib;
 using CarControl;
 
 namespace CarControlAndroid
@@ -28,8 +29,11 @@ namespace CarControlAndroid
         private string ecuPath;
         private BluetoothAdapter bluetoothAdapter = null;
         private CommThread commThread;
-        private Button buttonConnect;
-        private TextView textViewResult;
+        private ToggleButton buttonConnect;
+        private ToggleButton buttonAxisDown;
+        private ToggleButton buttonAxisUp;
+        private TextView textViewResultAxis;
+        private TextView textViewResultTest;
 
         protected override void OnCreate (Bundle bundle)
         {
@@ -37,9 +41,13 @@ namespace CarControlAndroid
 
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.main);
-            CreateTab("test", "Test");
-            buttonConnect = FindViewById<Button> (Resource.Id.buttonConnect);
-            textViewResult = FindViewById<TextView> (Resource.Id.textViewResult);
+            CreateTab("axis", GetString (Resource.String.tab_axis), Resource.Id.tabAxis);
+            CreateTab("test", GetString (Resource.String.tab_test), Resource.Id.tabTest);
+            buttonConnect = FindViewById<ToggleButton> (Resource.Id.buttonConnect);
+            buttonAxisUp = FindViewById<ToggleButton> (Resource.Id.button_axis_up);
+            buttonAxisDown = FindViewById<ToggleButton> (Resource.Id.button_axis_down);
+            textViewResultAxis = FindViewById<TextView> (Resource.Id.textViewResultAxis);
+            textViewResultTest = FindViewById<TextView> (Resource.Id.textViewResultTest);
 
             // Get local Bluetooth adapter
             bluetoothAdapter = BluetoothAdapter.DefaultAdapter;
@@ -61,16 +69,19 @@ namespace CarControlAndroid
 
             // Get our button from the layout resource,
             // and attach an event to it
-            buttonConnect.Click += ButtonClick;
+            TabHost.TabChanged += HandleTabChanged;
+            buttonConnect.Click += ButtonConnectClick;
+            buttonAxisDown.Click += ButtonAxisDownClick;
+            buttonAxisUp.Click += ButtonAxisUpClick;
 
             UpdateDisplay ();
         }
 
-        private void CreateTab(string tag, string label)
+        private void CreateTab(string tag, string label, int viewId)
         {
             TabHost.TabSpec spec = TabHost.NewTabSpec(tag);
             spec.SetIndicator(label);
-            spec.SetContent(Resource.Id.tabTest);
+            spec.SetContent(viewId);
             TabHost.AddTab(spec);
         }
 
@@ -159,7 +170,13 @@ namespace CarControlAndroid
             return base.OnOptionsItemSelected(item);
         }
 
-        protected void ButtonClick (object sender, EventArgs e)
+        protected void HandleTabChanged (object sender, TabHost.TabChangeEventArgs e)
+        {
+            UpdateSelectedDevice ();
+            UpdateDisplay ();
+        }
+
+        protected void ButtonConnectClick (object sender, EventArgs e)
         {
             if (commThread != null && commThread.ThreadRunning())
             {
@@ -168,8 +185,43 @@ namespace CarControlAndroid
             else
             {
                 StartCommThread ();
+                UpdateSelectedDevice();
             }
             UpdateDisplay();
+        }
+
+        protected void ButtonAxisDownClick (object sender, EventArgs e)
+        {
+            if (commThread == null)
+            {
+                return;
+            }
+            if (buttonAxisDown.Checked)
+            {
+                commThread.AxisOpMode = CommThread.OperationMode.OpModeDown;
+                buttonAxisUp.Checked = false;
+            }
+            else
+            {
+                if (commThread != null) commThread.AxisOpMode = CommThread.OperationMode.OpModeStatus;
+            }
+        }
+
+        protected void ButtonAxisUpClick (object sender, EventArgs e)
+        {
+            if (commThread == null)
+            {
+                return;
+            }
+            if (buttonAxisUp.Checked)
+            {
+                commThread.AxisOpMode = CommThread.OperationMode.OpModeUp;
+                buttonAxisDown.Checked = false;
+            }
+            else
+            {
+                commThread.AxisOpMode = CommThread.OperationMode.OpModeStatus;
+            }
         }
 
         private bool StartCommThread()
@@ -260,6 +312,54 @@ namespace CarControlAndroid
             UpdateDisplay();
         }
 
+        private void UpdateSelectedDevice()
+        {
+            if ((commThread == null) || !commThread.ThreadRunning())
+            {
+                return;
+            }
+
+            switch (TabHost.CurrentTab)
+            {
+                case 0:
+                default:
+                    commThread.Device = CommThread.SelectedDevice.DeviceAxis;
+                    break;
+#if false
+                case 1:
+                    commThread.Device = CommThread.SelectedDevice.DeviceMotor;
+                    break;
+
+                case 2:
+                    commThread.Device = CommThread.SelectedDevice.DeviceMotorUnevenRunning;
+                    break;
+
+                case 3:
+                    commThread.Device = CommThread.SelectedDevice.DeviceMotorRotIrregular;
+                    break;
+
+                case 4:
+                    commThread.Device = CommThread.SelectedDevice.DeviceMotorPM;
+                    break;
+
+                case 5:
+                    commThread.Device = CommThread.SelectedDevice.DeviceCccNav;
+                    break;
+
+                case 6:
+                    commThread.Device = CommThread.SelectedDevice.DeviceIhk;
+                    break;
+
+                case 7:
+                    commThread.Device = CommThread.SelectedDevice.DeviceErrors;
+                    break;
+#endif
+                case 1:
+                    commThread.Device = CommThread.SelectedDevice.Test;
+                    break;
+            }
+        }
+
         private void UpdateDisplay()
         {
             DataUpdatedMethode();
@@ -267,33 +367,281 @@ namespace CarControlAndroid
 
         private void DataUpdatedMethode()
         {
+            bool axisDataValid = false;
+            bool motorDataValid = false;
+            bool motorDataUnevenRunningValid = false;
+            bool motorRotIrregularValid = false;
+            bool motorPmValid = false;
+            bool cccNavValid = false;
+            bool ihkValid = false;
+            bool errorsValid = false;
             bool testValid = false;
-            bool buttonEnable = true;
+            bool buttonConnectEnable = true;
+
             if (commThread != null && commThread.ThreadRunning ())
             {
                 if (commThread.ThreadStopping ())
                 {
-                    buttonEnable = false;
+                    buttonConnectEnable = false;
                 }
-                buttonConnect.Text = GetString (Resource.String.button_disconnect);
+                switch (commThread.Device)
+                {
+                    case CommThread.SelectedDevice.DeviceAxis:
+                        axisDataValid = true;
+                        break;
+
+                    case CommThread.SelectedDevice.DeviceMotor:
+                        motorDataValid = true;
+                        break;
+
+                    case CommThread.SelectedDevice.DeviceMotorUnevenRunning:
+                        motorDataUnevenRunningValid = true;
+                        break;
+
+                    case CommThread.SelectedDevice.DeviceMotorRotIrregular:
+                        motorRotIrregularValid = true;
+                        break;
+
+                    case CommThread.SelectedDevice.DeviceMotorPM:
+                        motorPmValid = true;
+                        break;
+
+                    case CommThread.SelectedDevice.DeviceCccNav:
+                        cccNavValid = true;
+                        break;
+
+                    case CommThread.SelectedDevice.DeviceIhk:
+                        ihkValid = true;
+                        break;
+
+                    case CommThread.SelectedDevice.DeviceErrors:
+                        errorsValid = true;
+                        break;
+
+                    case CommThread.SelectedDevice.Test:
+                        testValid = true;
+                        break;
+                }
+
+                buttonConnect.Checked = true;
                 if (commThread.Device == CommThread.SelectedDevice.Test) testValid = true;
             }
             else
             {
-                buttonConnect.Text = GetString (Resource.String.button_connect);
+                buttonConnect.Checked = false;
             }
-            buttonConnect.Enabled = buttonEnable;
+            buttonConnect.Enabled = buttonConnectEnable;
 
-            string textViewText = string.Empty;
+            if (axisDataValid)
+            {
+                string outputText = string.Empty;
+                string tempText;
+                bool found;
+                Dictionary<string, EdiabasNet.ResultData> resultDict = null;
+                lock (CommThread.DataLock)
+                {
+                    resultDict = commThread.EdiabasResultDict;
+                }
+                Int64 axisMode = GetResultInt64(resultDict, "MODE_CTRL_LESEN_WERT", out found);
+                tempText = string.Empty;
+                if (found)
+                {
+                    if ((axisMode & CommThread.AxisModeConveyor) != 0x00)
+                    {
+                        tempText = GetString (Resource.String.axis_mode_conveyor);
+                    }
+                    else if ((axisMode & CommThread.AxisModeTransport) != 0x00)
+                    {
+                        tempText = GetString (Resource.String.axis_mode_transport);
+                    }
+                    else if ((axisMode & CommThread.AxisModeGarage) != 0x00)
+                    {
+                        tempText = GetString (Resource.String.axis_mode_garage);
+                    }
+                    else
+                    {
+                        tempText = GetString (Resource.String.axis_mode_normal);
+                    }
+                }
+                outputText += GetString (Resource.String.label_axis_mode) + " " + tempText + "\r\n";
+
+                tempText = FormatResultInt64(resultDict, "ORGFASTFILTER_RL", "{0,4}");
+                if (tempText.Length > 0) tempText += " / ";
+                tempText += FormatResultInt64(resultDict, "FASTFILTER_RL", "{0,4}");
+                outputText += GetString (Resource.String.label_axis_left) + " " + tempText + "\r\n";
+
+                tempText = FormatResultInt64(resultDict, "ORGFASTFILTER_RR", "{0,4}");
+                if (tempText.Length > 0) tempText += " / ";
+                tempText += FormatResultInt64(resultDict, "FASTFILTER_RR", "{0,4}");
+                outputText += GetString (Resource.String.label_axis_right) + " " + tempText + "\r\n";
+
+                Int64 voltage = GetResultInt64(resultDict, "ANALOG_U_KL30", out found);
+                if (found)
+                {
+                    tempText = string.Format("{0,6:0.00}", (double)voltage / 1000);
+                }
+                else
+                {
+                    tempText = string.Empty;
+                }
+                outputText += GetString (Resource.String.label_axis_bat_volt) + " " + tempText + "\r\n";
+
+                tempText = FormatResultInt64(resultDict, "STATE_SPEED", "{0,4}");
+                outputText += GetString (Resource.String.label_axis_speed) + " " + tempText + "\r\n";
+
+                tempText = string.Empty;
+                for (int channel = 0; channel < 4; channel++)
+                {
+                    tempText = FormatResultInt64(resultDict, string.Format("STATUS_SIGNALE_NUMERISCH{0}_WERT", channel), "{0}") + tempText;
+                }
+                outputText += GetString (Resource.String.label_axis_valve_state) + " " + tempText + "\r\n";
+
+                Int64 speed = GetResultInt64(resultDict, "STATE_SPEED", out found);
+                if (!found) speed = 0;
+
+                if (speed < 5)
+                {
+                    buttonAxisDown.Enabled = true;
+                }
+                else
+                {
+                    if (commThread.AxisOpMode == CommThread.OperationMode.OpModeDown)
+                    {
+                        commThread.AxisOpMode = CommThread.OperationMode.OpModeStatus;
+                    }
+                    buttonAxisDown.Checked = false;
+                    buttonAxisDown.Enabled = false;
+                }
+                buttonAxisUp.Enabled = true;
+                textViewResultAxis.Text = outputText;
+            }
+            else
+            {
+                textViewResultAxis.Text = string.Empty;
+                buttonAxisDown.Checked = false;
+                buttonAxisDown.Enabled = false;
+                buttonAxisUp.Checked = false;
+                buttonAxisUp.Enabled = false;
+                if (commThread != null) commThread.AxisOpMode = CommThread.OperationMode.OpModeStatus;
+            }
+
+            if (motorDataValid)
+            {
+            }
+
+            if (motorDataUnevenRunningValid)
+            {
+            }
+
+            if (motorRotIrregularValid)
+            {
+            }
+
+            if (motorPmValid)
+            {
+            }
+
+            if (cccNavValid)
+            {
+            }
+
+            if (ihkValid)
+            {
+            }
+
+            if (errorsValid)
+            {
+            }
+
             if (testValid)
             {
                 lock (CommThread.DataLock)
                 {
-                    textViewText = commThread.TestResult;
+                    textViewResultTest.Text = commThread.TestResult;
                 }
             }
+            else
+            {
+                textViewResultTest.Text = string.Empty;
+            }
+        }
 
-            textViewResult.Text = textViewText;
+        private String FormatResultDouble(Dictionary<string, EdiabasNet.ResultData> resultDict, string dataName, string format)
+        {
+            bool found;
+            double value = GetResultDouble(resultDict, dataName, out found);
+            if (found)
+            {
+                return string.Format(format, value);
+            }
+            return string.Empty;
+        }
+
+        private String FormatResultInt64(Dictionary<string, EdiabasNet.ResultData> resultDict, string dataName, string format)
+        {
+            bool found;
+            Int64 value = GetResultInt64(resultDict, dataName, out found);
+            if (found)
+            {
+                return string.Format(format, value);
+            }
+            return string.Empty;
+        }
+
+        private String FormatResultString(Dictionary<string, EdiabasNet.ResultData> resultDict, string dataName, string format)
+        {
+            bool found;
+            string value = GetResultString(resultDict, dataName, out found);
+            if (found)
+            {
+                return string.Format(format, value);
+            }
+            return string.Empty;
+        }
+
+        private Int64 GetResultInt64(Dictionary<string, EdiabasNet.ResultData> resultDict, string dataName, out bool found)
+        {
+            found = false;
+            EdiabasNet.ResultData resultData;
+            if (resultDict != null && resultDict.TryGetValue(dataName, out resultData))
+            {
+                if (resultData.opData.GetType() == typeof(Int64))
+                {
+                    found = true;
+                    return (Int64)resultData.opData;
+                }
+            }
+            return 0;
+        }
+
+        private Double GetResultDouble(Dictionary<string, EdiabasNet.ResultData> resultDict, string dataName, out bool found)
+        {
+            found = false;
+            EdiabasNet.ResultData resultData;
+            if (resultDict != null && resultDict.TryGetValue(dataName, out resultData))
+            {
+                if (resultData.opData.GetType() == typeof(Double))
+                {
+                    found = true;
+                    return (Double)resultData.opData;
+                }
+            }
+            return 0;
+        }
+
+        private String GetResultString(Dictionary<string, EdiabasNet.ResultData> resultDict, string dataName, out bool found)
+        {
+            found = false;
+            EdiabasNet.ResultData resultData;
+            if (resultDict != null && resultDict.TryGetValue(dataName, out resultData))
+            {
+                if (resultData.opData.GetType() == typeof(String))
+                {
+                    found = true;
+                    return (String)resultData.opData;
+                }
+            }
+            return string.Empty;
         }
 
         private bool CopyAssets(string ecuPath)
--- CarControl/CarControlAndroid/Resources/Resource.designer.cs
+++ CarControl/CarControlAndroid/Resources/Resource.designer.cs
@@ -70,8 +70,14 @@ namespace CarControlAndroid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f06000d
+			public const int buttonConnect = 2131099661;
+			
 			// aapt resource value: 0x7f060008
-			public const int buttonConnect = 2131099656;
+			public const int button_axis_down = 2131099656;
+			
+			// aapt resource value: 0x7f060009
+			public const int button_axis_up = 2131099657;
 			
 			// aapt resource value: 0x7f060004
 			public const int button_scan = 2131099652;
@@ -82,17 +88,26 @@ namespace CarControlAndroid
 			// aapt resource value: 0x7f060001
 			public const int paired_devices = 2131099649;
 			
-			// aapt resource value: 0x7f060009
-			public const int scan = 2131099657;
+			// aapt resource value: 0x7f06000e
+			public const int scan = 2131099662;
 			
 			// aapt resource value: 0x7f060006
-			public const int scrollViewText = 2131099654;
+			public const int scrollViewTextAxis = 2131099654;
+			
+			// aapt resource value: 0x7f06000b
+			public const int scrollViewTextTest = 2131099659;
 			
 			// aapt resource value: 0x7f060005
-			public const int tabTest = 2131099653;
+			public const int tabAxis = 2131099653;
+			
+			// aapt resource value: 0x7f06000a
+			public const int tabTest = 2131099658;
 			
 			// aapt resource value: 0x7f060007
-			public const int textViewResult = 2131099655;
+			public const int textViewResultAxis = 2131099655;
+			
+			// aapt resource value: 0x7f06000c
+			public const int textViewResultTest = 2131099660;
 			
 			// aapt resource value: 0x7f060002
 			public const int title_new_devices = 2131099650;
@@ -151,47 +166,89 @@ namespace CarControlAndroid
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040003
-			public const int app_name = 2130968579;
-			
-			// aapt resource value: 0x7f040005
-			public const int bt_not_available = 2130968581;
-			
-			// aapt resource value: 0x7f040004
-			public const int bt_not_enabled_leaving = 2130968580;
-			
 			// aapt resource value: 0x7f040001
-			public const int button_connect = 2130968577;
+			public const int app_name = 2130968577;
 			
-			// aapt resource value: 0x7f040002
-			public const int button_disconnect = 2130968578;
+			// aapt resource value: 0x7f04000a
+			public const int axis_mode_conveyor = 2130968586;
+			
+			// aapt resource value: 0x7f04000b
+			public const int axis_mode_garage = 2130968587;
 			
 			// aapt resource value: 0x7f04000c
-			public const int button_scan = 2130968588;
+			public const int axis_mode_normal = 2130968588;
 			
 			// aapt resource value: 0x7f04000d
-			public const int connect = 2130968589;
+			public const int axis_mode_transport = 2130968589;
+			
+			// aapt resource value: 0x7f040009
+			public const int bt_not_available = 2130968585;
+			
+			// aapt resource value: 0x7f040008
+			public const int bt_not_enabled_leaving = 2130968584;
+			
+			// aapt resource value: 0x7f040007
+			public const int button_axis_down = 2130968583;
+			
+			// aapt resource value: 0x7f040006
+			public const int button_axis_up = 2130968582;
+			
+			// aapt resource value: 0x7f040004
+			public const int button_connected = 2130968580;
+			
+			// aapt resource value: 0x7f040005
+			public const int button_disconnected = 2130968581;
+			
+			// aapt resource value: 0x7f04001a
+			public const int button_scan = 2130968602;
+			
+			// aapt resource value: 0x7f04001b
+			public const int connect = 2130968603;
+			
+			// aapt resource value: 0x7f040011
+			public const int label_axis_bat_volt = 2130968593;
+			
+			// aapt resource value: 0x7f04000f
+			public const int label_axis_left = 2130968591;
+			
+			// aapt resource value: 0x7f04000e
+			public const int label_axis_mode = 2130968590;
+			
+			// aapt resource value: 0x7f040010
+			public const int label_axis_right = 2130968592;
+			
+			// aapt resource value: 0x7f040012
+			public const int label_axis_speed = 2130968594;
+			
+			// aapt resource value: 0x7f040013
+			public const int label_axis_valve_state = 2130968595;
 			
 			// aapt resource value: 0x7f040000
 			public const int library_name = 2130968576;
 			
-			// aapt resource value: 0x7f040009
-			public const int none_found = 2130968585;
+			// aapt resource value: 0x7f040017
+			public const int none_found = 2130968599;
 			
-			// aapt resource value: 0x7f040008
-			public const int none_paired = 2130968584;
+			// aapt resource value: 0x7f040016
+			public const int none_paired = 2130968598;
 			
-			// aapt resource value: 0x7f040006
-			public const int scanning = 2130968582;
+			// aapt resource value: 0x7f040014
+			public const int scanning = 2130968596;
 			
-			// aapt resource value: 0x7f040007
-			public const int select_device = 2130968583;
+			// aapt resource value: 0x7f040015
+			public const int select_device = 2130968597;
 			
-			// aapt resource value: 0x7f04000b
-			public const int title_other_devices = 2130968587;
+			// aapt resource value: 0x7f040002
+			public const int tab_axis = 2130968578;
 			
-			// aapt resource value: 0x7f04000a
-			public const int title_paired_devices = 2130968586;
+			// aapt resource value: 0x7f040003
+			public const int tab_test = 2130968579;
+			
+			// aapt resource value: 0x7f040019
+			public const int title_other_devices = 2130968601;
+			
+			// aapt resource value: 0x7f040018
+			public const int title_paired_devices = 2130968600;
 			
 			static String()
 			{
