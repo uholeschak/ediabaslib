3313862170365874af3e2aa7790da6c62717d01b
uholeschak <ulrich@holeschak.de>
2015-02-14 00:09:22 +0100

    Removed unused components

--- CarControl/CarControlAndroid/Resources/Resource.designer.cs
+++ CarControl/CarControlAndroid/Resources/Resource.designer.cs
@@ -46,16 +46,7 @@ namespace CarControlAndroid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int ic_tab_test = 2130837504;
-			
-			// aapt resource value: 0x7f020001
-			public const int ic_tab_test_selected = 2130837505;
-			
-			// aapt resource value: 0x7f020002
-			public const int ic_tab_test_unselected = 2130837506;
-			
-			// aapt resource value: 0x7f020003
-			public const int Icon = 2130837507;
+			public const int Icon = 2130837504;
 			
 			static Drawable()
 			{
