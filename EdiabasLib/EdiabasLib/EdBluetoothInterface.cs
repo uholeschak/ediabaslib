bbe7ba8d2602604a351f0c0db1445f0370e7b605
uholeschak <ulrich@holeschak.de>
2015-03-02 13:16:38 +0100

    Timeout increased

--- EdiabasLib/EdiabasLib/EdBluetoothInterface.cs
+++ EdiabasLib/EdiabasLib/EdBluetoothInterface.cs
@@ -13,7 +13,7 @@ namespace EdiabasLib
         public const string PortID = "BLUETOOTH";
         private static UUID SPP_UUID = UUID.FromString ("00001101-0000-1000-8000-00805F9B34FB");
         private static readonly long tickResolMs = Stopwatch.Frequency / 1000;
-        private const int readTimeoutOffset = 1000;
+        private const int readTimeoutOffset = 1500;
 
         private static BluetoothSocket bluetoothSocket;
         private static Stream bluetoothInStream;
