0a9c8fc885a1dc3e20254ad2a422185c9245d8e4
uholeschak <ulrich@holeschak.de>
2015-10-19 12:56:57 +0200

    Started with ELM based adapter

---
+++ EdiabasLib/CanAdapterElm/CanAdapterElm.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=CanAdapterElm.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/CanAdapterElm.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=canadapterelm.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/canadapterelm.x.tar
