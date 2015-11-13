92984ea3ed9b3ef7a0d4ff52e5d77f28a500343c
uholeschak <ulrich@holeschak.de>
2015-11-14 00:47:26 +0100

    Added adapter firmware

--- BmwDeepObd/CreateReleasePackage.bat
+++ BmwDeepObd/CreateReleasePackage.bat
@@ -8,6 +8,8 @@ echo !DATESTR!
 set PACKAGEPATH="!BATPATH!Package\"
 set CONFIGPATH="!PACKAGEPATH!Config\"
 set ECUPATH="!PACKAGEPATH!Ecu\"
+set CANADAPTERPATH="!PACKAGEPATH!CanAdapter\"
+set CANADAPTERELMPATH="!PACKAGEPATH!CanAdapterElm\"
 if exist "!PACKAGEPATH!" rmdir /s /q "!PACKAGEPATH!"
 mkdir "!PACKAGEPATH!"
 copy "!BATPATH!ReadMe.txt" "!PACKAGEPATH!"
@@ -17,6 +19,14 @@ xcopy /y /e "!BATPATH!Xml\*.*" "!CONFIGPATH!"
 
 mkdir "!ECUPATH!"
 copy "!BATPATH!..\EdiabasLib\Test\Ecu\adapter_prg.prg" "!ECUPATH!"
+
+mkdir "!CANADAPTERPATH!"
+copy "!BATPATH!..\EdiabasLib\CanAdapter\CanAdapter\Release\*.hex" "!CANADAPTERPATH!"
+copy "!BATPATH!..\EdiabasLib\CanAdapter\Pld\*.jed" "!CANADAPTERPATH!"
+copy "!BATPATH!..\EdiabasLib\CanAdapter\UpdateLoader\bin\*.exe" "!CANADAPTERPATH!"
+
+mkdir "!CANADAPTERELMPATH!"
+copy "!BATPATH!..\EdiabasLib\CanAdapterElm\CanAdapterElm.X\dist\default\production\*.hex" "!CANADAPTERELMPATH!"
 
 set PACKAGEZIP="!BATPATH!Android-!DATESTR!.zip"
 if exist "!PACKAGEZIP!" del /f /q "!PACKAGEZIP!"
