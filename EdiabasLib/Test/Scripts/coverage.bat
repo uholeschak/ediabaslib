456f972812ca6fa609afe8820ad567c584ddf35a
uholeschak <ulrich@holeschak.de>
2016-01-02 12:24:48 +0100

    Removed timeout offset

--- EdiabasLib/Test/Scripts/coverage.bat
+++ EdiabasLib/Test/Scripts/coverage.bat
@@ -23,7 +23,7 @@ set EDIABAS_TEST=!BATPATH!\..\..\EdiabasTest\bin\Debug\EdiabasTest.exe
 set OUTFILE=output_lib.log
 rem set ADD_ARGS=-p !COMPORT! -o "!OUTFILE!" -a -c
 set ADD_ARGS=--ifh="!IFH!" -o "!OUTFILE!" -a -c
-set ADD_ARGS=!ADD_ARGS! --cfg="ObdComPort=!COMPORT!;ObdAddRecTimeout=500"
+set ADD_ARGS=!ADD_ARGS! --cfg="ObdComPort=!COMPORT!"
 set FILTERS=+[EdiabasLib]*
 set COVERAGE=1
 goto argsok
@@ -33,7 +33,7 @@ set EDIABAS_TEST=!BATPATH!\..\EdiabasLibCall\bin\Debug\EdiabasLibCall.exe
 set OUTFILE=output_apilib.log
 set ADD_ARGS=-o !OUTFILE! --ifh="!IFH!" --device="_" -a -c
 rem set ADD_ARGS=!ADD_ARGS! --cfg="@!BATPATH!\EdiabasLib.config"
-set ADD_ARGS=!ADD_ARGS! --cfg="ObdComPort=!COMPORT!;ObdAddRecTimeout=500"
+set ADD_ARGS=!ADD_ARGS! --cfg="ObdComPort=!COMPORT!"
 set FILTERS=+[EdiabasLib]* +[apiNET32]*
 set COVERAGE=1
 goto argsok
