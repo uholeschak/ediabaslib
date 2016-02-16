fe55359d1ad392b06891e0f3e59d509c119ccce4
uholeschak <ulrich@holeschak.de>
2016-02-16 11:19:41 +0100

    Prepared firmware updates

--- EdiabasLib/CanAdapterElm/Bluetooth/spp_uart/dfu_build.bat
+++ EdiabasLib/CanAdapterElm/Bluetooth/spp_uart/dfu_build.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\CSR\BlueSuite 2.6.2\DfuBuild.exe" -v -pedantic -uv 0xffff -up 0xffff -ui "Version 1.0" -f spp_uart.dfu -s "C:\Programs\BlueLab41\firmware\vm\unified\coyote\stack_unsigned.xpv" -p3 . spp_uart.psr . -a spp_uart.app
+"C:\Program Files (x86)\CSR\BlueSuite 2.6.2\DfuBuild.exe" -v -pedantic -uv 0x0a12 -up 0x0001 -ui "Version 1.0" -f spp_uart.dfu -s "C:\Programs\BlueLab41\firmware\vm\unified\coyote\stack_unsigned.xpv" -a spp_uart.app -p3 . spp_uart.psr .
