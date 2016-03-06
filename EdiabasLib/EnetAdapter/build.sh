4ef7d92e0e576f6f098ef74345c297a4a3ad68f6
uholeschak <ulrich@holeschak.de>
2016-03-06 12:14:17 +0100

    Added translations and theme

--- EdiabasLib/EnetAdapter/build.sh
+++ EdiabasLib/EnetAdapter/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # build with: http://downloads.openwrt.org/chaos_calmer/15.05/ramips/rt305x/OpenWrt-ImageBuilder-15.05-ramips-rt305x.Linux-x86_64.tar.bz2
-make image PROFILE="A5-V11" PACKAGES="luci" FILES=files/
+make image PROFILE="A5-V11" PACKAGES="luci luci-theme-openwrt luci-i18n-base-de luci-i18n-base-en luci-i18n-base-es luci-i18n-base-fr luci-i18n-base-it luci-i18n-base-ja luci-i18n-base-ru" FILES=files/
