3b67497a7c2e93dcee26d828129b782a8f43868c
uholeschak <ulrich@holeschak.de>
2016-09-26 23:32:48 +0200

    Use variable for keil armcc

--- EdiabasLib/CanAdapterElm/Other/Bk3231Flash/bk3231_make.sh
+++ EdiabasLib/CanAdapterElm/Other/Bk3231Flash/bk3231_make.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Use in mingw shell
-# KEIL 5 ARM MDK must be installed at: /c/Programs/Keil_v5
+# KEIL 5 ARM MDK must be installed at: PATH_KEILARM
 # flash_write_loader D:/Projects/EdiabasLib/EdiabasLib/CanAdapterElm/Other/Bk3231Flash/write_flash.bin
 
-export PATH=$PATH:/c/Programs/Keil_v5/ARM/ARMCC/bin/
+arm_path=$(echo "/$PATH_KEILARM/ARMCC/bin/" | sed 's/\\/\//g' | sed 's/://')
+export PATH=$arm_path:$PATH
 make -k -B
