0b4bf1e4c53828a97c3ce2ec421eeb6ac4e6005e
uholeschak <ulrich@holeschak.de>
2016-09-26 12:16:35 +0200

    Check if loader is present

--- EdiabasLib/CanAdapterElm/Other/Bk3231Flash/bk3231_make.sh
+++ EdiabasLib/CanAdapterElm/Other/Bk3231Flash/bk3231_make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Use in mingw shell
 # KEIL 5 ARM MDK must be installed at: /c/Programs/Keil_v5
-# flash_write_area D:/Projects/EdiabasLib/EdiabasLib/CanAdapterElm/Other/Bk3231Flash/write_flash.bin 0x3E000
+# flash_write_loader D:/Projects/EdiabasLib/EdiabasLib/CanAdapterElm/Other/Bk3231Flash/write_flash.bin
 
 export PATH=$PATH:/c/Programs/Keil_v5/ARM/ARMCC/bin/
 make -k -B
