ff6cb2928a782e7fba625a2f922371b2ca5e4500
uholeschak <ulrich@holeschak.de>
2016-11-20 10:31:33 +0100

    Added 32feetNET to api32.dll

---
+++ EdiabasLib/32feetNET/InTheHand.Net.Personal/InTheHand.Net.Personal/Properties/AssemblyInfoNetmodule.cs
@@ -0,0 +1,9 @@
+﻿//#define NO_SIGNING
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#if WinXP
+[assembly: ComVisible(false)]
+#endif
+[assembly: System.CLSCompliant(true)]
