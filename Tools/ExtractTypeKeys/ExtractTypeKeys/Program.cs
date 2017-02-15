d233b3cc3311c42406d8f836059a5983426a42f5
uholeschak <ulrich@holeschak.de>
2017-02-15 18:38:05 +0100

    Removed warnings

--- Tools/ExtractTypeKeys/ExtractTypeKeys/Program.cs
+++ Tools/ExtractTypeKeys/ExtractTypeKeys/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExtractTypeKeys
 {
