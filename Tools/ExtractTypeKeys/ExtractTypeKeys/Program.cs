d310ada44654e063bc81c2d83b84292208ea0e42
uholeschak <ulrich@holeschak.de>
2017-02-20 00:19:38 +0100

    Create database zip automatically

--- Tools/ExtractTypeKeys/ExtractTypeKeys/Program.cs
+++ Tools/ExtractTypeKeys/ExtractTypeKeys/Program.cs
@@ -1,73 +1,133 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
 
 namespace ExtractTypeKeys
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             if (args.Length < 1)
             {
                 Console.WriteLine("No Database specified");
-                return;
+                return 1;
             }
-            TextWriter writer = null;
+            if (args.Length < 2)
+            {
+                Console.WriteLine("No output directory specified");
+                return 1;
+            }
+            string outDir = args[1];
+            if (string.IsNullOrEmpty(outDir))
+            {
+                Console.WriteLine("No output directory empty");
+                return 1;
+            }
+
             try
             {
-                string outFile = (args.Length < 2) ? string.Empty : args[1];
-                writer = string.IsNullOrEmpty(outFile) ? Console.Out : new StreamWriter(outFile);
+                List<string> zipFiles = new List<string>();
                 string connection = "Data Source=\"" + args[0] + "\";";
                 using (SQLiteConnection mDbConnection = new SQLiteConnection(connection))
                 {
                     mDbConnection.SetPassword("6505EFBDC3E5F324");
                     mDbConnection.Open();
 
-                    string sql1 = @"SELECT t.NAME AS TYPEKEY, c.NAME AS EREIHE FROM XEP_CHARACTERISTICS t INNER JOIN XEP_VEHICLES v ON (v.TYPEKEYID = t.ID) INNER JOIN XEP_CHARACTERISTICS c ON (v.CHARACTERISTICID = c.ID) INNER JOIN XEP_CHARACTERISTICROOTS r ON (r.ID = c.PARENTID AND r.NODECLASS=40140802) WHERE t.NODECLASS = 40135042 ORDER BY TYPEKEY";
-                    SQLiteCommand command1 = new SQLiteCommand(sql1, mDbConnection);
-                    int count = 0;
-                    using (SQLiteDataReader reader = command1.ExecuteReader())
+                    string typeKeysFile = Path.Combine(outDir, "typekeys.txt");
+                    zipFiles.Add(typeKeysFile);
+                    using (StreamWriter swTypeKeys = new StreamWriter(typeKeysFile))
                     {
-                        while (reader.Read())
+                        string sql1 = @"SELECT t.NAME AS TYPEKEY, c.NAME AS EREIHE FROM XEP_CHARACTERISTICS t INNER JOIN XEP_VEHICLES v ON (v.TYPEKEYID = t.ID) INNER JOIN XEP_CHARACTERISTICS c ON (v.CHARACTERISTICID = c.ID) INNER JOIN XEP_CHARACTERISTICROOTS r ON (r.ID = c.PARENTID AND r.NODECLASS=40140802) WHERE t.NODECLASS = 40135042 ORDER BY TYPEKEY";
+                        SQLiteCommand command1 = new SQLiteCommand(sql1, mDbConnection);
+                        using (SQLiteDataReader reader = command1.ExecuteReader())
                         {
-                            writer.Write("{\"" + reader["TYPEKEY"] + "\", \"" + reader["EREIHE"] + "\"},");
-                            count++;
-                            if (count % 10 == 0)
+                            while (reader.Read())
                             {
-                                writer.WriteLine();
+                                swTypeKeys.WriteLine(reader["TYPEKEY"] + "," + reader["EREIHE"]);
                             }
                         }
                     }
 
-                    if (!string.IsNullOrEmpty(outFile))
+                    string vinRangeFile = Path.Combine(outDir, "vinranges.txt");
+                    zipFiles.Add(vinRangeFile);
+                    using (StreamWriter swVinranges = new StreamWriter(vinRangeFile))
                     {
-                        string vinRangeFile = Path.Combine(Path.GetDirectoryName(outFile), "vinranges.txt");
-                        using (StreamWriter swVinranges = new StreamWriter(vinRangeFile))
+                        string sql2 = @"SELECT v.VINBANDFROM AS VINBANDFROM, v.VINBANDTO AS VINBANDTO, v.TYPSCHLUESSEL AS TYPEKEY FROM VINRANGES v";
+                        SQLiteCommand command2 = new SQLiteCommand(sql2, mDbConnection);
+                        using (SQLiteDataReader reader = command2.ExecuteReader())
                         {
-                            string sql2 = @"SELECT v.VINBANDFROM AS VINBANDFROM, v.VINBANDTO AS VINBANDTO, v.TYPSCHLUESSEL AS TYPEKEY FROM VINRANGES v";
-                            SQLiteCommand command2 = new SQLiteCommand(sql2, mDbConnection);
-                            using (SQLiteDataReader reader = command2.ExecuteReader())
+                            while (reader.Read())
                             {
-                                while (reader.Read())
-                                {
-                                    swVinranges.WriteLine(reader["VINBANDFROM"] + "," + reader["VINBANDTO"] + "," + reader["TYPEKEY"]);
-                                }
+                                swVinranges.WriteLine(reader["VINBANDFROM"] + "," + reader["VINBANDTO"] + "," + reader["TYPEKEY"]);
                             }
                         }
                     }
+
                     mDbConnection.Close();
+                    if (!CreateZip(zipFiles, Path.Combine(outDir, "Database.zip")))
+                    {
+                        Console.WriteLine("Creating Zip file failed");
+                        return 1;
+                    }
                 }
-                writer.Close();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
-            finally
+            return 0;
+        }
+
+        private static bool CreateZip(List<string> inputFiles, string archiveFilenameOut)
+        {
+            try
             {
-                writer?.Close();
+                if (File.Exists(archiveFilenameOut))
+                {
+                    File.Delete(archiveFilenameOut);
+                }
+                FileStream fsOut = File.Create(archiveFilenameOut);
+                ZipOutputStream zipStream = new ZipOutputStream(fsOut);
+                zipStream.SetLevel(3);
+
+                try
+                {
+                    foreach (string filename in inputFiles)
+                    {
+
+                        FileInfo fi = new FileInfo(filename);
+                        string entryName = Path.GetFileName(filename);
+
+                        ZipEntry newEntry = new ZipEntry(entryName)
+                        {
+                            DateTime = fi.LastWriteTime,
+                            Size = fi.Length
+                        };
+                        zipStream.PutNextEntry(newEntry);
+
+                        byte[] buffer = new byte[4096];
+                        using (FileStream streamReader = File.OpenRead(filename))
+                        {
+                            StreamUtils.Copy(streamReader, zipStream, buffer);
+                        }
+                        zipStream.CloseEntry();
+                    }
+                }
+                finally
+                {
+                    zipStream.IsStreamOwner = true;
+                    zipStream.Close();
+                }
             }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
