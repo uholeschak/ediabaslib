5dc710166948ce4b9ba6a03a3b7f983ab958b3de
uholeschak <ulrich@holeschak.de>
2018-01-16 11:15:44 +0100

    Continued with uploader

--- Tools/ApkUploader/ApkUploader/FormMain.Designer.cs
+++ Tools/ApkUploader/ApkUploader/FormMain.Designer.cs
@@ -33,6 +33,9 @@
             this.buttonListApks = new System.Windows.Forms.Button();
             this.buttonClose = new System.Windows.Forms.Button();
             this.buttonAbort = new System.Windows.Forms.Button();
+            this.buttonUploadApk = new System.Windows.Forms.Button();
+            this.openFileDialogApk = new System.Windows.Forms.OpenFileDialog();
+            this.buttonListTracks = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBoxStatus
@@ -44,7 +47,7 @@
             this.textBoxStatus.Name = "textBoxStatus";
             this.textBoxStatus.ReadOnly = true;
             this.textBoxStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxStatus.Size = new System.Drawing.Size(500, 191);
+            this.textBoxStatus.Size = new System.Drawing.Size(500, 284);
             this.textBoxStatus.TabIndex = 0;
             this.textBoxStatus.TabStop = false;
             // 
@@ -53,7 +56,7 @@
             this.buttonListApks.Location = new System.Drawing.Point(12, 12);
             this.buttonListApks.Name = "buttonListApks";
             this.buttonListApks.Size = new System.Drawing.Size(115, 23);
-            this.buttonListApks.TabIndex = 1;
+            this.buttonListApks.TabIndex = 3;
             this.buttonListApks.Text = "List Apks";
             this.buttonListApks.UseVisualStyleBackColor = true;
             this.buttonListApks.Click += new System.EventHandler(this.buttonListApks_Click);
@@ -62,10 +65,10 @@
             // 
             this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonClose.Location = new System.Drawing.Point(437, 238);
+            this.buttonClose.Location = new System.Drawing.Point(437, 331);
             this.buttonClose.Name = "buttonClose";
             this.buttonClose.Size = new System.Drawing.Size(75, 23);
-            this.buttonClose.TabIndex = 3;
+            this.buttonClose.TabIndex = 1;
             this.buttonClose.Text = "Close";
             this.buttonClose.UseVisualStyleBackColor = true;
             this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
@@ -73,20 +76,46 @@
             // buttonAbort
             // 
             this.buttonAbort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonAbort.Location = new System.Drawing.Point(356, 238);
+            this.buttonAbort.Location = new System.Drawing.Point(356, 331);
             this.buttonAbort.Name = "buttonAbort";
             this.buttonAbort.Size = new System.Drawing.Size(75, 23);
-            this.buttonAbort.TabIndex = 4;
+            this.buttonAbort.TabIndex = 2;
             this.buttonAbort.Text = "Abort";
             this.buttonAbort.UseVisualStyleBackColor = true;
             this.buttonAbort.Click += new System.EventHandler(this.buttonAbort_Click);
+            // 
+            // buttonUploadApk
+            // 
+            this.buttonUploadApk.Location = new System.Drawing.Point(254, 12);
+            this.buttonUploadApk.Name = "buttonUploadApk";
+            this.buttonUploadApk.Size = new System.Drawing.Size(115, 23);
+            this.buttonUploadApk.TabIndex = 5;
+            this.buttonUploadApk.Text = "Upload Apk";
+            this.buttonUploadApk.UseVisualStyleBackColor = true;
+            this.buttonUploadApk.Click += new System.EventHandler(this.buttonUploadApk_Click);
+            // 
+            // openFileDialogApk
+            // 
+            this.openFileDialogApk.Filter = "Apk Files|*.apk";
+            // 
+            // buttonListTracks
+            // 
+            this.buttonListTracks.Location = new System.Drawing.Point(133, 12);
+            this.buttonListTracks.Name = "buttonListTracks";
+            this.buttonListTracks.Size = new System.Drawing.Size(115, 23);
+            this.buttonListTracks.TabIndex = 4;
+            this.buttonListTracks.Text = "List Tracks";
+            this.buttonListTracks.UseVisualStyleBackColor = true;
+            this.buttonListTracks.Click += new System.EventHandler(this.buttonListTracks_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonClose;
-            this.ClientSize = new System.Drawing.Size(524, 273);
+            this.ClientSize = new System.Drawing.Size(524, 366);
+            this.Controls.Add(this.buttonListTracks);
+            this.Controls.Add(this.buttonUploadApk);
             this.Controls.Add(this.buttonAbort);
             this.Controls.Add(this.buttonClose);
             this.Controls.Add(this.buttonListApks);
@@ -109,6 +138,9 @@
         private System.Windows.Forms.Button buttonListApks;
         private System.Windows.Forms.Button buttonClose;
         private System.Windows.Forms.Button buttonAbort;
+        private System.Windows.Forms.Button buttonUploadApk;
+        private System.Windows.Forms.OpenFileDialog openFileDialogApk;
+        private System.Windows.Forms.Button buttonListTracks;
     }
 }
 
--- Tools/ApkUploader/ApkUploader/FormMain.cs
+++ Tools/ApkUploader/ApkUploader/FormMain.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using Google.Apis.AndroidPublisher.v2;
 using Google.Apis.AndroidPublisher.v2.Data;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
+using Google.Apis.Upload;
 using Google.Apis.Util.Store;
 
 namespace ApkUploader
@@ -33,10 +36,60 @@ namespace ApkUploader
             }
 
             textBoxStatus.Text = message;
+            textBoxStatus.SelectionStart = textBoxStatus.TextLength;
+            textBoxStatus.Update();
+            textBoxStatus.ScrollToCaret();
+
             bool enable = _serviceThread == null;
             buttonListApks.Enabled = enable;
+            buttonListTracks.Enabled = enable;
+            buttonUploadApk.Enabled = enable;
             buttonAbort.Enabled = !enable;
             buttonClose.Enabled = enable;
+        }
+
+        private async Task<UserCredential> GetCredatials()
+        {
+            UserCredential credential;
+            using (var stream = new FileStream(Path.Combine(_apkPath, "client_secrets.json"), FileMode.Open, FileAccess.Read))
+            {
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[] { AndroidPublisherService.Scope.Androidpublisher },
+                    "ApkUploader", _cts.Token, new FileDataStore("ApkUploader"));
+            }
+
+            return credential;
+        }
+
+        private static BaseClientService.Initializer GetInitializer(UserCredential credential)
+        {
+            return new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = PackageName
+            };
+        }
+
+        private async Task PrintExpansion(StringBuilder sb, EditsResource edits, AppEdit appEdit, int version)
+        {
+            try
+            {
+                ExpansionFile expansionResponse = await edits.Expansionfiles.Get(PackageName, appEdit.Id, version, EditsResource.ExpansionfilesResource.GetRequest.ExpansionFileTypeEnum.Main).ExecuteAsync(_cts.Token);
+                if ((expansionResponse.FileSize ?? 0) > 0)
+                {
+                    sb.Append($"Expansion size: {expansionResponse.FileSize}");
+                    if (expansionResponse.ReferencesVersion != null)
+                    {
+                        sb.AppendLine($"ref ver: {expansionResponse.ReferencesVersion}");
+                    }
+                    sb.AppendLine();
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
         }
 
         // ReSharper disable once UnusedMethodReturnValue.Local
@@ -46,30 +99,15 @@ namespace ApkUploader
             {
                 return false;
             }
+            UpdateStatus(string.Empty);
             _cts = new CancellationTokenSource();
             _serviceThread = new Thread(async () =>
                 {
                     StringBuilder sb = new StringBuilder();
                     try
                     {
-                        UpdateStatus(sb.ToString());
-
-                        UserCredential credential;
-                        using (var stream = new FileStream(Path.Combine(_apkPath, "client_secrets.json"), FileMode.Open, FileAccess.Read))
-                        {
-                            credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                                GoogleClientSecrets.Load(stream).Secrets,
-                                new[] { AndroidPublisherService.Scope.Androidpublisher },
-                                "ApkUploader", _cts.Token, new FileDataStore("ApkUploader"));
-                        }
-
-                        BaseClientService.Initializer initializer =
-                            new BaseClientService.Initializer
-                            {
-                                HttpClientInitializer = credential,
-                                ApplicationName = PackageName
-                            };
-                        using (AndroidPublisherService service = new AndroidPublisherService(initializer))
+                        UserCredential credential = await GetCredatials();
+                        using (AndroidPublisherService service = new AndroidPublisherService(GetInitializer(credential)))
                         {
                             EditsResource edits = service.Edits;
                             EditsResource.InsertRequest editRequest = edits.Insert(null, PackageName);
@@ -79,7 +117,11 @@ namespace ApkUploader
                             sb.AppendLine("Apks:");
                             foreach (Apk apk in apksResponse.Apks)
                             {
-                                sb.AppendLine($"Version: {apk.VersionCode}, SHA1: {apk.Binary.Sha1}");
+                                if (apk.VersionCode != null)
+                                {
+                                    sb.AppendLine($"Version: {apk.VersionCode}, SHA1: {apk.Binary.Sha1}");
+                                    await PrintExpansion(sb, edits, appEdit, apk.VersionCode.Value);
+                                }
                             }
                         }
                     }
@@ -94,6 +136,159 @@ namespace ApkUploader
                         UpdateStatus(sb.ToString());
                     }
                 });
+            _serviceThread.Start();
+
+            return true;
+        }
+
+        // ReSharper disable once UnusedMethodReturnValue.Local
+        private bool ListTracks()
+        {
+            if (_serviceThread != null)
+            {
+                return false;
+            }
+            UpdateStatus(string.Empty);
+            _cts = new CancellationTokenSource();
+            _serviceThread = new Thread(async () =>
+            {
+                StringBuilder sb = new StringBuilder();
+                try
+                {
+                    UserCredential credential = await GetCredatials();
+                    using (AndroidPublisherService service = new AndroidPublisherService(GetInitializer(credential)))
+                    {
+                        EditsResource edits = service.Edits;
+                        EditsResource.InsertRequest editRequest = edits.Insert(null, PackageName);
+                        AppEdit appEdit = await editRequest.ExecuteAsync(_cts.Token);
+                        foreach (EditsResource.TracksResource.GetRequest.TrackEnum track in Enum.GetValues(typeof(EditsResource.TracksResource.GetRequest.TrackEnum)))
+                        {
+                            sb.AppendLine($"Track: {track.ToString()}");
+                            try
+                            {
+                                EditsResource.TracksResource.GetRequest getRequest = edits.Tracks.Get(PackageName, appEdit.Id, track);
+                                Track trackResponse = await getRequest.ExecuteAsync(_cts.Token);
+
+                                foreach (int? version in trackResponse.VersionCodes)
+                                {
+                                    if (version != null)
+                                    {
+                                        sb.AppendLine($"Version: {version.Value}");
+                                        await PrintExpansion(sb, edits, appEdit, version.Value);
+                                    }
+                                    else
+                                    {
+                                        sb.AppendLine("No version");
+                                    }
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                sb.AppendLine("No data");
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    sb.AppendLine($"Exception: {e.Message}");
+                }
+                finally
+                {
+                    _serviceThread = null;
+                    _cts.Dispose();
+                    UpdateStatus(sb.ToString());
+                }
+            });
+            _serviceThread.Start();
+
+            return true;
+        }
+
+        // ReSharper disable once UnusedMethodReturnValue.Local
+        private bool UploadApk(string apkFileName, EditsResource.TracksResource.UpdateRequest.TrackEnum track)
+        {
+            if (_serviceThread != null)
+            {
+                return false;
+            }
+
+            if (!File.Exists(apkFileName))
+            {
+                UpdateStatus("Apk file not existing");
+                return false;
+            }
+            UpdateStatus(string.Empty);
+            _cts = new CancellationTokenSource();
+            _serviceThread = new Thread(async () =>
+            {
+                StringBuilder sb = new StringBuilder();
+                try
+                {
+                    UserCredential credential = await GetCredatials();
+                    using (AndroidPublisherService service = new AndroidPublisherService(GetInitializer(credential)))
+                    {
+                        EditsResource edits = service.Edits;
+                        EditsResource.InsertRequest editRequest = edits.Insert(null, PackageName);
+                        AppEdit appEdit = await editRequest.ExecuteAsync(_cts.Token);
+
+                        Apk apkUploaded = null;
+                        using (FileStream apkStream = new FileStream(apkFileName, FileMode.Open, FileAccess.Read))
+                        {
+                            long fileLength = (apkStream.Length > 0) ? apkStream.Length : 1;
+                            EditsResource.ApksResource.UploadMediaUpload upload = edits.Apks.Upload(PackageName, appEdit.Id, apkStream, "application/vnd.android.package-archive");
+                            upload.ChunkSize = ResumableUpload.MinimumChunkSize;
+                            upload.ProgressChanged += progress =>
+                            {
+                                UpdateStatus($"Progress: {100 * progress.BytesSent / fileLength}%");
+                            };
+                            upload.ResponseReceived += apk =>
+                            {
+                                apkUploaded = apk;
+                            };
+                            IUploadProgress uploadProgress = await upload.UploadAsync(_cts.Token);
+                            sb.AppendLine($"Upload status: {uploadProgress.Status.ToString()}");
+                            UpdateStatus(sb.ToString());
+                            if (uploadProgress.Exception != null)
+                            {
+                                throw uploadProgress.Exception;
+                            }
+                        }
+
+                        int? versionCode = apkUploaded?.VersionCode;
+                        if (!versionCode.HasValue)
+                        {
+                            throw new Exception("No apk version code");
+                        }
+                        sb.AppendLine($"Version code uploaded: {versionCode.Value}");
+                        UpdateStatus(sb.ToString());
+
+                        //ExpansionFile expansionUpdate = new ExpansionFile {ReferencesVersion = 129};
+                        //edits.Expansionfiles.Update(expansionUpdate, PackageName, appEdit.Id, 130, EditsResource.ExpansionfilesResource.UpdateRequest.ExpansionFileTypeEnum.Main);
+
+                        Track updateTrack = new Track { VersionCodes = new List<int?> { versionCode.Value } };
+                        EditsResource.TracksResource.UpdateRequest updateRequest = edits.Tracks.Update(updateTrack, PackageName, appEdit.Id, track);
+                        Track updatedTrack = await updateRequest.ExecuteAsync(_cts.Token);
+                        sb.AppendLine($"Track updated: {updatedTrack.TrackValue}");
+                        UpdateStatus(sb.ToString());
+
+                        EditsResource.CommitRequest commitRequest = edits.Commit(PackageName, appEdit.Id);
+                        AppEdit appEditCommit = await commitRequest.ExecuteAsync(_cts.Token);
+                        sb.AppendLine($"App edit committed: {appEditCommit.Id}");
+                        UpdateStatus(sb.ToString());
+                    }
+                }
+                catch (Exception e)
+                {
+                    sb.AppendLine($"Exception: {e.Message}");
+                }
+                finally
+                {
+                    _serviceThread = null;
+                    _cts.Dispose();
+                    UpdateStatus(sb.ToString());
+                }
+            });
             _serviceThread.Start();
 
             return true;
@@ -120,6 +315,20 @@ namespace ApkUploader
         private void buttonListApks_Click(object sender, EventArgs e)
         {
             ListApks();
+        }
+
+        private void buttonListTracks_Click(object sender, EventArgs e)
+        {
+            ListTracks();
+        }
+
+        private void buttonUploadApk_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogApk.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            UploadApk(openFileDialogApk.FileName, EditsResource.TracksResource.UpdateRequest.TrackEnum.Alpha);
         }
 
         private void FormMain_FormClosed(object sender, FormClosedEventArgs e)
