8742618e129bfdeb31ab13fc11728aff507e2b51
Ulrich Holeschak <ulrich@holeschak.de>
2018-03-08 23:24:55 +0100

    Started with UDS file reader

---
+++ Tools/FileDecoder/UdsFileReader/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UdsFileReader
+{
+    static class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("No input file specified");
+                return 1;
+            }
+
+            List<string> fileList = args.ToList();
+
+            List<string> includeFiles = new List<string>();
+            if (UdsReader.GetIncludeFiles(fileList[0], includeFiles))
+            {
+                Console.WriteLine("Includes:");
+                foreach (string includeFile in includeFiles)
+                {
+                    Console.WriteLine(includeFile);
+                }
+            }
+
+            List<string[]> lineList = UdsReader.ExtractFileSegment(fileList, "MWB");
+
+            Console.WriteLine("MWB:");
+            foreach (string[] line in lineList)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (string entry in line)
+                {
+                    if (sb.Length > 0)
+                    {
+                        sb.Append("; ");
+                    }
+                    sb.Append("\"");
+                    sb.Append(entry);
+                    sb.Append("\"");
+                }
+                Console.WriteLine(sb.ToString());
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Tools/FileDecoder/UdsFileReader/UdsReader.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace UdsFileReader
+{
+    public static class UdsReader
+    {
+        private static readonly Encoding Encoding = Encoding.GetEncoding(1252);
+
+        public static List<string[]> ExtractFileSegment(List<string> fileList, string segmentName)
+        {
+            string segmentStart = "[" + segmentName + "]";
+            string segmentEnd = "[/" + segmentName + "]";
+
+            List<string[]> lineList = new List<string[]>();
+            foreach (string fileName in fileList)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(fileName, Encoding))
+                    {
+                        bool inSegment = false;
+                        for (;;)
+                        {
+                            string line = sr.ReadLine();
+                            if (line == null)
+                            {
+                                break;
+                            }
+
+                            if (line.StartsWith("["))
+                            {
+                                if (string.Compare(line, segmentStart, StringComparison.OrdinalIgnoreCase) == 0)
+                                {
+                                    inSegment = true;
+                                }
+                                else if (string.Compare(line, segmentEnd, StringComparison.OrdinalIgnoreCase) == 0)
+                                {
+                                    inSegment = false;
+                                }
+                                continue;
+                            }
+
+                            if (!inSegment)
+                            {
+                                continue;
+                            }
+                            string[] lineArray = line.Split(',');
+                            if (lineArray.Length > 0)
+                            {
+                                lineList.Add(lineArray);
+                            }
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+            return lineList;
+        }
+
+        public static bool GetIncludeFiles(string fileName, List<string> includeFiles)
+        {
+            try
+            {
+                string dir = Path.GetDirectoryName(fileName);
+                if (dir == null)
+                {
+                    return false;
+                }
+
+                List<string[]> lineList = ExtractFileSegment(new List<string> { fileName }, "INC");
+                if (lineList == null)
+                {
+                    return false;
+                }
+
+                foreach (string[] line in lineList)
+                {
+                    if (line.Length >= 2)
+                    {
+                        string file = line[1];
+                        if (!string.IsNullOrWhiteSpace(file))
+                        {
+                            string fileNameInc = Path.Combine(dir, file);
+                            if (!includeFiles.Contains(fileNameInc))
+                            {
+                                includeFiles.Add(fileNameInc);
+                                if (!GetIncludeFiles(fileNameInc, includeFiles))
+                                {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
