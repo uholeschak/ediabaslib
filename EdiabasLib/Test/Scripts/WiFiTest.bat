a99160cf62f6026b499bb0b63a675d390e37212c
uholeschak <ulrich@holeschak.de>
2018-05-14 09:46:00 +0200

    WiFi test improved

--- EdiabasLib/Test/Scripts/WiFiTest.bat
+++ EdiabasLib/Test/Scripts/WiFiTest.bat
@@ -3,7 +3,7 @@ SETLOCAL EnableDelayedExpansion
 
 set BATPATH=%~dp0
 :restart
-"!BATPATH!..\EdiabasLibCall\bin\Release\EdiabasLibCall.exe" --ifh="STD:OBD" --cfg="ObdComPort=DEEPOBDWIFI;IfhTrace=2" -s "C:\EDIABAS\Ecu\f01" -j "SVK_LESEN_FUNKTIONAL" || goto exit
+"!BATPATH!..\EdiabasLibCall\bin\Release\EdiabasLibCall.exe" --ifh="STD:OBD" --cfg="ObdComPort=DEEPOBDWIFI;IfhTrace=2" -s "C:\EDIABAS\Ecu\f01" -j "IDENT_FUNKTIONAL" -j "FS_LESEN_FUNKTIONAL" -j "IS_LESEN_FUNKTIONAL" -j "SVK_LESEN_FUNKTIONAL" -j "STATUS_ENERGIESPARMODE_FUNKTIONAL" -j "SERIENNUMMER_LESEN_FUNKTIONAL" || goto exit
 goto restart
 
 :exit
