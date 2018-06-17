c903fc175ce2c552aaf567c79a74220fe1bfbe98
uholeschak <ulrich@holeschak.de>
2018-06-17 13:38:47 +0200

    Version number increased

--- Tools/CarSimulator/DeviceTest.cs
+++ Tools/CarSimulator/DeviceTest.cs
@@ -337,7 +337,7 @@ namespace CarSimulator
             sr.Append("\r\n");
             sr.Append("Firmware: ");
             sr.Append(string.Format("{0}.{1}", firmware[2], firmware[3]));
-            if ((firmware[2] != 0x00) || (firmware[3] < 0x09))
+            if ((firmware[2] != 0x00) || (firmware[3] < 0x0B))
             {
                 sr.Append("\r\n");
                 sr.Append("Incorrect firmware version!");
