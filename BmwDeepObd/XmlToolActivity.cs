58904e9e9d86a7eddeae8cb200eb64c9475a427f
uholeschak <ulrich@holeschak.de>
2018-07-22 16:09:42 +0200

    Reset vag data in old mode

--- BmwDeepObd/XmlToolActivity.cs
+++ BmwDeepObd/XmlToolActivity.cs
@@ -2957,6 +2957,7 @@ namespace BmwDeepObd
 #endif
                 }
 
+                ecuInfo.MwTabFileName = string.Empty;
                 return true;
             }
             catch (Exception)
@@ -3377,6 +3378,9 @@ namespace BmwDeepObd
                         }
                         else
                         {
+                            ecuInfo.VagDataFileName = string.Empty;
+                            ecuInfo.VagUdsFileName = string.Empty;
+
                             if (mwTabNotPresent)
                             {
                                 List<string> mwTabFileNames = GetBestMatchingMwTab(ecuInfo, progress);
