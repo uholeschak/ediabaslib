13dfd8f465c7a22d24f2b29b2ae9fd3d286a8e93
uholeschak <ulrich@holeschak.de>
2018-10-10 00:35:34 +0200

    Reject WGSoft adapters

--- EdiabasLib/EdiabasLib/EdElmInterface.cs
+++ EdiabasLib/EdiabasLib/EdElmInterface.cs
@@ -29,6 +29,7 @@ namespace EdiabasLib
         {
             new ElmInitEntry("ATD"),
             new ElmInitEntry("ATE0"),
+            new ElmInitEntry("ATPP2COFF"),      // reject fake elms (disables also WGSoft adapters)
             new ElmInitEntry("ATSH6F1"),
             new ElmInitEntry("ATCF600"),
             new ElmInitEntry("ATCM700"),
