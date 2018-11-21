8f7a1d1083e9a6b7f3d7184939763f3ea1243ca5
uholeschak <ulrich@holeschak.de>
2018-11-21 13:41:02 +0100

    Simulate security access

--- Tools/CarSimulator/CommThread.cs
+++ Tools/CarSimulator/CommThread.cs
@@ -7203,7 +7203,7 @@ namespace CarSimulator
 #if true
                                     byte[] dummyResponse = { 0x82, _receiveData[1], _receiveData[2], 0x7B, _receiveData[4], 0x00 };   // positive write ACK
 #else
-                                    byte[] dummyResponse = { 0x83, _receiveData[2], _receiveData[1], 0x7F, _receiveData[3], 0x33, 0x00 };   // access denied
+                                    byte[] dummyResponse = { 0x83, _receiveData[1], _receiveData[2], 0x7F, _receiveData[3], 0x33, 0x00 };   // access denied
 #endif
                                     ObdSend(dummyResponse);
                                 }
@@ -7213,6 +7213,30 @@ namespace CarSimulator
                                     Debug.WriteLine("Dummy service09: {0:X02}{1:X02}", _receiveData[4], _receiveData[5]);
                                     byte[] dummyResponse = { 0x83, _receiveData[1], _receiveData[2], 0x7F, _receiveData[3], 0x12, 0x00 };   // sub function not supported
                                     ObdSend(dummyResponse);
+                                }
+                                else if (_receiveData.Length >= 6 && (_receiveData[0] & 0x80) == 0x80 && _receiveData[3] == 0x27)
+                                {   // service 27 (security access)
+                                    found = true;
+                                    if (_receiveData[0] == 0x82)
+                                    {
+                                        Debug.WriteLine("Request seed accmode: {0:X02}", _receiveData[4]);
+                                        byte[] dummyResponse = { 0x86, _receiveData[1], _receiveData[2], 0x67, _receiveData[4], 0x12, 0x34, 0x56, 0x78, 0x00 };   // send seed
+                                        ObdSend(dummyResponse);
+                                    }
+                                    else
+                                    {
+                                        if (_receiveData[0] == 0x86)
+                                        {
+                                            Debug.WriteLine("Receive key accmode: {0:X02}, key: {1:X02} {2:X02} {3:X02} {4:X02}",
+                                                _receiveData[4], _receiveData[5], _receiveData[6], _receiveData[7], _receiveData[8]);
+                                        }
+                                        else
+                                        {
+                                            Debug.WriteLine("Dummy service27: {0:X02}", _receiveData[4]);
+                                        }
+                                        byte[] dummyResponse = { 0x82, _receiveData[1], _receiveData[2], 0x67, _receiveData[4], 0x00 };   // positive ACK
+                                        ObdSend(dummyResponse);
+                                    }
                                 }
                             }
                             else
