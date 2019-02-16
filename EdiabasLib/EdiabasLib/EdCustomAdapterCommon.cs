5cad9d10e24ca2e0fd8be86e2ff58234ea21577a
uholeschak <ulrich@holeschak.de>
2019-02-16 13:00:00 +0100

    Started with min baud rate 980

--- EdiabasLib/EdiabasLib/EdCustomAdapterCommon.cs
+++ EdiabasLib/EdiabasLib/EdCustomAdapterCommon.cs
@@ -101,6 +101,28 @@ namespace EdiabasLib
 
         public bool ReconnectRequired { get; set; }
 
+        public int MaxBaudRate
+        {
+            get
+            {
+                // ReSharper disable once ArrangeAccessorOwnerBody
+                return 25000;
+            }
+        }
+
+        public int MinBaudRate
+        {
+            get
+            {
+                if (AdapterType < 0x0002 || AdapterVersion < 0x000D)
+                {
+                    return 4000;
+                }
+
+                return 980;
+            }
+        }
+
         public EdCustomAdapterCommon(SendDataDelegate sendDataFunc, ReceiveDataDelegate receiveDataFunc,
             DiscardInBufferDelegate discardInBufferFunc, ReadInBufferDelegate readInBufferFunc,
             int readTimeoutOffsetLong, int readTimeoutOffsetShort, int echoTimeout, bool echoFailureReconnect)
@@ -157,7 +179,7 @@ namespace EdiabasLib
                 return null;
             }
             if ((CurrentBaudRate != 115200) &&
-                ((CurrentBaudRate < 4000) || (CurrentBaudRate > 25000)))
+                ((CurrentBaudRate < MinBaudRate) || (CurrentBaudRate > MaxBaudRate)))
             {
                 if (Ediabas != null)
                 {
@@ -247,7 +269,7 @@ namespace EdiabasLib
                 }
                 return null;
             }
-            if ((CurrentBaudRate != EdInterfaceBase.BaudAuto) && ((CurrentBaudRate < 4000) || (CurrentBaudRate > 25000)))
+            if ((CurrentBaudRate != EdInterfaceBase.BaudAuto) && ((CurrentBaudRate < MinBaudRate) || (CurrentBaudRate > MaxBaudRate)))
             {
                 if (Ediabas != null)
                 {
