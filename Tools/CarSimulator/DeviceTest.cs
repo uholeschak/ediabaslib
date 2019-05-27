c5c56bf28b6d5768cc73fd3fd257c298b533e538
uholeschak <ulrich@holeschak.de>
2019-05-28 01:18:51 +0200

    Firmware 14

--- Tools/CarSimulator/DeviceTest.cs
+++ Tools/CarSimulator/DeviceTest.cs
@@ -358,7 +358,7 @@ namespace CarSimulator
             sr.Append("\r\n");
             sr.Append("Firmware: ");
             sr.Append(string.Format("{0}.{1}", firmware[2], firmware[3]));
-            if ((firmware[2] != 0x00) || (firmware[3] < 13))
+            if ((firmware[2] != 0x00) || (firmware[3] < 14))
             {
                 sr.Append("\r\n");
                 sr.Append("Incorrect firmware version!");
