b8877f63c731b70465f176dd188acc7ee4588f70
uholeschak <ulrich@holeschak.de>
2019-07-22 06:32:47 +0200

    Added more configurations

--- EdiabasLib/CanAdapterElm/CanAdapterElm.X/ELM327V15.X/nbproject/Makefile-variables.mk
+++ EdiabasLib/CanAdapterElm/CanAdapterElm.X/ELM327V15.X/nbproject/Makefile-variables.mk
@@ -11,3 +11,17 @@ CND_ARTIFACT_PATH_default=dist/default/production/ELM327V15.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
 CND_PACKAGE_NAME_default=elm327v15.x.tar
 CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/elm327v15.x.tar
+# spp_uart configuration
+CND_ARTIFACT_DIR_spp_uart=dist/spp_uart/production
+CND_ARTIFACT_NAME_spp_uart=ELM327V15.X.production.hex
+CND_ARTIFACT_PATH_spp_uart=dist/spp_uart/production/ELM327V15.X.production.hex
+CND_PACKAGE_DIR_spp_uart=${CND_DISTDIR}/spp_uart/package
+CND_PACKAGE_NAME_spp_uart=elm327v15.x.tar
+CND_PACKAGE_PATH_spp_uart=${CND_DISTDIR}/spp_uart/package/elm327v15.x.tar
+# spp_uart2 configuration
+CND_ARTIFACT_DIR_spp_uart2=dist/spp_uart2/production
+CND_ARTIFACT_NAME_spp_uart2=ELM327V15.X.production.hex
+CND_ARTIFACT_PATH_spp_uart2=dist/spp_uart2/production/ELM327V15.X.production.hex
+CND_PACKAGE_DIR_spp_uart2=${CND_DISTDIR}/spp_uart2/package
+CND_PACKAGE_NAME_spp_uart2=elm327v15.x.tar
+CND_PACKAGE_PATH_spp_uart2=${CND_DISTDIR}/spp_uart2/package/elm327v15.x.tar
