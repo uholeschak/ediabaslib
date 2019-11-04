831b8cd54d974a4a1a18d2ba2d6b28ef2d1d373d
uholeschak <ulrich@holeschak.de>
2019-11-04 09:58:50 +0100

    Started with offline extraction

--- BmwDeepObd/ActivityCommon.cs
+++ BmwDeepObd/ActivityCommon.cs
@@ -255,6 +255,7 @@ namespace BmwDeepObd
         public const string EcuDirNameBmw = "EcuBmw";
         public const string EcuDirNameVag = "EcuVag";
         public const string AppNameSpace = "de.holeschak.bmw_deep_obd";
+        public const string ContactMail = "ulrich@holeschak.de";
         public const string ActionUsbPermission = AppNameSpace + ".USB_PERMISSION";
         public const string ActionPackageName = AppNameSpace + ".Action.PackageName";
         public const string BroadcastXmlEditorPackageName = "XmlEditorPackageName";
