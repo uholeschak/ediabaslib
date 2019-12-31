039c4c6b9d122a3271b63ad31a2cbecab2bb449f
uholeschak <ulrich@holeschak.de>
2019-12-31 12:36:51 +0100

    Create zip file

--- Tools/ExtractTypeKeys/ExtractEcuFunctions/Program.cs
+++ Tools/ExtractTypeKeys/ExtractEcuFunctions/Program.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data.SQLite;
 using System.IO;
 using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
 using System.Xml.Serialization;
 using ICSharpCode.SharpZipLib.Core;
@@ -600,13 +601,26 @@ namespace ExtractEcuFunctions
             try
             {
                 string outDirSub = Path.Combine(outDir, "EcuFunctions");
+                string zipFile = Path.Combine(outDir, "EcuFunctions.zip");
                 try
                 {
                     if (Directory.Exists(outDirSub))
                     {
-                        Directory.Delete(outDirSub);
+                        Directory.Delete(outDirSub, true);
                     }
                     Directory.CreateDirectory(outDirSub);
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+
+                try
+                {
+                    if (File.Exists(zipFile))
+                    {
+                        File.Delete(zipFile);
+                    }
                 }
                 catch (Exception)
                 {
@@ -640,7 +654,6 @@ namespace ExtractEcuFunctions
                             outTextWriter?.WriteLine(ecuVariant);
 
                             string xmlFile = Path.Combine(outDirSub, name.ToLowerInvariant() + ".xml");
-
                             XmlSerializer serializer = new XmlSerializer(ecuVariant.GetType());
                             using (TextWriter writer = new StreamWriter(xmlFile))
                             {
@@ -650,6 +663,12 @@ namespace ExtractEcuFunctions
                     }
 
                     mDbConnection.Close();
+                }
+
+                if (!CreateZipFile(outDirSub, zipFile))
+                {
+                    outTextWriter?.WriteLine("Create ZIP failed");
+                    return 1;
                 }
             }
             catch (Exception e)
@@ -998,53 +1017,117 @@ namespace ExtractEcuFunctions
             return ecuVariant;
         }
 
-        // ReSharper disable once UnusedMember.Local
-        private static bool CreateZip(List<string> inputFiles, string archiveFilenameOut)
+        private static bool CreateZipFile(string inDir, string outFile, string key = null)
         {
             try
             {
-                if (File.Exists(archiveFilenameOut))
-                {
-                    File.Delete(archiveFilenameOut);
-                }
-                FileStream fsOut = File.Create(archiveFilenameOut);
-                ZipOutputStream zipStream = new ZipOutputStream(fsOut);
-                zipStream.SetLevel(3);
-
+                AesCryptoServiceProvider crypto = null;
+                FileStream fsOut = null;
+                ZipOutputStream zipStream = null;
                 try
                 {
-                    foreach (string filename in inputFiles)
+                    if (!string.IsNullOrEmpty(key))
                     {
-
-                        FileInfo fi = new FileInfo(filename);
-                        string entryName = Path.GetFileName(filename);
-
-                        ZipEntry newEntry = new ZipEntry(entryName)
+                        crypto = new AesCryptoServiceProvider
                         {
-                            DateTime = fi.LastWriteTime,
-                            Size = fi.Length
+                            Mode = CipherMode.CBC,
+                            Padding = PaddingMode.PKCS7,
+                            KeySize = 256
                         };
-                        zipStream.PutNextEntry(newEntry);
-
-                        byte[] buffer = new byte[4096];
-                        using (FileStream streamReader = File.OpenRead(filename))
+                        using (SHA256Managed sha256 = new SHA256Managed())
                         {
-                            StreamUtils.Copy(streamReader, zipStream, buffer);
+                            crypto.Key = sha256.ComputeHash(Encoding.ASCII.GetBytes(key));
                         }
-                        zipStream.CloseEntry();
+                        using (var md5 = MD5.Create())
+                        {
+                            crypto.IV = md5.ComputeHash(Encoding.ASCII.GetBytes(key));
+                        }
                     }
+
+                    fsOut = File.Create(outFile);
+                    // ReSharper disable once ConvertIfStatementToConditionalTernaryExpression
+                    if (crypto != null)
+                    {
+                        CryptoStream crStream = new CryptoStream(fsOut,
+                            crypto.CreateEncryptor(), CryptoStreamMode.Write);
+                        zipStream = new ZipOutputStream(crStream);
+                    }
+                    else
+                    {
+                        zipStream = new ZipOutputStream(fsOut);
+                    }
+
+
+                    zipStream.SetLevel(9); //0-9, 9 being the highest level of compression
+
+                    // This setting will strip the leading part of the folder path in the entries, to
+                    // make the entries relative to the starting folder.
+                    // To include the full path for each entry up to the drive root, assign folderOffset = 0.
+                    int folderOffset = inDir.Length + (inDir.EndsWith("\\") ? 0 : 1);
+
+                    CompressFolder(inDir, zipStream, folderOffset);
                 }
                 finally
                 {
-                    zipStream.IsStreamOwner = true;
-                    zipStream.Close();
+                    if (zipStream != null)
+                    {
+                        zipStream.IsStreamOwner = true; // Makes the Close also Close the underlying stream
+                        zipStream.Close();
+                    }
+                    fsOut?.Close();
+                    crypto?.Dispose();
                 }
             }
             catch (Exception)
             {
                 return false;
             }
+
             return true;
+        }
+
+        private static void CompressFolder(string path, ZipOutputStream zipStream, int folderOffset)
+        {
+            string[] files = Directory.GetFiles(path);
+
+            foreach (string filename in files)
+            {
+
+                FileInfo fi = new FileInfo(filename);
+
+                string entryName = filename.Substring(folderOffset); // Makes the name in zip based on the folder
+                entryName = ZipEntry.CleanName(entryName); // Removes drive from name and fixes slash direction
+                ZipEntry newEntry = new ZipEntry(entryName);
+                newEntry.DateTime = fi.LastWriteTime; // Note the zip format stores 2 second granularity
+
+                // Specifying the AESKeySize triggers AES encryption. Allowable values are 0 (off), 128 or 256.
+                // A password on the ZipOutputStream is required if using AES.
+                //   newEntry.AESKeySize = 256;
+
+                // To permit the zip to be unpacked by built-in extractor in WinXP and Server2003, WinZip 8, Java, and other older code,
+                // you need to do one of the following: Specify UseZip64.Off, or set the Size.
+                // If the file may be bigger than 4GB, or you do not need WinXP built-in compatibility, you do not need either,
+                // but the zip will be in Zip64 format which not all utilities can understand.
+                //   zipStream.UseZip64 = UseZip64.Off;
+                newEntry.Size = fi.Length;
+
+                zipStream.PutNextEntry(newEntry);
+
+                // Zip the file in buffered chunks
+                // the "using" will close the stream even if an exception occurs
+                byte[] buffer = new byte[4096];
+                using (FileStream streamReader = File.OpenRead(filename))
+                {
+                    StreamUtils.Copy(streamReader, zipStream, buffer);
+                }
+                zipStream.CloseEntry();
+            }
+
+            string[] folders = Directory.GetDirectories(path);
+            foreach (string folder in folders)
+            {
+                CompressFolder(folder, zipStream, folderOffset);
+            }
         }
     }
 }
