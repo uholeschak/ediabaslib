8008271b32e88e4e28cd56e28c9d0b26e08afeb8
uholeschak <ulrich@holeschak.de>
2020-02-05 21:08:34 +0100

    Dynamic app name

--- BmwDeepObd/BaseActivity.cs
+++ BmwDeepObd/BaseActivity.cs
@@ -1,4 +1,6 @@
-﻿using Android.Content;
+﻿using System;
+using Android.Content;
+using Android.Content.PM;
 using Android.Content.Res;
 using Android.OS;
 using Android.Support.V7.App;
@@ -8,6 +10,12 @@ namespace BmwDeepObd
     public class BaseActivity : AppCompatActivity
     {
         private const string TestLanguage = null; // "ru";
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            ResetTitle();
+        }
 
         protected override void AttachBaseContext(Context @base)
         {
@@ -20,36 +28,59 @@ namespace BmwDeepObd
             SetLocale(this, TestLanguage);
         }
 
+        public void ResetTitle()
+        {
+            try
+            {
+                int? label = PackageManager?.GetActivityInfo(ComponentName, PackageInfoFlags.MetaData)?.LabelRes;
+                if (label.HasValue && label != 0)
+                {
+                    SetTitle(label.Value);
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
         public static Context SetLocale(Context context, string language)
         {
-            if (string.IsNullOrEmpty(language))
+            try
             {
-                return context;
-            }
+                if (string.IsNullOrEmpty(language))
+                {
+                    return context;
+                }
 
-            Java.Util.Locale locale = new Java.Util.Locale(language);
-            Java.Util.Locale.Default = locale;
+                Java.Util.Locale locale = new Java.Util.Locale(language);
+                Java.Util.Locale.Default = locale;
 
-            Resources resources = context.Resources;
-            Configuration configuration = resources.Configuration;
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.N)
-            {
-                configuration.SetLocale(locale);
-            }
-            else
-            {
-                configuration.Locale = locale;
-            }
+                Resources resources = context.Resources;
+                Configuration configuration = resources.Configuration;
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.N)
+                {
+                    configuration.SetLocale(locale);
+                }
+                else
+                {
+                    configuration.Locale = locale;
+                }
 
-            if (Build.VERSION.SdkInt < BuildVersionCodes.JellyBeanMr1)
-            {
+                if (Build.VERSION.SdkInt < BuildVersionCodes.JellyBeanMr1)
+                {
 #pragma warning disable 618
-                resources.UpdateConfiguration(configuration, resources.DisplayMetrics);
+                    resources.UpdateConfiguration(configuration, resources.DisplayMetrics);
 #pragma warning restore 618
+                    return context;
+                }
+
+                return context.CreateConfigurationContext(configuration);
+            }
+            catch (Exception)
+            {
                 return context;
             }
-
-            return context.CreateConfigurationContext(configuration);
         }
     }
 }
