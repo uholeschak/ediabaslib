eb0b609dd7e050e4ecd1a34d57b1103501b4b43b
uholeschak <ulrich@holeschak.de>
2020-02-26 00:47:36 +0100

    Added application class

---
+++ BmwDeepObd/MyApplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BmwDeepObd
+{
+    [Android.App.Application(ResizeableActivity = true, LargeHeap = true)]
+    // ReSharper disable once UnusedMember.Global
+    public class MyApplication : Android.App.Application
+    {
+        public MyApplication(IntPtr handle, Android.Runtime.JniHandleOwnership ownerShip) : base(handle, ownerShip)
+        {
+        }
+
+        // ReSharper disable once RedundantOverriddenMember
+        public override void OnCreate()
+        {
+            base.OnCreate();
+        }
+    }
+}
