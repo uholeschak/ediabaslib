fb901ed5c53292cc8ae22c88710c0ebba5d7fc19
uholeschak <ulrich@holeschak.de>
2020-05-21 16:52:05 +0200

    Prepared for XC8 assembly langauge

--- EdiabasLib/CanAdapterElm/CanAdapterElm.X/bootloader/PIC18Bootloader.asm
+++ EdiabasLib/CanAdapterElm/CanAdapterElm.X/bootloader/PIC18Bootloader.asm
@@ -116,7 +116,7 @@
 ; *****************************************************************************
 
 ; *****************************************************************************
-#define STX             0x0F            
+#define STX             0x0F
 #define ETX             0x04
 #define DLE             0x05
 #define NTX             0xFF
@@ -236,7 +236,7 @@ BootloaderBreakCheck:
     movlb   0x0F
     bcf     INTCON2, RBPU
     ; check for software reset
-    btfss   RCON, RI
+    btfss   _RI_
     bra     BootloadMode
 CheckApplication:
     ; check for adapter type
@@ -261,11 +261,11 @@ CheckApplication:
     movwf   T0CON
     clrf    TMR0H               ; reset timer count value
     clrf    TMR0L
-    bcf     INTCON, TMR0IF
-    bsf     T0CON, TMR0ON       ; start timer
+    bcf     _TMR0IF_
+    bsf     _TMR0ON_		; start timer
 StableWait:
     clrwdt
-    btfss   INTCON, TMR0IF      ; wait for TMR0 overflow
+    btfss   _TMR0IF_		; wait for TMR0 overflow
     bra     StableWait
 #ifdef BRG16
     movlw   0x02 ;b'00000010'         ; 1:8 prescaler - no division required later (but no rounding possible)
@@ -357,8 +357,8 @@ GotoAppVector:
 BootloadMode:
 	; end BOOTLOADER_ADDRESS == 0 ******************************************
 #endif
-    lfsr    FSR2, 0             ; for compatibility with Extended Instructions mode.
-    bcf     RCON, RI            ; [UH] clear hardware reset bit
+    lfsr    _FSR2_, 0           ; for compatibility with Extended Instructions mode.
+    bcf     _RI_		; [UH] clear hardware reset bit
 
 #ifdef USE_MAX_INTOSC
     movlw   0x70 ;b'01110000'         ; set INTOSC to maximum speed (usually 8MHz)
@@ -438,15 +438,15 @@ BootloadMode:
 #endif
     ; [UH] switch on both LED
 #if (ADAPTER_TYPE == 0x06) || (ADAPTER_TYPE == 0x07)
-    bcf     LATB, LATB4
-    bcf     LATB, LATB6
-    bcf     TRISB, TRISB4
-    bcf     TRISB, TRISB6
+    bcf     _LATB4_
+    bcf     _LATB6_
+    bcf     _TRISB4_
+    bcf     _TRISB6_
 #else
-    bcf     LATB, LATB6
-    bcf     LATB, LATB7
-    bcf     TRISB, TRISB6
-    bcf     TRISB, TRISB7
+    bcf     _LATB6_
+    bcf     _LATB7_
+    bcf     _TRISB6_
+    bcf     _TRISB7_
 #endif
 
 ; *****************************************************************************
@@ -475,13 +475,13 @@ DoAutoBaud:
 RetryAutoBaud:
     clrf    TMR0H               ; reset timer count value
     clrf    TMR0L
-    bcf     INTCON, TMR0IF
+    bcf     _TMR0IF_
     rcall   WaitForRise         ; wait for a start bit to pass by
-    bsf     T0CON, TMR0ON       ; start timer counting for entire D7..D0 data bit period.
+    bsf     _TMR0ON_		; start timer counting for entire D7..D0 data bit period.
     rcall   WaitForRise         ; wait for stop bit
-    bcf     T0CON, TMR0ON       ; stop the timer from counting further. 
+    bcf     _TMR0ON_		; stop the timer from counting further. 
 
-    btfsc   INTCON, TMR0IF      ; if TMR0 overflowed, we did not get a good baud capture
+    btfsc   _TMR0IF_	        ; if TMR0 overflowed, we did not get a good baud capture
     bra     RetryAutoBaud       ; try again
 
     #ifdef BRG16
@@ -493,7 +493,7 @@ RetryAutoBaud:
     ; TMR0H:TMR0L holds (p / 16).
     rrcf    TMR0H, w            ; divide by 2
     rrcf    UxSPBRG, F            
-    btfss   STATUS, C           ; rounding
+    btfss   _CARRY_		; rounding
     decf    UxSPBRG, F    
     #endif
 
@@ -530,8 +530,8 @@ StartOfLine:
     movlw   STX                     ; send back start of response
     rcall   SendHostByte
 
-    lfsr    FSR0, COMMAND-1         ; Point to the buffer
-        
+    lfsr    _FSR0_, COMMAND-1         ; Point to the buffer
+
 ReceiveDataLoop:
     rcall   ReadHostByte            ; Get the data
     xorlw   STX                     ; Check for an unexpected STX
@@ -554,7 +554,7 @@ AppendDataBuffer:
     bra     ReceiveDataLoop
 
 VerifyPacketCRC:
-    lfsr    FSR1, COMMAND
+    lfsr    _FSR1_, COMMAND
     clrf    CRCL
     clrf    CRCH
     movff   POSTDEC0, PRODH         ; Save host packet's CRCH to PRODH for later comparison
@@ -593,7 +593,7 @@ VerifyPacketCrcLoop:
     movwf   EEADRH
 #endif
     movff   ADDRESS_U, TBLPTRU
-    lfsr    FSR0, PACKET_DATA
+    lfsr    _FSR0_, PACKET_DATA
 ; ***********************************************
 
  
@@ -656,8 +656,8 @@ WtSR:
 WaitForRise:
     clrwdt
 
-WaitForRiseLoop
-    btfsc   INTCON, TMR0IF  ; if TMR0 overflowed, we did not get a good baud capture
+WaitForRiseLoop:
+    btfsc   _TMR0IF_	    ; if TMR0 overflowed, we did not get a good baud capture
     return                  ; abort
 
     btfsc   RXPORT, RXPIN   ; Wait for a falling edge
