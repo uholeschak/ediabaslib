1af2df5c86d90fff3dfd752bc97ba0f71b2857d7
uholeschak <ulrich@holeschak.de>
2021-01-11 00:19:00 +0100

    Added queue stream

---
+++ EdiabasLib/EdiabasLib/MemoryQueueBufferStream.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+// ReSharper disable ConvertPropertyToExpressionBody
+
+namespace EdiabasLib
+{
+    /// <summary>
+    /// This stream maintains data only until the data is read, then it is purged from the stream.
+    /// </summary>
+    public class MemoryQueueBufferStream : Stream
+    {
+        /// <summary>
+        /// Represents a single write into the MemoryQueueBufferStream.  Each write is a seperate chunk
+        /// </summary>
+        private class Chunk
+        {
+            /// <summary>
+            /// As we read through the chunk, the start index will increment.  When we get to the end of the chunk,
+            /// we will remove the chunk
+            /// </summary>
+            public int ChunkReadStartIndex { get; set; }
+
+            /// <summary>
+            /// Actual Data
+            /// </summary>
+            public byte[] Data { get; set; }
+        }
+
+        //Maintains the streams data.  The Queue object provides an easy and efficient way to add and remove data
+        //Each item in the queue represents each write to the stream.  Every call to write translates to an item in the queue
+        private readonly Queue<Chunk> _lstBuffers;
+
+        public MemoryQueueBufferStream()
+        {
+            _lstBuffers = new Queue<Chunk>();
+        }
+
+        public bool IsDataAvailable()
+        {
+            return Length > 0;
+        }
+
+        /// <summary>
+        /// Reads up to count bytes from the stream, and removes the read data from the stream.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            this.ValidateBufferArgs(buffer, offset, count);
+
+            int iRemainingBytesToRead = count;
+
+            int iTotalBytesRead = 0;
+
+            //Read until we hit the requested count, or until we hav nothing left to read
+            while (iTotalBytesRead <= count && _lstBuffers.Count > 0)
+            {
+                //Get first chunk from the queue
+                Chunk chunk = _lstBuffers.Peek();
+
+                //Determine how much of the chunk there is left to read
+                int iUnreadChunkLength = chunk.Data.Length - chunk.ChunkReadStartIndex;
+
+                //Determine how much of the unread part of the chunk we can actually read
+                int iBytesToRead = Math.Min(iUnreadChunkLength, iRemainingBytesToRead);
+
+                if (iBytesToRead > 0)
+                {
+                    //Read from the chunk into the buffer
+                    Buffer.BlockCopy(chunk.Data, chunk.ChunkReadStartIndex, buffer, offset + iTotalBytesRead, iBytesToRead);
+
+                    iTotalBytesRead += iBytesToRead;
+                    iRemainingBytesToRead -= iBytesToRead;
+
+                    //If the entire chunk has been read,  remove it
+                    if (chunk.ChunkReadStartIndex + iBytesToRead >= chunk.Data.Length)
+                    {
+                        _lstBuffers.Dequeue();
+                    }
+                    else
+                    {
+                        //Otherwise just update the chunk read start index, so we know where to start reading on the next call
+                        chunk.ChunkReadStartIndex = chunk.ChunkReadStartIndex + iBytesToRead;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return iTotalBytesRead;
+        }
+
+        private void ValidateBufferArgs(byte[] buffer, int offset, int count)
+        {
+            if (offset < 0)
+            {
+                throw new ArgumentOutOfRangeException("offset", "offset must be non-negative");
+            }
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException("count", "count must be non-negative");
+            }
+            if ((buffer.Length - offset) < count)
+            {
+                throw new ArgumentException("requested count exceeds available size");
+            }
+        }
+
+        /// <summary>
+        /// Writes data to the stream
+        /// </summary>
+        /// <param name="buffer">Data to copy into the stream</param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            this.ValidateBufferArgs(buffer, offset, count);
+
+            //We don't want to use the buffer passed in, as it could be altered by the caller
+            byte[] bufSave = new byte[count];
+            Buffer.BlockCopy(buffer, offset, bufSave, 0, count);
+
+            //Add the data to the queue
+            _lstBuffers.Enqueue(new Chunk() { ChunkReadStartIndex = 0, Data = bufSave });
+        }
+
+        public override bool CanSeek
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Always returns 0
+        /// </summary>
+        public override long Position
+        {
+            get
+            {
+                //We're always at the start of the stream, because as the stream purges what we've read
+                return 0;
+            }
+            set
+            {
+                throw new NotSupportedException(this.GetType().Name + " is not seekable");
+            }
+        }
+
+        public override bool CanWrite
+        {
+            get { return true; }
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException(this.GetType().Name + " is not seekable");
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException(this.GetType().Name + " length can not be changed");
+        }
+
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        public override long Length
+        {
+            get
+            {
+
+                if (_lstBuffers == null)
+                {
+                    return 0;
+                }
+
+                if (_lstBuffers.Count == 0)
+                {
+                    return 0;
+                }
+
+                return _lstBuffers.Sum(b => b.Data.Length - b.ChunkReadStartIndex);
+            }
+        }
+
+        public override void Flush()
+        {
+        }
+    }
+}
