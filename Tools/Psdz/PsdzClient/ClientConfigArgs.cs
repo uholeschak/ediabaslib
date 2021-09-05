f0b616dd70817e581a195f603426b457cf6b8c1c
uholeschak <ulrich@holeschak.de>
2021-09-05 15:53:05 +0200

    Added service arguments

---
+++ Tools/Psdz/PsdzClient/ClientConfigArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsdzClient
+{
+    [DataContract]
+    public class ClientConfigArgs
+    {
+        // Token: 0x17000001 RID: 1
+        // (get) Token: 0x06000001 RID: 1 RVA: 0x000020D0 File Offset: 0x000002D0
+        // (set) Token: 0x06000002 RID: 2 RVA: 0x000020D8 File Offset: 0x000002D8
+        [DataMember]
+        public string DealerID { get; set; }
+
+        // Token: 0x06000003 RID: 3 RVA: 0x000020E1 File Offset: 0x000002E1
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendFormat(CultureInfo.InvariantCulture, "{0}:             {1}\n", "DealerID", this.DealerID);
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Tools/Psdz/PsdzClient/PsdzConfig.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PsdzClient
+{
+    class PsdzConfig
+    {
+        private static PsdzServiceArgs CreateServiceArgs(string istaFolder, string psdzLogFilePath)
+        {
+            string psdzSubDir = Environment.Is64BitOperatingSystem ? @"PSdZ\binx64" : @"PSdZ\bin";
+            string psdzBinaryPath = Path.Combine(istaFolder, psdzSubDir);
+            PsdzServiceArgs psdzServiceArgs = new PsdzServiceArgs();
+            string jreSubDir = Environment.Is64BitOperatingSystem ? @"OpenJREx64" : @"OpenJREx86";
+            psdzServiceArgs.JrePath = Path.Combine(istaFolder, jreSubDir);
+            psdzServiceArgs.JvmOptions = PsdzConfig.GetPsdzJvmOptions(psdzBinaryPath, psdzLogFilePath);
+            psdzServiceArgs.PsdzBinaryPath = psdzBinaryPath;
+            psdzServiceArgs.PsdzDataPath = Path.Combine(istaFolder, @"PSdZ\data");
+            psdzServiceArgs.EdiabasBinPath = Path.Combine(istaFolder, @"Ediabas\BIN");
+            psdzServiceArgs.IsTestRun = false;
+            psdzServiceArgs.IdleTimeout = 10000;
+            psdzServiceArgs.ClientConfigArgs = new ClientConfigArgs();
+            psdzServiceArgs.ClientConfigArgs.DealerID = "32395";
+            return psdzServiceArgs;
+        }
+
+        private static string[] GetPsdzJvmOptions(string psdzBinaryPath, string psdzLogFilePath)
+        {
+            int memory = 1024;
+            if (Environment.Is64BitOperatingSystem)
+            {
+                memory *= 2;
+            }
+
+            string defaultValue = string.Join(" ", new List<string>
+            {
+                "-XX:ThreadStackSize=1024",
+                string.Format(CultureInfo.InvariantCulture, "-Xmx{0}m", memory),
+                "-XX:+UseG1GC",
+                "-XX:MaxGCPauseMillis=50",
+                "-Dcom.sun.management.jmxremote",
+                "-Djava.endorsed.dirs=${PSDZ_BIN_PATH}\\endorsed",
+                "-Djava.library.path='${PSDZ_BIN_PATH}\\prodias; ${PSDZ_BIN_PATH}\\psdz'"
+            });
+
+            string text = defaultValue.Replace("${PSDZ_BIN_PATH}", Path.GetFullPath(psdzBinaryPath));
+
+            return new List<string>(Regex.Split(text, "\\s+(?=\\-)"))
+            {
+                "-DPsdzLogFilePath=" + psdzLogFilePath,
+                "-Dlog4j.configuration=psdzservicehost.properties"
+            }.ToArray();
+        }
+    }
+}
