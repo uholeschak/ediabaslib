9d48912865154a46e849d97d5086b9c4e68efa54
uholeschak <ulrich@holeschak.de>
2021-09-06 12:20:09 +0200

    Added service interfaces

---
+++ Tools/Psdz/PsdzClient/IPsdzSecureTokenForTal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsdzClient
+{
+    public interface IPsdzSecureTokenForTal
+    {
+        IPsdzEcuIdentifier EcuIdentifier { get; }
+
+        long FeatureId { get; }
+
+        string SerializedSecureToken { get; }
+
+        string TokenId { get; }
+    }
+}
---
+++ Tools/Psdz/PsdzClient/PsdzSFAWriteTA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsdzClient
+{
+    [KnownType(typeof(PsdzSecureTokenForTal))]
+    [DataContract]
+    public class PsdzSFAWriteTA : PsdzTa, IPsdzFsaTa, IPsdzTa, IPsdzTalElement
+    {
+        [DataMember]
+        public long EstimatedExecutionTime { get; set; }
+
+        [DataMember]
+        public long FeatureId { get; set; }
+
+        [DataMember]
+        public IPsdzSecureTokenForTal SecureToken { get; set; }
+    }
+}
