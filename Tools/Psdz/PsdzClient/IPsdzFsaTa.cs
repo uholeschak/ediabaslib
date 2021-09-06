13f1051294a5bd3f707fe3301c411ad471dcc6f3
uholeschak <ulrich@holeschak.de>
2021-09-06 11:50:21 +0200

    Added service interfaces

---
+++ Tools/Psdz/PsdzClient/IPsdzFsaTa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsdzClient
+{
+    public interface IPsdzFsaTa : IPsdzTa, IPsdzTalElement
+    {
+        long EstimatedExecutionTime { get; set; }
+
+        long FeatureId { get; set; }
+    }
+}
