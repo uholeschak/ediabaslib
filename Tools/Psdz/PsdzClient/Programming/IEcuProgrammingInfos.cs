5689ea19390a2f288b8437399ad9c66242b58a23
uholeschak <ulrich@holeschak.de>
2021-09-30 00:15:16 +0200

    Added event class

---
+++ Tools/Psdz/PsdzClient/Programming/IEcuProgrammingInfos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BMW.Rheingold.CoreFramework.Contracts.Vehicle;
+
+namespace PsdzClient.Programming
+{
+    public interface IEcuProgrammingInfos : IEnumerable<IEcuProgrammingInfo>, IEnumerable
+    {
+        IEcuProgrammingInfo GetItem(IEcu ecu);
+
+        IEcuProgrammingInfo GetItem(IEcu ecu, string category);
+
+        void SelectProgrammingForIndustrialCustomer(IEcu ecu, string category, bool value);
+
+        void SelectCodingForIndustrialCustomer(IEcu ecu, string category, bool value);
+
+        void SelectReplacementForIndustrialCustomer(IEcu ecu, string category, bool value);
+
+        void EstablishSelection();
+
+        IEcuProgrammingInfosData DataContext { get; }
+    }
+}
