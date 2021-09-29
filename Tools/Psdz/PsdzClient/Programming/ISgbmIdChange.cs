935d9d841b5ee29bbeb528529e499936e5a4777e
uholeschak <ulrich@holeschak.de>
2021-09-30 00:14:36 +0200

    Added event class

---
+++ Tools/Psdz/PsdzClient/Programming/ISgbmIdChange.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsdzClient.Programming
+{
+    public interface ISgbmIdChange : INotifyPropertyChanged
+    {
+        string Actual { get; }
+
+        string Target { get; }
+    }
+}
