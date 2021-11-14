a35e60c17ea0f697c6d870ec9aa82c206d9cd3b0
uholeschak <ulrich@holeschak.de>
2021-11-14 17:28:07 +0100

    Log expceptions

--- Tools/Psdz/PsdzClient/Program.cs
+++ Tools/Psdz/PsdzClient/Program.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using log4net;
 
 namespace PsdzClient
 {
     static class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += (sender, args) =>
+            {
+                log.ErrorFormat("ThreadException: {0}", args.Exception.Message);
+            };
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
