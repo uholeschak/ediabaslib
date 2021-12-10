6af8e5260c02c66f6a370c568773b23d35002dc0
uholeschak <ulrich@holeschak.de>
2021-12-10 15:18:23 +0100

    Added more events

--- Tools/Psdz/WebPsdzClient/Global.asax.cs
+++ Tools/Psdz/WebPsdzClient/Global.asax.cs
@@ -11,11 +11,45 @@ namespace WebPsdzClient
 {
     public class Global : HttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
+        public override void Init()
+        {
+            base.Init();
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
         {
             // Code, der beim Anwendungsstart ausgeführt wird
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_EndRequest(Object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
         }
     }
 }
