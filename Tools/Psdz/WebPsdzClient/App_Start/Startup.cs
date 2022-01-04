01731982729103c84da6063c31d2109ef8d7436b
uholeschak <ulrich@holeschak.de>
2022-01-04 15:55:00 +0100

    Added signalr

---
+++ Tools/Psdz/WebPsdzClient/App_Start/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(WebPsdzClient.Startup))]
+
+namespace WebPsdzClient
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+#if false
+            app.Run(context =>
+            {
+                string t = DateTime.Now.Millisecond.ToString();
+                return context.Response.WriteAsync(t + " Production OWIN App");
+            });
+#endif
+        }
+    }
+}
