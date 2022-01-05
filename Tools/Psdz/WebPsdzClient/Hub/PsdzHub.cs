e1c8d15ce62494f014f98f995ad064ad7045c6b3
uholeschak <ulrich@holeschak.de>
2022-01-05 16:52:18 +0100

    Log connections

--- Tools/Psdz/WebPsdzClient/Hub/PsdzHub.cs
+++ Tools/Psdz/WebPsdzClient/Hub/PsdzHub.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNet.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
+using log4net;
 using Microsoft.AspNet.SignalR.Hubs;
 
 namespace PsdzClient
@@ -11,10 +8,24 @@ namespace PsdzClient
     [HubName("psdzVehicleHub")]
     public class PsdzVehicleHub : Hub<IPsdzClient>
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(PsdzVehicleHub));
+
         [HubMethodName("vehicleResponse")]
         public async Task VehicleResponse(string message)
         {
-            await Clients.All.vehicleRequest("Response: " + message);
+            await Clients.Caller.vehicleRequest("Response: " + message);
+        }
+
+        public override Task OnConnected()
+        {
+            log.InfoFormat("Connected ID: {0}", Context.ConnectionId);
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            log.InfoFormat("Disconnected ID: {0}", Context.ConnectionId);
+            return base.OnDisconnected(stopCalled);
         }
     }
 
