26543e10af3fde9e55a60ecc675e780c1653f092
uholeschak <ulrich@holeschak.de>
2022-01-05 01:55:37 +0100

    Added identity

--- Tools/Psdz/WebPsdzClient/App_Start/Startup.cs
+++ Tools/Psdz/WebPsdzClient/App_Start/Startup.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
 using System;
-using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
 
 [assembly: OwinStartup(typeof(WebPsdzClient.Startup))]
 
@@ -12,6 +13,34 @@ namespace WebPsdzClient
         public void Configuration(IAppBuilder app)
         {
             app.MapSignalR();
+            ConfigureAuth(app);
+        }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Enable the application to use a cookie to store information for the signed in user
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+            // Use a cookie to temporarily store information about a user logging in with a third party login provider
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Uncomment the following lines to enable logging in with third party login providers
+            //app.UseMicrosoftAccountAuthentication(
+            // clientId: "",
+            // clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            // consumerKey: "",
+            // consumerSecret: "");
+
+            //app.UseFacebookAuthentication(
+            // appId: "",
+            // appSecret: "");
+
+            //app.UseGoogleAuthentication();
         }
     }
 }
