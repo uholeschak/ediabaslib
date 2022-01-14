530c6bafb23762535fc58451c71dc28c45b1cb41
uholeschak <ulrich@holeschak.de>
2022-01-14 11:51:24 +0100

    Disabled route switcher

--- Tools/Psdz/WebPsdzClient/App_Start/RouteConfig.cs
+++ Tools/Psdz/WebPsdzClient/App_Start/RouteConfig.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Web;
 using System.Web.Routing;
+using System.Web.UI;
 using Microsoft.AspNet.FriendlyUrls;
 
 namespace WebPsdzClient
@@ -12,7 +13,22 @@ namespace WebPsdzClient
         {
             var settings = new FriendlyUrlSettings();
             settings.AutoRedirectMode = RedirectMode.Permanent;
-            routes.EnableFriendlyUrls(settings);
+            routes.EnableFriendlyUrls(settings, new CustionWebFormsFriendlyUrlResolver());
+        }
+    }
+
+    public class CustionWebFormsFriendlyUrlResolver : Microsoft.AspNet.FriendlyUrls.Resolvers.WebFormsFriendlyUrlResolver
+    {
+        protected override bool TrySetMobileMasterPage(HttpContextBase httpContext, Page page, String mobileSuffix)
+        {
+            if (mobileSuffix == "Mobile")
+            {
+                return false;
+            }
+            else
+            {
+                return base.TrySetMobileMasterPage(httpContext, page, mobileSuffix);
+            }
         }
     }
 }
