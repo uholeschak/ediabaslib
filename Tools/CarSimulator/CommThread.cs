d9e202d3430c3d0d8c86b4a612aac3454d093757
uholeschak <ulrich@holeschak.de>
2022-02-08 16:56:30 +0100

    Added dummy RC_RLEBI_IDR response

--- Tools/CarSimulator/CommThread.cs
+++ Tools/CarSimulator/CommThread.cs
@@ -5266,16 +5266,24 @@ namespace CarSimulator
                     {
                         // dummy ok response for service 31 (routine control)
                         Debug.WriteLine("Dummy service 31: {0:X02}: {1:X02}{2:X02}", _receiveData[4], _receiveData[5], _receiveData[6]);
-                        if (_receiveData[4] == 0x01 && _receiveData[5] == 0x10 && _receiveData[5] == 0x02)
+                        if (_receiveData[4] == 0x01 && _receiveData[5] == 0x10 && _receiveData[6] == 0x02)
                         {
-                            Debug.WriteLine("Dummy RC_RLEBI_IDR response");
+                            int index = (_receiveData[10] << 8) | _receiveData[11];
+                            Debug.WriteLine("RC_RLEBI_IDR Index:{0}", index);
                             byte[] response =
                             {
                                 0x9F, 0xF1, 0x63, 0x71, 0x01, 0x10, 0x02, 0x01,
                                 0x00, 0x02, 0x58, 0x0E, 0x10, 0x0E, 0x10, 0xFF,
                                 0xFF, 0x00, 0x46, 0x10, 0x02, 0x00, 0x00, 0x00,
-                                0x02, 0x00, 0x00, 0x00, 0x00, 0x04, 0xFE, 0x0E, 0x00, 0x01
+                                0x02, 0x00, 0x00, 0x00, 0x00, 0x04, 0xFE, 0x0E,
+                                0x00, 0x01
                             };
+
+                            if (index > 10)
+                            {
+                                response[8] = 0x02; // final (0x01=not valid and not final)
+                            }
+
                             Array.Copy(response, _sendData, response.Length);
                         }
                         else
