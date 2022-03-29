fa61075459240f30333fa486cf149b440f2f7c48
uholeschak <ulrich@holeschak.de>
2022-03-29 14:42:23 +0200

    Allow to insert instructions

--- Tools/Psdz/dnpatch/PatchHelper.cs
+++ Tools/Psdz/dnpatch/PatchHelper.cs
@@ -987,7 +987,7 @@ namespace dnpatch
                 for (int i = 0; i < target.Indices.Length; i++)
                 {
                     var index = target.Indices[i];
-                    instructions.Insert(index, target.Instruction);
+                    instructions.Insert(index, target.Instructions[i]);
                 }
             }
             else
