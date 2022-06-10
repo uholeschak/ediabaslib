ad69fdc345363e42417feddff4b36a513966a819
uholeschak <ulrich@holeschak.de>
2022-06-10 12:31:34 +0200

    Using external structs

---
+++ EdiabasLib/BmwFileReader/VehicleStructsBmw.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml.Serialization;
+using PsdzClient;
+
+namespace BmwFileReader
+{
+    public static class VehicleStructsBmw
+    {
+        [XmlType("VehicleSeriesInfo")]
+        public class VehicleSeriesInfo
+        {
+            public VehicleSeriesInfo()
+            {
+            }
+
+            public VehicleSeriesInfo(string series, string brSgbd, string brand, string date, string dateCompare)
+            {
+                Series = series;
+                BrSgbd = brSgbd;
+                Brand = brand;
+                Date = date;
+                DateCompare = dateCompare;
+            }
+
+            public void ResetDate()
+            {
+                Date = string.Empty;
+                DateCompare = string.Empty;
+            }
+
+            [XmlElement("Series"), DefaultValue(null)] public string Series { get; set; }
+            [XmlElement("BrSgbd"), DefaultValue(null)] public string BrSgbd { get; set; }
+            [XmlElement("Brand"), DefaultValue(null)] public string Brand { get; set; }
+            [XmlElement("Date"), DefaultValue(null)] public string Date { get; set; }
+            [XmlElement("DateCompare"), DefaultValue(null)] public string DateCompare { get; set; }
+        }
+
+        [XmlInclude(typeof(VehicleSeriesInfo))]
+        [XmlType("VehicleSeriesInfoDataXml")]
+        public class VehicleSeriesInfoData
+        {
+            public VehicleSeriesInfoData() : this(null)
+            {
+            }
+
+            public VehicleSeriesInfoData(SerializableDictionary<string, List<VehicleSeriesInfo>> vehicleSeriesDict)
+            {
+                VehicleSeriesDict = vehicleSeriesDict;
+            }
+
+            [XmlElement("VehicleSeriesDict"), DefaultValue(null)] public SerializableDictionary<string, List<VehicleSeriesInfo>> VehicleSeriesDict { get; set; }
+        }
+    }
+}
