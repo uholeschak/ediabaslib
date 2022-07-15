4b3873f4f50dcaa7614fe6295487f7f9080b3b51
uholeschak <ulrich@holeschak.de>
2022-07-15 23:42:38 +0200

    Started generate rules cs file

--- EdiabasLib/BmwFileReader/SerializableDictionary.cs
+++ EdiabasLib/BmwFileReader/SerializableDictionary.cs
@@ -3,7 +3,7 @@ using System.Xml;
 using System.Xml.Schema;
 using System.Xml.Serialization;
 
-namespace EdiabasLib
+namespace BmwFileReader
 {
     /// <summary>
     /// Base on https://weblogs.asp.net/pwelter34/444961
