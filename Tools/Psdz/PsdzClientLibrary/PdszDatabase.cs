3576bb4ad6c323453d38a0a6498cf2df187348b9
uholeschak <ulrich@holeschak.de>
2022-08-31 21:40:50 +0200

    Store database version in class

--- Tools/Psdz/PsdzClientLibrary/PdszDatabase.cs
+++ Tools/Psdz/PsdzClientLibrary/PdszDatabase.cs
@@ -2688,7 +2688,7 @@ public class RulesInfo
 {{
     public const string DatabaseVersion = ""{dbInfo.Version}"";
 
-    public const string DatabaseDate = {dbInfo.DateTime};
+    public const DateTime DatabaseDate = DateTime.FromBinary({dbInfo.DateTime.ToBinary()});
 
     public RuleEvalBmw RuleEvalClass {{ get; private set; }}
 
