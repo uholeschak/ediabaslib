2361ef4b21c3d051f4e2fa7e6732ab2770027355
uholeschak <ulrich@holeschak.de>
2022-09-06 18:20:21 +0200

    Added custom backup agent

---
+++ BmwDeepObd/CustomBackupAgent.cs
@@ -0,0 +1,49 @@
+﻿using Android.App.Backup;
+using Android.OS;
+
+namespace BmwDeepObd;
+
+public class CustomBackupAgent : BackupAgent
+{
+#if DEBUG
+    private static readonly string Tag = typeof(CustomBackupAgent).FullName;
+#endif
+
+    public override void OnCreate()
+    {
+        base.OnCreate();
+#if DEBUG
+        Android.Util.Log.Info(Tag, "OnCreate");
+#endif
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+#if DEBUG
+        Android.Util.Log.Info(Tag, "OnDestroy");
+#endif
+    }
+
+    public override void OnBackup(ParcelFileDescriptor oldState, BackupDataOutput data, ParcelFileDescriptor newState)
+    {
+#if DEBUG
+        Android.Util.Log.Info(Tag, string.Format("OnBackup: Flags={0}", data?.TransportFlags));
+#endif
+    }
+
+    public override void OnRestore(BackupDataInput data, int appVersionCode, ParcelFileDescriptor newState)
+    {
+#if DEBUG
+        Android.Util.Log.Info(Tag, string.Format("OnRestore: Version={0}", appVersionCode));
+#endif
+    }
+
+    public override void OnQuotaExceeded(long backupDataBytes, long quotaBytes)
+    {
+        base.OnQuotaExceeded(backupDataBytes, quotaBytes);
+#if DEBUG
+        Android.Util.Log.Info(Tag, string.Format("OnQuotaExceeded: Backup={0}, Quota={1}", backupDataBytes, quotaBytes));
+#endif
+    }
+}
