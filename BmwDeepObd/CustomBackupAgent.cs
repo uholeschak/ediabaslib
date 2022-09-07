9215e46cec565117f63a2c33eb4039bb4f89da4f
uholeschak <ulrich@holeschak.de>
2022-09-07 14:02:23 +0200

    Log Backup operation

--- BmwDeepObd/CustomBackupAgent.cs
+++ BmwDeepObd/CustomBackupAgent.cs
@@ -6,77 +6,91 @@ namespace BmwDeepObd;
 
 public class CustomBackupAgent : BackupAgent
 {
-#if DEBUG
     private static readonly string Tag = typeof(CustomBackupAgent).FullName;
-#endif
 
     public override void OnCreate()
     {
-#if DEBUG
         Android.Util.Log.Info(Tag, "OnCreate");
-#endif
         base.OnCreate();
     }
 
     public override void OnDestroy()
     {
-#if DEBUG
         Android.Util.Log.Info(Tag, "OnDestroy");
-#endif
         base.OnDestroy();
     }
 
     public override void OnBackup(ParcelFileDescriptor oldState, BackupDataOutput data, ParcelFileDescriptor newState)
     {
-#if DEBUG
-        Android.Util.Log.Info(Tag, string.Format("OnBackup: Flags={0}", data?.TransportFlags));
-#endif
+        BackupTransportFlags transportFlags = 0;
+        long quota = 0;
+        if (data != null)
+        {
+            transportFlags = data.TransportFlags;
+            quota = data.Quota;
+        }
+
+        Android.Util.Log.Info(Tag, string.Format("OnBackup: Flags={0}, Quota={1}", transportFlags, quota));
     }
 
     public override void OnRestore(BackupDataInput data, int appVersionCode, ParcelFileDescriptor newState)
     {
-#if DEBUG
-        Android.Util.Log.Info(Tag, string.Format("OnRestore: Version={0}", appVersionCode));
-#endif
+        int dataSize = 0;
+        if (data != null)
+        {
+            dataSize = data.DataSize;
+        }
+
+        Android.Util.Log.Info(Tag, string.Format("OnRestore: Version={0}, Size={1}", appVersionCode, dataSize));
     }
 
     public override void OnRestore(BackupDataInput data, long appVersionCode, ParcelFileDescriptor newState)
     {
-#if DEBUG
-        Android.Util.Log.Info(Tag, string.Format("OnRestore: Version={0}", appVersionCode));
-#endif
+        int dataSize = 0;
+        if (data != null)
+        {
+            dataSize = data.DataSize;
+        }
+
+        Android.Util.Log.Info(Tag, string.Format("OnRestore: Version={0}, Size={1}", appVersionCode, dataSize));
         base.OnRestore(data, appVersionCode, newState);
     }
 
     public override void OnFullBackup(FullBackupDataOutput data)
     {
-#if DEBUG
-        Android.Util.Log.Info(Tag, string.Format("OnFullBackup: Quota={0}", data?.Quota));
-#endif
+        BackupTransportFlags transportFlags = 0;
+        long quota = 0;
+        if (data != null)
+        {
+            transportFlags = data.TransportFlags;
+            quota = data.Quota;
+        }
+
+        Android.Util.Log.Info(Tag, string.Format("OnFullBackup: Flags={0}, Quota={1}", transportFlags, quota));
         base.OnFullBackup(data);
     }
 
     public override void OnRestoreFile(ParcelFileDescriptor data, long size, File destination, BackupFileType type, long mode, long mtime)
     {
-#if DEBUG
-        Android.Util.Log.Info(Tag, string.Format("OnRestoreFile: Name={0}, Size={1}", destination?.AbsoluteFile.Name, size));
-#endif
+        string fileName = string.Empty;
+        if (destination != null)
+        {
+            fileName = destination.AbsoluteFile.Name;
+        }
+
+        Android.Util.Log.Info(Tag, string.Format("OnRestoreFile: Name={0}, Size={1}", fileName, size));
         base.OnRestoreFile(data, size, destination, type, mode, mtime);
     }
 
     public override void OnRestoreFinished()
     {
-#if DEBUG
         Android.Util.Log.Info(Tag, "OnRestoreFinished");
-#endif
         base.OnRestoreFinished();
     }
 
     public override void OnQuotaExceeded(long backupDataBytes, long quotaBytes)
     {
-#if DEBUG
         Android.Util.Log.Info(Tag, string.Format("OnQuotaExceeded: Backup={0}, Quota={1}", backupDataBytes, quotaBytes));
-#endif
         base.OnQuotaExceeded(backupDataBytes, quotaBytes);
     }
 }
