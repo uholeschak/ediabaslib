af375d8696f7f392ea197f42431ce5e948073832
uholeschak <ulrich@holeschak.de>
2022-09-12 12:21:43 +0200

    Always request permission

--- BmwDeepObd/ExpansionDownloaderActivity.cs
+++ BmwDeepObd/ExpansionDownloaderActivity.cs
@@ -892,10 +892,7 @@ namespace BmwDeepObd
                 Finish();
             }
 
-            if (ActivityCommon.IsExtrenalStorageAccessSupported())
-            {
-                ActivityCompat.RequestPermissions(this, _permissionsExternalStorage, ActivityCommon.RequestPermissionExternalStorage);
-            }
+            ActivityCompat.RequestPermissions(this, _permissionsExternalStorage, ActivityCommon.RequestPermissionExternalStorage);
         }
 
         private void StoragePermissionGranted()
