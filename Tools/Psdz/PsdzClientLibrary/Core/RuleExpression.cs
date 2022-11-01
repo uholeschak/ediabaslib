ff3781440982a9da91d84666435292262d4e3874
uholeschak <ulrich@holeschak.de>
2022-11-01 20:46:33 +0100

    Code updated

--- Tools/Psdz/PsdzClientLibrary/Core/RuleExpression.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/RuleExpression.cs
@@ -28,29 +28,30 @@ namespace PsdzClient.Core
             public string OperatorSeparator { get; private set; }
         }
 
-		public enum EExpressionType
+        // ToDo: Check on update
+        public enum EExpressionType
         {
-            COMP,
-            AND,
-            OR,
-            NOT,
-            DATE,
-            VALUE,
-            ISTUFE,
-            VALID_FROM,
-            VALID_TO,
-            COUNTRY,
-            ECUGROUP,
-            ECUVARIANT,
-            ECUCLIQUE,
-            EQUIPMENT,
-            SALAPA,
-            SIFA,
-            VARIABLE,
-            CHARACTERISTIC,
-            ECUREPRESENTATIVE,
-            MANUFACTORINGDATE,
-            ISTUFEX,
+            COMP = 0,
+            AND = 1,
+            OR = 2,
+            NOT = 3,
+            DATE = 4,
+            VALUE = 5,
+            ISTUFE = 6,
+            VALID_FROM = 7,
+            VALID_TO = 8,
+            COUNTRY = 9,
+            ECUGROUP = 10,
+            ECUVARIANT = 11,
+            ECUCLIQUE = 12,
+            EQUIPMENT = 13,
+            SALAPA = 14,
+            SIFA = 15,
+            VARIABLE = 16,
+            CHARACTERISTIC = 17,
+            ECUREPRESENTATIVE = 18,
+            MANUFACTORINGDATE = 19,
+            ISTUFEX = 20,
             ECUPROGRAMMINGVARIANT = 22
         }
 
