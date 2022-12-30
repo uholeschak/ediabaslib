36eae28b89b8d89ab288302c0b28f84a7cbcb6ac
uholeschak <ulrich@holeschak.de>
2022-12-30 14:02:31 +0100

    Updated AssemblyDirectory

--- EdiabasLib/EdiabasLibConfigTool/Patch.cs
+++ EdiabasLib/EdiabasLibConfigTool/Patch.cs
@@ -77,7 +77,16 @@ namespace EdiabasLibConfigTool
 
         public static bool CopyRuntimeRequired { get; private set; }
 
-        public static string AssemblyDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
 
         public static bool IsOriginalDll(string fileName)
         {
