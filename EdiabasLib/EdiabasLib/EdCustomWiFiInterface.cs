bd2cd7c0062ed741d6df03df5c782114d17b8ab1
uholeschak <ulrich@holeschak.de>
2023-02-15 14:45:22 +0100

    Using debug macro

--- EdiabasLib/EdiabasLib/EdCustomWiFiInterface.cs
+++ EdiabasLib/EdiabasLib/EdCustomWiFiInterface.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿#if DEBUG && Android
+#define DEBUG_ANDROID
+#endif
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -23,7 +26,7 @@ namespace EdiabasLib
         }
 #endif
 
-#if DEBUG && Android
+#if DEBUG_ANDROID
         private static readonly string Tag = typeof(EdCustomWiFiInterface).FullName;
 #endif
         public const string PortId = "DEEPOBDWIFI";
@@ -468,7 +471,7 @@ namespace EdiabasLib
 
         private static void SendData(byte[] buffer, int length)
         {
-#if DEBUG && Android
+#if DEBUG_ANDROID
             List<byte> sendList = buffer.ToList().GetRange(0, length);
             Android.Util.Log.Info(Tag, string.Format("Send: {0}", BitConverter.ToString(sendList.ToArray()).Replace("-", " ")));
 #endif
@@ -517,7 +520,7 @@ namespace EdiabasLib
                 buffer[offset + recLen] = (byte)data;
                 recLen++;
             }
-#if DEBUG && Android
+#if DEBUG_ANDROID
             if (recLen > 0)
             {
                 List<byte> recList = buffer.ToList().GetRange(offset, recLen);
@@ -526,7 +529,7 @@ namespace EdiabasLib
 #endif
             if (recLen < length)
             {
-#if DEBUG && Android
+#if DEBUG_ANDROID
                 Android.Util.Log.Info(Tag, string.Format("Rec len={0}, expected={1}", recLen, length));
 #endif
                 ediabasLog?.LogData(EdiabasNet.EdLogLevel.Ifh, buffer, offset, recLen, "Rec ");
@@ -565,7 +568,7 @@ namespace EdiabasLib
                     responseList.Add((byte)data);
                 }
             }
-#if DEBUG && Android
+#if DEBUG_ANDROID
             if (responseList.Count > 0)
             {
                 Android.Util.Log.Info(Tag, string.Format("Rec: {0}", BitConverter.ToString(responseList.ToArray()).Replace("-", " ")));
