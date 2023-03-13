5c22b5097ba7f47b95a6c4536dbb0c9010fed9c9
uholeschak <ulrich@holeschak.de>
2023-03-13 13:04:52 +0100

    Using ReadBytesAsync

--- EdiabasLib/EdiabasLib/StreamExtension.cs
+++ EdiabasLib/EdiabasLib/StreamExtension.cs
@@ -32,77 +32,14 @@ namespace EdiabasLib
 
         public static int ReadByteAsync(this Stream inStream, ManualResetEvent cancelEvent = null, int timeout = 2000)
         {
-            if (inStream == null)
+            byte[] dataBuffer = new byte[1];
+            int result = ReadBytesAsync(inStream, dataBuffer, 0, dataBuffer.Length, cancelEvent, timeout);
+            if (result != 1)
             {
                 return -1;
             }
 
-            if (cancelEvent != null)
-            {
-                if (cancelEvent.WaitOne(0))
-                {
-                    return -1;
-                }
-            }
-
-            if (inStream is MemoryQueueBufferStream memoryStream)
-            {
-                return memoryStream.ReadByte();
-            }
-
-            if (inStream is EscapeStreamReader escapeStream)
-            {
-                return escapeStream.ReadByte();
-            }
-
-            byte[] dataBuffer = new byte[1];
-            int result = -1;
-            Thread abortThread = null;
-            AutoResetEvent threadFinishEvent = null;
-            try
-            {
-                using (CancellationTokenSource cts = new CancellationTokenSource())
-                {
-                    threadFinishEvent = new AutoResetEvent(false);
-                    Task<int> readTask = inStream.ReadAsync(dataBuffer, 0, dataBuffer.Length, cts.Token);
-                    if (cancelEvent != null)
-                    {
-                        WaitHandle[] waitHandles = { threadFinishEvent, cancelEvent };
-                        abortThread = new Thread(() =>
-                        {
-                            if (WaitHandle.WaitAny(waitHandles, timeout) == 1)
-                            {
-                                // ReSharper disable once AccessToDisposedClosure
-                                cts.Cancel();
-                            }
-                        });
-                        abortThread.Start();
-                    }
-
-                    if (!readTask.Wait(timeout))
-                    {
-                        cts.Cancel();
-                    }
-
-                    if (readTask.Status == TaskStatus.RanToCompletion && readTask.Result == dataBuffer.Length &&
-                        !cts.IsCancellationRequested)
-                    {
-                        result = dataBuffer[0];
-                    }
-                }
-            }
-            finally
-            {
-                if (abortThread != null)
-                {
-                    threadFinishEvent.Set();
-                    abortThread.Join();
-                }
-
-                threadFinishEvent?.Dispose();
-            }
-
-            return result;
+            return dataBuffer[0];
         }
 
         public static int ReadBytesAsync(this Stream inStream, byte[] buffer, int offset, int count, ManualResetEvent cancelEvent = null, int timeout = 2000)
