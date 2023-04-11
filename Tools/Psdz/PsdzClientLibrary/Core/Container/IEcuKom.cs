3268a8d389078782d732db3900ede5f620ca049a
uholeschak <ulrich@holeschak.de>
2023-04-11 15:54:27 +0200

    Interfaces added

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Container/IEcuKom.cs
@@ -0,0 +1,45 @@
+﻿// BMW.Rheingold.CoreFramework.Contracts.VehicleCommunication.IEcuKom
+using BMW.Rheingold.CoreFramework.Contracts.Vehicle;
+
+namespace PsdzClient.Core.Container
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    public interface IEcuKom : IEcuKomApi
+    {
+        uint EdiabasHandle { get; }
+
+        bool IsInSimulationMode { get; }
+
+        bool IsProblemHandlingTraceRunning { get; }
+
+        IEcuJob ApiJob(string ecu, string job, string param, string resultFilter, bool cacheAdding);
+
+        IEcuJob DefaultApiJob(string ecu, string job, string param, string resultFilter);
+
+        IEcuJob ApiJobWithRetries(string variant, string job, string param, string resultFilter, int retries);
+
+        void End();
+
+        string GetEdiabasIniFilePath(string iniFilename);
+
+        bool InitVCI(IVciDevice vciDevice);
+
+        bool Refresh();
+
+        void SetLogLevelToMax();
+
+        void SetLogLevelToNormal();
+
+        int GetCacheHitCounter();
+
+        int GetCacheMissCounter();
+
+        int GetCacheListNumberOfJobs();
+
+        int GetCacheListNumberOfJobsToBeRetrieved();
+
+        bool ApiInitExt(string ifh, string unit, string app, string reserved);
+
+        void SetEcuPath(bool logging);
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Core/Container/IEcuKomApi.cs
@@ -0,0 +1,16 @@
+﻿// BMW.Rheingold.CoreFramework.Contracts.VehicleCommunication.IEcuKomApi
+
+namespace PsdzClient.Core.Container
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    public interface IEcuKomApi
+    {
+        IEcuJob ApiJob(string ecu, string job, string param, string resultFilter = "", int retries = 0, bool fastaActive = true);
+
+        IEcuJob ApiJob(string ecu, string job, string param, int retries, int millisecondsTimeout);
+
+        IEcuJob ApiJobData(string ecu, string job, byte[] param, int paramlen, string resultFilter = "", int retries = 0);
+
+        IEcuJob ExecuteJobOverEnet(string icomAddress, string ecu, string job, string param, string resultFilter = "", int retries = 0);
+    }
+}
