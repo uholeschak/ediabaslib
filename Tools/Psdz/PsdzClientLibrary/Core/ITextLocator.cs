ad2c83c039e339a249c1d63269a6db0ce8f44335
uholeschak <ulrich@holeschak.de>
2023-05-17 12:24:35 +0200

    ITextLocator

---
+++ Tools/Psdz/PsdzClientLibrary/Core/ITextLocator.cs
@@ -0,0 +1,21 @@
+﻿using PsdzClient.Core.Container;
+using System.Collections.Generic;
+
+namespace PsdzClient.Core
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    public interface ITextLocator : ISPELocator
+    {
+        string Text { get; }
+
+        ITextContent TextContent { get; set; }
+
+        ITextLocator Concat(ITextLocator theTextLocator);
+
+        ITextLocator Concat(ITextLocator theTextLocator, bool theAddLineBreakAfter);
+
+        ITextLocator Concat(IEnumerable<ITextLocator> theTextLocator);
+
+        ITextLocator Concat(IEnumerable<ITextLocator> theTextLocator, bool theAddLineBreakAfter);
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Core/LocalizedText.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace PsdzClient.Core
+{
+    [DataContract]
+    public class LocalizedText : ICloneable
+    {
+        private string textItem;
+
+        private string language;
+
+        [DataMember]
+        public string TextItem
+        {
+            get
+            {
+                return textItem;
+            }
+            set
+            {
+                textItem = value;
+            }
+        }
+
+        [DataMember]
+        public string Language
+        {
+            get
+            {
+                return language;
+            }
+            set
+            {
+                language = value;
+            }
+        }
+
+        public LocalizedText(string textItem, string language)
+        {
+            this.textItem = textItem;
+            this.language = language;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj != null && obj is LocalizedText)
+            {
+                LocalizedText localizedText = (LocalizedText)obj;
+                if (Language == localizedText.Language)
+                {
+                    return TextItem == localizedText.TextItem;
+                }
+                return false;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Language + TextItem).GetHashCode();
+        }
+
+        public object Clone()
+        {
+            return new LocalizedText(TextItem, Language);
+        }
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture, "[{0},{1}]", Language, TextItem);
+        }
+    }
+}
