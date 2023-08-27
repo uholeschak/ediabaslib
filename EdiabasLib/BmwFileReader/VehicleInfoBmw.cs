a30c6dac5b954bdf79ff287e4c2b5eb6f35f20fb
uholeschak <ulrich@holeschak.de>
2023-08-27 18:52:45 +0200

    Improved series extraction

--- EdiabasLib/BmwFileReader/VehicleInfoBmw.cs
+++ EdiabasLib/BmwFileReader/VehicleInfoBmw.cs
@@ -948,7 +948,7 @@ namespace BmwFileReader
                 case 'J':
                 case 'U':
                     ediabas?.LogString(EdiabasNet.EdLogLevel.Ifh, "Using fallback from first letter");
-                    return new VehicleStructsBmw.VehicleSeriesInfo(series, "F01", "BN2020");
+                    return new VehicleStructsBmw.VehicleSeriesInfo(series, null, "F01", "BN2020");
             }
 
             ediabas?.LogString(EdiabasNet.EdLogLevel.Ifh, "No vehicle series info found");
