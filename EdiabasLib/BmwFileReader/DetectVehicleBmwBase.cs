abf24c65b8cfb5b2bb796059e12d8255c11b43ea
uholeschak <ulrich@holeschak.de>
2023-09-18 22:33:10 +0200

    Reused common code

--- EdiabasLib/BmwFileReader/DetectVehicleBmwBase.cs
+++ EdiabasLib/BmwFileReader/DetectVehicleBmwBase.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Xml;
-using System.Xml.Serialization;
-using BmwDeepObd;
 using EdiabasLib;
 
 namespace BmwFileReader
