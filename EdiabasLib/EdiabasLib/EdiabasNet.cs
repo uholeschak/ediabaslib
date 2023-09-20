e21c43a06b8bb4537f03c8995dbf7d138caab087
uholeschak <ulrich@holeschak.de>
2023-09-20 23:36:54 +0200

    Log android info

--- EdiabasLib/EdiabasLib/EdiabasNet.cs
+++ EdiabasLib/EdiabasLib/EdiabasNet.cs
@@ -5750,6 +5750,10 @@ namespace EdiabasLib
                         {
                             string currDateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
                             _swLog.WriteLine(string.Format(CultureInfo.InvariantCulture, "Date: {0}", currDateTime));
+#if Android
+                            _swLog.WriteLine(string.Format(CultureInfo.InvariantCulture, "Android version: {0}", (long)Android.OS.Build.VERSION.SdkInt));
+                            _swLog.WriteLine(string.Format(CultureInfo.InvariantCulture, "Android fingerprint: {0}", Android.OS.Build.Fingerprint));
+#endif
                         }
                         _swLog.WriteLine(info);
                     }
