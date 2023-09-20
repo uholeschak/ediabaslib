522763f4c5cf6e9c167d0633f9ba41196cf4f52f
uholeschak <ulrich@holeschak.de>
2023-09-20 16:14:30 +0200

    Improved ELM parsing

--- EdiabasLib/EdiabasLib/EdElmInterface.cs
+++ EdiabasLib/EdiabasLib/EdElmInterface.cs
@@ -266,7 +266,8 @@ namespace EdiabasLib
                 Ediabas?.LogString(EdiabasNet.EdLogLevel.Ifh, "Sending @1 failed");
                 return false;
             }
-            string elmDevDesc = Elm327ReceiveAnswer(Elm327CommandTimeout);
+
+            string elmDevDesc = TrimElmResponse(Elm327ReceiveAnswer(Elm327CommandTimeout));
             Ediabas?.LogFormat(EdiabasNet.EdLogLevel.Ifh, "ELM ID: {0}", elmDevDesc);
             if (elmDevDesc.ToUpperInvariant().Contains(Elm327CarlyIdentifier))
             {
@@ -278,13 +279,13 @@ namespace EdiabasLib
                 Ediabas?.LogString(EdiabasNet.EdLogLevel.Ifh, "Sending #1 failed");
                 return false;
             }
-            string elmManufact = Elm327ReceiveAnswer(Elm327CommandTimeout);
+
+            string elmManufact = TrimElmResponse(Elm327ReceiveAnswer(Elm327CommandTimeout));
             Ediabas?.LogFormat(EdiabasNet.EdLogLevel.Ifh, "ELM Manufacturer: {0}", elmManufact);
 
             if (_elm327TransportType == TransportType.Standard && elmManufact.ToUpperInvariant().Contains(Elm327WgSoftIdentifier))
             {
-                string verString = elmDevDesc.Trim('\r', '\n', '>', ' ');
-                if (double.TryParse(verString, NumberStyles.Float, CultureInfo.InvariantCulture, out double version))
+                if (double.TryParse(elmDevDesc, NumberStyles.Float, CultureInfo.InvariantCulture, out double version))
                 {
                     if (version < Elm327WgSoftMinVer)
                     {
@@ -298,7 +299,8 @@ namespace EdiabasLib
                 Ediabas?.LogString(EdiabasNet.EdLogLevel.Ifh, "Sending STI failed");
                 return false;
             }
-            string stnVers = Elm327ReceiveAnswer(Elm327CommandTimeout);
+
+            string stnVers = TrimElmResponse(Elm327ReceiveAnswer(Elm327CommandTimeout));
             if (stnVers.ToUpperInvariant().Contains("STN"))
             {
                 Ediabas?.LogFormat(EdiabasNet.EdLogLevel.Ifh, "STN Version: {0}", stnVers);
@@ -308,10 +310,10 @@ namespace EdiabasLib
                     return false;
                 }
 
-                string stnVersExt = Elm327ReceiveAnswer(Elm327CommandTimeout);
+                string stnVersExt = TrimElmResponse(Elm327ReceiveAnswer(Elm327CommandTimeout));
                 if (!string.IsNullOrEmpty(stnVersExt))
                 {
-                    Ediabas?.LogFormat(EdiabasNet.EdLogLevel.Ifh, "STN Ext Version: {0}", stnVers);
+                    Ediabas?.LogFormat(EdiabasNet.EdLogLevel.Ifh, "STN Ext Version: {0}", stnVersExt);
                 }
             }
 
@@ -1617,6 +1619,16 @@ namespace EdiabasLib
             {
                 return '.';
             }
+        }
+
+        public static string TrimElmResponse(string elmResponse)
+        {
+            if (string.IsNullOrWhiteSpace(elmResponse))
+            {
+                return string.Empty;
+            }
+
+            return elmResponse.Trim('\r', '\n', '>', ' ');
         }
 
         public void Dispose()
