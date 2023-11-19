3c821f907962a9124473c72b9732cc5b9c54d997
uholeschak <ulrich@holeschak.de>
2023-11-19 19:31:13 +0100

    SetSvkTargetForEachEcu

--- Tools/Psdz/PsdzClientLibrary/Programming/EcuProgrammingInfos.cs
+++ Tools/Psdz/PsdzClientLibrary/Programming/EcuProgrammingInfos.cs
@@ -259,36 +259,33 @@ namespace PsdzClient.Programming
 			this.RefreshProgrammingInfoBeforeReplace(this.DataContext.List.ToList<IEcuProgrammingInfoData>());
 		}
 
-		internal void SetSvkTargetForEachEcu(ISvt svt)
-		{
-			if (svt == null)
-			{
-				foreach (EcuProgrammingInfo ecuProgrammingInfo in this.ecuProgrammingInfos)
-				{
-					ecuProgrammingInfo.SvkTarget = null;
-				}
-				return;
-			}
-			foreach (IEcuObj ecuObj in svt.Ecus)
-			{
-				EcuProgrammingInfo ecuProgrammingInfo2 = this.GetItemFromProgrammingInfos((long)ecuObj.EcuIdentifier.DiagAddrAsInt);
-				if (ecuProgrammingInfo2 != null)
-				{
-					ecuProgrammingInfo2.SvkTarget = ecuObj.StandardSvk;
-				}
-				else
-				{
-					ECU ecu = this.programmingObjectBuilder.Build(ecuObj);
-					if (ecu != null)
-					{
-						ecuProgrammingInfo2 = this.GetEcuProgrammingInfo(ecu);
-                        ecuProgrammingInfo2.SvkTarget = ecuObj.StandardSvk;
-					}
-				}
-			}
-		}
+        internal void SetSvkTargetForEachEcu(ISvt svt)
+        {
+            if (svt == null)
+            {
+                foreach (EcuProgrammingInfo ecuProgrammingInfo in ecuProgrammingInfos)
+                {
+                    ecuProgrammingInfo.SvkTarget = null;
+                }
+                return;
+            }
+            foreach (IEcuObj ecu in svt.Ecus)
+            {
+                EcuProgrammingInfo itemFromProgrammingInfos = GetItemFromProgrammingInfos(ecu.EcuIdentifier.DiagAddrAsInt);
+                if (itemFromProgrammingInfos != null)
+                {
+                    itemFromProgrammingInfos.SvkTarget = ecu.StandardSvk;
+                    continue;
+                }
+                ECU eCU = programmingObjectBuilder.Build(ecu);
+                if (eCU != null)
+                {
+                    itemFromProgrammingInfos = GetEcuProgrammingInfo(eCU);
+                }
+            }
+        }
 
-		internal void UpdateProgrammingActions(IPsdzTal tal, int escalationStep)
+        internal void UpdateProgrammingActions(IPsdzTal tal, int escalationStep)
 		{
 			foreach (IPsdzEcuIdentifier psdzEcuIdentifier in tal.AffectedEcus)
 			{
