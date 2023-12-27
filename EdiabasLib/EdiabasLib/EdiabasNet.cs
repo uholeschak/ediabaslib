4cfc412c0124017ffd94668c3e91e1e7edccf741
uholeschak <ulrich@holeschak.de>
2023-12-27 14:41:39 +0100

    Added NET adaptions

--- EdiabasLib/EdiabasLib/EdiabasNet.cs
+++ EdiabasLib/EdiabasLib/EdiabasNet.cs
@@ -3206,10 +3206,15 @@ namespace EdiabasLib
         {
             get
             {
+#if NET
+                string location = Assembly.GetExecutingAssembly().Location;
+                return Path.GetDirectoryName(location);
+#else
                 string codeBase = Assembly.GetExecutingAssembly().CodeBase;
                 UriBuilder uri = new UriBuilder(codeBase);
                 string path = Uri.UnescapeDataString(uri.Path);
                 return Path.GetDirectoryName(path);
+#endif
             }
         }
 
