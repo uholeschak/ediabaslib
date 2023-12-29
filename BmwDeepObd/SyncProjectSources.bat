694e480f04aa7f79d64ae5e975b2b5ec21c02ad2
uholeschak <ulrich@holeschak.de>
2023-12-30 00:48:37 +0100

    Allow to specify direction

--- BmwDeepObd/SyncProjectSources.bat
+++ BmwDeepObd/SyncProjectSources.bat
@@ -5,9 +5,22 @@ set "BatDir=%~dp0"
 set "RootDir=!BatDir!\.."
 for %%i in ("!RootDir!") do SET "RootDir=%%~fi"
 
-set "SrcDir=!RootDir!\BmwDeepObdNet"
-set "DstDir=!RootDir!\BmwDeepObd"
+if "%1"=="FROM_NET" (
+  set "FromNet=1"
+  set "SrcDir=!RootDir!\BmwDeepObdNet"
+  set "DstDir=!RootDir!\BmwDeepObd"
+) else (
+  if "%1"=="TO_NET" (
+    set "FromNet=0"
+    set "DstDir=!RootDir!\BmwDeepObd"
+    set "SrcDir=!RootDir!\BmwDeepObdNet"
+  ) else (
+    echo valid modes: TO_NET, FROM_NET    
+    exit /b 1
+  )
+)
 
+echo FromNet: !FromNet!
 echo Source: !SrcDir!
 echo Dest: !DstDir!
 
@@ -19,4 +32,11 @@ ROBOCOPY "!SrcDir!\Resources" "!DstDir!\Resources" /MIR
 ROBOCOPY "!SrcDir!\Scripts" "!DstDir!\Scripts" /MIR
 ROBOCOPY "!SrcDir!\Xml" "!DstDir!\Xml" /MIR
 ROBOCOPY "!SrcDir!" "!DstDir!" *.cs *.cfg
-ROBOCOPY "!SrcDir!" "!DstDir!\Properties" AndroidManifest.xml
+if "!FromNet!"=="1" (
+  ROBOCOPY "!SrcDir!" "!DstDir!\Properties" AndroidManifest.xml
+) else (
+  ROBOCOPY "!SrcDir!\Properties" "!DstDir!" AndroidManifest.xml
+)
+
+echo done
+exit /b 0
