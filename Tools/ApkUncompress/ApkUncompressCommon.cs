4503004b8eb5cb2d15f1ee8e2c67dcf6aadf0687
uholeschak <ulrich@holeschak.de>
2023-12-30 22:27:03 +0100

    Move parts to common

--- Tools/ApkUncompress/ApkUncompressCommon.cs
+++ Tools/ApkUncompress/ApkUncompressCommon.cs
@@ -4,6 +4,8 @@ using System;
 using System.Buffers;
 using System.Text;
 using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
+using Xamarin.Android.AssemblyStore;
 
 namespace ApkUncompress;
 
@@ -86,5 +88,120 @@ public class ApkUncompressCommon
         }
 
         return retVal;
+    }
+
+    public bool UncompressDLL(string filePath, string prefix, string? outputPath)
+    {
+        using (var fs = File.Open(filePath, FileMode.Open, FileAccess.Read))
+        {
+            return UncompressDLL(fs, Path.GetFileName(filePath), prefix, outputPath);
+        }
+    }
+
+    public bool UncompressFromAPK_IndividualEntries(ZipFile apk, string filePath, string assembliesPath, string prefix, string? outputPath)
+    {
+        foreach (ZipEntry entry in apk)
+        {
+            if (!entry.IsFile)
+            {
+                continue;
+            }
+
+            if (!entry.Name.StartsWith(assembliesPath, StringComparison.Ordinal))
+            {
+                continue;
+            }
+
+            if (!entry.Name.EndsWith(".dll", StringComparison.Ordinal))
+            {
+                continue;
+            }
+
+            using (var stream = new MemoryStream())
+            {
+                byte[] buffer = new byte[4096]; // 4K is optimum
+                using (Stream zipStream = apk.GetInputStream(entry))
+                {
+                    StreamUtils.Copy(zipStream, stream, buffer);
+                }
+
+                stream.Seek(0, SeekOrigin.Begin);
+                string fileName = entry.Name.Substring(assembliesPath.Length);
+                UncompressDLL(stream, fileName, prefix, outputPath);
+            }
+        }
+
+        return true;
+    }
+
+    public bool UncompressFromAPK_AssemblyStores(string filePath, string prefix, string? outputPath)
+    {
+        var explorer = new AssemblyStoreExplorer(filePath, keepStoreInMemory: true);
+        foreach (AssemblyStoreAssembly assembly in explorer.Assemblies)
+        {
+            string assemblyName = assembly.DllName;
+
+            if (!String.IsNullOrEmpty(assembly.Store.Arch))
+            {
+                assemblyName = Path.Combine(assembly.Store.Arch, assemblyName);
+            }
+
+            using (var stream = new MemoryStream())
+            {
+                assembly.ExtractImage(stream);
+                stream.Seek(0, SeekOrigin.Begin);
+                UncompressDLL(stream, assemblyName, prefix, outputPath);
+            }
+        }
+
+        return true;
+    }
+
+    public bool UncompressFromAPK(string filePath, string assembliesPath, string prefix, string? outputPath)
+    {
+        string blobName = $"{assembliesPath}assemblies.blob";
+
+        try
+        {
+            ZipFile? zf = null;
+            try
+            {
+                bool blobFound = false;
+                FileStream fs = File.OpenRead(filePath);
+                zf = new ZipFile(fs);
+                foreach (ZipEntry zipEntry in zf)
+                {
+                    if (!zipEntry.IsFile)
+                    {
+                        continue; // Ignore directories
+                    }
+
+                    if (string.Compare(zipEntry.Name, blobName, StringComparison.OrdinalIgnoreCase) == 0)
+                    {
+                        blobFound = true;
+                        break;
+                    }
+                }
+
+                if (!blobFound)
+                {
+                    return UncompressFromAPK_IndividualEntries(zf, filePath, assembliesPath, prefix, outputPath);
+                }
+            }
+            finally
+            {
+                if (zf != null)
+                {
+                    zf.IsStreamOwner = true; // Makes close also shut the underlying stream
+                    zf.Close(); // Ensure we release resources
+                }
+            }
+
+            return UncompressFromAPK_AssemblyStores(filePath, prefix, outputPath);
+        }
+        catch (Exception)
+        {
+            return false;
+        }
     }
 }
--- Tools/ApkUncompress/main.cs
+++ Tools/ApkUncompress/main.cs
@@ -1,10 +1,5 @@
-using ICSharpCode.SharpZipLib.Core;
-using ICSharpCode.SharpZipLib.Zip;
-using K4os.Compression.LZ4;
 using System;
-using System.Buffers;
 using System.IO;
-using Xamarin.Android.AssemblyStore;
 
 namespace ApkUncompress
 {
@@ -19,122 +14,6 @@ namespace ApkUncompress
             Console.WriteLine("DLL files passed on command line are uncompressed to the file directory with the `uncompressed-` prefix added to their name.");
             Console.WriteLine("DLL files from AAB/APK archives are uncompressed to a subdirectory of the file directory named after the archive with extension removed");
             return 1;
-        }
-
-        static bool UncompressDLL(string filePath, string prefix, string? outputPath)
-        {
-            using (var fs = File.Open(filePath, FileMode.Open, FileAccess.Read))
-            {
-                return apkUncompress.UncompressDLL(fs, Path.GetFileName(filePath), prefix, outputPath);
-            }
-        }
-
-        static bool UncompressFromAPK_IndividualEntries(ZipFile apk, string filePath, string assembliesPath, string prefix, string? outputPath)
-        {
-            foreach (ZipEntry entry in apk)
-            {
-                if (!entry.IsFile)
-                {
-                    continue;
-                }
-
-                if (!entry.Name.StartsWith(assembliesPath, StringComparison.Ordinal))
-                {
-                    continue;
-                }
-
-                if (!entry.Name.EndsWith(".dll", StringComparison.Ordinal))
-                {
-                    continue;
-                }
-
-                using (var stream = new MemoryStream())
-                {
-                    byte[] buffer = new byte[4096]; // 4K is optimum
-                    using (Stream zipStream = apk.GetInputStream(entry))
-                    {
-                        StreamUtils.Copy(zipStream, stream, buffer);
-                    }
-
-                    stream.Seek(0, SeekOrigin.Begin);
-                    string fileName = entry.Name.Substring(assembliesPath.Length);
-                    apkUncompress.UncompressDLL(stream, fileName, prefix, outputPath);
-                }
-            }
-
-            return true;
-        }
-
-        static bool UncompressFromAPK_AssemblyStores(string filePath, string prefix, string? outputPath)
-        {
-            var explorer = new AssemblyStoreExplorer(filePath, keepStoreInMemory: true);
-            foreach (AssemblyStoreAssembly assembly in explorer.Assemblies)
-            {
-                string assemblyName = assembly.DllName;
-
-                if (!String.IsNullOrEmpty(assembly.Store.Arch))
-                {
-                    assemblyName = Path.Combine(assembly.Store.Arch, assemblyName);
-                }
-
-                using (var stream = new MemoryStream())
-                {
-                    assembly.ExtractImage(stream);
-                    stream.Seek(0, SeekOrigin.Begin);
-                    apkUncompress.UncompressDLL(stream, assemblyName, prefix, outputPath);
-                }
-            }
-
-            return true;
-        }
-
-        static bool UncompressFromAPK(string filePath, string assembliesPath, string? outputPath)
-        {
-            string prefix = $"uncompressed-{Path.GetFileNameWithoutExtension(filePath)}";
-            string blobName = $"{assembliesPath}assemblies.blob";
-
-            try
-            {
-                ZipFile? zf = null;
-                try
-                {
-                    bool blobFound = false;
-                    FileStream fs = File.OpenRead(filePath);
-                    zf = new ZipFile(fs);
-                    foreach (ZipEntry zipEntry in zf)
-                    {
-                        if (!zipEntry.IsFile)
-                        {
-                            continue; // Ignore directories
-                        }
-
-                        if (string.Compare(zipEntry.Name, blobName, StringComparison.OrdinalIgnoreCase) == 0)
-                        {
-                            blobFound = true;
-                            break;
-                        }
-                    }
-
-                    if (!blobFound)
-                    {
-                        return UncompressFromAPK_IndividualEntries(zf, filePath, assembliesPath, prefix, outputPath);
-                    }
-                }
-                finally
-                {
-                    if (zf != null)
-                    {
-                        zf.IsStreamOwner = true; // Makes close also shut the underlying stream
-                        zf.Close(); // Ensure we release resources
-                    }
-                }
-
-                return UncompressFromAPK_AssemblyStores(filePath, prefix, outputPath);
-            }
-            catch (Exception)
-            {
-                return false;
-            }
         }
 
         static int Main(string[] args)
@@ -154,9 +33,11 @@ namespace ApkUncompress
                     continue;
                 }
                 string? outputPath = Path.GetDirectoryName(fullPath);
+                string prefix = $"uncompressed-{Path.GetFileNameWithoutExtension(file)}";
+
                 if (String.Compare(".dll", ext, StringComparison.OrdinalIgnoreCase) == 0)
                 {
-                    if (!UncompressDLL(file, "uncompressed-", outputPath))
+                    if (!apkUncompress.UncompressDLL(file, "uncompressed-", outputPath))
                     {
                         Console.WriteLine("Uncompress failed: {0}", file);
                         haveErrors = true;
@@ -168,7 +49,7 @@ namespace ApkUncompress
 
                 if (String.Compare(".apk", ext, StringComparison.OrdinalIgnoreCase) == 0)
                 {
-                    if (!UncompressFromAPK(file, "assemblies/", outputPath))
+                    if (!apkUncompress.UncompressFromAPK(file, "assemblies/", prefix, outputPath))
                     {
                         Console.WriteLine("Uncompress failed: {0}", file);
                         haveErrors = true;
@@ -180,7 +61,7 @@ namespace ApkUncompress
 
                 if (String.Compare(".aab", ext, StringComparison.OrdinalIgnoreCase) == 0)
                 {
-                    if (!UncompressFromAPK(file, "base/root/assemblies/", outputPath))
+                    if (!apkUncompress.UncompressFromAPK(file, "base/root/assemblies/", prefix, outputPath))
                     {
                         Console.WriteLine("Uncompress failed: {0}", file);
                         haveErrors = true;
