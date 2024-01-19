5f29383a714e36f3a7ae35c68f01d4a58ae15c23
uholeschak <ulrich@holeschak.de>
2024-01-19 13:17:48 +0100

    Fixed result count calculation

--- BmwDeepObd/XmlToolActivity.cs
+++ BmwDeepObd/XmlToolActivity.cs
@@ -5788,7 +5788,8 @@ namespace BmwDeepObd
                                 }
 
                                 jobInfo.Comments.Add(string.Format(Culture, GetString(Resource.String.xml_tool_num_job_results), jobInfo.Results.Count));
-                                if (jobInfo.Results.Count < 2)
+                                int commentCount = jobInfo.Results.Count(resultInfo => !string.IsNullOrWhiteSpace(resultInfo.DisplayName));
+                                if (commentCount <= 3)
                                 {
                                     foreach (XmlToolEcuActivity.ResultInfo resultInfo in jobInfo.Results)
                                     {
@@ -5800,6 +5801,7 @@ namespace BmwDeepObd
                                         jobInfo.Comments.Add("- " + resultInfo.DisplayName);
                                     }
                                 }
+
                                 jobInfo.CommentsTransRequired = false;
                                 jobList.Add(jobInfo);
                                 break;
