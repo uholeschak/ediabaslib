71cee399ace9bc198537b70be80803c0cc2e370d
uholeschak <ulrich@holeschak.de>
2024-02-16 14:35:51 +0100

    Moved sql test to separate app

---
+++ Tools/ExtractEcuFunctions/SqlTest/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.IO;
+
+namespace SqlTest
+{
+    internal class Program
+    {
+        public const string DatabasePassword = "6505EFBDC3E5F324";
+
+        static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: SqlTest <database file>");
+                return 1;
+            }
+
+            try
+            {
+                string databaseFile = args[0];
+                if (!File.Exists(databaseFile))
+                {
+                    Console.WriteLine("Database file not found: " + databaseFile);
+                    return 1;
+                }
+
+                SqliteConnectionStringBuilder connectionBuilder = new SqliteConnectionStringBuilder
+                {
+                    DataSource = databaseFile,
+                    Mode = SqliteOpenMode.ReadOnly,
+                    Password = DatabasePassword
+                };
+
+                using (SqliteConnection sqliteConnection = new SqliteConnection(connectionBuilder.ConnectionString))
+                {
+                    sqliteConnection.Open();
+
+                    string rootENameClassId = GetNodeClassId(sqliteConnection, @"RootEBezeichnung");
+                    string typeKeyClassId = GetNodeClassId(sqliteConnection, @"Typschluessel");
+
+                    Console.WriteLine("RootEBezeichnung: {0}", rootENameClassId);
+                    Console.WriteLine("Typschluessel: {0}", rootENameClassId);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+                return 1;
+            }
+
+            return 0;
+        }
+
+        public static string GetNodeClassId(SqliteConnection sqliteConnection, string nodeClassName)
+        {
+            string result = string.Empty;
+            string sql = string.Format(@"SELECT ID FROM XEP_NODECLASSES WHERE NAME = '{0}'", nodeClassName);
+            using (SqliteCommand command = sqliteConnection.CreateCommand())
+            {
+                command.CommandText = sql;
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        result = reader["ID"].ToString();
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
