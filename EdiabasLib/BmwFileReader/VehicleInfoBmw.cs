5b4948c9ecead72944a5ef781bb6e3e3015cc0e0
uholeschak <ulrich@holeschak.de>
2024-02-23 07:44:20 +0100

    Use UTF8

--- EdiabasLib/BmwFileReader/VehicleInfoBmw.cs
+++ EdiabasLib/BmwFileReader/VehicleInfoBmw.cs
@@ -566,7 +566,7 @@ namespace BmwFileReader
                             {
                                 using (Stream zipStream = zf.GetInputStream(zipEntry))
                                 {
-                                    using (StreamReader sr = new StreamReader(zipStream, Encoding.ASCII, false, 0x1000))
+                                    using (StreamReader sr = new StreamReader(zipStream, Encoding.UTF8, false, 0x1000))
                                     {
                                         while (!sr.EndOfStream)
                                         {
@@ -675,7 +675,7 @@ namespace BmwFileReader
                             {
                                 using (Stream zipStream = zf.GetInputStream(zipEntry))
                                 {
-                                    using (StreamReader sr = new StreamReader(zipStream, Encoding.ASCII, false, 0x1000))
+                                    using (StreamReader sr = new StreamReader(zipStream, Encoding.UTF8, false, 0x1000))
                                     {
                                         while (!sr.EndOfStream)
                                         {
@@ -697,34 +697,34 @@ namespace BmwFileReader
                                             }
 
                                             string[] lineArray = line.Split(',');
-                                            if (lineArray.Length >= 3 &&
+                                            if (lineArray.Length >= 4 &&
                                                 lineArray[0].Length == 7 && lineArray[1].Length == 7)
                                             {
                                                 if (string.Compare(vin7, lineArray[0], StringComparison.OrdinalIgnoreCase) >= 0 &&
                                                     string.Compare(vin7, lineArray[1], StringComparison.OrdinalIgnoreCase) <= 0)
                                                 {
-                                                    string typeKey = lineArray[2];
-                                                    ediabas?.LogFormat(EdiabasNet.EdLogLevel.Ifh, "Serial matched: '{0}'-'{1}', TypeKey='{2}'",
+                                                    string typeKey = lineArray[3];
+                                                    ediabas?.LogFormat(EdiabasNet.EdLogLevel.Ifh, "Vin7 matched: '{0}'-'{1}', TypeKey='{2}'",
                                                         lineArray[0], lineArray[1], typeKey);
 
                                                     string prodYear = null;
                                                     string prodMonth = null;
-                                                    if (lineArray.Length >= 5)
+                                                    if (lineArray.Length >= 6)
                                                     {
-                                                        prodYear = lineArray[3];
-                                                        prodMonth = lineArray[4];
+                                                        prodYear = lineArray[4];
+                                                        prodMonth = lineArray[5];
                                                     }
 
                                                     string releaseState = null;
-                                                    if (lineArray.Length >= 6)
+                                                    if (lineArray.Length >= 7)
                                                     {
-                                                        releaseState = lineArray[5];
+                                                        releaseState = lineArray[6];
                                                     }
 
                                                     string gearBox = null;
-                                                    if (lineArray.Length >= 7)
+                                                    if (lineArray.Length >= 8)
                                                     {
-                                                        gearBox = lineArray[6];
+                                                        gearBox = lineArray[7];
                                                     }
 
                                                     vinRangeInfo = new VinRangeInfo(lineArray[0], lineArray[1], typeKey, prodYear, prodMonth, releaseState, gearBox);
