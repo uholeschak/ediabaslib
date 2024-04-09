e6d3fd281a22f626e63682cc252ca77f391843e8
uholeschak <ulrich@holeschak.de>
2024-04-09 11:38:33 +0200

    Added stream mutex

--- EdiabasLib/EdiabasLib/MemoryQueueBufferStream.cs
+++ EdiabasLib/EdiabasLib/MemoryQueueBufferStream.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 
 // ReSharper disable ConvertPropertyToExpressionBody
 
@@ -32,12 +33,14 @@ namespace EdiabasLib
         //Maintains the streams data.  The Queue object provides an easy and efficient way to add and remove data
         //Each item in the queue represents each write to the stream.  Every call to write translates to an item in the queue
         private readonly Queue<Chunk> _lstBuffers;
+        private Mutex _readMutex;
 
         public bool ChunkMode { get; set; }
 
         public MemoryQueueBufferStream(bool chunkMode = false)
         {
             _lstBuffers = new Queue<Chunk>();
+            _readMutex = new Mutex(false);
             ChunkMode = chunkMode;
         }
 
@@ -55,55 +58,67 @@ namespace EdiabasLib
         /// <returns></returns>
         public override int Read(byte[] buffer, int offset, int count)
         {
-            this.ValidateBufferArgs(buffer, offset, count);
-
-            int iRemainingBytesToRead = count;
-
-            int iTotalBytesRead = 0;
-
-            //Read until we hit the requested count, or until we hav nothing left to read
-            while (iTotalBytesRead <= count && _lstBuffers.Count > 0)
+            if (!AcquireReadMutex())
             {
-                //Get first chunk from the queue
-                Chunk chunk = _lstBuffers.Peek();
+                return -1;
+            }
 
-                //Determine how much of the chunk there is left to read
-                int iUnreadChunkLength = chunk.Data.Length - chunk.ChunkReadStartIndex;
+            try
+            {
+                this.ValidateBufferArgs(buffer, offset, count);
 
-                //Determine how much of the unread part of the chunk we can actually read
-                int iBytesToRead = Math.Min(iUnreadChunkLength, iRemainingBytesToRead);
+                int iRemainingBytesToRead = count;
 
-                if (iBytesToRead > 0)
+                int iTotalBytesRead = 0;
+
+                //Read until we hit the requested count, or until we hav nothing left to read
+                while (iTotalBytesRead <= count && _lstBuffers.Count > 0)
                 {
-                    //Read from the chunk into the buffer
-                    Buffer.BlockCopy(chunk.Data, chunk.ChunkReadStartIndex, buffer, offset + iTotalBytesRead, iBytesToRead);
+                    //Get first chunk from the queue
+                    Chunk chunk = _lstBuffers.Peek();
 
-                    iTotalBytesRead += iBytesToRead;
-                    iRemainingBytesToRead -= iBytesToRead;
+                    //Determine how much of the chunk there is left to read
+                    int iUnreadChunkLength = chunk.Data.Length - chunk.ChunkReadStartIndex;
 
-                    //If the entire chunk has been read,  remove it
-                    if (chunk.ChunkReadStartIndex + iBytesToRead >= chunk.Data.Length)
+                    //Determine how much of the unread part of the chunk we can actually read
+                    int iBytesToRead = Math.Min(iUnreadChunkLength, iRemainingBytesToRead);
+
+                    if (iBytesToRead > 0)
                     {
-                        _lstBuffers.Dequeue();
+                        //Read from the chunk into the buffer
+                        Buffer.BlockCopy(chunk.Data, chunk.ChunkReadStartIndex, buffer, offset + iTotalBytesRead, iBytesToRead);
+
+                        iTotalBytesRead += iBytesToRead;
+                        iRemainingBytesToRead -= iBytesToRead;
+
+                        //If the entire chunk has been read,  remove it
+                        if (chunk.ChunkReadStartIndex + iBytesToRead >= chunk.Data.Length)
+                        {
+                            _lstBuffers.Dequeue();
+                        }
+                        else
+                        {
+                            //Otherwise just update the chunk read start index, so we know where to start reading on the next call
+                            chunk.ChunkReadStartIndex = chunk.ChunkReadStartIndex + iBytesToRead;
+                        }
+
+                        if (ChunkMode)
+                        {
+                            break;
+                        }
                     }
                     else
-                    {
-                        //Otherwise just update the chunk read start index, so we know where to start reading on the next call
-                        chunk.ChunkReadStartIndex = chunk.ChunkReadStartIndex + iBytesToRead;
-                    }
-
-                    if (ChunkMode)
                     {
                         break;
                     }
                 }
-                else
-                {
-                    break;
-                }
-            }
 
-            return iTotalBytesRead;
+                return iTotalBytesRead;
+            }
+            finally
+            {
+                ReleaseReadMutex();
+            }
         }
 
         private void ValidateBufferArgs(byte[] buffer, int offset, int count)
@@ -130,14 +145,26 @@ namespace EdiabasLib
         /// <param name="count"></param>
         public override void Write(byte[] buffer, int offset, int count)
         {
-            this.ValidateBufferArgs(buffer, offset, count);
+            if (!AcquireReadMutex())
+            {
+                return;
+            }
 
-            //We don't want to use the buffer passed in, as it could be altered by the caller
-            byte[] bufSave = new byte[count];
-            Buffer.BlockCopy(buffer, offset, bufSave, 0, count);
+            try
+            {
+                this.ValidateBufferArgs(buffer, offset, count);
 
-            //Add the data to the queue
-            _lstBuffers.Enqueue(new Chunk() { ChunkReadStartIndex = 0, Data = bufSave });
+                //We don't want to use the buffer passed in, as it could be altered by the caller
+                byte[] bufSave = new byte[count];
+                Buffer.BlockCopy(buffer, offset, bufSave, 0, count);
+
+                //Add the data to the queue
+                _lstBuffers.Enqueue(new Chunk() { ChunkReadStartIndex = 0, Data = bufSave });
+            }
+            finally
+            {
+                ReleaseReadMutex();
+            }
         }
 
         public override bool CanSeek
@@ -185,23 +212,79 @@ namespace EdiabasLib
         {
             get
             {
-
-                if (_lstBuffers == null)
+                if (!AcquireReadMutex())
                 {
                     return 0;
                 }
 
-                if (_lstBuffers.Count == 0)
+                try
                 {
-                    return 0;
-                }
+                    if (_lstBuffers == null)
+                    {
+                        return 0;
+                    }
 
-                return _lstBuffers.Sum(b => b.Data.Length - b.ChunkReadStartIndex);
+                    if (_lstBuffers.Count == 0)
+                    {
+                        return 0;
+                    }
+
+                    return _lstBuffers.Sum(b => b.Data.Length - b.ChunkReadStartIndex);
+                }
+                finally
+                {
+                    ReleaseReadMutex();
+                }
             }
         }
 
         public override void Flush()
         {
+        }
+
+        public override void Close()
+        {
+            try
+            {
+                if (_readMutex != null)
+                {
+                    _readMutex.Close();
+                    _readMutex = null;
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
+        private bool AcquireReadMutex(int timeout = 10000)
+        {
+            try
+            {
+                if (!_readMutex.WaitOne(timeout))
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private void ReleaseReadMutex()
+        {
+            try
+            {
+                _readMutex.ReleaseMutex();
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
         }
     }
 }
