fa2b97b14396f68741d8a4d4ef5bbddef42050b6
uholeschak <ulrich@holeschak.de>
2024-04-09 16:34:52 +0200

    Using correct stream

--- EdiabasLib/EdiabasLib/EdElmWifiInterface.cs
+++ EdiabasLib/EdiabasLib/EdElmWifiInterface.cs
@@ -34,13 +34,13 @@ namespace EdiabasLib
         protected static object NetworkData;
         private static EdElmInterface _edElmInterface;
 
-        static EdElmWifiInterface()
-        {
-        }
-
         public static NetworkStream NetworkStream => TcpElmStream;
 
         public static EdiabasNet Ediabas { get; set; }
+
+        static EdElmWifiInterface()
+        {
+        }
 
         public static bool InterfaceConnect(string port, object parameter)
         {
