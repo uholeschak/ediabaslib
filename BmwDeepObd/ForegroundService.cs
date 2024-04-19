a1aa89b07588ee033c75023b64be2332b3111280
uholeschak <ulrich@holeschak.de>
2024-04-19 20:10:35 +0200

    Check for active communication

--- BmwDeepObd/ForegroundService.cs
+++ BmwDeepObd/ForegroundService.cs
@@ -86,7 +86,10 @@ namespace BmwDeepObd
                         RegisterForegroundService();
                         if (startComm)
                         {
-                            StartCommTimer();
+                            if (!ActivityCommon.CommActive)
+                            {
+                                StartCommTimer();
+                            }
                         }
                         _isStarted = true;
                     }
@@ -360,6 +363,15 @@ namespace BmwDeepObd
 
         private void CommTimerCallback(object state)
         {
+            if (ActivityCommon.CommActive)
+            {
+                StopCommTimer();
+#if DEBUG
+                Android.Util.Log.Info(Tag, "CommTimerCallback: Communication active, stopping timer");
+#endif
+                return;
+            }
+
             if (_instanceData == null)
             {
                 if (!_activityCommon.IsExStorageAvailable())
