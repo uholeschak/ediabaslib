8c393fcfb42a746a4cb3186561e68b970cabb66b
uholeschak <ulrich@holeschak.de>
2024-05-02 18:21:48 +0200

    Removed login http client

--- BmwDeepObd/ActivityCommon.cs
+++ BmwDeepObd/ActivityCommon.cs
@@ -285,7 +285,7 @@ namespace BmwDeepObd
             }
         }
 
-        [XmlInclude(typeof(ActivityCommon.SerialInfoEntry))]
+        [XmlInclude(typeof(SerialInfoEntry))]
         [XmlType("Settings")]
         public class StorageData
         {
@@ -1093,7 +1093,6 @@ namespace BmwDeepObd
         private HttpClient _translateHttpClient;
         private HttpClient _sendHttpClient;
         private HttpClient _updateHttpClient;
-        private HttpClient _transLoginHttpClient;
         private bool _updateCheckActive;
         private bool _translateLockAquired;
         private string _yandexCloudIamToken;
@@ -1861,19 +1860,6 @@ namespace BmwDeepObd
                             // ignored
                         }
                         _updateHttpClient = null;
-                    }
-
-                    if (_transLoginHttpClient != null)
-                    {
-                        try
-                        {
-                            _transLoginHttpClient.Dispose();
-                        }
-                        catch (Exception)
-                        {
-                            // ignored
-                        }
-                        _transLoginHttpClient = null;
                     }
 
                     UnRegisterInternetCellularCallback();
