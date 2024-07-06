dff78665b748ee8679e5a336adbe0402e14d6858
uholeschak <ulrich@holeschak.de>
2024-07-06 22:45:25 +0200

    Version 1.3.227

--- EdiabasLib/EdiabasLib/MemoryQueueBufferStream.cs
+++ EdiabasLib/EdiabasLib/MemoryQueueBufferStream.cs
@@ -308,10 +308,10 @@ namespace EdiabasLib
                     {
                         // ignored
                     }
-
-                    // Note disposing has been done.
-                    _disposed = true;
                 }
+
+                // Note disposing has been done.
+                _disposed = true;
             }
 
             base.Dispose(disposing);
