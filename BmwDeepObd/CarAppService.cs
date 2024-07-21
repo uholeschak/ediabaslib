6c9536872c33c486ca7e95dd8ee2a4bc49be8970
uholeschak <ulrich@holeschak.de>
2024-07-21 17:58:37 +0200

    Added CarService

---
+++ BmwDeepObd/CarAppService.cs
@@ -0,0 +1,45 @@
+﻿using Android.Content;
+using AndroidX.Car.App;
+using AndroidX.Car.App.Validation;
+
+namespace BmwDeepObd;
+
+[Android.App.Service(
+    Exported = true,
+    Enabled = true,
+    Name = ActivityCommon.AppNameSpace + "." + nameof(CarService)
+)]
+[Android.App.IntentFilter(new[]
+    {
+        IntentCarAppService,
+    },
+    Categories = new[]
+    {
+        CategoryIot
+    })
+]
+
+public class CarService : CarAppService
+{
+    public const string IntentCarAppService = "androidx.car.app.CarAppService";
+    public const string CategoryIot = "androidx.car.app.category.IOT";
+
+    public override HostValidator CreateHostValidator()
+    {
+        return HostValidator.AllowAllHostsValidator;
+    }
+
+    public override Session OnCreateSession()
+    {
+        return new CarSession();
+    }
+
+    public class CarSession : Session
+    {
+        public override Screen OnCreateScreen(Intent intent)
+        {
+            //return MainScreen(CarContext);
+            return null;
+        }
+    }
+}
