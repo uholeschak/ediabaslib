d7ea89d3e180a463ac6113eaeecda31432d1f465
uholeschak <ulrich@holeschak.de>
2024-08-24 12:07:30 +0200

    Check if settings are valid

--- BmwDeepObd/MainActivity.cs
+++ BmwDeepObd/MainActivity.cs
@@ -179,9 +179,9 @@ namespace BmwDeepObd
         public const string ExtraShowTitle = "show_title";
         public const string ExtraNoAutoconnect = "no_autoconnect";
         public const string ExtraCommOption = "comm_option";
-        public const string ExtraStoreOption = "store_option";
         public const string CommOptionConnect = "connect";
         public const string CommOptionDisconnect = "disconnect";
+        public const string ExtraStoreOption = "store_option";
         public const string StoreOptionSettings = "store_settings";
         public static readonly CultureInfo Culture = CultureInfo.InvariantCulture;
         public static bool StoreXmlEditor = Build.VERSION.SdkInt >= BuildVersionCodes.LollipopMr1;
@@ -2386,6 +2386,18 @@ namespace BmwDeepObd
                     _instanceData.AutoConnectExecuted = false;
                 }
 
+                string storeOption = intent.GetStringExtra(ExtraStoreOption);
+                if (!string.IsNullOrEmpty(storeOption))
+                {
+                    if (string.Compare(storeOption, StoreOptionSettings, StringComparison.OrdinalIgnoreCase) == 0)
+                    {
+                        if (_instanceData.GetSettingsCalled)
+                        {
+                            StoreSettings();
+                        }
+                    }
+                }
+
                 string commOption = intent.GetStringExtra(ExtraCommOption);
                 if (!string.IsNullOrEmpty(commOption))
                 {
@@ -2410,15 +2422,6 @@ namespace BmwDeepObd
                                 _updateHandler.Post(_handleConnectOptionRunnable);
                             }
                         }
-                    }
-                }
-
-                string storeOption = intent.GetStringExtra(ExtraStoreOption);
-                if (!string.IsNullOrEmpty(storeOption))
-                {
-                    if (string.Compare(storeOption, StoreOptionSettings, StringComparison.OrdinalIgnoreCase) == 0)
-                    {
-                        StoreSettings();
                     }
                 }
             }
