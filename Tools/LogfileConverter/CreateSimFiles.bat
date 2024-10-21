181513ce30108f18b70e3b6ae6110b39d1a5fd4c
uholeschak <ulrich@holeschak.de>
2024-10-21 12:46:11 +0200

    Added converter scripts

--- Tools/LogfileConverter/CreateSimFiles.bat
+++ Tools/LogfileConverter/CreateSimFiles.bat
@@ -25,7 +25,9 @@ if NOT EXIST "!CONVERTER_EXE!" (
 "!CONVERTER_EXE!" -m "!RESPONSE_PATH!e61.txt" -m "!RESPONSE_PATH!e61_sim.txt" -r --sim "!RESPONSE_PATH!E61\obd.sim" || exit /b 1
 "!CONVERTER_EXE!" -m "!RESPONSE_PATH!e90.txt" -m "!RESPONSE_PATH!e90_sim.txt" -r --sim "!RESPONSE_PATH!E90\obd.sim" || exit /b 1
 "!CONVERTER_EXE!" -m "!RESPONSE_PATH!E36\E36_ISTA.txt" -r --sim "!RESPONSE_PATH!E36\obd.sim" || exit /b 1
+"!CONVERTER_EXE!" -m "!RESPONSE_PATH!E38\E38.txt" -r --sim "!RESPONSE_PATH!E38\obd.sim" || exit /b 1
 "!CONVERTER_EXE!" -m "!RESPONSE_PATH!E39\E39_ISTA.txt" -r --sim "!RESPONSE_PATH!E39\obd.sim" || exit /b 1
+"!CONVERTER_EXE!" -m "!RESPONSE_PATH!E53\E53.txt" -r --sim "!RESPONSE_PATH!E53\obd.sim" || exit /b 1
 
 echo Sim files successfully created
 exit /b 0
