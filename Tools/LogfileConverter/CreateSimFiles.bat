6052e2c986e9da728189b3f8ff94680bbc037533
uholeschak <ulrich@holeschak.de>
2024-10-22 00:04:05 +0200

    SIM files updated

--- Tools/LogfileConverter/CreateSimFiles.bat
+++ Tools/LogfileConverter/CreateSimFiles.bat
@@ -3,6 +3,17 @@ SETLOCAL EnableDelayedExpansion
 
 set BATPATH=%~dp0
 set "RESPONSE_PATH=!BATPATH!..\CarSimulator\Response\"
+set "XML_PATH=!BATPATH!..\..\BmwDeepObd\Xml\"
+
+if NOT EXIST "!RESPONSE_PATH!" (
+    echo !RESPONSE_PATH! not existing
+    exit /b 1
+)
+
+if NOT EXIST "!XML_PATH!" (
+    echo !XML_PATH! not existing
+    exit /b 1
+)
 
 set "CONVERTER_EXE=!BATPATH!artifacts\bin\LogfileConverter\release\LogfileConverter.exe"
 if NOT EXIST "!CONVERTER_EXE!" (
@@ -22,13 +33,17 @@ if NOT EXIST "!CONVERTER_EXE!" (
 "!CONVERTER_EXE!" -m "!RESPONSE_PATH!VW\touareg1.txt" -r --sim "!RESPONSE_PATH!VW\edic_touareg1.sim" || exit /b 1
 "!CONVERTER_EXE!" -m "!RESPONSE_PATH!VW\Touran2010.txt" -r --sim "!RESPONSE_PATH!VW\edic_Touran2010.sim" || exit /b 1
 "!CONVERTER_EXE!" -m "!RESPONSE_PATH!g31_coding.txt" -m "!RESPONSE_PATH!g31_coding_sim.txt" -r --sim "!RESPONSE_PATH!G31\enet.sim" || exit /b 1
+xcopy /y "!RESPONSE_PATH!G31\enet.sim" "!XML_PATH!G31\" || exit /b 1
 "!CONVERTER_EXE!" -m "!RESPONSE_PATH!e61.txt" -m "!RESPONSE_PATH!e61_sim.txt" -r --sim "!RESPONSE_PATH!E61\obd.sim" || exit /b 1
+xcopy /y "!RESPONSE_PATH!E61\obd.sim" "!XML_PATH!E61\" || exit /b 1
 "!CONVERTER_EXE!" -m "!RESPONSE_PATH!e90.txt" -m "!RESPONSE_PATH!e90_sim.txt" -r --sim "!RESPONSE_PATH!E90\obd.sim" || exit /b 1
+xcopy /y "!RESPONSE_PATH!E90\obd.sim" "!XML_PATH!E90\" || exit /b 1
+"!CONVERTER_EXE!" -m "!RESPONSE_PATH!E61R\E61R.txt" -r --sim "!RESPONSE_PATH!E61R\obd.sim" || exit /b 1
+xcopy /y "!RESPONSE_PATH!E61R\obd.sim" "!XML_PATH!E61R\" || exit /b 1
 "!CONVERTER_EXE!" -m "!RESPONSE_PATH!E36\E36_ISTA.txt" -r --sim "!RESPONSE_PATH!E36\obd.sim" || exit /b 1
 "!CONVERTER_EXE!" -m "!RESPONSE_PATH!E38\E38.txt" -r --sim "!RESPONSE_PATH!E38\obd.sim" || exit /b 1
 "!CONVERTER_EXE!" -m "!RESPONSE_PATH!E39\E39_ISTA.txt" -r --sim "!RESPONSE_PATH!E39\obd.sim" || exit /b 1
 "!CONVERTER_EXE!" -m "!RESPONSE_PATH!E53\E53.txt" -r --sim "!RESPONSE_PATH!E53\obd.sim" || exit /b 1
-"!CONVERTER_EXE!" -m "!RESPONSE_PATH!E61R\E61R.txt" -r --sim "!RESPONSE_PATH!E61R\obd.sim" || exit /b 1
 
 echo Sim files successfully created
 exit /b 0
