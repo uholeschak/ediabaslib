3ab85cb0c63d19d56fb0a97781515e52fc8d55b5
uholeschak <ulrich@holeschak.de>
2024-12-08 19:26:21 +0100

    Compile with original complier

--- EdiabasLib/Test/Ecu/Compile.bat
+++ EdiabasLib/Test/Ecu/Compile.bat
@@ -3,36 +3,43 @@ SETLOCAL EnableDelayedExpansion
 
 set "BATPATH=%~dp0"
 set "ECU_PATH=!BATPATH!"
-set "BESTNET_PATH=!BATPATH!..\..\..\Tools\BestNet\artifacts\bin\BestNet\release\BestNet.exe"
+set "EDIABAS_BIN_PATH=!EDIABAS_PATH!\BIN\"
+set "BEST1_PATH=!EDIABAS_BIN_PATH!Best1.exe"
+set "BEST2_PATH=!EDIABAS_BIN_PATH!Best2.exe"
 
-IF NOT EXIST "!BESTNET_PATH!" (
-  echo !BESTNET_PATH! not existing
+IF NOT EXIST "!BEST1_PATH!" (
+  echo !BEST1_PATH! not existing
+  EXIT /b 1
+)
+
+IF NOT EXIST "!BEST2_PATH!" (
+  echo !BEST2_PATH! not existing
   EXIT /b 1
 )
 
 echo:
 echo compiling base1.b1v
-!BESTNET_PATH! -i "!ECU_PATH!\base1.b1v" EXIT /b 1
+"!BEST1_PATH!" -Q "!ECU_PATH!\base1.b1v" || EXIT /b 1
 echo done
 
 echo:
 echo compiling base2.b1v
-!BESTNET_PATH! -i "!ECU_PATH!\base2.b1v" EXIT /b 1
+"!BEST1_PATH!" -Q "!ECU_PATH!\base2.b1v" || EXIT /b 1
 echo done
 
 echo:
 echo compiling cmd_test1.b1v
-!BESTNET_PATH! -i "!ECU_PATH!\cmd_test1.b1v" EXIT /b 1
+"!BEST1_PATH!" -Q "!ECU_PATH!\cmd_test1.b1v" || EXIT /b 1
 echo done
 
 echo:
 echo compiling cmd_test2.b2v
-!BESTNET_PATH! -i "!ECU_PATH!\cmd_test2.b2v" -l "!ECU_PATH!\test.lib" || EXIT /b 1
+"!BEST2_PATH!" -Q -L "!ECU_PATH!\test.lib" "!ECU_PATH!\cmd_test2.b2v" || EXIT /b 1
 echo done
 
 echo:
 echo compiling cmd_ident.b1g
-!BESTNET_PATH! -i "!ECU_PATH!\cmd_ident.b1g" EXIT /b 1
+"!BEST1_PATH!" -Q  "!ECU_PATH!\cmd_ident.b1g" || EXIT /b 1
 echo done
 
 echo:
