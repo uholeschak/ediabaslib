13b0982a2747bf29b0592002f6039d1d6bb3576f
uholeschak <ulrich@holeschak.de>
2024-12-12 00:17:47 +0100

    Test version command

--- EdiabasLib/Test/Ecu/CompileNet.bat
+++ EdiabasLib/Test/Ecu/CompileNet.bat
@@ -4,6 +4,8 @@ SETLOCAL EnableDelayedExpansion
 set "BATPATH=%~dp0"
 set "ECU_PATH=!BATPATH!"
 set "BESTNET_PATH=!BATPATH!..\..\..\Tools\BestNet\artifacts\bin\BestNet\release\BestNet.exe"
+rem set "EXTRA_OPT=-k"
+set "EXTRA_OPT="
 
 IF NOT EXIST "!BESTNET_PATH!" (
   echo !BESTNET_PATH! not existing
@@ -12,42 +14,42 @@ IF NOT EXIST "!BESTNET_PATH!" (
 
 echo:
 echo compiling base1.b1v
-"!BESTNET_PATH!" -i "!ECU_PATH!\base1.b1v" || EXIT /b 1
+"!BESTNET_PATH!" !EXTRA_OPT! -i "!ECU_PATH!\base1.b1v" || EXIT /b 1
 echo done
 
 echo:
 echo compiling base2.b1v
-"!BESTNET_PATH!" -i "!ECU_PATH!\base2.b1v" || EXIT /b 1
+"!BESTNET_PATH!" !EXTRA_OPT! -i "!ECU_PATH!\base2.b1v" || EXIT /b 1
 echo done
 
 echo:
 echo compiling cmd_test1.b1v
-"!BESTNET_PATH!" -i "!ECU_PATH!\cmd_test1.b1v" || EXIT /b 1
+"!BESTNET_PATH!" !EXTRA_OPT! -i "!ECU_PATH!\cmd_test1.b1v" || EXIT /b 1
 echo done
 
 echo:
 echo compiling cmd_test2.b2v
-"!BESTNET_PATH!" -i "!ECU_PATH!\cmd_test2.b2v" -l "!ECU_PATH!\test.lib" || EXIT /b 1
+"!BESTNET_PATH!" !EXTRA_OPT! -i "!ECU_PATH!\cmd_test2.b2v" -l "!ECU_PATH!\test.lib" || EXIT /b 1
 echo done
 
 echo:
 echo compiling cmd_ident.b1g
-"!BESTNET_PATH!" -i "!ECU_PATH!\cmd_ident.b1g" || EXIT /b 1
+"!BESTNET_PATH!" !EXTRA_OPT! -i "!ECU_PATH!\cmd_ident.b1g" || EXIT /b 1
 echo done
 
 echo:
 echo compiling adapter_prg.b2v
-"!BESTNET_PATH!" -i "!ECU_PATH!\adapter_prg.b2v" || EXIT /b 1
+"!BESTNET_PATH!" !EXTRA_OPT! -i "!ECU_PATH!\adapter_prg.b2v" || EXIT /b 1
 echo done
 
 echo:
 echo compiling d60m47a0_dcan.b1v
-"!BESTNET_PATH!" -i "!ECU_PATH!\d60m47a0_dcan.b1v" || EXIT /b 1
+"!BESTNET_PATH!" !EXTRA_OPT! -i "!ECU_PATH!\d60m47a0_dcan.b1v" || EXIT /b 1
 echo done
 
 echo:
 echo dm338ds1_interbyte.b1v
-"!BESTNET_PATH!" -i "!ECU_PATH!\dm338ds1_interbyte.b1v" || EXIT /b 1
+"!BESTNET_PATH!" !EXTRA_OPT! -i "!ECU_PATH!\dm338ds1_interbyte.b1v" || EXIT /b 1
 echo done
 
 echo:
