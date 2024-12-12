a00460c56bc358cfd0fc8c054d0ade285a213c88
uholeschak <ulrich@holeschak.de>
2024-12-12 01:11:25 +0100

    Added extra options

--- EdiabasLib/Test/Ecu/CompileStd.bat
+++ EdiabasLib/Test/Ecu/CompileStd.bat
@@ -6,6 +6,9 @@ set "ECU_PATH=!BATPATH!"
 set "EDIABAS_BIN_PATH=!EDIABAS_PATH!\BIN\"
 set "BEST1_PATH=!EDIABAS_BIN_PATH!Best164.exe"
 set "BEST2_PATH=!EDIABAS_BIN_PATH!Best264.exe"
+set "EXTRA_OPT1="
+rem set "EXTRA_OPT2=-S -I"
+set "EXTRA_OPT2="
 
 IF NOT EXIST "!BEST1_PATH!" (
   echo !BEST1_PATH! not existing
@@ -19,42 +22,42 @@ IF NOT EXIST "!BEST2_PATH!" (
 
 echo:
 echo compiling base1.b1v
-"!BEST1_PATH!" -Q "!ECU_PATH!\base1.b1v" || EXIT /b 1
+"!BEST1_PATH!" !EXTRA_OPT1! -Q "!ECU_PATH!\base1.b1v" || EXIT /b 1
 echo done
 
 echo:
 echo compiling base2.b1v
-"!BEST1_PATH!" -Q "!ECU_PATH!\base2.b1v" || EXIT /b 1
+"!BEST1_PATH!" !EXTRA_OPT1! -Q "!ECU_PATH!\base2.b1v" || EXIT /b 1
 echo done
 
 echo:
 echo compiling cmd_test1.b1v
-"!BEST1_PATH!" -Q "!ECU_PATH!\cmd_test1.b1v" || EXIT /b 1
+"!BEST1_PATH!" !EXTRA_OPT1! -Q "!ECU_PATH!\cmd_test1.b1v" || EXIT /b 1
 echo done
 
 echo:
 echo compiling cmd_test2.b2v
-"!BEST2_PATH!" -Q -L "!ECU_PATH!\test.lib" "!ECU_PATH!\cmd_test2.b2v" || EXIT /b 1
+"!BEST2_PATH!" !EXTRA_OPT2! -Q -L "!ECU_PATH!\test.lib" "!ECU_PATH!\cmd_test2.b2v" || EXIT /b 1
 echo done
 
 echo:
 echo compiling cmd_ident.b1g
-"!BEST1_PATH!" -Q  "!ECU_PATH!\cmd_ident.b1g" || EXIT /b 1
+"!BEST1_PATH!" !EXTRA_OPT1! -Q  "!ECU_PATH!\cmd_ident.b1g" || EXIT /b 1
 echo done
 
 echo:
 echo compiling adapter_prg.b2v
-"!BEST2_PATH!" -Q "!ECU_PATH!\adapter_prg.b2v" || EXIT /b 1
+"!BEST2_PATH!" !EXTRA_OPT2! -Q "!ECU_PATH!\adapter_prg.b2v" || EXIT /b 1
 echo done
 
 echo:
 echo compiling d60m47a0_dcan.b1v
-"!BEST1_PATH!" -Q  "!ECU_PATH!\d60m47a0_dcan.b1v" || EXIT /b 1
+"!BEST1_PATH!" !EXTRA_OPT1! -Q  "!ECU_PATH!\d60m47a0_dcan.b1v" || EXIT /b 1
 echo done
 
 echo:
 echo compiling dm338ds1_interbyte.b1v
-"!BEST1_PATH!" -Q  "!ECU_PATH!\dm338ds1_interbyte.b1v" || EXIT /b 1
+"!BEST1_PATH!" !EXTRA_OPT1! -Q  "!ECU_PATH!\dm338ds1_interbyte.b1v" || EXIT /b 1
 echo done
 
 echo:
