e0699da67b16e3a9df20d6f88ce5202039e2b3e5
uholeschak <ulrich@holeschak.de>
2024-12-12 15:53:53 +0100

    Check for Framework 4.8

--- EdiabasLib/EdiabasLibConfigTool/Program.cs
+++ EdiabasLib/EdiabasLibConfigTool/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Runtime.Versioning;
 using System.Text;
 using System.Windows.Forms;
@@ -17,7 +18,100 @@ namespace EdiabasLibConfigTool
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            if (GetNetFrameworkVersion() < 0x480)
+            {
+                string message = string.Format(Resources.Strings.NetFrameworkMissing, "4.8");
+                MessageBox.Show(message, Resources.Strings.TitleError, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             Application.Run(new FormMain());
+        }
+
+        public static int GetNetFrameworkVersion()
+        {
+            try
+            {
+                using (RegistryKey localMachine32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32))
+                {
+                    using (RegistryKey ndpKey = localMachine32.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\"))
+                    {
+                        if (ndpKey != null)
+                        {
+                            object releaseValue = ndpKey.GetValue("Release");
+                            if (releaseValue is int releaseKey)
+                            {
+                                return ConvertNetFrameworkVersion(releaseKey);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+            return -1;
+        }
+
+        public static int ConvertNetFrameworkVersion(int releaseKey)
+        {
+            if (releaseKey >= 533320)
+            {
+                return 0x481;
+            }
+
+            if (releaseKey >= 528040)
+            {
+                return 0x480;
+            }
+
+            if (releaseKey >= 461808)
+            {
+                return 0x472;
+            }
+
+            if (releaseKey >= 461308)
+            {
+                return 0x471;
+            }
+
+            if (releaseKey >= 460798)
+            {
+                return 0x470;
+            }
+
+            if (releaseKey >= 394802)
+            {
+                return 0x462;
+            }
+
+            if (releaseKey >= 394254)
+            {
+                return 0x461;
+            }
+
+            if (releaseKey >= 393295)
+            {
+                return 0x460;
+            }
+
+            if (releaseKey >= 379893)
+            {
+                return 0x452;
+            }
+
+            if (releaseKey >= 378675)
+            {
+                return 0x451;
+            }
+
+            if (releaseKey >= 378389)
+            {
+                return 0x450;
+            }
+
+            return 0x000;
         }
     }
 }
