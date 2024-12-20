2bc9fefbd9599557dc1772300c66d5732a1a717c
uholeschak <ulrich@holeschak.de>
2024-12-20 01:43:30 +0100

    Use SuppressMessage

--- BmwDeepObd/GlobalSettingsActivity.cs
+++ BmwDeepObd/GlobalSettingsActivity.cs
@@ -1076,6 +1076,7 @@ namespace BmwDeepObd
             }
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416: Validate platform compatibilitys")]
         private bool SelectCopyDocumentTree(bool fromApp)
         {
             if (!ActivityCommon.IsDocumentTreeSupported())
