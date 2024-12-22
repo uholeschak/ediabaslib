2735dc57880db14c2c3ebb4d944c0656c92d8fb3
uholeschak <ulrich@holeschak.de>
2024-12-22 10:27:19 +0100

    Added stream dispose

--- Tools/ApkUncompress2/AssemblyStore/AssemblyStoreReader.cs
+++ Tools/ApkUncompress2/AssemblyStore/AssemblyStoreReader.cs
@@ -7,10 +7,11 @@ using Xamarin.Android.Tools;
 
 namespace Xamarin.Android.AssemblyStore;
 
-abstract class AssemblyStoreReader
+abstract class AssemblyStoreReader : IDisposable
 {
 	static readonly UTF8Encoding ReaderEncoding = new UTF8Encoding (false);
 
+    private bool _disposed;
 	protected Stream StoreStream                { get; }
 
 	public abstract string Description          { get; }
@@ -84,4 +85,32 @@ abstract class AssemblyStoreReader
 
 		return stream;
 	}
+
+    public void Dispose()
+    {
+        Dispose(true);
+        // This object will be cleaned up by the Dispose method.
+        // Therefore, you should call GC.SupressFinalize to
+        // take this object off the finalization queue
+        // and prevent finalization code for this object
+        // from executing a second time.
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        // Check to see if Dispose has already been called.
+        if (!_disposed)
+        {
+            // If disposing equals true, dispose all managed
+            // and unmanaged resources.
+            if (disposing)
+            {
+                StoreStream?.Dispose();
+            }
+
+            // Note disposing has been done.
+            _disposed = true;
+        }
+    }
 }
