7d9d5060a08a830476c53c9973fff1698b950303
uholeschak <ulrich@holeschak.de>
2025-01-11 02:13:12 +0100

    Script for sample zip creation

---
+++ BmwDeepObd/Xml/CreateSampleZip.bat
@@ -0,0 +1,12 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+set "BATPATH=%~dp0"
+set "SAMPLEPATH=!BATPATH!Sample\"
+
+set SAMPLEZIP="!BATPATH!Sample.zip"
+if exist "!SAMPLEZIP!" del /f /q "!SAMPLEZIP!"
+"!PATH_7ZIP!\7z.exe" a -tzip -aoa "!SAMPLEZIP!" "!SAMPLEPATH!*" || EXIT /b 1
+
+echo !SAMPLEZIP! successfully created
+exit /b 0
