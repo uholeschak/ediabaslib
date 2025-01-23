d29fde4867b4ec183cb6e42f3c8ca8e20a70697e
uholeschak <ulrich@holeschak.de>
2025-01-23 19:15:23 +0100

    Using github.io URL

--- BmwDeepObd/EdiabasToolActivity.cs
+++ BmwDeepObd/EdiabasToolActivity.cs
@@ -957,7 +957,7 @@ namespace BmwDeepObd
                             return;
                         }
 
-                        _activityCommon.OpenWebUrl("https://github.com/uholeschak/ediabaslib/blob/master/docs/EdiabasTool.md");
+                        _activityCommon.OpenWebUrl("https://uholeschak.github.io/ediabaslib/docs/EdiabasTool.html");
                     });
                     return true;
             }
