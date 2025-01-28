226b235b37e5b9e957c400dc19ca91087e4aee54
uholeschak <ulrich@holeschak.de>
2025-01-28 01:03:18 +0100

    Converted back to NET 4.8

--- Tools/Psdz/AssemblyPatcher/Program.cs
+++ Tools/Psdz/AssemblyPatcher/Program.cs
@@ -4,9 +4,6 @@ using System.Configuration;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using dnlib.DotNet.Emit;
 using dnpatch;
 
