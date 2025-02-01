eebfd4c5f36abb2eaba28ae7377556218aedb419
uholeschak <ulrich@holeschak.de>
2025-02-01 18:12:45 +0100

    Show different VIN for DoIP

--- Tools/CarSimulator/CommThread.cs
+++ Tools/CarSimulator/CommThread.cs
@@ -390,7 +390,8 @@ namespace CarSimulator
         private readonly Stopwatch _receiveStopWatch;
 
         private const string TestMac = "D8182B890A8B";
-        private const string TestVin = "WBAJM71000B055940";
+        private const string TestVinEnet = "WBAJM71000B055940";
+        private const string TestVinDoIp = "WBAJM71000B055941";
         private const string IcomVehicleAddress = "192.168.11.1";
         private const string IcomDhcpAddress = "192.168.201.1";
 
@@ -2099,7 +2100,7 @@ namespace CarSimulator
             identMessage[idx++] = (byte)'I';
             identMessage[idx++] = (byte)'N';
 
-            byte[] vinBytes = Encoding.ASCII.GetBytes(TestVin);
+            byte[] vinBytes = Encoding.ASCII.GetBytes(TestVinEnet);
             int vinLen = vinBytes.Length;
             Array.Copy(vinBytes, 0, identMessage, idx, vinLen);
             idx += vinLen;
@@ -2198,7 +2199,7 @@ namespace CarSimulator
                 {
                     // ident response
                     resPayloadType = 0x0004;
-                    byte[] vinBytes = Encoding.ASCII.GetBytes(TestVin);
+                    byte[] vinBytes = Encoding.ASCII.GetBytes(TestVinDoIp);
                     resData.AddRange(vinBytes);
                     // log address
                     resData.Add((byte)(_doIpGwAddr >> 8));
@@ -2571,7 +2572,7 @@ namespace CarSimulator
 
                     string attributs =
                         string.Format("(DevId=G31),(Serial={2}{0}),(MacAddress={1}),(DevType=ENET),(Color=#00ff00),(State=4),(Kl15Voltage=12000),(Kl30Voltage=12000),(VIN={2}),(PowerSupply=12000),(VciChannels=[0?;1?;2?;3+]),(IPAddress={3})",
-                            BitConverter.ToString(localIp.GetAddressBytes()).Replace("-", ""), TestMac, TestVin, localIp);
+                            BitConverter.ToString(localIp.GetAddressBytes()).Replace("-", ""), TestMac, TestVinEnet, localIp);
                     byte[] attrBytes = Encoding.ASCII.GetBytes(attributs);
                     int attrLen = attrBytes.Length;
                     response.Add((byte)(attrLen >> 8));      // List length
