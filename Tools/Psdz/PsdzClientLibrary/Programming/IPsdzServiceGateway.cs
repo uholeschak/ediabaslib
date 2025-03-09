5f43f9a49c036ed850c2b7c11538b38324318242
uholeschak <ulrich@holeschak.de>
2025-03-09 16:24:47 +0100

    Added PsdzServiceGateway

---
+++ Tools/Psdz/PsdzClientLibrary/Programming/IPsdzServiceGateway.cs
@@ -0,0 +1,16 @@
+﻿using BMW.Rheingold.Psdz.Client;
+using BMW.Rheingold.Psdz;
+
+namespace PsdzClientLibrary.Programming
+{
+    public interface IPsdzServiceGateway
+    {
+        string PsdzServiceLogFilePath { get; }
+
+        string PsdzLogFilePath { get; }
+
+        void SetLogLevel(PsdzLoglevel psdzLoglevel, ProdiasLoglevel prodiasLoglevel);
+
+        void Shutdown();
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Programming/PsdzServiceGateway.cs
@@ -0,0 +1,182 @@
+﻿using BMW.Rheingold.CoreFramework.Contracts.Vehicle;
+using BMW.Rheingold.Programming;
+using BMW.Rheingold.Psdz.Client;
+using BMW.Rheingold.Psdz;
+using PsdzClient.Programming;
+using PsdzClientLibrary.Core;
+using System.Threading.Tasks;
+using System;
+using PsdzClient;
+
+namespace PsdzClientLibrary.Programming
+{
+    public class PsdzServiceGateway : IPsdzServiceGateway, IDisposable
+    {
+        public enum Type
+        {
+            PsdzServiceHost,
+            PsdzWebService
+        }
+
+        private readonly PsdzConfig _psdzConfig;
+
+        private readonly PsdzServiceWrapper _psdzServiceHostWrapper;
+
+        //private readonly PsdzWebServiceWrapper _psdzWebServiceWrapper;
+
+        private readonly Action _psdzServiceHostStarter;
+
+        private bool disposedValue;
+
+        public static Type PsdzServiceType { get; set; }
+
+        public IPsdz Psdz
+        {
+            get
+            {
+#if false
+                if (PsdzServiceType != 0)
+                {
+                    return _psdzWebServiceWrapper;
+                }
+#endif
+                return _psdzServiceHostWrapper;
+            }
+        }
+
+        public string PsdzServiceLogFilePath
+        {
+            get
+            {
+#if false
+                if (PsdzServiceType != 0)
+                {
+                    return _psdzWebServiceWrapper.PsdzServiceLogFilePath;
+                }
+#endif
+                return _psdzServiceHostWrapper.PsdzServiceLogFilePath;
+            }
+        }
+
+        public string PsdzLogFilePath
+        {
+            get
+            {
+#if false
+                if (PsdzServiceType != 0)
+                {
+                    return _psdzWebServiceWrapper.PsdzLogFilePath;
+                }
+#endif
+                return _psdzServiceHostWrapper.PsdzLogFilePath;
+            }
+        }
+
+        public PsdzServiceGateway(PsdzConfig psdzConfig = null, Action psdzServiceHostStarter = null)
+        {
+            _psdzServiceHostStarter = psdzServiceHostStarter;
+            _psdzConfig = psdzConfig;
+            _psdzServiceHostWrapper = new PsdzServiceWrapper(_psdzConfig);
+            //_psdzWebServiceWrapper = new PsdzWebServiceWrapper(new PsdzWebServiceConfig(null, BMW.Rheingold.CoreFramework.LicenseHelper.DealerInstance.GetDistributionPartnerNumber(5)));
+            PsdzServiceType = ((ConfigSettings.getConfigString("BMW.Rheingold.Programming.PsdzServiceType", "PsdzServiceHost") == "PsdzWebService") ? Type.PsdzWebService : Type.PsdzServiceHost);
+        }
+
+        public void StartIfNotRunning(IVehicle vehicle = null)
+        {
+            Log.Info(Log.CurrentMethod(), "Start.");
+#if false
+            if (ConfigSettings.getConfigStringAsBoolean("BMW.Rheingold.Programming.PsdzWebservice.Enabled", defaultValue: false))
+            {
+                _psdzWebServiceWrapper.StartIfNotRunning();
+                Log.Info(Log.CurrentMethod(), "End.");
+                return;
+            }
+#endif
+            if (ClientContext.EnablePsdzMultiSession())
+            {
+                _psdzServiceHostWrapper.StartHostIfNotRunning(vehicle);
+            }
+            else if (_psdzServiceHostStarter != null)
+            {
+                _psdzServiceHostStarter();
+            }
+            else
+            {
+                _psdzServiceHostWrapper.StartHostIfNotRunning(vehicle);
+            }
+            WaitForPsdzServiceHostInitialization();
+            Log.Info(Log.CurrentMethod(), "End.");
+        }
+
+        public void CloseConnectionsToPsdz()
+        {
+            try
+            {
+#if false
+                if (ConfigSettings.getConfigStringAsBoolean("BMW.Rheingold.Programming.PsdzWebservice.Enabled", defaultValue: false))
+                {
+                    _psdzWebServiceWrapper.Shutdown();
+                }
+#endif
+                if (ConfigSettings.getConfigStringAsBoolean("BMW.Rheingold.Programming.PsdzService.MultiSession", defaultValue: false))
+                {
+                    _psdzServiceHostWrapper.Shutdown();
+                }
+                else
+                {
+                    _psdzServiceHostWrapper.CloseConnectionsToPsdzHost();
+                }
+            }
+            catch (Exception exception)
+            {
+                Log.WarningException(Log.CurrentMethod(), exception);
+            }
+        }
+
+        private void WaitForPsdzServiceHostInitialization()
+        {
+            int num = 40;
+            DateTime dateTime = DateTime.Now.AddSeconds(num);
+            while (!_psdzServiceHostWrapper.IsPsdzInitialized)
+            {
+                if (DateTime.Now > dateTime)
+                {
+                    Log.Error(Log.CurrentMethod(), $"PsdzServiceHost failed to start in {num} seconds. The method will stop waiting for it.");
+                    return;
+                }
+                Task.Delay(500).Wait();
+            }
+            _psdzServiceHostWrapper.DoInitSettings();
+        }
+
+        public void SetLogLevel(PsdzLoglevel psdzLoglevel, ProdiasLoglevel prodiasLoglevel)
+        {
+            _psdzServiceHostWrapper.SetLogLevel(psdzLoglevel, prodiasLoglevel);
+            //_psdzWebServiceWrapper.SetLogLevel(psdzLoglevel, prodiasLoglevel);
+        }
+
+        public void Shutdown()
+        {
+            _psdzServiceHostWrapper.Shutdown();
+            //_psdzWebServiceWrapper.Shutdown();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _psdzServiceHostWrapper.Dispose();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
