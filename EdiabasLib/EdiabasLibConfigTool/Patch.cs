90baca415a6d47ee084b292b212d09722b78d129
uholeschak <ulrich@holeschak.de>
2025-03-11 20:17:40 +0100

    Check if correct VC runtime is installed

--- EdiabasLib/EdiabasLibConfigTool/Patch.cs
+++ EdiabasLib/EdiabasLibConfigTool/Patch.cs
@@ -41,6 +41,9 @@ namespace EdiabasLibConfigTool
         public const string IniFileName = @"EDIABAS.INI";
         public const string RegKeyReingold = @"SOFTWARE\BMWGroup\ISPI\Rheingold";
         public const string RegKeyFtdiBus = @"SYSTEM\CurrentControlSet\Enum\FTDIBUS";
+        public const string RegKeyVcRuntime14Base = @"SOFTWARE\Microsoft\VisualStudio\14.0\VC\Runtimes";
+        public const string RegKeyVcRuntime14X86 = RegKeyVcRuntime14Base + @"\x86";
+        public const string RegKeyVcRuntime14X64 = RegKeyVcRuntime14Base + @"\x64";
         public const string RegValueFtdiLatencyTimer = @"LatencyTimer";
         public const string RegKeyIsta = @"SOFTWARE\BMWGroup\ISPI\ISTA";
         public const string RegKeyRheingoldNameStart = @"BMW.Rheingold.";
@@ -528,6 +531,14 @@ namespace EdiabasLibConfigTool
                         return false;
                     }
                 }
+
+                if (!IsVcRedistRegPresent(false) || !IsVcRedistRegPresent(true))
+                {
+                    sr.Append("\r\n");
+                    sr.Append(string.Format(Resources.Strings.PatchLoadApiDllFailed, Api32DllName, VcRedistLink));
+                    return false;
+                }
+
                 sr.Append("\r\n");
                 sr.Append(string.Format(Resources.Strings.PatchApiVersion, version32));
 
@@ -1291,6 +1302,49 @@ namespace EdiabasLibConfigTool
             }
 
             return patchRegType;
+        }
+
+        public static bool IsVcRedistRegPresent(bool check64Bit)
+        {
+            bool majorValid = false;
+            bool minorValid = false;
+            try
+            {
+                using (RegistryKey localMachine = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32))
+                {
+                    string subKeyName = check64Bit ? RegKeyVcRuntime14X64 : RegKeyVcRuntime14X86;
+                    using (RegistryKey key = localMachine.OpenSubKey(subKeyName, false))
+                    {
+                        if (key != null)
+                        {
+                            object majorObject = key.GetValue("Major");
+                            if (majorObject is int majorValue)
+                            {
+                                if (majorValue == 14)
+                                {
+                                    majorValid = true;
+                                }
+                            }
+
+                            object minorObject = key.GetValue("Minor");
+                            if (minorObject is int minorValue)
+                            {
+                                if (minorValue >= 13)
+                                {
+                                    minorValid = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                majorValid = false;
+                minorValid = false;
+            }
+
+            return majorValid && minorValid;
         }
     }
 }
