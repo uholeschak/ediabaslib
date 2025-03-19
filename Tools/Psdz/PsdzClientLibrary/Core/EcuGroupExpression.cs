1eb13e7ba60892d26274fc936b5c0f55996d8672
uholeschak <ulrich@holeschak.de>
2025-03-19 12:57:31 +0100

    Code update

--- Tools/Psdz/PsdzClientLibrary/Core/EcuGroupExpression.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/EcuGroupExpression.cs
@@ -15,64 +15,67 @@ namespace PsdzClient.Core
 		{
 		}
 
-		public EcuGroupExpression(long ecuGroupId)
-		{
-			this.value = ecuGroupId;
-		}
+        public EcuGroupExpression(long ecuGroupId)
+        {
+            value = ecuGroupId;
+        }
 
-		public override bool Evaluate(Vehicle vec, IFFMDynamicResolver ffmResolver, IRuleEvaluationServices ruleEvaluationUtils, ValidationRuleInternalResults internalResult)
-		{
-			if (vec == null)
-			{
-				Log.Warning("EcuGroupExpression.Evaluate()", "vec was null", Array.Empty<object>());
-				return false;
-			}
-			PsdzDatabase.EcuGroup ecuGroupById = ClientContext.GetDatabase(vec)?.GetEcuGroupById(this.value.ToString(CultureInfo.InvariantCulture));
-			if (ecuGroupById == null || string.IsNullOrEmpty(ecuGroupById.Name))
-			{
-				return false;
-			}
-			if (vec.VCI != null && (vec.VehicleIdentLevel == IdentificationLevel.BasicFeatures || vec.VehicleIdentLevel == IdentificationLevel.VINBasedFeatures || vec.VehicleIdentLevel == IdentificationLevel.VINOnly))
-			{
-				return true;
-			}
-			if (vec.VehicleIdentLevel == IdentificationLevel.VINBasedOnlineUpdated && vec.ECU != null && vec.ECU.Count == 0)
-			{
-				return true;
-			}
+        public override bool Evaluate(Vehicle vec, IFFMDynamicResolver ffmResolver, IRuleEvaluationServices ruleEvaluationServices, ValidationRuleInternalResults internalResult)
+        {
+            if (vec == null)
+            {
+                ruleEvaluationServices.Logger.Warning("EcuGroupExpression.Evaluate()", "vec was null");
+                return false;
+            }
+            PsdzDatabase.EcuGroup ecuGroupById = ClientContext.GetDatabase(vec)?.GetEcuGroupById(value.ToString(CultureInfo.InvariantCulture));
+            if (ecuGroupById == null || string.IsNullOrEmpty(ecuGroupById.Name))
+            {
+                ruleEvaluationServices.Logger.Warning("EcuGroupExpression.Evaluate()", "no valid group information found for id: {0}", value);
+                return false;
+            }
+            if (vec.VCI != null && (vec.VehicleIdentLevel == IdentificationLevel.BasicFeatures || vec.VehicleIdentLevel == IdentificationLevel.VINBasedFeatures || vec.VehicleIdentLevel == IdentificationLevel.VINOnly))
+            {
+                ruleEvaluationServices.Logger.Info("EcuGroupExpression.Evaluate()", "Infosession and manual VIN input => no ECU variant evaluation for {0} due to VehicleIdentLevel: {1}", ecuGroupById.Name, vec.VehicleIdentLevel);
+                return true;
+            }
+            if (vec.VehicleIdentLevel == IdentificationLevel.VINBasedOnlineUpdated && vec.ECU != null && !vec.ECU.Any())
+            {
+                ruleEvaluationServices.Logger.Info("EcuGroupExpression.Evaluate()", "Infosession and manual VIN input => no ECU representative evaluation for {0} due to VehicleIdentLevel: {1}", ecuGroupById.Name, vec.VehicleIdentLevel);
+                return true;
+            }
             bool flag = vec.getECUbyECU_GRUPPE(ecuGroupById.Name) != null;
-            Log.Debug("EcuGroupExpression.Evaluate()", "EcuGroupId: {0} (original rule: {1})  result: {2}", value, ecuGroupById.Name, flag);
-			return flag;
+            ruleEvaluationServices.Logger.Debug("EcuGroupExpression.Evaluate()", "EcuGroupId: {0} (original rule: {1})  result: {2}", value, ecuGroupById.Name, flag);
+            return flag;
 		}
 
-		public override EEvaluationResult EvaluateVariantRule(ClientDefinition client, CharacteristicSet baseConfiguration, EcuConfiguration ecus)
-		{
-			if (ecus.EcuGroups.ToList<long>().BinarySearch(this.value) >= 0)
-			{
-				return EEvaluationResult.VALID;
-			}
-			if (ecus.UnknownEcuGroups.ToList<long>().BinarySearch(this.value) >= 0)
-			{
-				return EEvaluationResult.MISSING_VARIANT;
-			}
-			return EEvaluationResult.INVALID;
-		}
+        public override EEvaluationResult EvaluateVariantRule(ClientDefinition client, CharacteristicSet baseConfiguration, EcuConfiguration ecus)
+        {
+            if (ecus.EcuGroups.ToList().BinarySearch(value) >= 0)
+            {
+                return EEvaluationResult.VALID;
+            }
+            if (ecus.UnknownEcuGroups.ToList().BinarySearch(value) >= 0)
+            {
+                return EEvaluationResult.MISSING_VARIANT;
+            }
+            return EEvaluationResult.INVALID;
+        }
 
-		public override IList<long> GetUnknownVariantIds(EcuConfiguration ecus)
-		{
-			List<long> list = new List<long>();
-			if (ecus.UnknownEcuGroups.ToList<long>().BinarySearch(this.value) >= 0)
-			{
-				list.Add(this.value);
-			}
-			return list;
-		}
+        public override IList<long> GetUnknownVariantIds(EcuConfiguration ecus)
+        {
+            List<long> list = new List<long>();
+            if (ecus.UnknownEcuGroups.ToList().BinarySearch(value) >= 0)
+            {
+                list.Add(value);
+            }
+            return list;
+        }
 
-		public override void Serialize(MemoryStream ms)
-		{
-			ms.WriteByte(10);
-			base.Serialize(ms);
-		}
+        public override void Serialize(MemoryStream ms)
+        {
+            ms.WriteByte(10);
+            base.Serialize(ms);
+        }
 
         public override string ToFormula(FormulaConfig formulaConfig)
         {
@@ -96,9 +99,9 @@ namespace PsdzClient.Core
             return stringBuilder.ToString();
         }
 
-		public override string ToString()
-		{
-			return "EcuGroup=" + this.value.ToString(CultureInfo.InvariantCulture);
-		}
-	}
+        public override string ToString()
+        {
+            return "EcuGroup=" + value.ToString(CultureInfo.InvariantCulture);
+        }
+    }
 }
