d2a6a9a051b13cd55c212647f2a16e13c9e178d6
uholeschak <ulrich@holeschak.de>
2025-03-20 20:42:12 +0100

    Code update

--- Tools/Psdz/PsdzClientLibrary/Core/AndExpression.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/AndExpression.cs
@@ -42,7 +42,7 @@ namespace PsdzClient.Core
             operands[1] = secondOperand;
         }
 
-        public new static AndExpression Deserialize(Stream ms, Vehicle vec)
+        public new static AndExpression Deserialize(Stream ms, ILogger logger, Vehicle vec)
         {
             int value = 0;
             byte[] bytes = BitConverter.GetBytes(value);
@@ -51,7 +51,7 @@ namespace PsdzClient.Core
             AndExpression andExpression = new AndExpression();
             for (int i = 0; i < value; i++)
             {
-                andExpression.AddOperand(RuleExpression.Deserialize(ms, vec));
+                andExpression.AddOperand(RuleExpression.Deserialize(ms, logger, vec));
             }
             return andExpression;
         }
