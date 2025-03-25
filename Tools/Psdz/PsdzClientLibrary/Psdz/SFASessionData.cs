633a2a0db36d48cd7782395f4d1fa201ce0ab3e9
uholeschak <ulrich@holeschak.de>
2025-03-25 14:54:27 +0100

    Added missing file

---
+++ Tools/Psdz/PsdzClientLibrary/Psdz/SFASessionData.cs
@@ -0,0 +1,27 @@
+﻿using BMW.Rheingold.Psdz.Model.Sfa;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PsdzClient.Programming
+{
+    public class SFASessionData
+    {
+        public IList<IPsdzFeatureLongStatusCto> CurrentFeatures { get; set; }
+
+        public bool IsValid
+        {
+            get
+            {
+                if (CurrentFeatures != null && CurrentFeatures.Any() && SecureTokens != null && SecureTokens.Any() && TargetFeatures != null)
+                {
+                    return TargetFeatures.Any();
+                }
+                return false;
+            }
+        }
+
+        public IList<IPsdzSecureTokenEto> SecureTokens { get; set; }
+
+        public IList<IPsdzEcuFeatureTokenRelationCto> TargetFeatures { get; set; }
+    }
+}
