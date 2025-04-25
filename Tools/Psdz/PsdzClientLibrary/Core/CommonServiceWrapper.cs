aa5d9c7083fafcb4f0bdcc2416ada16bcaf78869
uholeschak <ulrich@holeschak.de>
2025-04-25 16:12:57 +0200

    Code update

---
+++ Tools/Psdz/PsdzClientLibrary/Core/CommonServiceWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace PsdzClient.Core
+{
+    public class CommonServiceWrapper
+    {
+        private static readonly object featureSwitchLockObj = new object();
+        private static readonly List<string> loggedSwitches = new List<string>();
+
+        public CommonServiceWrapper()
+        {
+        }
+
+        public (bool IsActive, string Message) GetFeatureEnabledStatus(string feature)
+        {
+            string configString = ConfigSettings.getConfigString(LBPFeatureSwitches.FeatureRegistryKey(feature));
+            if (!string.IsNullOrEmpty(configString) && bool.TryParse(configString, out var result))
+            {
+                return (IsActive: result, Message: GetAndLogOutputMessage(feature, result, "REGISTRY KEY"));
+            }
+            bool flag = LBPFeatureSwitches.Features.DefaultValue(feature);
+            return (IsActive: flag, Message: GetAndLogOutputMessage(feature, flag, "DEFAULT VALUE"));
+        }
+
+        private string GetAndLogOutputMessage(string feature, bool value, string type)
+        {
+            string text = LBPFeatureSwitches.OutputMessage(feature, value, type);
+            lock (featureSwitchLockObj)
+            {
+                if (!loggedSwitches.Contains(feature))
+                {
+                    Log.Info(Log.CurrentMethod(), text);
+                    loggedSwitches.Add(feature);
+                }
+            }
+            return text;
+        }
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Core/LBPFeatureSwitches.cs
@@ -0,0 +1,80 @@
+﻿namespace PsdzClient.Core
+{
+    public static class LBPFeatureSwitches
+    {
+        public static class Features
+        {
+            public const string ForceUsageInternetUrl = "ForceUsageInternetUrl";
+
+            public const string PsdzWebservice = "PsdzWebservice";
+
+            public const string VcmComparison = "VcmComparison";
+
+            public const string UseJreWithTLS13Support = "UseJreWithTLS13Support";
+
+            public const string SdpOnlinePatchAndMultisession = "SdpOnlinePatchAndMultisession";
+
+            public const string ForceUsingJava64Bit = "ForceUsingJava64Bit";
+
+            public static bool DefaultValue(string featureName)
+            {
+                switch (featureName)
+                {
+                    case "UseJreWithTLS13Support":
+                    case "VcmComparison":
+                    case "SdpOnlinePatchAndMultisession":
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        }
+
+        public static class SwitchTypes
+        {
+            public const string RegistryKey = "REGISTRY KEY";
+
+            public const string EnabledByDeviceLBP = "ENABLED BY DEVICE LBP";
+
+            public const string DeactivatedByOutletLBP = "DEACTIVATED BY OUTLET LBP";
+
+            public const string ActivatedByLBP = "ACTIVATED BY LBP";
+
+            public const string MissingClientConfiguration = "MISSING CLIENT CONFIGURATION - RETURN DEFAULT VALUE";
+
+            public const string DefaultValue = "DEFAULT VALUE";
+
+            public const string DisabledByIstaVersion = "DISABLED BY ISTA VERSION";
+        }
+
+        public static string FeatureActivateKey(string feature)
+        {
+            return feature + "_Activate";
+        }
+
+        public static string FeatureActivateDevicesKey(string feature)
+        {
+            return feature + "_ActivateDevices";
+        }
+
+        public static string FeatureDeactivateOutletsKey(string feature)
+        {
+            return feature + "_DeactivateOutlets";
+        }
+
+        public static string FeatureRegistryKey(string feature)
+        {
+            return "BMW.Rheingold." + feature + "_Activate";
+        }
+
+        public static string FeatureCheckLBPsSinceIstaVersion(string feature)
+        {
+            return feature + "_CheckLBPsSinceIstaVersion";
+        }
+
+        public static string OutputMessage(string feature, bool value, string type)
+        {
+            return $"[FEATURE SWITCH] Feature: `{feature}` flag set using: `{type}` with value: `{value}`";
+        }
+    }
+}
