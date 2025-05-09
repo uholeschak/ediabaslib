39a084ffbc1b1f0ee15c2d8e626b701bd0f0b543
uholeschak <ulrich@holeschak.de>
2025-05-09 20:01:32 +0200

    Detect new vehicles

--- EdiabasLib/BmwFileReader/DetectVehicleBmw.cs
+++ EdiabasLib/BmwFileReader/DetectVehicleBmw.cs
@@ -424,6 +424,10 @@ namespace BmwFileReader
                         _ediabas.ResolveSgbdFile(jobInfo.SgdbName);
 
                         _ediabas.ArgString = string.Empty;
+                        if (!string.IsNullOrEmpty(jobInfo.JobArgs))
+                        {
+                            _ediabas.ArgString = jobInfo.JobArgs;
+                        }
                         _ediabas.ArgBinaryStd = null;
                         _ediabas.ResultsRequests = string.Empty;
                         _ediabas.ExecuteJob(jobInfo.JobName);
