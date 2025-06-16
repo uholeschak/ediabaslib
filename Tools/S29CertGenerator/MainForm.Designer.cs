99e8bea1d74db956d1b403ec8a95c8c16ba90bba
uholeschak <ulrich@holeschak.de>
2025-06-16 02:25:25 +0200

    Handle settings

--- Tools/S29CertGenerator/MainForm.Designer.cs
+++ Tools/S29CertGenerator/MainForm.Designer.cs
@@ -30,6 +30,9 @@
         {
             folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
             openCertFileDialog = new System.Windows.Forms.OpenFileDialog();
+            buttonSelectCaKeyFile = new System.Windows.Forms.Button();
+            textBoxCaCeyFile = new System.Windows.Forms.TextBox();
+            buttonClose = new System.Windows.Forms.Button();
             SuspendLayout();
             // 
             // folderBrowserDialog
@@ -43,22 +46,59 @@
             openCertFileDialog.Filter = "Cert|*.key|All files|*.*";
             openCertFileDialog.Title = "Select server cert";
             // 
+            // buttonSelectCaKeyFile
+            // 
+            buttonSelectCaKeyFile.Location = new System.Drawing.Point(12, 12);
+            buttonSelectCaKeyFile.Name = "buttonSelectCaKeyFile";
+            buttonSelectCaKeyFile.Size = new System.Drawing.Size(147, 23);
+            buttonSelectCaKeyFile.TabIndex = 0;
+            buttonSelectCaKeyFile.Text = "Select CA Key";
+            buttonSelectCaKeyFile.UseVisualStyleBackColor = true;
+            // 
+            // textBoxCaCeyFile
+            // 
+            textBoxCaCeyFile.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            textBoxCaCeyFile.Location = new System.Drawing.Point(165, 12);
+            textBoxCaCeyFile.Name = "textBoxCaCeyFile";
+            textBoxCaCeyFile.ReadOnly = true;
+            textBoxCaCeyFile.Size = new System.Drawing.Size(623, 23);
+            textBoxCaCeyFile.TabIndex = 1;
+            // 
+            // buttonClose
+            // 
+            buttonClose.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            buttonClose.Location = new System.Drawing.Point(713, 415);
+            buttonClose.Name = "buttonClose";
+            buttonClose.Size = new System.Drawing.Size(75, 23);
+            buttonClose.TabIndex = 2;
+            buttonClose.Text = "Close";
+            buttonClose.UseVisualStyleBackColor = true;
+            buttonClose.Click += buttonClose_Click;
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            CancelButton = buttonClose;
             ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(buttonClose);
+            Controls.Add(textBoxCaCeyFile);
+            Controls.Add(buttonSelectCaKeyFile);
             Name = "MainForm";
             Text = "S29CertGenerator";
             FormClosing += MainForm_FormClosing;
             FormClosed += MainForm_FormClosed;
             Load += MainForm_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
         private System.Windows.Forms.OpenFileDialog openCertFileDialog;
+        private System.Windows.Forms.Button buttonSelectCaKeyFile;
+        private System.Windows.Forms.TextBox textBoxCaCeyFile;
+        private System.Windows.Forms.Button buttonClose;
     }
 }
--- Tools/S29CertGenerator/MainForm.cs
+++ Tools/S29CertGenerator/MainForm.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
@@ -14,16 +15,50 @@ namespace S29CertGenerator
         private void MainForm_Load(object sender, System.EventArgs e)
         {
             Icon = Properties.Resources.AppIcon;
+
+            LoadSettings();
         }
 
         private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-
+            StoreSettings();
         }
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
 
+        }
+
+        private void buttonClose_Click(object sender, System.EventArgs e)
+        {
+            Close();
+        }
+
+        private bool LoadSettings()
+        {
+            try
+            {
+                textBoxCaCeyFile.Text = Properties.Settings.Default.CaKeyFile;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private bool StoreSettings()
+        {
+            try
+            {
+                Properties.Settings.Default.CaKeyFile = textBoxCaCeyFile.Text;
+                Properties.Settings.Default.Save();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
         }
     }
 }
