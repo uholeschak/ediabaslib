ed95b672b61e6ef4e5e53e5efa748ee9cbb05437
uholeschak <ulrich@holeschak.de>
2025-07-13 17:32:13 +0200

    Use base directory

--- EdiabasLib/EdiabasLibConfigTool/Patch.cs
+++ EdiabasLib/EdiabasLibConfigTool/Patch.cs
@@ -865,14 +865,20 @@ namespace EdiabasLibConfigTool
                     return false;
                 }
 
-                string s29Path = Path.Combine(ediabasDir, "Security", "S29");
+                string ediabasBaseDir = Directory.GetParent(ediabasDir)?.FullName;
+                if (string.IsNullOrEmpty(ediabasBaseDir))
+                {
+                    return false;
+                }
+
+                string s29Path = Path.Combine(ediabasBaseDir, "Security", "S29");
                 if (!Directory.Exists(s29Path))
                 {
                     return false;
                 }
 
                 bool result = true;
-                string certPath = Path.Combine(ediabasDir, s29Path, "Certificates");
+                string certPath = Path.Combine(s29Path, "Certificates");
                 if (Directory.Exists(certPath))
                 {
                     string machineName = Environment.MachineName;
@@ -909,7 +915,7 @@ namespace EdiabasLibConfigTool
                     }
                 }
 
-                string jsonRequestPath = Path.Combine(ediabasDir, s29Path, "JSONRequests");
+                string jsonRequestPath = Path.Combine(ediabasBaseDir, "JSONRequests");
                 if (Directory.Exists(jsonRequestPath))
                 {
                     IEnumerable<string> jsonFiles = Directory.EnumerateFiles(jsonRequestPath, "*.json", SearchOption.AllDirectories);
@@ -932,7 +938,7 @@ namespace EdiabasLibConfigTool
                     }
                 }
 
-                string jsonResponsePath = Path.Combine(ediabasDir, s29Path, "JSONResponses");
+                string jsonResponsePath = Path.Combine(ediabasBaseDir, "JSONResponses");
                 if (Directory.Exists(jsonResponsePath))
                 {
                     IEnumerable<string> jsonFiles = Directory.EnumerateFiles(jsonResponsePath, "*.json", SearchOption.AllDirectories);
