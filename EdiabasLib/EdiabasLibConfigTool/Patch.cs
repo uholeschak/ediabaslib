39c98592f6a0df9047e4056989ca16ee2dd366ae
uholeschak <ulrich@holeschak.de>
2025-07-30 14:04:11 +0200

    Removed unused code

--- EdiabasLib/EdiabasLibConfigTool/Patch.cs
+++ EdiabasLib/EdiabasLibConfigTool/Patch.cs
@@ -161,49 +161,6 @@ namespace EdiabasLibConfigTool
             }
         }
 
-        public static string NormalizePath(string path)
-        {
-            try
-            {
-                if (string.IsNullOrEmpty(path))
-                {
-                    return null;
-                }
-
-                return Path.GetFullPath(new Uri(path).LocalPath)
-                    .TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar)
-                    .ToUpperInvariant();
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
-
-        public static bool PathStartWith(string fullPath, string subPath)
-        {
-            try
-            {
-                string fullPathNorm = NormalizePath(fullPath);
-                string subPathNorm = NormalizePath(subPath);
-                if (string.IsNullOrEmpty(fullPathNorm) || string.IsNullOrEmpty(subPathNorm))
-                {
-                    return false;
-                }
-
-                if (fullPathNorm.IndexOf(subPathNorm, StringComparison.OrdinalIgnoreCase) > -1)
-                {
-                    return true;
-                }
-            }
-            catch (Exception)
-            {
-                // ignored
-            }
-
-            return false;
-        }
-
         public static bool IsOriginalDll(string fileName)
         {
             try
