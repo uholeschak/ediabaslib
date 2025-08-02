4ce3c49970b62c44ee1e5f551120c0d8001a9396
uholeschak <ulrich@holeschak.de>
2025-08-02 13:23:46 +0200

    Code updated

--- Tools/Psdz/PsdzClientLibrary/Core/IInteractionService.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/IInteractionService.cs
@@ -3,6 +3,24 @@
     // [UH] dummy file
     public interface IInteractionService
     {
+        void Deregister(InteractionModel model);
+
+        //void DeregisterBackground(IInteractionProgressModel model);
+
         void Register(InteractionModel model);
+
+        //Task<TResponse> RegisterAsync<TResponse>(InteractionRequestModel<TResponse> model) where TResponse : InteractionResponse;
+
+        //void RegisterBackground(IInteractionProgressModel model);
+
+        //InteractionMessageModel RegisterMessage(string title, string message, string details = "", int dialogSize = 0);
+
+        //InteractionMessageModel RegisterMessage(IList<LocalizedText> titleList, IList<LocalizedText> msgList, string details = "", int dialogSize = 0);
+
+        //Task<InteractionButtonResponse> RegisterMessageAsync(string title, string message, string details = "", int dialogSize = 0);
+
+        //InteractionQuestionModel RegisterQuestion(string title, string message);
+
+        //InteractionQuestionModel RegisterQuestion(IList<LocalizedText> titleList, IList<LocalizedText> msgList);
     }
 }
