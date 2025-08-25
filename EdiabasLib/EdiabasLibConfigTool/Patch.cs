c85b99aa82be0945ce44179d9fa2e6dd7faad976
uholeschak <ulrich@holeschak.de>
2025-08-25 13:09:08 +0200

    Check for new reg keys

--- EdiabasLib/EdiabasLibConfigTool/Patch.cs
+++ EdiabasLib/EdiabasLibConfigTool/Patch.cs
@@ -52,6 +52,8 @@ namespace EdiabasLibConfigTool
         public const string RegKeyRheingoldNameStart = @"BMW.Rheingold.";
         public const string RegKeyIstaBinPath = @"BMW.Rheingold.ISTAGUI.BinPathModifications";
         public const string RegKeyIstaIdesBinPath = @"BMW.Rheingold.ISTAGUI.EdiabasIDESBinPathModifications";
+        public const string RegKeyIstaIcomNextPackageVer = @"IcomNext.Package.Version";
+        public const string RegKeyIstaIcomNextPackageVerComp = @"IcomNext.Package.Version.Compatible";
         public const string RegKeyIstaBinFull = RegKeyReingold + @": " + RegKeyIstaBinPath;
         public const string RegKeyIstaIdesBinFull = RegKeyReingold + @": " + RegKeyIstaIdesBinPath;
         public const string RegKeyIstaOpMode = @"BMW.Rheingold.OperationalMode";
@@ -803,6 +805,23 @@ namespace EdiabasLibConfigTool
             return true;
         }
 
+        public static bool RemoveIstaReg(StringBuilder sr)
+        {
+            // ReSharper disable once ReplaceWithSingleAssignment.True
+            bool result = true;
+            if (!RemoveIstaReg(sr, RegistryView.Registry32))
+            {
+                result = false;
+            }
+
+            if (!RemoveIstaReg(sr, RegistryView.Registry64))
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
         public static bool RemoveIstaReg(StringBuilder sr, RegistryView? registryViewIsta)
         {
             if (registryViewIsta != null)
@@ -1242,7 +1261,10 @@ namespace EdiabasLibConfigTool
                 }
 
                 sr.AppendFormat(Resources.Strings.PatchDirectory, dirName);
-                RemoveIstaReg(sr, registryViewIsta);
+                if (registryViewIsta != null)
+                {
+                    RemoveIstaReg(sr);
+                }
 
                 if (!string.IsNullOrEmpty(ediabasDir))
                 {
@@ -1341,9 +1363,12 @@ namespace EdiabasLibConfigTool
                 result = false;
             }
 
-            if (!RemoveIstaReg(sr, registryViewIsta))
+            if (registryViewIsta != null)
             {
-                result = false;
+                if (!RemoveIstaReg(sr))
+                {
+                    result = false;
+                }
             }
 
             if (!string.IsNullOrEmpty(ediabasDir))
@@ -1359,6 +1384,29 @@ namespace EdiabasLibConfigTool
 
         public static RegistryView? GetIstaReg()
         {
+            try
+            {
+                using (RegistryKey localMachine64 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+                {
+                    using (RegistryKey key = localMachine64.OpenSubKey(RegKeyReingold, false))
+                    {
+                        if (key != null)
+                        {
+                            string[] valueNames = key.GetValueNames();
+                            if (valueNames.Any(x => string.Compare(x, RegKeyIstaIcomNextPackageVerComp, StringComparison.OrdinalIgnoreCase) != 0 ||
+                                                            string.Compare(x, RegKeyIstaIcomNextPackageVer, StringComparison.OrdinalIgnoreCase) != 0))
+                            {
+                                return RegistryView.Registry64;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
             try
             {
                 using (RegistryKey localMachine32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32))
