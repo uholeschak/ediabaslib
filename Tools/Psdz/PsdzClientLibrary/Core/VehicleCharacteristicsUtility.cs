a3c06598272e819d57c670f315e7ff2212ad98ae
uholeschak <ulrich@holeschak.de>
2025-10-09 16:56:23 +0200

    Code update

--- Tools/Psdz/PsdzClientLibrary/Core/VehicleCharacteristicsUtility.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/VehicleCharacteristicsUtility.cs
@@ -9,57 +9,57 @@ namespace PsdzClient.Core
     {
         public static bool HasMrr30(this IVehicle vec)
         {
-            return vec.HasEcu("MRR_30");
+            return HasEcu(vec, "MRR_30");
         }
 
         public static bool HasHuMgu(this IVehicle vec)
         {
-            return vec.HasEcu("HU_MGU");
+            return HasEcu(vec, "HU_MGU");
         }
 
         public static bool HasEnavevo(this IVehicle vec)
         {
-            return vec.HasEcu("ENAVEVO");
+            return HasEcu(vec, "ENAVEVO");
         }
 
         public static bool HasNbtevo(this IVehicle vec)
         {
-            return vec.HasEcu("NBTEVO");
+            return HasEcu(vec, "NBTEVO");
         }
 
         public static bool HasAmpt70(this IVehicle vec)
         {
-            return vec.HasEcu("AMPT70");
+            return HasEcu(vec, "AMPT70");
         }
 
         public static bool HasAmph70(this IVehicle vec)
         {
-            return vec.HasEcu("AMPH70");
+            return HasEcu(vec, "AMPH70");
         }
 
         public static bool IsBev(this IVehicle vec)
         {
-            return vec.HasHybridMark("BEVE");
+            return HasHybridMark(vec, "BEVE");
         }
 
         public static bool IsPhev(this IVehicle vec)
         {
-            return vec.HasHybridMark("PHEV");
+            return HasHybridMark(vec, "PHEV");
         }
 
         public static bool IsHybr(this IVehicle vec)
         {
-            return vec.HasHybridMark("HYBR");
+            return HasHybridMark(vec, "HYBR");
         }
 
         public static bool IsNohy(this IVehicle vec)
         {
-            return vec.HasHybridMark("NOHY");
+            return HasHybridMark(vec, "NOHY");
         }
 
         public static bool IsErex(this IVehicle vec)
         {
-            return vec.HasHybridMark("EREX");
+            return HasHybridMark(vec, "EREX");
         }
 
         private static bool HasEcu(this IVehicle vec, string sgbd)
@@ -68,7 +68,7 @@ namespace PsdzClient.Core
             {
                 return false;
             }
-            return vec.ECU.Any((ecu) => ecu.ECU_SGBD?.Equals(sgbd, StringComparison.InvariantCultureIgnoreCase) ?? false);
+            return vec.ECU.Any((IEcu ecu) => ecu.ECU_SGBD?.Equals(sgbd, StringComparison.InvariantCultureIgnoreCase) ?? false);
         }
 
         private static bool HasHybridMark(this IVehicle vec, string mark)
