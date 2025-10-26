6ca2ded655306b008bc4f3adec41f20a047d24f2
uholeschak <ulrich@holeschak.de>
2025-10-26 20:33:36 +0100

    Create PsdzWebService

---
+++ Tools/Psdz/PsdzClientLibrary/Psdz/ApiResult.cs
@@ -0,0 +1,23 @@
+﻿namespace BMW.Rheingold.Psdz
+{
+    public class ApiResult
+    {
+        public bool IsSuccessful { get; }
+
+        public ApiResult(bool isSuccessful)
+        {
+            IsSuccessful = isSuccessful;
+        }
+    }
+
+    public class ApiResult<T> : ApiResult
+    {
+        public T Data { get; }
+
+        public ApiResult(T data, bool isSuccessful)
+            : base(isSuccessful)
+        {
+            Data = data;
+        }
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Psdz/IWebCallHandler.cs
@@ -0,0 +1,15 @@
+﻿using java.lang.reflect;
+using PsdzClient.Contracts;
+using System.Collections.Generic;
+
+namespace BMW.Rheingold.Psdz
+{
+    public interface IWebCallHandler : ILifeCycleDependencyProvider
+    {
+        bool IgnorePrepareExecuteRequest { get; set; }
+
+        ApiResult ExecuteRequest(string serviceName, string endpoint, Method method, object requestBodyObject = null, IDictionary<string, string> queryParameters = null);
+
+        ApiResult<T> ExecuteRequest<T>(string serviceName, string endpoint, Method method, object requestBodyObject = null, IDictionary<string, string> queryParameters = null);
+    }
+}
