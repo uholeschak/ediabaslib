f3c91922f2ad205198b9654b8f06f97ec979ad1d
uholeschak <ulrich@holeschak.de>
2025-10-28 13:23:41 +0100

    Interfaces updated

---
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Mapper/ReadSecurityMemoryObjectResultMapper.cs
@@ -0,0 +1,34 @@
+﻿using BMW.Rheingold.Psdz.Model.Certificate;
+using System.Linq;
+
+namespace BMW.Rheingold.Psdz
+{
+    internal static class ReadSecurityMemoryObjectResultMapper
+    {
+        public static PsdzReadCertMemoryObjectResult Map(ReadSecurityMemoryObjectResultModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            return new PsdzReadCertMemoryObjectResult
+            {
+                FailedEcus = model.FailedEcus?.Select(EcuFailureResponseCtoMapper.Map).ToArray(),
+                MemoryObjects = model.MemoryObjects?.Select(SecurityMemoryObjectEtoMapper.Map).ToArray()
+            };
+        }
+
+        public static ReadSecurityMemoryObjectResultModel Map(PsdzReadCertMemoryObjectResult psdzObject)
+        {
+            if (psdzObject == null)
+            {
+                return null;
+            }
+            return new ReadSecurityMemoryObjectResultModel
+            {
+                FailedEcus = psdzObject.FailedEcus?.Select(EcuFailureResponseCtoMapper.Map).ToList(),
+                MemoryObjects = psdzObject.MemoryObjects?.Select(SecurityMemoryObjectEtoMapper.Map).ToList()
+            };
+        }
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Mapper/RequestEcuSecCheckingResultMapper.cs
@@ -0,0 +1,21 @@
+﻿using BMW.Rheingold.Psdz.Model.Certificate;
+using System.Linq;
+
+namespace BMW.Rheingold.Psdz
+{
+    internal static class RequestEcuSecCheckingResultMapper
+    {
+        internal static PsdzRequestEcuSecCheckingResult Map(RequestEcuSecCheckingResultModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            return new PsdzRequestEcuSecCheckingResult
+            {
+                EcuSecCheckingMaxWaitingTimes = model.EcuSecCheckingMaxWaitingTimes?.ToDictionary((EcuCheckingMaxWaitingTimeResultModel kvPair) => EcuIdentifierMapper.Map(kvPair.EcuIdentifierModel), (EcuCheckingMaxWaitingTimeResultModel kvPair) => kvPair.MaxWaitingTime),
+                FailedEcus = model.FailedEcus?.Select(EcuFailureResponseCtoMapper.Map)
+            };
+        }
+    }
+}
