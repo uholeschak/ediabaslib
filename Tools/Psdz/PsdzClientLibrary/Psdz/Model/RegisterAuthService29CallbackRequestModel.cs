7343354a378847e762b1e333489f1a17e1f721af
uholeschak <ulrich@holeschak.de>
2025-10-29 00:51:21 +0100

    SecureDiagnosticsService

---
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Model/RegisterAuthService29CallbackRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace BMW.Rheingold.Psdz
+{
+    public class RegisterAuthService29CallbackRequestModel
+    {
+        [JsonProperty("s29CertificateChainByteArray", NullValueHandling = NullValueHandling.Ignore)]
+        public byte[] S29CertificateChainByteArray { get; set; }
+
+        [JsonProperty("serializedPrivateKey", NullValueHandling = NullValueHandling.Ignore)]
+        public byte[] SerializedPrivateKey { get; set; }
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Service/SecureDiagnosticsService.cs
@@ -0,0 +1,64 @@
+﻿using BMW.Rheingold.Psdz;
+using BMW.Rheingold.Psdz.Client;
+using BMW.Rheingold.Psdz.Model;
+using PsdzClient.Core;
+using System;
+using RestSharp;
+
+namespace BMW.Rheingold.Psdz
+{
+    internal class SecureDiagnosticsService : ISecureDiagnosticsService
+    {
+        private readonly IWebCallHandler _webCallHandler;
+
+        private readonly string endpointService = "securediagnostics";
+
+        public SecureDiagnosticsService(IWebCallHandler webCallHandler)
+        {
+            _webCallHandler = webCallHandler;
+        }
+
+        public void RegisterAuthService29Callback(byte[] s29CertificateChainByteArray, byte[] serializedPrivateKey, IPsdzConnection connection)
+        {
+            Log.Info(Log.CurrentMethod(), $"Called. Connection: {connection}");
+            try
+            {
+                if (connection == null)
+                {
+                    throw new ArgumentNullException("connection");
+                }
+                RegisterAuthService29CallbackRequestModel requestBodyObject = new RegisterAuthService29CallbackRequestModel
+                {
+                    S29CertificateChainByteArray = s29CertificateChainByteArray,
+                    SerializedPrivateKey = serializedPrivateKey
+                };
+                ApiResult apiResult = _webCallHandler.ExecuteRequest(endpointService, $"registerauthservice29callback/{connection.Id}", Method.Post, requestBodyObject);
+                Log.Info(Log.CurrentMethod(), $"Finished. ResultIsSuccessful: {apiResult.IsSuccessful}");
+            }
+            catch (Exception exception)
+            {
+                Log.ErrorException(Log.CurrentMethod(), exception);
+                throw;
+            }
+        }
+
+        public void UnlockGateway(IPsdzConnection connection)
+        {
+            Log.Info(Log.CurrentMethod(), $"Called. Connection: {connection}");
+            try
+            {
+                if (connection == null)
+                {
+                    throw new ArgumentNullException("connection");
+                }
+                ApiResult apiResult = _webCallHandler.ExecuteRequest(endpointService, $"unlockgateway/{connection.Id}", Method.Post);
+                Log.Info(Log.CurrentMethod(), $"Finished. ResultIsSuccessful: {apiResult.IsSuccessful}");
+            }
+            catch (Exception exception)
+            {
+                Log.ErrorException(Log.CurrentMethod(), exception);
+                throw;
+            }
+        }
+    }
+}
