757bcabc8d4be969abcaade1c565ecc978fcaebb
uholeschak <ulrich@holeschak.de>
2025-11-09 22:47:12 +0100

    Code moved

---
+++ Tools/Psdz/PsdzClientLibrary/Vehicle/UwType.cs
@@ -0,0 +1,13 @@
+﻿namespace BMW.Rheingold.CoreFramework.Contracts.Vehicle
+{
+    public enum UwType
+    {
+        NONE,
+        Discrete,
+        DATA,
+        LP,
+        TEXT,
+        WMU,
+        UNKNOWN
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Vehicle/VisibilityType.cs
@@ -0,0 +1,9 @@
+﻿namespace BMW.Rheingold.CoreFramework.Contracts.Vehicle
+{
+    public enum VisibilityType
+    {
+        Hidden,
+        Collapsed,
+        Visible
+    }
+}
