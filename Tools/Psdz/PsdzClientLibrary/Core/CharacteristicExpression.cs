2088092d22409159d02aeb4f1639efcc8a3b6efa
uholeschak <ulrich@holeschak.de>
2025-11-09 21:08:45 +0100

    Use preserve attribute

--- Tools/Psdz/PsdzClientLibrary/Core/CharacteristicExpression.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/CharacteristicExpression.cs
@@ -1,32 +1,27 @@
-﻿using System;
+﻿using BMW.Rheingold.CoreFramework.Contracts.Vehicle;
+using PsdzClientLibrary;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Runtime.Serialization;
 using System.Text;
-using System.Threading.Tasks;
-using BMW.Rheingold.CoreFramework.Contracts.Vehicle;
 
 namespace PsdzClient.Core
 {
-	[Serializable]
-	public class CharacteristicExpression : RuleExpression
-	{
+    [Serializable]
+    public class CharacteristicExpression : RuleExpression
+    {
         private readonly long dataclassId;
-
         private readonly long datavalueId;
-
         private string characteristicRoot;
-
         private string characteristicValue;
 
+        [PreserveSource(Hint = "Modified")]
         private readonly PsdzDatabase dataProvider;
 
         private ILogger logger;
-
         public long DataClassId => dataclassId;
-
         public long DataValueId => datavalueId;
 
         private string CharacteristicRoot
@@ -37,8 +32,10 @@ namespace PsdzClient.Core
                 {
                     CharacteristicRoot = GetCharacteristicRootFromDb(dataProvider);
                 }
+
                 return characteristicRoot;
             }
+
             set
             {
                 characteristicRoot = value;
@@ -54,13 +51,17 @@ namespace PsdzClient.Core
                     CharacteristicValue = GetCharacteristicValueFromDb(dataProvider);
                     return characteristicValue;
                 }
+
                 return characteristicValue;
             }
+
             set
             {
                 characteristicValue = value;
             }
         }
+
+        [PreserveSource(Hint = "Modified")]
         public CharacteristicExpression(long dataclassId, long datavalueId, Vehicle vec)
         {
             this.dataclassId = dataclassId;
@@ -70,6 +71,7 @@ namespace PsdzClient.Core
             CharacteristicValue = GetCharacteristicValueFromDb(this.dataProvider);
         }
 
+        [PreserveSource(Hint = "Modified")]
         public static CharacteristicExpression Deserialize(Stream ms, Vehicle vec)
         {
             byte[] array = new byte[16];
@@ -79,6 +81,7 @@ namespace PsdzClient.Core
             return new CharacteristicExpression(num, num2, vec);
         }
 
+        [PreserveSource(Hint = "VehicleHelper replaced")]
         public override bool Evaluate(Vehicle vec, IFFMDynamicResolver ffmResolver, IRuleEvaluationServices ruleEvaluationServices, ValidationRuleInternalResults internalResult)
         {
             logger = ruleEvaluationServices.Logger;
@@ -90,6 +93,7 @@ namespace PsdzClient.Core
                 {
                     return ruleEvaluationServices.ConfigSettings.SelectedBrand.Any((BrandName b) => string.Equals(GetBrandNameAsString(b), CharacteristicValue, StringComparison.InvariantCultureIgnoreCase));
                 }
+
                 if ("Sicherheitsrelevant".Equals(CharacteristicRoot, StringComparison.OrdinalIgnoreCase) || "Sicherheitsfahrzeug".Equals(CharacteristicRoot, StringComparison.OrdinalIgnoreCase))
                 {
                     ruleEvaluationServices.Logger.Info("CharacteristicExpression.Evaluate()", "Failed to evaluate {0} without vehcile context; will answer false for 'Sicherheitsrelevant'", CharacteristicRoot);
@@ -103,9 +107,9 @@ namespace PsdzClient.Core
             }
             else
             {
-                // [UH]  VehicleHelper replaced
                 flag = vec.getISTACharacteristics(dataclassId, out value, datavalueId, internalResult);
             }
+
             ruleEvaluationServices.Logger.Debug("CharacteristicExpression.Evaluate()", "rule: {0}={1} result: {2} (session context: {3}) [original rule: {4}={5}]", CharacteristicRoot, CharacteristicValue, flag, value, dataclassId, datavalueId);
             return flag;
         }
@@ -118,8 +122,10 @@ namespace PsdzClient.Core
                 {
                     return EEvaluationResult.VALID;
                 }
+
                 return EEvaluationResult.INVALID;
             }
+
             return EEvaluationResult.MISSING_CHARACTERISTIC;
         }
 
@@ -140,6 +146,7 @@ namespace PsdzClient.Core
             {
                 list.Add(dataclassId);
             }
+
             return list;
         }
 
@@ -149,28 +156,19 @@ namespace PsdzClient.Core
             ms.Write(BitConverter.GetBytes(datavalueId), 0, 8);
         }
 
-        // [UH] added
-        public override string ToFormula(FormulaConfig formulaConfig)
-        {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.Append(FormulaSeparator(formulaConfig));
-            stringBuilder.Append(formulaConfig.CheckStringFunc);
-            stringBuilder.Append("(\"");
-
-            stringBuilder.Append(this.CharacteristicRoot);
-            stringBuilder.Append("\", ");
-
-            stringBuilder.Append("\"");
-            stringBuilder.Append(this.CharacteristicValue);
-            stringBuilder.Append("\")");
-            stringBuilder.Append(FormulaSeparator(formulaConfig));
-
-            return stringBuilder.ToString();
-        }
-
         public override string ToString()
         {
-            string[] obj = new string[8] { CharacteristicRoot, "=", CharacteristicValue, " [", null, null, null, null };
+            string[] obj = new string[8]
+            {
+                CharacteristicRoot,
+                "=",
+                CharacteristicValue,
+                " [",
+                null,
+                null,
+                null,
+                null
+            };
             long num = dataclassId;
             obj[4] = num.ToString(CultureInfo.InvariantCulture);
             obj[5] = "=";
@@ -180,6 +178,7 @@ namespace PsdzClient.Core
             return string.Concat(obj);
         }
 
+        [PreserveSource(Hint = "Modified")]
         private string GetCharacteristicRootFromDb(PsdzDatabase database)
         {
             string result = string.Empty;
@@ -191,6 +190,7 @@ namespace PsdzClient.Core
             return result;
         }
 
+        [PreserveSource(Hint = "Modified")]
         private string GetCharacteristicValueFromDb(PsdzDatabase database)
         {
             return database?.LookupVehicleCharDeDeById(this.datavalueId.ToString(CultureInfo.InvariantCulture));
@@ -220,6 +220,22 @@ namespace PsdzClient.Core
                     logger?.Warning(logger.CurrentMethod(), $"Unknown vehicle brand: {brand}");
                     return string.Empty;
             }
+        }
+
+        [PreserveSource(Hint = "Added")]
+        public override string ToFormula(FormulaConfig formulaConfig)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(FormulaSeparator(formulaConfig));
+            stringBuilder.Append(formulaConfig.CheckStringFunc);
+            stringBuilder.Append("(\"");
+            stringBuilder.Append(this.CharacteristicRoot);
+            stringBuilder.Append("\", ");
+            stringBuilder.Append("\"");
+            stringBuilder.Append(this.CharacteristicValue);
+            stringBuilder.Append("\")");
+            stringBuilder.Append(FormulaSeparator(formulaConfig));
+            return stringBuilder.ToString();
         }
     }
 }
