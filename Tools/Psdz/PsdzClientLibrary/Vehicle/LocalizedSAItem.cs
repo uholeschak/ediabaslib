8935e03c840930b2210bea1fc2be5483291f7c7a
uholeschak <ulrich@holeschak.de>
2025-11-09 22:48:52 +0100

    Code update

--- Tools/Psdz/PsdzClientLibrary/Vehicle/LocalizedSAItem.cs
+++ Tools/Psdz/PsdzClientLibrary/Vehicle/LocalizedSAItem.cs
@@ -6,26 +6,25 @@ namespace BMW.Rheingold.CoreFramework.Contracts.Vehicle
     [DataContract]
     public class LocalizedSAItem
     {
-        public LocalizedSAItem()
-        {
-        }
-
-        public LocalizedSAItem(string id, string title)
-        {
-            this.Id = id;
-            this.Title = title;
-        }
-
         [DataMember]
         public string Id { get; set; }
 
         [DataMember]
         public string Title { get; set; }
 
+        public LocalizedSAItem()
+        {
+        }
+
+        public LocalizedSAItem(string id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
         public override int GetHashCode()
         {
-            string id = this.Id;
-            return (id != null) ? id.GetHashCode() : -1;
+            return Id?.GetHashCode() ?? (-1);
         }
     }
 }
