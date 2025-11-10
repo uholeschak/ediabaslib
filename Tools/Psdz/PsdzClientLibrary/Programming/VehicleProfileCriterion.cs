00e61948557b34474fbfb6943d82b8a5c63477a3
uholeschak <ulrich@holeschak.de>
2025-11-10 22:51:10 +0100

    Automatic code update

--- Tools/Psdz/PsdzClientLibrary/Programming/VehicleProfileCriterion.cs
+++ Tools/Psdz/PsdzClientLibrary/Programming/VehicleProfileCriterion.cs
@@ -6,19 +6,17 @@ using System.Threading.Tasks;
 
 namespace PsdzClient.Programming
 {
-    public class VehicleProfileCriterion : IVehicleProfileCriterion
+    internal class VehicleProfileCriterion : IVehicleProfileCriterion
     {
+        public string Name { get; private set; }
+        public string NameEn { get; private set; }
+        public int Value { get; private set; }
+
         internal VehicleProfileCriterion(int value, string name, string nameEn)
         {
-            this.Value = value;
-            this.Name = name;
-            this.NameEn = nameEn;
+            Value = value;
+            Name = name;
+            NameEn = nameEn;
         }
-
-        public string Name { get; private set; }
-
-        public string NameEn { get; private set; }
-
-        public int Value { get; private set; }
     }
 }
