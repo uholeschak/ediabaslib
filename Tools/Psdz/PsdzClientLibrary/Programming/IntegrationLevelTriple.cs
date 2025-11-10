44eb5ee770ad0c03bb2a5c0790f4bde7b01b6768
uholeschak <ulrich@holeschak.de>
2025-11-10 23:17:34 +0100

    Automatic code update

--- Tools/Psdz/PsdzClientLibrary/Programming/IntegrationLevelTriple.cs
+++ Tools/Psdz/PsdzClientLibrary/Programming/IntegrationLevelTriple.cs
@@ -6,19 +6,17 @@ using System.Threading.Tasks;
 
 namespace PsdzClient.Programming
 {
-    public class IntegrationLevelTriple : IIstufenTriple
+    internal class IntegrationLevelTriple : IIstufenTriple
     {
+        public string Current { get; private set; }
+        public string Last { get; private set; }
+        public string Shipment { get; private set; }
+
         public IntegrationLevelTriple(string shipment, string last, string current)
         {
-            this.Shipment = shipment;
-            this.Last = last;
-            this.Current = current;
+            Shipment = shipment;
+            Last = last;
+            Current = current;
         }
-
-        public string Current { get; private set; }
-
-        public string Last { get; private set; }
-
-        public string Shipment { get; private set; }
     }
 }
