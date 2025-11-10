89d929516da55d4a5836d6785ff1f62d5f2ac932
uholeschak <ulrich@holeschak.de>
2025-11-11 00:54:29 +0100

    Automatic code update

--- Tools/Psdz/PsdzClientLibrary/Programming/VehicleProfile.cs
+++ Tools/Psdz/PsdzClientLibrary/Programming/VehicleProfile.cs
@@ -6,61 +6,55 @@ using System.Threading.Tasks;
 
 namespace PsdzClient.Programming
 {
-    public class VehicleProfile : IVehicleProfile
+    internal class VehicleProfile : IVehicleProfile
     {
+        private readonly IDictionary<int, IEnumerable<IVehicleProfileCriterion>> category2Criteria;
+        private readonly IDictionary<int, string> categoryId2CategoryName;
+        public string AsString { get; private set; }
+        public IEnumerable<int> CategoryIds => categoryId2CategoryName.Keys;
+        public string Entwicklungsbaureihe { get; internal set; }
+        public string Baureihenverbund { get; internal set; }
+
         internal VehicleProfile(string asString, IDictionary<int, string> categoryId2CategoryName, IDictionary<int, IEnumerable<IVehicleProfileCriterion>> category2Criteria)
         {
             if (asString == null)
             {
                 throw new ArgumentNullException("asString");
             }
+
             if (categoryId2CategoryName == null)
             {
                 throw new ArgumentNullException("categoryId2CategoryName");
             }
+
             if (category2Criteria == null)
             {
                 throw new ArgumentNullException("category2Criteria");
             }
+
             this.categoryId2CategoryName = categoryId2CategoryName;
             this.category2Criteria = category2Criteria;
-            this.AsString = asString;
-        }
-
-        public string AsString { get; private set; }
-
-        public IEnumerable<int> CategoryIds
-        {
-            get
-            {
-                return this.categoryId2CategoryName.Keys;
-            }
+            AsString = asString;
         }
 
         public string GetCategoryNameById(int categoryId)
         {
-            if (!this.categoryId2CategoryName.ContainsKey(categoryId))
+            if (!categoryId2CategoryName.ContainsKey(categoryId))
             {
                 return null;
             }
-            return this.categoryId2CategoryName[categoryId];
+
+            return categoryId2CategoryName[categoryId];
         }
 
         public IEnumerable<IVehicleProfileCriterion> GetCriteriaByCategoryId(int categoryId)
         {
-            if (!this.category2Criteria.ContainsKey(categoryId))
+            if (!category2Criteria.ContainsKey(categoryId))
             {
                 return null;
             }
-            return this.category2Criteria[categoryId];
+
+            return category2Criteria[categoryId];
         }
-
-        public string Entwicklungsbaureihe { get; internal set; }
-
-        public string Baureihenverbund { get; internal set; }
-
-        private readonly IDictionary<int, IEnumerable<IVehicleProfileCriterion>> category2Criteria;
-
-        private readonly IDictionary<int, string> categoryId2CategoryName;
     }
 }
