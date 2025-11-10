3833953fcbce2401b32a964ab713a1329cc697a4
uholeschak <ulrich@holeschak.de>
2025-11-11 00:48:17 +0100

    Automatic code update

--- Tools/Psdz/PsdzClientLibrary/Programming/TargetSelectorChooser.cs
+++ Tools/Psdz/PsdzClientLibrary/Programming/TargetSelectorChooser.cs
@@ -6,173 +6,172 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using BMW.Rheingold.Psdz.Model;
+using PsdzClient.Core;
+using PsdzClientLibrary;
 
 namespace PsdzClient.Programming
 {
-	public class TargetSelectorChooser
-	{
-        public TargetSelectorChooser(IEnumerable<IPsdzTargetSelector> targetSelectors)
-		{
-			this.targetSelectors = new List<TargetSelectorChooser.TargetSelectorDetail>();
-			if (targetSelectors != null && targetSelectors.Any<IPsdzTargetSelector>())
-			{
-				foreach (IPsdzTargetSelector psdzTargetSelector in targetSelectors)
-				{
-					if (!psdzTargetSelector.IsDirect)
-					{
-						this.targetSelectors.Add(new TargetSelectorChooser.TargetSelectorDetail(psdzTargetSelector));
-					}
-				}
-			}
-		}
+    internal class TargetSelectorChooser
+    {
+        private sealed class TargetSelectorDetail : IComparable<TargetSelectorDetail>
+        {
+            internal IPsdzTargetSelector TargetSelector { get; private set; }
+            private int ContainerTypeWeight { get; set; }
+            private int ILevelMonth { get; set; }
+            private int ILevelNumber { get; set; }
+            private int ILevelYear { get; set; }
+            private int Major { get; set; }
+            private int Minor { get; set; }
+            private int Patch { get; set; }
 
-		public IPsdzTargetSelector GetNewestTargetSelectorByMainSeries(string mainSeries)
-		{
-			IEnumerable<TargetSelectorChooser.TargetSelectorDetail> targetSelectorsByMainSeries = this.GetTargetSelectorsByMainSeries(mainSeries);
-			if (targetSelectorsByMainSeries != null && targetSelectorsByMainSeries.Any<TargetSelectorChooser.TargetSelectorDetail>())
-			{
-				List<TargetSelectorChooser.TargetSelectorDetail> list = new List<TargetSelectorChooser.TargetSelectorDetail>(targetSelectorsByMainSeries);
-				list.Sort();
-				return list.Last<TargetSelectorChooser.TargetSelectorDetail>().TargetSelector;
-			}
-			return null;
-		}
+            public TargetSelectorDetail(IPsdzTargetSelector targetSelector)
+            {
+                TargetSelector = targetSelector;
+                if (!TryParseDetailsByShortName(targetSelector.Project))
+                {
+                    ParseDetails(targetSelector.Project);
+                }
+            }
 
-		private IEnumerable<TargetSelectorChooser.TargetSelectorDetail> GetTargetSelectorsByMainSeries(string mainSeries)
-		{
-			return from ts in this.targetSelectors
-				   where string.Equals(mainSeries, ts.TargetSelector.Baureihenverbund, StringComparison.OrdinalIgnoreCase)
-				   select ts;
-		}
+            public int CompareTo(TargetSelectorDetail other)
+            {
+                int num = ContainerTypeWeight.CompareTo(other.ContainerTypeWeight);
+                if (num != 0)
+                {
+                    return num;
+                }
 
-		public const string PatternTargetSelectorILevelContainerShort = "^(?<mainSeries>\\w{4})_(?<iLevelYear>\\d{2})_(?<iLevelMonth>\\d{2})_(?<iLevelNumber>\\d{3})$";
+                num = ILevelYear.CompareTo(other.ILevelYear);
+                if (num != 0)
+                {
+                    return num;
+                }
 
-		public const string PatternTargetSelectorILevelContainer = "^(?<mainSeries>\\w{4})_(?<iLevelYear>\\d{2})_(?<iLevelMonth>\\d{2})_(?<iLevelNumber>\\d{3})_(?<containerType>[VTE])_(?<major>\\d{3})_(?<minor>\\d{3})_(?<patch>\\d{3})$";
+                num = ILevelMonth.CompareTo(other.ILevelMonth);
+                if (num != 0)
+                {
+                    return num;
+                }
 
-		private readonly List<TargetSelectorChooser.TargetSelectorDetail> targetSelectors;
+                num = ILevelNumber.CompareTo(other.ILevelNumber);
+                if (num != 0)
+                {
+                    return num;
+                }
 
-		private class TargetSelectorDetail : IComparable<TargetSelectorChooser.TargetSelectorDetail>
-		{
-			public TargetSelectorDetail(IPsdzTargetSelector targetSelector)
-			{
-				this.TargetSelector = targetSelector;
-				if (!this.TryParseDetailsByShortName(targetSelector.Project))
-				{
-					this.ParseDetails(targetSelector.Project);
-				}
-			}
+                num = Major.CompareTo(other.Major);
+                if (num != 0)
+                {
+                    return num;
+                }
 
-			internal IPsdzTargetSelector TargetSelector { get; private set; }
+                num = Minor.CompareTo(other.Minor);
+                if (num != 0)
+                {
+                    return num;
+                }
 
-			private int ContainerTypeWeight { get; set; }
+                return Patch.CompareTo(other.Patch);
+            }
 
-			private int ILevelMonth { get; set; }
+            private int GetContainerTypeWeight(string containerType)
+            {
+                switch (containerType)
+                {
+                    case "V":
+                        return 2;
+                    case "T":
+                        return 1;
+                    case "E":
+                        return 0;
+                    default:
+                        return -1;
+                }
+            }
 
-			private int ILevelNumber { get; set; }
+            private bool TryParseDetailsByShortName(string project)
+            {
+                Match match = Regex.Match((!string.IsNullOrEmpty(project)) ? project.Trim() : string.Empty, "^(?<mainSeries>\\w{4})_(?<iLevelYear>\\d{2})_(?<iLevelMonth>\\d{2})_(?<iLevelNumber>\\d{3})$");
+                if (match.Success)
+                {
+                    ContainerTypeWeight = GetContainerTypeWeight("V");
+                    ILevelYear = int.Parse(match.Groups["iLevelYear"].Value, NumberStyles.None);
+                    ILevelMonth = int.Parse(match.Groups["iLevelMonth"].Value, NumberStyles.None);
+                    ILevelNumber = int.Parse(match.Groups["iLevelNumber"].Value, NumberStyles.None);
+                    Major = 255;
+                    Minor = 255;
+                    Patch = 255;
+                    return true;
+                }
 
-			private int ILevelYear { get; set; }
+                return false;
+            }
 
-			private int Major { get; set; }
+            private void ParseDetails(string project)
+            {
+                Match match = Regex.Match((!string.IsNullOrEmpty(project)) ? project.Trim() : string.Empty, "^(?<mainSeries>\\w{4})_(?<iLevelYear>\\d{2})_(?<iLevelMonth>\\d{2})_(?<iLevelNumber>\\d{3})_(?<containerType>[VTE])_(?<major>\\d{3})_(?<minor>\\d{3})_(?<patch>\\d{3})$");
+                if (match.Success)
+                {
+                    ContainerTypeWeight = GetContainerTypeWeight(match.Groups["containerType"].Value);
+                    ILevelYear = int.Parse(match.Groups["iLevelYear"].Value, NumberStyles.None);
+                    ILevelMonth = int.Parse(match.Groups["iLevelMonth"].Value, NumberStyles.None);
+                    ILevelNumber = int.Parse(match.Groups["iLevelNumber"].Value, NumberStyles.None);
+                    Major = int.Parse(match.Groups["major"].Value, NumberStyles.None);
+                    Minor = int.Parse(match.Groups["minor"].Value, NumberStyles.None);
+                    Patch = int.Parse(match.Groups["patch"].Value, NumberStyles.None);
+                }
+                else
+                {
+                    Log.Warning("TargetSelectorChooser.ParseDetails()", "Project '{0}' does not seem to be a valid IC!", project);
+                    ILevelYear = -1;
+                    ILevelMonth = -1;
+                    ILevelNumber = -1;
+                    ContainerTypeWeight = -1;
+                    Major = -1;
+                    Minor = -1;
+                    Patch = -1;
+                }
+            }
+        }
 
-			private int Minor { get; set; }
+        public const string PatternTargetSelectorILevelContainerShort = "^(?<mainSeries>\\w{4})_(?<iLevelYear>\\d{2})_(?<iLevelMonth>\\d{2})_(?<iLevelNumber>\\d{3})$";
+        public const string PatternTargetSelectorILevelContainer = "^(?<mainSeries>\\w{4})_(?<iLevelYear>\\d{2})_(?<iLevelMonth>\\d{2})_(?<iLevelNumber>\\d{3})_(?<containerType>[VTE])_(?<major>\\d{3})_(?<minor>\\d{3})_(?<patch>\\d{3})$";
+        private readonly List<TargetSelectorDetail> targetSelectors;
+        private readonly IInteractionService interactionService;
+        internal TargetSelectorChooser(IEnumerable<IPsdzTargetSelector> targetSelectors, IInteractionService interactionService = null)
+        {
+            this.targetSelectors = new List<TargetSelectorDetail>();
+            if (targetSelectors != null && targetSelectors.Any())
+            {
+                foreach (IPsdzTargetSelector targetSelector in targetSelectors)
+                {
+                    if (!targetSelector.IsDirect)
+                    {
+                        this.targetSelectors.Add(new TargetSelectorDetail(targetSelector));
+                    }
+                }
+            }
 
-			private int Patch { get; set; }
+            this.interactionService = interactionService;
+        }
 
-			public int CompareTo(TargetSelectorChooser.TargetSelectorDetail other)
-			{
-				int num = this.ContainerTypeWeight.CompareTo(other.ContainerTypeWeight);
-				if (num != 0)
-				{
-					return num;
-				}
-				num = this.ILevelYear.CompareTo(other.ILevelYear);
-				if (num != 0)
-				{
-					return num;
-				}
-				num = this.ILevelMonth.CompareTo(other.ILevelMonth);
-				if (num != 0)
-				{
-					return num;
-				}
-				num = this.ILevelNumber.CompareTo(other.ILevelNumber);
-				if (num != 0)
-				{
-					return num;
-				}
-				num = this.Major.CompareTo(other.Major);
-				if (num != 0)
-				{
-					return num;
-				}
-				num = this.Minor.CompareTo(other.Minor);
-				if (num != 0)
-				{
-					return num;
-				}
-				return this.Patch.CompareTo(other.Patch);
-			}
+        [PreserveSource(Hint = "Modified")]
+        public IPsdzTargetSelector GetNewestTargetSelectorByMainSeries(string mainSeries, bool notifyIfTargetSelectorCouldNotBeFound = false, string localizationId = null)
+        {
+            IEnumerable<TargetSelectorDetail> targetSelectorsByMainSeries = GetTargetSelectorsByMainSeries(mainSeries);
+            if (targetSelectorsByMainSeries != null && targetSelectorsByMainSeries.Any())
+            {
+                List<TargetSelectorDetail> list = new List<TargetSelectorDetail>(targetSelectorsByMainSeries);
+                list.Sort();
+                return list.Last().TargetSelector;
+            }
 
-			private int GetContainerTypeWeight(string containerType)
-			{
-				if (containerType != null)
-				{
-					if (containerType == "V")
-					{
-						return 2;
-					}
-					if (containerType == "T")
-					{
-						return 1;
-					}
-					if (containerType == "E")
-					{
-						return 0;
-					}
-				}
-				return -1;
-			}
+            return null;
+        }
 
-			private bool TryParseDetailsByShortName(string project)
-			{
-				Match match = Regex.Match((!string.IsNullOrEmpty(project)) ? project.Trim() : string.Empty, PatternTargetSelectorILevelContainerShort);
-				if (match.Success)
-				{
-					this.ContainerTypeWeight = this.GetContainerTypeWeight("V");
-					this.ILevelYear = int.Parse(match.Groups["iLevelYear"].Value, NumberStyles.None);
-					this.ILevelMonth = int.Parse(match.Groups["iLevelMonth"].Value, NumberStyles.None);
-					this.ILevelNumber = int.Parse(match.Groups["iLevelNumber"].Value, NumberStyles.None);
-					this.Major = 255;
-					this.Minor = 255;
-					this.Patch = 255;
-					return true;
-				}
-				return false;
-			}
-
-			private void ParseDetails(string project)
-			{
-				Match match = Regex.Match((!string.IsNullOrEmpty(project)) ? project.Trim() : string.Empty, PatternTargetSelectorILevelContainer);
-				if (match.Success)
-				{
-					this.ContainerTypeWeight = this.GetContainerTypeWeight(match.Groups["containerType"].Value);
-					this.ILevelYear = int.Parse(match.Groups["iLevelYear"].Value, NumberStyles.None);
-					this.ILevelMonth = int.Parse(match.Groups["iLevelMonth"].Value, NumberStyles.None);
-					this.ILevelNumber = int.Parse(match.Groups["iLevelNumber"].Value, NumberStyles.None);
-					this.Major = int.Parse(match.Groups["major"].Value, NumberStyles.None);
-					this.Minor = int.Parse(match.Groups["minor"].Value, NumberStyles.None);
-					this.Patch = int.Parse(match.Groups["patch"].Value, NumberStyles.None);
-					return;
-				}
-
-                this.ILevelYear = -1;
-				this.ILevelMonth = -1;
-				this.ILevelNumber = -1;
-				this.ContainerTypeWeight = -1;
-				this.Major = -1;
-				this.Minor = -1;
-				this.Patch = -1;
-			}
-		}
-	}
+        private IEnumerable<TargetSelectorDetail> GetTargetSelectorsByMainSeries(string mainSeries)
+        {
+            return targetSelectors.Where((TargetSelectorDetail ts) => string.Equals(mainSeries, ts.TargetSelector.Baureihenverbund, StringComparison.OrdinalIgnoreCase));
+        }
+    }
 }
