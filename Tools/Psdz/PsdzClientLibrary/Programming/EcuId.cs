14978906e027dd87be4593f067127de2f3d1c968
uholeschak <ulrich@holeschak.de>
2025-11-11 00:55:43 +0100

    Automatic code update

--- Tools/Psdz/PsdzClientLibrary/Programming/EcuId.cs
+++ Tools/Psdz/PsdzClientLibrary/Programming/EcuId.cs
@@ -2,30 +2,35 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PsdzClient.Programming
 {
-    public class EcuId : IEcuIdentifier
+    [DataContract]
+    internal class EcuId : IEcuIdentifier
     {
+        [DataMember]
+        public string BaseVariant { get; private set; }
+
+        [DataMember]
+        public int DiagAddrAsInt { get; private set; }
+
         public EcuId(string baseVariant, int diagAddrAsInt)
         {
             if (baseVariant == null)
             {
                 throw new ArgumentNullException("baseVariant");
             }
-            this.BaseVariant = baseVariant;
-            this.DiagAddrAsInt = diagAddrAsInt;
+
+            BaseVariant = baseVariant;
+            DiagAddrAsInt = diagAddrAsInt;
         }
-
-        public string BaseVariant { get; private set; }
-
-        public int DiagAddrAsInt { get; private set; }
 
         public override string ToString()
         {
-            return string.Format(CultureInfo.InvariantCulture, "{0}_0x{1:X2}", this.BaseVariant, this.DiagAddrAsInt);
+            return string.Format(CultureInfo.InvariantCulture, "{0}_0x{1:X2}", BaseVariant, DiagAddrAsInt);
         }
     }
 }
