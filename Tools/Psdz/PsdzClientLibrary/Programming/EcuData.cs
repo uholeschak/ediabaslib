d2abba26fae5447e8c66bd4210c5e04c6182217c
uholeschak <ulrich@holeschak.de>
2025-11-11 00:57:17 +0100

    Automatic code update

--- Tools/Psdz/PsdzClientLibrary/Programming/EcuData.cs
+++ Tools/Psdz/PsdzClientLibrary/Programming/EcuData.cs
@@ -8,32 +8,19 @@ using System.Threading.Tasks;
 namespace BMW.Rheingold.Programming.Controller.SecureCoding.Model
 {
     [DataContract]
-    public class EcuData
+    internal class EcuData
     {
-        public List<string> CafdId
-        {
-            get
-            {
-                return (from a in this.cafd
-                    select a.Split(new char[]
-                    {
-                        '.'
-                    })[0].Replace("cafd_", "").ToUpper()).ToList<string>();
-            }
-        }
-
-        internal new string ToString
-        {
-            get
-            {
-                return "BTLD-SGBM-NO :" + this.btld + " - CAFD-SGBM-ID:" + string.Join("/", this.cafd);
-            }
-        }
-
         [DataMember(Name = "BTLD-SGBM-NO")]
         public readonly string btld;
-
         [DataMember(Name = "CAFD-SGBM-ID")]
         public readonly string[] cafd;
+        public List<string> CafdId => cafd.Select((string a) => a.Split('.')[0].Replace("cafd_", "").ToUpper()).ToList();
+
+        public override string ToString()
+        {
+            string arg = btld;
+            string[] array = cafd;
+            return string.Format("BTLD-SGBM-NO :{0} - CAFD-SGBM-ID:{1}", arg, (array != null && array.Any()) ? string.Join("/", cafd) : string.Empty);
+        }
     }
 }
