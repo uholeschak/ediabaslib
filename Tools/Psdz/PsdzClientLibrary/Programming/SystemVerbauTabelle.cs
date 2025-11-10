1578cc50b4b0ddd90a633dcfeec6a874ad7be812
uholeschak <ulrich@holeschak.de>
2025-11-10 23:07:55 +0100

    Automatic code update

--- Tools/Psdz/PsdzClientLibrary/Programming/SystemVerbauTabelle.cs
+++ Tools/Psdz/PsdzClientLibrary/Programming/SystemVerbauTabelle.cs
@@ -6,32 +6,22 @@ using System.Threading.Tasks;
 
 namespace PsdzClient.Programming
 {
-    public class SystemVerbauTabelle : ISvt
+    internal class SystemVerbauTabelle : ISvt
     {
-        public IEnumerable<IEcuObj> Ecus
-        {
-            get
-            {
-                return this.ecus;
-            }
-        }
-
+        private readonly IList<IEcuObj> ecus = new List<IEcuObj>();
+        public IEnumerable<IEcuObj> Ecus => ecus;
         public byte[] HoSignature { get; internal set; }
-
         public DateTime HoSignatureDate { get; internal set; }
-
         public int Version { get; internal set; }
 
         public bool RemoveEcu(IEcuObj ecu)
         {
-            return this.ecus.Remove(ecu);
+            return ecus.Remove(ecu);
         }
 
         internal void AddEcu(IEcuObj ecu)
         {
-            this.ecus.Add(ecu);
+            ecus.Add(ecu);
         }
-
-        private readonly IList<IEcuObj> ecus = new List<IEcuObj>();
     }
 }
