c2a4e490d0e373a5621381d00e1a8523275c1782
uholeschak <ulrich@holeschak.de>
2025-11-11 15:58:30 +0100

    Automatic code update

--- Tools/Psdz/PsdzClientLibrary/Core/IRuleExpression.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/IRuleExpression.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PsdzClientLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace PsdzClient.Core
 {
     public interface IRuleExpression
     {
-        // [UH] arguments modified
+        [PreserveSource(Hint = "Modified")]
         bool Evaluate(Vehicle vec, IFFMDynamicResolver ffmResolver, IRuleEvaluationServices ruleEvaluationUtils, ValidationRuleInternalResults internalResult);
     }
 }
