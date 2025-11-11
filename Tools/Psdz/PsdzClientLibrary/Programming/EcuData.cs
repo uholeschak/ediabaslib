70f92cde70613661984135748f465e68e92007c8
uholeschak <ulrich@holeschak.de>
2025-11-11 01:29:48 +0100

    Set attributes

--- Tools/Psdz/PsdzClientLibrary/Programming/EcuData.cs
+++ Tools/Psdz/PsdzClientLibrary/Programming/EcuData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PsdzClientLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace BMW.Rheingold.Programming.Controller.SecureCoding.Model
 {
+    [PreserveSource(Hint = "Changed to public")]
     [DataContract]
     public class EcuData
     {
