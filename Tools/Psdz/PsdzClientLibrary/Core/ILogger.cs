9425da74c60abbcc51e0b8fd89dfa0ae5d802cd0
uholeschak <ulrich@holeschak.de>
2025-11-11 20:19:30 +0100

    Automatic code update

--- Tools/Psdz/PsdzClientLibrary/Core/ILogger.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/ILogger.cs
@@ -7,17 +7,13 @@ namespace PsdzClient.Core
     public interface ILogger
     {
         void Debug(string method, string msg, params object[] args);
-
         void Error(string method, string msg, params object[] args);
-
         void ErrorException(string method, Exception exception);
-
+        void ErrorException(string method, string msg, Exception exception);
         void Info(string method, string msg, params object[] args);
-
         void Warning(string method, string msg, params object[] args);
-
         void WarningException(string method, Exception exception);
-
+        void WarningException(string method, string msg, Exception exception);
         [PreserveSource(Hint = "Modified")]
         string CurrentMethod([CallerMemberName] string memberName = null, [CallerFilePath] string sourceFilePath = null);
     }
