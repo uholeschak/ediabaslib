a78e4b6faf37ed04da5257cc09cf9140c820bf31
uholeschak <ulrich@holeschak.de>
2025-11-11 10:54:07 +0100

    Automatic code update

--- Tools/Psdz/PsdzClientLibrary/Core/CompareExpression.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/CompareExpression.cs
@@ -1,11 +1,8 @@
 ﻿using PsdzClientLibrary;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace PsdzClient.Core
 {
