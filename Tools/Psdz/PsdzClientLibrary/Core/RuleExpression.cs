707f666f5830e6840388b3fca7eaa2feab3da1fa
uholeschak <ulrich@holeschak.de>
2025-11-19 13:41:39 +0100

    Code update

--- Tools/Psdz/PsdzClientLibrary/Core/RuleExpression.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/RuleExpression.cs
@@ -11,39 +11,10 @@ namespace PsdzClient.Core
     [Serializable]
     public abstract class RuleExpression : IRuleExpression
     {
-        [PreserveSource(Hint = "Added")]
-        public class FormulaConfig
-        {
-            public FormulaConfig(string getStringFunc, string getLongFunc, string checkStringFunc, string checkLongFunc, string ruleValidFunc, bool isRuleValidNumFunc = false, List<string> subRuleIds = null, string operatorSeparator = null)
-            {
-                GetStringFunc = getStringFunc;
-                GetLongFunc = getLongFunc;
-                CheckStringFunc = checkStringFunc;
-                CheckLongFunc = checkLongFunc;
-                RuleValidFunc = ruleValidFunc;
-                IsRuleValidNumFunc = isRuleValidNumFunc;
-                SubRuleIds = subRuleIds;
-                OperatorSeparator = operatorSeparator;
-            }
-
-            public string GetStringFunc { get; private set; }
-            public string GetLongFunc { get; private set; }
-            public string CheckStringFunc { get; private set; }
-            public string CheckLongFunc { get; private set; }
-            public string RuleValidFunc { get; private set; }
-            public bool IsRuleValidNumFunc { get; private set; }
-            public List<string> SubRuleIds { get; private set; }
-            public string OperatorSeparator { get; private set; }
-        }
-
         public static IList<string> RuleEvaluationProtocol;
-
         public const long MISSING_DATE_EXPRESSION = -1L;
-
         protected const long MEMORYSIZE_OBJECT = 8L;
-
         protected const long MEMORYSIZE_REFERENCE = 8L;
-
         [PreserveSource(Hint = "dataProvider replaced by vec")]
         public static RuleExpression Deserialize(Stream ms, ILogger logger, Vehicle vec)
         {
@@ -90,100 +61,127 @@ namespace PsdzClient.Core
         public static bool Evaluate(Vehicle vec, IRuleExpression exp, IFFMDynamicResolver ffmResolver, IRuleEvaluationServices ruleEvaluationUtils = null, ValidationRuleInternalResults internalResult = null)
         {
             if (ruleEvaluationUtils == null)
-            {   // [UH] [IGNORE] added
+            { // [UH] [IGNORE] added
                 ruleEvaluationUtils = new RuleEvaluationServices(vec);
             }
+
             if (internalResult == null)
             {
                 internalResult = new ValidationRuleInternalResults();
             }
+
             if (exp is AndExpression || exp is OrExpression || exp is CharacteristicExpression || exp is DateExpression || exp is EcuCliqueExpression || exp is NotExpression || exp is SaLaPaExpression || exp is CountryExpression || exp is IStufeExpression || exp is IStufeXExpression || exp is EquipmentExpression || exp is ValidFromExpression || exp is ValidToExpression || exp is SiFaExpression || exp is EcuRepresentativeExpression || exp is ManufactoringDateExpression || exp is EcuVariantExpression || exp is EcuProgrammingVariantExpression)
             {
                 // [UH] [IGNORE] removed dealer and dataProvider
                 return exp.Evaluate(vec, ffmResolver, ruleEvaluationUtils, internalResult);
             }
+
             ruleEvaluationUtils.Logger.Error("RuleExpression.Evaluate(Vehicle vec, RuleExpression exp)", "RuleExpression {0} not implemented.", exp.ToString());
             return false;
         }
 
         [PreserveSource(Hint = "dataProvider removed")]
         public static string ParseAndSerializeVariantRule(string rule)
-		{
+        {
             RuleExpression expression = VariantRuleParser.Parse(rule);
             return SerializeToString(expression);
-		}
+        }
 
         [PreserveSource(Hint = "dataProvider removed")]
-		public static RuleExpression ParseEmpiricalRule(string rule)
-		{
-			return EmpiricalRuleParser.Parse(rule);
-		}
+        public static RuleExpression ParseEmpiricalRule(string rule)
+        {
+            return EmpiricalRuleParser.Parse(rule);
+        }
 
         [PreserveSource(Hint = "dataProvider removed")]
-		public static RuleExpression ParseFaultClassRule(string rule)
-		{
-			return FaultClassRuleParser.Parse(rule);
-		}
+        public static RuleExpression ParseFaultClassRule(string rule)
+        {
+            return FaultClassRuleParser.Parse(rule);
+        }
 
         [PreserveSource(Hint = "dataProvider removed")]
-		public static RuleExpression ParseVariantRule(string rule)
-		{
-			return VariantRuleParser.Parse(rule);
-		}
+        public static RuleExpression ParseVariantRule(string rule)
+        {
+            return VariantRuleParser.Parse(rule);
+        }
 
-		public static byte[] SerializeToByteArray(RuleExpression expression)
-		{
-			MemoryStream memoryStream = new MemoryStream();
-			expression.Serialize(memoryStream);
-			return memoryStream.ToArray();
-		}
+        public static byte[] SerializeToByteArray(RuleExpression expression)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            expression.Serialize(memoryStream);
+            return memoryStream.ToArray();
+        }
 
-		public static string SerializeToString(RuleExpression expression)
-		{
-			MemoryStream memoryStream = new MemoryStream();
-			expression.Serialize(memoryStream);
-			return Convert.ToBase64String(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
-		}
+        public static string SerializeToString(RuleExpression expression)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            expression.Serialize(memoryStream);
+            return Convert.ToBase64String(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+        }
 
         [PreserveSource(Hint = "removed dealer and dataProvider")]
-		public virtual bool Evaluate(Vehicle vec, IFFMDynamicResolver ffmResolver, IRuleEvaluationServices ruleEvaluationUtils, ValidationRuleInternalResults internalResult)
+        public virtual bool Evaluate(Vehicle vec, IFFMDynamicResolver ffmResolver, IRuleEvaluationServices ruleEvaluationUtils, ValidationRuleInternalResults internalResult)
         {
             ruleEvaluationUtils.Logger.Error("RuleExpression.Evaluate(Vehicle vec)", "method Evaluate(Vehicle vec) is missing.");
             return false;
-		}
+        }
 
-		public virtual EEvaluationResult EvaluateEmpiricalRule(long[] premises)
-		{
-			return EEvaluationResult.INVALID;
-		}
+        public virtual EEvaluationResult EvaluateEmpiricalRule(long[] premises)
+        {
+            return EEvaluationResult.INVALID;
+        }
 
-		public virtual EEvaluationResult EvaluateFaultClassRule(Dictionary<string, List<double>> variables)
-		{
-			return EEvaluationResult.INVALID;
-		}
+        public virtual EEvaluationResult EvaluateFaultClassRule(Dictionary<string, List<double>> variables)
+        {
+            return EEvaluationResult.INVALID;
+        }
 
-		public virtual EEvaluationResult EvaluateVariantRule(ClientDefinition client, CharacteristicSet baseConfiguration, EcuConfiguration ecus)
-		{
-			return EEvaluationResult.INVALID;
-		}
+        public virtual EEvaluationResult EvaluateVariantRule(ClientDefinition client, CharacteristicSet baseConfiguration, EcuConfiguration ecus)
+        {
+            return EEvaluationResult.INVALID;
+        }
 
-		public abstract long GetExpressionCount();
+        public abstract long GetExpressionCount();
+        public abstract long GetMemorySize();
+        public virtual IList<long> GetUnknownCharacteristics(CharacteristicSet baseConfiguration)
+        {
+            return new List<long>();
+        }
 
-		public abstract long GetMemorySize();
+        public virtual IList<long> GetUnknownVariantIds(EcuConfiguration ecus)
+        {
+            return new List<long>();
+        }
 
-		public virtual IList<long> GetUnknownCharacteristics(CharacteristicSet baseConfiguration)
-		{
-			return new List<long>();
-		}
+        public virtual void Optimize()
+        {
+        }
 
-		public virtual IList<long> GetUnknownVariantIds(EcuConfiguration ecus)
-		{
-			return new List<long>();
-		}
+        public abstract void Serialize(MemoryStream ms);
+        [PreserveSource(Hint = "Added")]
+        public class FormulaConfig
+        {
+            public FormulaConfig(string getStringFunc, string getLongFunc, string checkStringFunc, string checkLongFunc, string ruleValidFunc, bool isRuleValidNumFunc = false, List<string> subRuleIds = null, string operatorSeparator = null)
+            {
+                GetStringFunc = getStringFunc;
+                GetLongFunc = getLongFunc;
+                CheckStringFunc = checkStringFunc;
+                CheckLongFunc = checkLongFunc;
+                RuleValidFunc = ruleValidFunc;
+                IsRuleValidNumFunc = isRuleValidNumFunc;
+                SubRuleIds = subRuleIds;
+                OperatorSeparator = operatorSeparator;
+            }
 
-		public virtual void Optimize()
-		{
-		}
+            public string GetStringFunc { get; private set; }
+            public string GetLongFunc { get; private set; }
+            public string CheckStringFunc { get; private set; }
+            public string CheckLongFunc { get; private set; }
+            public string RuleValidFunc { get; private set; }
+            public bool IsRuleValidNumFunc { get; private set; }
+            public List<string> SubRuleIds { get; private set; }
+            public string OperatorSeparator { get; private set; }
+        }
 
         [PreserveSource(Hint = "Added")]
         public virtual string ToFormula(FormulaConfig formulaConfig)
@@ -201,7 +199,5 @@ namespace PsdzClient.Core
 
             return string.Empty;
         }
-
-		public abstract void Serialize(MemoryStream ms);
-	}
+    }
 }
