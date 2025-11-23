0f959e239db6a7aa87a81a4ae434ab7b1bfd2797
uholeschak <ulrich@holeschak.de>
2025-11-23 21:05:36 +0100

    Added source tags

--- Tools/Psdz/PsdzClientLibrary/Psdz/Service/IProgrammingService.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Service/IProgrammingService.cs
@@ -5,6 +5,7 @@ using BMW.Rheingold.Psdz.Model.Ecu;
 using BMW.Rheingold.Psdz.Model.Exceptions;
 using BMW.Rheingold.Psdz.Model.Swt;
 using BMW.Rheingold.Psdz.Model.Tal;
+using PsdzClient;
 
 namespace BMW.Rheingold.Psdz
 {
@@ -30,41 +31,51 @@ namespace BMW.Rheingold.Psdz
 
         IPsdzTal Cancel(string executionId); [OperationContract]
 
+        [PreserveSource(KeepAttribute = true)]
         [FaultContract(typeof(PsdzRuntimeException))]
         IEnumerable<IPsdzEcuIdentifier> CheckProgrammingCounter(IPsdzConnection connection, IPsdzTal tal);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         bool DisableFsc(IPsdzConnection connection, IPsdzEcuIdentifier ecuIdentifier, IPsdzSwtApplicationId swtApplicationId);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IDictionary<string, object> ExecuteAsamJob(IPsdzConnection connection, IPsdzEcuIdentifier ecuIdentifier, string jobId, IPsdzAsamJobInputDictionary inputDictionary);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         long GetExecutionTimeEstimate(IPsdzConnection connection, IPsdzTal tal, bool isParallel);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         byte[] GetFsc(IPsdzConnection connection, IPsdzEcuIdentifier ecuIdentifier, IPsdzSwtApplicationId swtApplicationId);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         void RequestBackupdata(string talExecutionId, string targetDir);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract(Name = "RequestSwtStatusForSingleEcu")]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzSwtAction RequestSwtAction(IPsdzConnection connection, IPsdzEcuIdentifier ecuIdentifier, IPsdzSwtApplicationId swtApplicationId, bool periodicalCheck);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzSwtAction RequestSwtAction(IPsdzConnection connection, bool periodicalCheck);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         bool StoreFsc(IPsdzConnection connection, byte[] fsc, IPsdzEcuIdentifier ecuIdentifier, IPsdzSwtApplicationId swtApplicationId);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         void TslUpdate(IPsdzConnection connection, bool complete, IPsdzSvt svtActual, IPsdzSvt svtTarget);
