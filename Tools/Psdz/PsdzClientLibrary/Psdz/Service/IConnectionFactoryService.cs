a42f0f3230836ed31e98878cffbfacb8b454e6e5
uholeschak <ulrich@holeschak.de>
2025-11-23 22:31:02 +0100

    Keep configured attribs

--- Tools/Psdz/PsdzClientLibrary/Psdz/Service/IConnectionFactoryService.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Service/IConnectionFactoryService.cs
@@ -1,20 +1,23 @@
-﻿using System;
+﻿using BMW.Rheingold.Psdz.Model;
+using BMW.Rheingold.Psdz.Model.Exceptions;
+using PsdzClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
-using BMW.Rheingold.Psdz.Model;
-using BMW.Rheingold.Psdz.Model.Exceptions;
 
 namespace BMW.Rheingold.Psdz
 {
+    [PreserveSource(AttributesModified = true)]
     [ServiceContract(SessionMode = SessionMode.Required)]
     [ServiceKnownType(typeof(PsdzTargetSelector))]
     public interface IConnectionFactoryService
     {
         IEnumerable<VehicleId> RequestAvailableVehicles();
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IEnumerable<IPsdzTargetSelector> GetTargetSelectors();
