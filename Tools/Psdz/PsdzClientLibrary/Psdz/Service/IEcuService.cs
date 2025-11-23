c8bba26d3c2aa3cbb26bd9f8ee5ba16ff93a4c42
uholeschak <ulrich@holeschak.de>
2025-11-23 22:29:26 +0100

    Keep configured attribs

--- Tools/Psdz/PsdzClientLibrary/Psdz/Service/IEcuService.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Service/IEcuService.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.Generic;
-using System.ServiceModel;
-using BMW.Rheingold.Psdz.Model;
+﻿using BMW.Rheingold.Psdz.Model;
 using BMW.Rheingold.Psdz.Model.Ecu;
 using BMW.Rheingold.Psdz.Model.Exceptions;
+using PsdzClient;
+using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace BMW.Rheingold.Psdz
 {
+    [PreserveSource(AttributesModified = true)]
     [ServiceContract(SessionMode = SessionMode.Required)]
     [ServiceKnownType(typeof(PsdzConnection))]
     [ServiceKnownType(typeof(PsdzEcuIdentifier))]
@@ -15,26 +17,32 @@ namespace BMW.Rheingold.Psdz
     [ServiceKnownType(typeof(PsdzSvt))]
     public interface IEcuService
     {
+        [PreserveSource(AttributesModified = true)]
         [OperationContract(Name = "RequestSvtFunctional")]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzStandardSvt RequestSvt(IPsdzConnection connection);
 
+        [PreserveSource(AttributesModified = true)]
         [OperationContract(Name = "RequestSvtFunctionalWithPhysicalRequest")]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzStandardSvt RequestSvt(IPsdzConnection connection, IEnumerable<IPsdzEcuIdentifier> installedEcus);
 
+        [PreserveSource(AttributesModified = true)]
         [OperationContract(Name = "RequestSvtFunctionalWithSmacs")]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzSvt RequestSvtWithSmacs(IPsdzConnection connection, IEnumerable<IPsdzEcuIdentifier> installedEcus);
 
+        [PreserveSource(AttributesModified = true)]
         [OperationContract(Name = "RequestSVTwithSmAcAndMirror")]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzSvt RequestSVTwithSmAcAndMirror(IPsdzConnection connection, IEnumerable<IPsdzEcuIdentifier> installedEcus);
 
+        [PreserveSource(AttributesModified = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IEnumerable<IPsdzEcuContextInfo> RequestEcuContextInfos(IPsdzConnection connection, IEnumerable<IPsdzEcuIdentifier> installedEcus);
 
+        [PreserveSource(AttributesModified = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzResponse UpdatePiaPortierungsmaster(IPsdzConnection connection, IPsdzSvt svt);
