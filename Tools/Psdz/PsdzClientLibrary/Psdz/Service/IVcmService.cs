e0a46b6e40fc18dea4f66f3c4589886632ba1392
uholeschak <ulrich@holeschak.de>
2025-11-23 21:55:38 +0100

    Keep configured attribs

--- Tools/Psdz/PsdzClientLibrary/Psdz/Service/IVcmService.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Service/IVcmService.cs
@@ -1,6 +1,7 @@
-﻿using System.ServiceModel;
-using BMW.Rheingold.Psdz.Model;
+﻿using BMW.Rheingold.Psdz.Model;
 using BMW.Rheingold.Psdz.Model.Exceptions;
+using PsdzClient;
+using System.ServiceModel;
 
 namespace BMW.Rheingold.Psdz
 {
@@ -12,6 +13,7 @@ namespace BMW.Rheingold.Psdz
         UNKNOWN
     }
 
+    [PreserveSource(AttributesModified = true)]
     [ServiceContract(SessionMode = SessionMode.Required)]
     [ServiceKnownType(typeof(PsdzConnection))]
     [ServiceKnownType(typeof(PsdzFa))]
@@ -25,62 +27,77 @@ namespace BMW.Rheingold.Psdz
     [ServiceKnownType(typeof(PsdzReadVpcFromVcmCto))]
     public interface IVcmService
     {
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzIstufenTriple GetIStufenTripleActual(IPsdzConnection connection);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzIstufenTriple GetIStufenTripleBackup(IPsdzConnection connection);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzStandardFa GetStandardFaActual(IPsdzConnection connection);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzStandardFa GetStandardFaBackup(IPsdzConnection connection);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzStandardFp GetStandardFp(IPsdzConnection connection);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzStandardSvt GetStandardSvtActual(IPsdzConnection connection);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzVin GetVinFromBackup(IPsdzConnection connection);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzVin GetVinFromMaster(IPsdzConnection connection);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         PsdzResultStateEto WriteFa(IPsdzConnection connection, IPsdzStandardFa standardFa);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         PsdzResultStateEto WriteFaToBackup(IPsdzConnection connection, IPsdzStandardFa standardFa);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         PsdzResultStateEto WriteFp(IPsdzConnection connection, IPsdzStandardFp standardFp);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         PsdzResultStateEto WriteIStufen(IPsdzConnection connection, string iStufeShipment, string iStufeLast, string iStufeCurrent);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         PsdzResultStateEto WriteIStufenToBackup(IPsdzConnection connection, string iStufeShipment, string iStufeLast, string iStufeCurrent);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         PsdzResultStateEto WriteSvt(IPsdzConnection connection, IPsdzStandardSvt standardSvt);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzReadVpcFromVcmCto RequestVpcFromVcm(IPsdzConnection connection);
