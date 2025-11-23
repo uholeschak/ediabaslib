7b92d27bc700bd24559b7a7e28afafe73e23f394
uholeschak <ulrich@holeschak.de>
2025-11-23 22:35:40 +0100

    Keep configured attribs

--- Tools/Psdz/PsdzClientLibrary/Psdz/Service/IEcuService.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Service/IEcuService.cs
@@ -17,32 +17,32 @@ namespace BMW.Rheingold.Psdz
     [ServiceKnownType(typeof(PsdzSvt))]
     public interface IEcuService
     {
-        [PreserveSource(AttributesModified = true)]
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract(Name = "RequestSvtFunctional")]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzStandardSvt RequestSvt(IPsdzConnection connection);
 
-        [PreserveSource(AttributesModified = true)]
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract(Name = "RequestSvtFunctionalWithPhysicalRequest")]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzStandardSvt RequestSvt(IPsdzConnection connection, IEnumerable<IPsdzEcuIdentifier> installedEcus);
 
-        [PreserveSource(AttributesModified = true)]
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract(Name = "RequestSvtFunctionalWithSmacs")]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzSvt RequestSvtWithSmacs(IPsdzConnection connection, IEnumerable<IPsdzEcuIdentifier> installedEcus);
 
-        [PreserveSource(AttributesModified = true)]
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract(Name = "RequestSVTwithSmAcAndMirror")]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzSvt RequestSVTwithSmAcAndMirror(IPsdzConnection connection, IEnumerable<IPsdzEcuIdentifier> installedEcus);
 
-        [PreserveSource(AttributesModified = true)]
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IEnumerable<IPsdzEcuContextInfo> RequestEcuContextInfos(IPsdzConnection connection, IEnumerable<IPsdzEcuIdentifier> installedEcus);
 
-        [PreserveSource(AttributesModified = true)]
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzResponse UpdatePiaPortierungsmaster(IPsdzConnection connection, IPsdzSvt svt);
