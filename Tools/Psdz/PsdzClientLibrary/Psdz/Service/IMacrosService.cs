f057be9701e38ab0a905e6283404b81b636b8e92
uholeschak <ulrich@holeschak.de>
2025-11-23 22:23:30 +0100

    Keep configured attribs

--- Tools/Psdz/PsdzClientLibrary/Psdz/Service/IMacrosService.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Service/IMacrosService.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.Generic;
-using System.ServiceModel;
-using BMW.Rheingold.Psdz.Model;
+﻿using BMW.Rheingold.Psdz.Model;
 using BMW.Rheingold.Psdz.Model.Ecu;
 using BMW.Rheingold.Psdz.Model.Exceptions;
+using PsdzClient;
+using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace BMW.Rheingold.Psdz
 {
+    [PreserveSource(AttributesModified = true)]
     [ServiceContract(SessionMode = SessionMode.Required)]
     [ServiceKnownType(typeof(PsdzSgbmId))]
     [ServiceKnownType(typeof(PsdzEcuIdentifier))]
@@ -14,14 +16,17 @@ namespace BMW.Rheingold.Psdz
     [ServiceKnownType(typeof(PsdzConnection))]
     public interface IMacrosService
     {
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IEnumerable<IPsdzSgbmId> CheckSoftwareEntries(IEnumerable<IPsdzSgbmId> sgbmIds);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IEnumerable<IPsdzEcuIdentifier> GetInstalledEcuList(IPsdzFa fa, IPsdzIstufe iStufe);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IEnumerable<IPsdzEcuIdentifier> GetInstalledEcuListWithConnection(IPsdzConnection connection, IPsdzFa fa, IPsdzIstufe iStufe);
