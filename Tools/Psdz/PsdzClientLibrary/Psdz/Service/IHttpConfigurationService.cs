80f5956f0ac623b9244f3f721a31ea4154b18e7b
uholeschak <ulrich@holeschak.de>
2025-11-23 22:27:46 +0100

    Keep configured attribs

--- Tools/Psdz/PsdzClientLibrary/Psdz/Service/IHttpConfigurationService.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Service/IHttpConfigurationService.cs
@@ -1,21 +1,26 @@
 ﻿using BMW.Rheingold.Psdz.Model.Exceptions;
+using PsdzClient;
 using System.ServiceModel;
 
 namespace BMW.Rheingold.Psdz
 {
+    [PreserveSource(AttributesModified = true)]
     [ServiceContract(SessionMode = SessionMode.Required, CallbackContract = typeof(IPsdzProgressListener))]
     public interface IHttpConfigurationService
     {
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         int GetHttpServerPort();
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         string GetNetworkEndpointSet();
 
         void SetHttpServerAddress(string address);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         void SetHttpServerPort(int port);
