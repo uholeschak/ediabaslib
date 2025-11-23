d5221099ea7e8a89a57fd2129746be0693ef27e7
uholeschak <ulrich@holeschak.de>
2025-11-23 22:21:44 +0100

    Keep configured attribs

--- Tools/Psdz/PsdzClientLibrary/Psdz/Service/ISecureCodingService.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Service/ISecureCodingService.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
-using System.ServiceModel;
-using BMW.Rheingold.Psdz.Model;
+﻿using BMW.Rheingold.Psdz.Model;
 using BMW.Rheingold.Psdz.Model.Exceptions;
 using BMW.Rheingold.Psdz.Model.SecureCoding;
 using BMW.Rheingold.Psdz.Model.Sfa;
 using BMW.Rheingold.Psdz.Model.Sfa.RequestNcdSignatureResponseCto;
 using BMW.Rheingold.Psdz.Model.Tal;
+using PsdzClient;
+using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace BMW.Rheingold.Psdz
 {
@@ -19,6 +20,7 @@ namespace BMW.Rheingold.Psdz
         FWL
     }
 
+    [PreserveSource(AttributesModified = true)]
     [ServiceContract(SessionMode = SessionMode.Required, CallbackContract = typeof(IPsdzProgressListener))]
     [ServiceKnownType(typeof(PsdzCheckNcdResultEto))]
     [ServiceKnownType(typeof(PsdzTal))]
@@ -33,18 +35,22 @@ namespace BMW.Rheingold.Psdz
     [ServiceKnownType(typeof(PsdzSgbmId))]
     public interface ISecureCodingService
     {
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzCheckNcdResultEto CheckNcdAvailabilityForGivenTal(IPsdzTal tal, string ncdDirectory, IPsdzVin vin);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzNcd ReadNcdFromFile(string ncdDirectory, IPsdzVin vin, IPsdzSgbmId cafdSgbmid, string btldSgbmNumber);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IList<IPsdzSecurityBackendRequestFailureCto> RequestCalculationNcdAndSignatureOffline(IList<IPsdzRequestNcdEto> sgbmidsForNcdCalculation, string jsonRequestFilePath, IPsdzSecureCodingConfigCto secureCodingConfigCto, IPsdzVin vin, IPsdzFa fa, byte[] vpc);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         IPsdzRequestNcdSignatureResponseCto RequestSignatureOnline(IList<IPsdzRequestNcdEto> sgbmidsForNcdCalculation, IPsdzSecureCodingConfigCto secureCodingConfigCto, IPsdzVin vin);
--- Tools/Psdz/PsdzClientLibrary/Psdz/Service/ISecureDiagnosticsService.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Service/ISecureDiagnosticsService.cs
@@ -1,19 +1,23 @@
-﻿using BMW.Rheingold.Psdz.Model.Exceptions;
+﻿using BMW.Rheingold.Psdz.Client;
 using BMW.Rheingold.Psdz.Model;
+using BMW.Rheingold.Psdz.Model.Exceptions;
+using PsdzClient;
 using System.ServiceModel;
-using BMW.Rheingold.Psdz.Client;
 
 namespace BMW.Rheingold.Psdz
 {
+    [PreserveSource(AttributesModified = true)]
     [ServiceContract(SessionMode = SessionMode.Required, CallbackContract = typeof(IPsdzProgressListener))]
     [ServiceKnownType(typeof(SecureDiagnosticsCallback))]
     [ServiceKnownType(typeof(PsdzConnection))]
     public interface ISecureDiagnosticsService
     {
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         void RegisterAuthService29Callback(byte[] s29CertificateChainByteArray, byte[] serializedPrivateKey, IPsdzConnection connection);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         void UnlockGateway(IPsdzConnection connection);
