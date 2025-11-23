ac95c0ea5cb779a0e0acddf9dfa34e37d6f48816
uholeschak <ulrich@holeschak.de>
2025-11-23 22:31:49 +0100

    Keep configured attribs

--- Tools/Psdz/PsdzClientLibrary/Psdz/Service/IConfigurationService.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Service/IConfigurationService.cs
@@ -1,43 +1,52 @@
-﻿using System;
+﻿using BMW.Rheingold.Psdz.Model.Exceptions;
+using PsdzClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
-using BMW.Rheingold.Psdz.Model.Exceptions;
 
 namespace BMW.Rheingold.Psdz
 {
+    [PreserveSource(AttributesModified = true)]
     [ServiceContract(SessionMode = SessionMode.Required)]
     public interface IConfigurationService
     {
         bool IsReady();
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         string GetExpectedPsdzVersion();
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         string GetPsdzVersion();
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         string GetRootDirectory();
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(ArgumentException))]
         [FaultContract(typeof(PsdzRuntimeException))]
         bool ImportPdx(string pathToPdxContainer, string projectName);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         string RequestBaureihenverbund(string baureihe);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         void SetRootDirectory(string rootDir);
 
+        [PreserveSource(KeepAttribute = true)]
         [OperationContract]
         [FaultContract(typeof(PsdzRuntimeException))]
         void UnsetRootDirectory();
