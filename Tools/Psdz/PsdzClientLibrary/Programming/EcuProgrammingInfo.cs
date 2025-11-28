be7331b1493eca8d4361e0a5011a86bc32636c3f
uholeschak <ulrich@holeschak.de>
2025-11-28 01:00:21 +0100

    Code update

--- Tools/Psdz/PsdzClientLibrary/Programming/EcuProgrammingInfo.cs
+++ Tools/Psdz/PsdzClientLibrary/Programming/EcuProgrammingInfo.cs
@@ -16,16 +16,6 @@ using System.Linq;
 
 namespace PsdzClient.Programming
 {
-    [Flags]
-    public enum EcuScheduledState
-    {
-        NothingScheduled = 0,
-        ProgrammingScheduledByLogistic = 1,
-        ProgrammingScheduledByUser = 2,
-        CodingScheduledByLogistic = 4,
-        CodingScheduledByUser = 8
-    }
-
     [PreserveSource(Hint = "Modified", AccessModified = true)]
     public class EcuProgrammingInfo : INotifyPropertyChanged, IEcuProgrammingInfo
 	{
---
+++ Tools/Psdz/PsdzClientLibrary/Programming/EcuScheduledState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PsdzClient.Programming
+{
+    [Flags]
+    public enum EcuScheduledState
+    {
+        NothingScheduled = 0,
+        ProgrammingScheduledByLogistic = 1,
+        ProgrammingScheduledByUser = 2,
+        CodingScheduledByLogistic = 4,
+        CodingScheduledByUser = 8
+    }
+}
