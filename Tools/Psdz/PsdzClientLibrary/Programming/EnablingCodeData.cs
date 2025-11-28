8c9716358d43f47599ba33787a009a85355c7b42
uholeschak <ulrich@holeschak.de>
2025-11-28 12:47:27 +0100

    Code update

--- Tools/Psdz/PsdzClientLibrary/Programming/EnablingCodeData.cs
+++ Tools/Psdz/PsdzClientLibrary/Programming/EnablingCodeData.cs
@@ -16,27 +16,10 @@ namespace PsdzClient.Programming
 
         public FscState FscState { get; set; }
 
+        [PreserveSource(Hint = "Cleaned")]
         public static string GetEnablingCodeName(int applicationId, int upgradeIndex, Vehicle vehicle, IFFMDynamicResolver dynamicResolver)
         {
-#if false
-            IDatabaseProvider instance = DatabaseProviderFactory.Instance;
-            XEP_SWIACTIVATIONCODE_SWT xEP_SWIACTIVATIONCODE_SWT = null;
-            xEP_SWIACTIVATIONCODE_SWT = instance.GetSwiActivationCodeSWTByAppIdUpgIdx(applicationId.ToString(CultureInfo.InvariantCulture), upgradeIndex.ToString(CultureInfo.InvariantCulture), vehicle, dynamicResolver) ?? instance.GetSwiActivationCodeSWTByAppIdUpgIdx(applicationId.ToString(CultureInfo.InvariantCulture), "65535", vehicle, dynamicResolver);
-            if (xEP_SWIACTIVATIONCODE_SWT == null)
-            {
-                Log.Warning(Log.CurrentMethod(), $"Unable to resolve title for enabling code data with applicationId {applicationId} and upgradeIndex {upgradeIndex}.");
-                return FormatedData.Localize("#TherapyPlanEntryOrigin.unknown");
-            }
-            string text = (string.IsNullOrEmpty(xEP_SWIACTIVATIONCODE_SWT.Title) ? xEP_SWIACTIVATIONCODE_SWT.Title_engb : xEP_SWIACTIVATIONCODE_SWT.Title);
-#else
-            string text = string.Empty;
-#endif
-            if (string.IsNullOrEmpty(text))
-            {
-                Log.Warning(Log.CurrentMethod(), $"Unable to resolve title translation for enabling code data with applicationId {applicationId} and upgradeIndex {upgradeIndex}, even after trying the fallback.");
-                return FormatedData.Localize("#TherapyPlanEntryOrigin.unknown");
-            }
-            return text;
+            return string.Empty;
         }
     }
 }
