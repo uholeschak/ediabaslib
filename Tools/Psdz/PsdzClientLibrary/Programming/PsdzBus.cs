2f6db4fcbe58c3d1d18a306355c009e848ce7cab
uholeschak <ulrich@holeschak.de>
2025-12-01 21:34:59 +0100

    Moved old code to extra file

--- Tools/Psdz/PsdzClientLibrary/Programming/PsdzBus.cs
+++ Tools/Psdz/PsdzClientLibrary/Programming/PsdzBus.cs
@@ -1,50 +1,9 @@
 ﻿using System.Linq;
 using System;
 
+#if !OLD_PSDZ_BUS
 namespace BMW.Rheingold.Psdz.Model.Ecu
 {
-#if OLD_PSDZ_BUS
-#warning OLD_PSDZ_BUS activated. Do not use for release builds.
-    public enum PsdzBus
-    {
-        Unknown = -1,
-        KCan = 0,
-        BodyCan = 1,
-        SfCan = 2,
-        Most = 3,
-        FaCan = 4,
-        FlexRay = 5,
-        SCan = 6,
-        InfraCan = 7,
-        B3Can = 12,
-        B2Can = 13,
-        Ethernet = 15,
-        DCan = 16,
-        ACan = 17,
-        LeCan = 18,
-        LpCan = 19,
-        IkCan = 20,
-        ZsgCan = 21,
-        ZgwCan = 24,
-        SystemBusEthernet = 27,
-        Le2Can = 28,
-        AeCanFd = 29,
-        ApCanFd = 30,
-        UssCanFd = 31,
-        FasCanFd = 32,
-        SrrCanFd = 33,
-        Ipb11CanFd = 34,
-        Ipb12CanFd = 35,
-        Ipb13CanFd = 36,
-        Ipb14CanFd = 37,
-        Ipb15CanFd = 38,
-        Ipb16CanFd = 47,
-        Zim11CanFd = 48,
-        Zim12CanFd = 49,
-        Ipb17CanFd = 50,
-    }
-#else
-    // Todo: Check on update
     public class PsdzBus : IComparable<PsdzBus>
     {
         public const string ETHERNET_PATTERN = "ETHERNET";
@@ -351,5 +310,5 @@ namespace BMW.Rheingold.Psdz.Model.Ecu
             return Id.CompareTo(other.Id);
         }
     }
-#endif
 }
+#endif
