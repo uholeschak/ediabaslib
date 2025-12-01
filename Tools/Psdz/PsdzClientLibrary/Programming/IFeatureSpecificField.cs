737af4a9632c0da884fa30fbf4c07ac533d4c282
uholeschak <ulrich@holeschak.de>
2025-12-01 19:02:05 +0100

    Comments removed

--- Tools/Psdz/PsdzClientLibrary/Programming/IFeatureSpecificField.cs
+++ Tools/Psdz/PsdzClientLibrary/Programming/IFeatureSpecificField.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PsdzClient.Core;
 
 namespace PsdzClient.Programming
 {
-    //[AuthorAPI(SelectableTypeDeclaration = true)]
+    [AuthorAPI(SelectableTypeDeclaration = true)]
     public interface IFeatureSpecificField
     {
         int FieldType { get; set; }
