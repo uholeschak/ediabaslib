e9afbeb64640a2e60fbb5bb80132b8ba787f2b13
uholeschak <ulrich@holeschak.de>
2025-12-19 17:59:19 +0100

    Hash added

--- Tools/Psdz/PsdzClientLibrary/Core/CountryExpression.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/CountryExpression.cs
@@ -14,7 +14,7 @@ namespace PsdzClient.Core
         [PreserveSource(Hint = "IDataProviderRuleEvaluation", Placeholder = true)]
         private readonly PlaceholderType dataProvider;
 
-        [PreserveSource(Hint = "Use database", OriginalHash = "")]
+        [PreserveSource(Hint = "Use database", OriginalHash = "FC0F981C34C6C14223BC005A1E70E9DE")]
         private string CountryCode
         {
             get
@@ -29,18 +29,18 @@ namespace PsdzClient.Core
             }
         }
 
-        [PreserveSource(Hint = "dataProvider removed", OriginalHash = "")]
+        [PreserveSource(Hint = "dataProvider removed", OriginalHash = "F47C25B9514B07B236F1A56D11D577F3")]
         public CountryExpression()
         {
         }
 
-        [PreserveSource(Hint = "dataProvider removed", OriginalHash = "")]
+        [PreserveSource(Hint = "dataProvider removed", OriginalHash = "0ACDF08839B829F7C53F3944F492ED20")]
         public CountryExpression(long countryId)
         {
             this.value = countryId;
         }
 
-        [PreserveSource(Hint = "dataProvider removed", OriginalHash = "")]
+        [PreserveSource(Hint = "dataProvider removed", OriginalHash = "5C241DE3DACE1C95CDA99CE2ACB0A19A")]
         public override bool Evaluate(Vehicle vec, IFFMDynamicResolver ffmResolver, IRuleEvaluationServices ruleEvaluationServices, ValidationRuleInternalResults internalResult)
         {
             this.vecInfo = vec;
