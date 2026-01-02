103f793ae854b36dff1ec0b9a13f9730b17e30a8
uholeschak <ulrich@holeschak.de>
2026-01-02 17:45:19 +0100

    Cleaned class

--- Tools/Psdz/PsdzClientLibrary/Utility/EventIdCatalog.cs
+++ Tools/Psdz/PsdzClientLibrary/Utility/EventIdCatalog.cs
@@ -7,9 +7,7 @@ using System.Runtime.CompilerServices;
 
 namespace PsdzClient.Utility
 {
-    [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
-    [DebuggerNonUserCode]
-    [CompilerGenerated]
+    [PreserveSource(Hint = "Simplified")]
     internal class EventIdCatalog
     {
         private static ResourceManager resourceMan;
@@ -40,96 +38,6 @@ namespace PsdzClient.Utility
             {
                 resourceCulture = value;
             }
-        }
-
-        internal static string ICS0001 => ResourceManager.GetString("ICS0001", resourceCulture);
-
-        internal static string ICS0002 => ResourceManager.GetString("ICS0002", resourceCulture);
-
-        internal static string ICS0003 => ResourceManager.GetString("ICS0003", resourceCulture);
-
-        internal static string ICS0004 => ResourceManager.GetString("ICS0004", resourceCulture);
-
-        internal static string ICS0005 => ResourceManager.GetString("ICS0005", resourceCulture);
-
-        internal static string ICS0006 => ResourceManager.GetString("ICS0006", resourceCulture);
-
-        internal static string ICS0007 => ResourceManager.GetString("ICS0007", resourceCulture);
-
-        internal static string ICS0008 => ResourceManager.GetString("ICS0008", resourceCulture);
-
-        internal static string ICS0009 => ResourceManager.GetString("ICS0009", resourceCulture);
-
-        internal static string ICS0010 => ResourceManager.GetString("ICS0010", resourceCulture);
-
-        internal static string ICS0015 => ResourceManager.GetString("ICS0015", resourceCulture);
-
-        internal static string ICS0022 => ResourceManager.GetString("ICS0022", resourceCulture);
-
-        internal static string ICS0046 => ResourceManager.GetString("ICS0046", resourceCulture);
-
-        internal static string ICS0049 => ResourceManager.GetString("ICS0049", resourceCulture);
-
-        internal static string ICS0115 => ResourceManager.GetString("ICS0115", resourceCulture);
-
-        internal static string ICS0116 => ResourceManager.GetString("ICS0116", resourceCulture);
-
-        internal static string ICS0117 => ResourceManager.GetString("ICS0117", resourceCulture);
-
-        internal static string ICS0122 => ResourceManager.GetString("ICS0122", resourceCulture);
-
-        internal static string ICS0123 => ResourceManager.GetString("ICS0123", resourceCulture);
-
-        internal static string ICS0124 => ResourceManager.GetString("ICS0124", resourceCulture);
-
-        internal static string ICS0125 => ResourceManager.GetString("ICS0125", resourceCulture);
-
-        internal static string ICS0126 => ResourceManager.GetString("ICS0126", resourceCulture);
-
-        internal static string ICS0127 => ResourceManager.GetString("ICS0127", resourceCulture);
-
-        internal static string ICS0128 => ResourceManager.GetString("ICS0128", resourceCulture);
-
-        internal static string ICS0129 => ResourceManager.GetString("ICS0129", resourceCulture);
-
-        internal static string ICS0130 => ResourceManager.GetString("ICS0130", resourceCulture);
-
-        internal static string ICS0131 => ResourceManager.GetString("ICS0131", resourceCulture);
-
-        internal static string ICS0132 => ResourceManager.GetString("ICS0132", resourceCulture);
-
-        internal static string ICS0133 => ResourceManager.GetString("ICS0133", resourceCulture);
-
-        internal static string ICS0134 => ResourceManager.GetString("ICS0134", resourceCulture);
-
-        internal static string ICS0135 => ResourceManager.GetString("ICS0135", resourceCulture);
-
-        internal static string ICS0136 => ResourceManager.GetString("ICS0136", resourceCulture);
-
-        internal static string ICS0137 => ResourceManager.GetString("ICS0137", resourceCulture);
-
-        internal static string ICS0139 => ResourceManager.GetString("ICS0139", resourceCulture);
-
-        internal static string ICS0140 => ResourceManager.GetString("ICS0140", resourceCulture);
-
-        internal static string ICS0141 => ResourceManager.GetString("ICS0141", resourceCulture);
-
-        internal static string ICS0142 => ResourceManager.GetString("ICS0142", resourceCulture);
-
-        internal static string ICS0143 => ResourceManager.GetString("ICS0143", resourceCulture);
-
-        internal static string ICS0144 => ResourceManager.GetString("ICS0144", resourceCulture);
-
-        internal static string ICS0145 => ResourceManager.GetString("ICS0145", resourceCulture);
-
-        internal static string ICS0146 => ResourceManager.GetString("ICS0146", resourceCulture);
-
-        internal static string ICS5066 => ResourceManager.GetString("ICS5066", resourceCulture);
-
-        internal static string ICSNone => ResourceManager.GetString("ICSNone", resourceCulture);
-
-        internal EventIdCatalog()
-        {
         }
     }
 }
