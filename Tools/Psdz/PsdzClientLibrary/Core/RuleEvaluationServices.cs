503d7386ff93cc9bc3b246786a8ba572d0da1933
uholeschak <ulrich@holeschak.de>
2026-01-02 14:00:11 +0100

    Hash added

--- Tools/Psdz/PsdzClientLibrary/Core/RuleEvaluationServices.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/RuleEvaluationServices.cs
@@ -4,13 +4,13 @@
     {
         public ILogger Logger => LoggerRuleEvaluationFactory.Create();
 
-        [PreserveSource(Hint = "Modified")]
+        [PreserveSource(Hint = "Initialized in constructor", OriginalHash = "8EB361A838945350E54E3512E44D57A0")]
         public IConfigSettingsRuleEvaluation ConfigSettings { get; }
 
         [PreserveSource(Hint = "Added")]
         public Vehicle Vec { get; }
 
-        [PreserveSource(Hint = "Constructor added, using ClientContext")]
+        [PreserveSource(Hint = "Constructor added, store vec, using ClientContext")]
         public RuleEvaluationServices(Vehicle vec)
         {
             this.Vec = vec;
