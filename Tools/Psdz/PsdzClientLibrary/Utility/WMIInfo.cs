68f200f28221227494b3baccb070759c7b7f4a40
uholeschak <ulrich@holeschak.de>
2026-01-02 18:33:31 +0100

    Code update

--- Tools/Psdz/PsdzClientLibrary/Utility/WMIInfo.cs
+++ Tools/Psdz/PsdzClientLibrary/Utility/WMIInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Management;
+using System.Windows;
 
 #pragma warning disable CS0168
 namespace PsdzClient.Utility
@@ -23,7 +24,6 @@ namespace PsdzClient.Utility
             }
         }
 
-        [PreserveSource(Hint = "Modified")]
         public static string GetWMIInfoITools(string segment, string info, string argument = null)
         {
             ExceptionAtStartup = false;
@@ -34,9 +34,9 @@ namespace PsdzClient.Utility
                 managementObjectSearcher = new ManagementObjectSearcher(new ManagementScope("root\\ITOOLS"), query);
                 return GetSearchResult(info, managementObjectSearcher);
             }
-            catch (ManagementException)
+            catch (ManagementException exception)
             {
-                // [IGNORE] Logger.Instance()?.Log(ICSEventId.ICS0001, "WMIInfo.GetWMIInfoITools", "Could not retrieve WMIInfoITools", EventKind.Technical, LogLevel.Warning, exception);
+                Logger.Instance()?.Log(ICSEventId.ICS0001, "WMIInfo.GetWMIInfoITools", "Could not retrieve WMIInfoITools", EventKind.Technical, LogLevel.Warning, exception);
                 return string.Empty;
             }
             finally
@@ -45,7 +45,7 @@ namespace PsdzClient.Utility
             }
         }
 
-        [PreserveSource(Hint = "Modified")]
+        [PreserveSource(Hint = "MessageBox disabled", OriginalHash = "E315C7CE7D5B280E5354CDC03BB03EC8")]
         private static string GetSearchResult(string info, ManagementObjectSearcher searcher)
         {
             try
@@ -67,9 +67,9 @@ namespace PsdzClient.Utility
             {
                 if (ExceptionAtStartup)
                 {
-                    // [IGNORE] Logger.Instance()?.Log(ICSEventId.ICS0006, "WMIInfo.GetSearchResult", "Could not retrieve WMIInfo, possible WMI repository corruption", EventKind.Technical, LogLevel.Error, exception);
-                    // [IGNORE] string caption = "ISPI AdminClient error ICS0006";
-                    // [IGNORE] MessageBox.Show("Kritischer Fehler beim Lesen der lokalen WMI-Datenbank: Repository  ist möglicherweise beschädigt oder Windows Management Instrumentation Service muss neu gestartet werden. Bitte öffnen Sie ein Ticket für den technischen Support und geben Sie an, dass auf diesem Gerät der Fehler ICS0006 aufgetreten ist.\n\nCritical error in reading local WMI database: repository is corrupted or Windows Management Instrumentation service needs to be restarted. Please open a ticket to technical support mentioning error ICS0006 has occurred on this device.", caption, MessageBoxButtons.OK, MessageBoxIcon.Hand, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification);
+                    Logger.Instance()?.Log(ICSEventId.ICS0006, "WMIInfo.GetSearchResult", "Could not retrieve WMIInfo, possible WMI repository corruption", EventKind.Technical, LogLevel.Error, exception);
+                    string caption = "ISPI AdminClient error ICS0006";
+                    // [UH] [IGNORE] MessageBox.Show("Kritischer Fehler beim Lesen der lokalen WMI-Datenbank: Repository  ist möglicherweise beschädigt oder Windows Management Instrumentation Service muss neu gestartet werden. Bitte öffnen Sie ein Ticket für den technischen Support und geben Sie an, dass auf diesem Gerät der Fehler ICS0006 aufgetreten ist.\n\nCritical error in reading local WMI database: repository is corrupted or Windows Management Instrumentation service needs to be restarted. Please open a ticket to technical support mentioning error ICS0006 has occurred on this device.", caption, MessageBoxButtons.OK, MessageBoxIcon.Hand, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification);
                     throw new ManagementException("Critical error in reading WMI database: repository may be corrupted or Windows Management Instrumentation service needs to be restarted.");
                 }
                 return string.Empty;
@@ -80,7 +80,6 @@ namespace PsdzClient.Utility
             }
         }
 
-        [PreserveSource(Hint = "Modified")]
         private static string GetObjectValue(string info, string result, ManagementBaseObject obj)
         {
             try
@@ -89,9 +88,9 @@ namespace PsdzClient.Utility
                 obj.Dispose();
                 return result;
             }
-            catch (NullReferenceException)
+            catch (NullReferenceException ex)
             {
-                // [IGNORE] Logger.Instance()?.Log(ICSEventId.ICS0123, "WMIInfo.GetObjectValue", ex.Message, EventKind.Technical, LogLevel.Warning, ex);
+                Logger.Instance()?.Log(ICSEventId.ICS0123, "WMIInfo.GetObjectValue", ex.Message, EventKind.Technical, LogLevel.Warning, ex);
                 return string.Empty;
             }
         }
