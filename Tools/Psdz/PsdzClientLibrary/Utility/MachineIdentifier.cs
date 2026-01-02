0903261d2f15c3803c231b837a3a9b38f162049a
uholeschak <ulrich@holeschak.de>
2026-01-02 17:31:18 +0100

    Use logger

--- Tools/Psdz/PsdzClientLibrary/Utility/MachineIdentifier.cs
+++ Tools/Psdz/PsdzClientLibrary/Utility/MachineIdentifier.cs
@@ -9,10 +9,9 @@ namespace PsdzClient.Utility
 {
     internal static class MachineIdentifier
     {
-        [PreserveSource(Hint = "Log removed", OriginalHash = "D124FC999B22B5EF403D6F87F669E809")]
         public static string GetVolumeSerialNumber()
         {
-            string pathRoot = Path.GetPathRoot(Environment.SystemDirectory);
+            string pathRoot = Path.GetPathRoot(System.Environment.SystemDirectory);
             pathRoot = pathRoot.Replace("\\", string.Empty);
             string argument = "DeviceID =\"" + pathRoot + "\"";
             string text = WMIInfo.GetWMIInfo("Win32_LogicalDisk", "VolumeSerialNumber", argument);
@@ -20,6 +19,7 @@ namespace PsdzClient.Utility
             {
                 text = "a";
             }
+
             text = text.Replace("-", string.Empty);
             int num = 1;
             while (text.Length < 8)
@@ -27,11 +27,11 @@ namespace PsdzClient.Utility
                 text += num.ToString(CultureInfo.InvariantCulture);
                 num++;
             }
-            // [IGNORE] Logger.Instance()?.Log(ICSEventId.ICSNone, "MachineIdentifier.GetVolumeSerialNumber", "Volume serial number: " + text, EventKind.Technical, LogLevel.Info);
+
+            Logger.Instance()?.Log(ICSEventId.ICSNone, "MachineIdentifier.GetVolumeSerialNumber", "Volume serial number: " + text, EventKind.Technical, LogLevel.Info);
             return text.Substring(0, 8);
         }
 
-        [PreserveSource(Hint = "Log removed", OriginalHash = "21EA6126E88BC71D4844111AFF84A320")]
         public static string GetMachineGuid()
         {
             string text = string.Empty;
@@ -45,34 +45,36 @@ namespace PsdzClient.Utility
                 {
                     return text;
                 }
+
                 text = registryKey2.GetValue("MachineGuid").ToString();
                 if (string.IsNullOrEmpty(text) || text.Length < 16)
                 {
                     return text;
                 }
+
                 return string.IsNullOrEmpty(text) ? string.Empty : text.ToUpper().Replace("-", string.Empty);
             }
-            catch (UnauthorizedAccessException)
+            catch (UnauthorizedAccessException ex)
             {
-                // [IGNORE] Logger.Instance()?.Log(ICSEventId.ICS0005, "MachineIdentifier.GetMachineGuid", $"The user does not have the necessary registry rights: Error  {ex}", EventKind.Technical, LogLevel.Error, ex);
+                Logger.Instance()?.Log(ICSEventId.ICS0005, "MachineIdentifier.GetMachineGuid", $"The user does not have the necessary registry rights: Error  {ex}", EventKind.Technical, LogLevel.Error, ex);
             }
-            catch (SecurityException)
+            catch (SecurityException ex2)
             {
-                // [IGNORE] Logger.Instance()?.Log(ICSEventId.ICS0131, "MachineIdentifier.GetMachineGuid", $"The user does not have the permissions required to perform this action.: Error  {ex2}", EventKind.Technical, LogLevel.Error, ex2);
+                Logger.Instance()?.Log(ICSEventId.ICS0131, "MachineIdentifier.GetMachineGuid", $"The user does not have the permissions required to perform this action.: Error  {ex2}", EventKind.Technical, LogLevel.Error, ex2);
             }
-            catch (IOException)
+            catch (IOException ex3)
             {
-                // [IGNORE] Logger.Instance()?.Log(ICSEventId.ICS0117, "MachineIdentifier.GetMachineGuid", $"The RegistryKey that contains the specified value has been marked for deletion.: Error  {ex3}", EventKind.Technical, LogLevel.Error, ex3);
+                Logger.Instance()?.Log(ICSEventId.ICS0117, "MachineIdentifier.GetMachineGuid", $"The RegistryKey that contains the specified value has been marked for deletion.: Error  {ex3}", EventKind.Technical, LogLevel.Error, ex3);
             }
             finally
             {
                 registryKey?.Dispose();
                 registryKey2?.Dispose();
             }
+
             return text;
         }
 
-        [PreserveSource(Hint = "Log removed", OriginalHash = "2718A3F6063FB675B1CD704CC0E9BD86")]
         public static string GetUuid()
         {
             ProcessStartInfo startInfo = new ProcessStartInfo("cmd", "/c wmic csproduct get UUID")
@@ -81,14 +83,18 @@ namespace PsdzClient.Utility
                 UseShellExecute = false,
                 CreateNoWindow = true
             };
-            char[] trimChars = new char[4] { ' ', '\n', '\r', '\t' };
+            char[] trimChars = new char[4]
+            {
+                ' ',
+                '\n',
+                '\r',
+                '\t'
+            };
             Process process = new Process();
             process.StartInfo = startInfo;
             process.Start();
-            string text = process.StandardOutput.ReadToEnd().Replace("UUID", string.Empty).Replace("-", string.Empty)
-                .Trim(trimChars)
-                .ToUpper();
-            // [IGNORE] Logger.Instance()?.Log(ICSEventId.ICS0117, "MachineIdentifier.GetUuid", "id: " + text, EventKind.Technical, LogLevel.Info);
+            string text = process.StandardOutput.ReadToEnd().Replace("UUID", string.Empty).Replace("-", string.Empty).Trim(trimChars).ToUpper();
+            Logger.Instance()?.Log(ICSEventId.ICS0117, "MachineIdentifier.GetUuid", "id: " + text, EventKind.Technical, LogLevel.Info);
             process.Close();
             return text;
         }
