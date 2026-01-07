cf835455fc02d492bb07ea7d391e527c5d13e002
uholeschak <ulrich@holeschak.de>
2026-01-07 01:49:35 +0100

    Comments adapted

--- Tools/Psdz/PsdzClientLibrary/Psdz/PsdzObjectBuilder.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/PsdzObjectBuilder.cs
@@ -34,11 +34,12 @@ namespace BMW.Rheingold.Psdz
         [PreserveSource(Hint = "Namespace modified")]
         private readonly PsdzClient.Programming.SoftwareSigStateEnumMapper softwareSigStateEnumMapper = new PsdzClient.Programming.SoftwareSigStateEnumMapper();
         private readonly TaCategoriesEnumMapper taCategoriesEnumMapper = new TaCategoriesEnumMapper();
-        [PreserveSource(Hint = "iPsdz added", OriginalHash = "AA09B3E8CD6FBB71B5ADFB0FD55BF1E5")]
+        [PreserveSource(Hint = "iPsdz added", SignatureModified = true)]
         public PsdzObjectBuilder(IObjectBuilderService objectBuilderService, IPsdz iPsdz)
         {
             this.objectBuilderService = objectBuilderService;
-            baureiheReader = new BaureiheReader(iPsdz); // [UH] [IGNORE] added
+            //[+] baureiheReader = new BaureiheReader(iPsdz);
+            baureiheReader = new BaureiheReader(iPsdz);
         }
 
         public IPsdzDiagAddress BuildDiagAddress(int diagAddress)
