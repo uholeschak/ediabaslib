6c2ec3b761361783530a9207fddb7c89b722b344
uholeschak <ulrich@holeschak.de>
2026-01-18 17:48:27 +0100

    Bluetooth LE update

--- EdiabasLib/EdiabasLib/EdInterfaceEnet.cs
+++ EdiabasLib/EdiabasLib/EdInterfaceEnet.cs
@@ -15,7 +15,6 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Security;
 using System.Net.Sockets;
-using System.Reflection;
 using System.Security.Authentication;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
