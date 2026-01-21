6f9927e32a5b28c79b70eccab3138a808c01c366
uholeschak <ulrich@holeschak.de>
2026-01-21 19:17:01 +0100

    Allow to update classes with removed contract

--- Tools/Psdz/PsdzClientLibrary/Psdz/PsdzBindingCalculationFailure.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/PsdzBindingCalculationFailure.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization;
+using PsdzClient;
 
 namespace BMW.Rheingold.Psdz.Model.Certificate
 {
+    [PreserveSource(AttributesModified = true)]
     [DataContract]
     public class PsdzBindingCalculationFailure
     {
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public string Url { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public int Retry { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public string Reason { get; set; }
     }
