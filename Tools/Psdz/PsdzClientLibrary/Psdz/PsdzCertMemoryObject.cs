b36c1a2ba9f5567e37bfd6636cf24f0bd30b703d
uholeschak <ulrich@holeschak.de>
2026-01-22 00:03:46 +0100

    Keep attributes

--- Tools/Psdz/PsdzClientLibrary/Psdz/PsdzCertMemoryObject.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/PsdzCertMemoryObject.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using BMW.Rheingold.Psdz.Model.Ecu;
+using PsdzClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
-using BMW.Rheingold.Psdz.Model.Ecu;
 
 namespace BMW.Rheingold.Psdz.Model.Certificate
 {
@@ -25,19 +26,24 @@ namespace BMW.Rheingold.Psdz.Model.Certificate
         VEHICLE
     }
 
+    [PreserveSource(AttributesModified = true)]
     [KnownType(typeof(PsdzEcuIdentifier))]
     [DataContract]
     public class PsdzCertMemoryObject
     {
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public IPsdzEcuIdentifier Ecu { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public string SerializedCertificate { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public PsdzCertMemoryObjectType CertMemoryObjectType { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public PsdzCertMemoryObjectSource CertMemoryObjectSource { get; set; }
     }
