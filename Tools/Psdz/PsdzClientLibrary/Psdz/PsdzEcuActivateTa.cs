f78dcd79438ab4fe923a8941c07710b5ba79664b
uholeschak <ulrich@holeschak.de>
2026-01-22 11:17:26 +0100

    Keep attributes

--- Tools/Psdz/PsdzClientLibrary/Psdz/PsdzEcuActivateTa.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/PsdzEcuActivateTa.cs
@@ -1,17 +1,22 @@
 ﻿using BMW.Rheingold.Psdz.Model.Tal;
+using PsdzClient;
 using System.Runtime.Serialization;
 
 namespace BMW.Rheingold.Psdz.Model.Tal
 {
+    [PreserveSource(AttributesModified = true)]
     [DataContract]
     public class PsdzEcuActivateTa : PsdzTa
     {
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public int EstimatedTime { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public PsdzMirrorProtocolVersionCto ProtocolVersion { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public byte[] ProgrammingToken { get; set; }
     }
