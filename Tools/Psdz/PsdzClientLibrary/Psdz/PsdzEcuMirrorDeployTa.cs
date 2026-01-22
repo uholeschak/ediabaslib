bd00961b438cda0463ef0069248bbdeb2df5978d
uholeschak <ulrich@holeschak.de>
2026-01-22 14:49:31 +0100

    Keep attributes

--- Tools/Psdz/PsdzClientLibrary/Psdz/PsdzEcuMirrorDeployTa.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/PsdzEcuMirrorDeployTa.cs
@@ -1,33 +1,42 @@
-﻿using BMW.Rheingold.Psdz.Model.Tal;
+﻿using PsdzClient;
 using System.Runtime.Serialization;
 
 namespace BMW.Rheingold.Psdz.Model.Tal
 {
+    [PreserveSource(AttributesModified = true)]
     [DataContract]
     [KnownType(typeof(PsdzProtocol))]
     public class PsdzEcuMirrorDeployTa : PsdzTa
     {
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public PsdzProtocol? ActualProtocol { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public PsdzProtocol? PreferredProtocol { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public long EstimatedExecutionTime { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public long FlashFileSize { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public PsdzMirrorProtocolVersionCto ProtocolVersion { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public byte[] ProgrammingToken { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool UseDeltaSwe { get; set; } = true;
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public string SweFlashFile { get; set; }
     }
--- Tools/Psdz/PsdzClientLibrary/Psdz/PsdzEcuPdxInfo.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/PsdzEcuPdxInfo.cs
@@ -1,79 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using PsdzClient;
 using System.Globalization;
-using System.Linq;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BMW.Rheingold.Psdz.Model.Ecu
 {
+    [PreserveSource(AttributesModified = true)]
     [DataContract]
     public class PsdzEcuPdxInfo : IPsdzEcuPdxInfo
     {
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public int CertVersion { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsCert2018 { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsCert2021 { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsCert2025 { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsCertEnabled { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsSecOcEnabled { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsSecOcMaster { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsSfaEnabled { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsIPSecEnabled { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsLcsServicePackSupported { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsLcsSystemTimeSwitchSupported { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsMirrorProtocolSupported { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsEcuAuthEnabled { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsIPsecBitmaskSupported { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public int ProgrammingProtectionLevel { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsMACsecEnabled { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool AclEnabled { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool IsSmartActuatorMaster { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool UpdateSmartActuatorConfigurationSupported { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool LcsIntegrityProtectionOCSupported { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public bool LcsIukCluster { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public int ServicePack { get; set; }
 
@@ -109,22 +128,33 @@ namespace BMW.Rheingold.Psdz.Model.Ecu
 
         public override bool Equals(object obj)
         {
-            PsdzEcuPdxInfo psdzEcuPdxInfo = obj as PsdzEcuPdxInfo;
-            if (CertVersion == psdzEcuPdxInfo.CertVersion && IsCert2018 == psdzEcuPdxInfo.IsCert2018 && IsCert2021 == psdzEcuPdxInfo.IsCert2021 && IsCertEnabled == psdzEcuPdxInfo.IsCertEnabled && IsSecOcEnabled == psdzEcuPdxInfo.IsSecOcEnabled && IsSfaEnabled == psdzEcuPdxInfo.IsSfaEnabled && IsIPSecEnabled == psdzEcuPdxInfo.IsIPSecEnabled && IsLcsServicePackSupported == psdzEcuPdxInfo.IsLcsServicePackSupported && IsLcsSystemTimeSwitchSupported == psdzEcuPdxInfo.IsLcsSystemTimeSwitchSupported && IsMirrorProtocolSupported == psdzEcuPdxInfo.IsMirrorProtocolSupported && IsEcuAuthEnabled == psdzEcuPdxInfo.IsEcuAuthEnabled && IsIPsecBitmaskSupported == psdzEcuPdxInfo.IsIPsecBitmaskSupported)
+            if (this == obj)
             {
-                return ProgrammingProtectionLevel == psdzEcuPdxInfo.ProgrammingProtectionLevel;
+                return true;
             }
+
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            PsdzEcuPdxInfo psdzEcuPdxInfo = (PsdzEcuPdxInfo)obj;
+            if (CertVersion == psdzEcuPdxInfo.CertVersion && IsCert2018 == psdzEcuPdxInfo.IsCert2018 && IsCert2021 == psdzEcuPdxInfo.IsCert2021 && IsCert2025 == psdzEcuPdxInfo.IsCert2025 && IsCertEnabled == psdzEcuPdxInfo.IsCertEnabled && IsSecOcEnabled == psdzEcuPdxInfo.IsSecOcEnabled && IsSecOcMaster == psdzEcuPdxInfo.IsSecOcMaster && IsSfaEnabled == psdzEcuPdxInfo.IsSfaEnabled && IsIPSecEnabled == psdzEcuPdxInfo.IsIPSecEnabled && IsLcsServicePackSupported == psdzEcuPdxInfo.IsLcsServicePackSupported && IsLcsSystemTimeSwitchSupported == psdzEcuPdxInfo.IsLcsSystemTimeSwitchSupported && IsMirrorProtocolSupported == psdzEcuPdxInfo.IsMirrorProtocolSupported && IsEcuAuthEnabled == psdzEcuPdxInfo.IsEcuAuthEnabled && IsIPsecBitmaskSupported == psdzEcuPdxInfo.IsIPsecBitmaskSupported && ProgrammingProtectionLevel == psdzEcuPdxInfo.ProgrammingProtectionLevel && IsMACsecEnabled == psdzEcuPdxInfo.IsMACsecEnabled && AclEnabled == psdzEcuPdxInfo.AclEnabled && IsSmartActuatorMaster == psdzEcuPdxInfo.IsSmartActuatorMaster && UpdateSmartActuatorConfigurationSupported == psdzEcuPdxInfo.UpdateSmartActuatorConfigurationSupported && LcsIntegrityProtectionOCSupported == psdzEcuPdxInfo.LcsIntegrityProtectionOCSupported && LcsIukCluster == psdzEcuPdxInfo.LcsIukCluster)
+            {
+                return ServicePack == psdzEcuPdxInfo.ServicePack;
+            }
+
             return false;
         }
 
         public override int GetHashCode()
         {
-            return ToString().GetHashCode();
+            return (CertVersion, IsCert2018, IsCert2021, IsCert2025, IsCertEnabled, IsSecOcEnabled, IsSecOcMaster, IsSfaEnabled, IsIPSecEnabled, IsLcsServicePackSupported, IsLcsSystemTimeSwitchSupported, IsMirrorProtocolSupported, IsEcuAuthEnabled, IsIPsecBitmaskSupported, ProgrammingProtectionLevel, IsMACsecEnabled, AclEnabled, IsSmartActuatorMaster, UpdateSmartActuatorConfigurationSupported, LcsIntegrityProtectionOCSupported, LcsIukCluster, ServicePack).GetHashCode();
         }
 
         public override string ToString()
         {
-            return string.Format(CultureInfo.InvariantCulture, "{0}:{1}:{2}:{3}:{4}:{5}:{6}:{7}:{8}", CertVersion, IsCert2018, IsCert2021, IsCertEnabled, IsSecOcEnabled, IsSfaEnabled, IsMirrorProtocolSupported, IsIPsecBitmaskSupported, ProgrammingProtectionLevel);
+            return string.Format(CultureInfo.InvariantCulture, "{0}:{1}:{2}:{3}:{4}:{5}:{6}:{7}:{8}:{9}:{10}:{11}:{12}:{13}:{14}:{15}:{16}:{17}:{18}:{19}:{20}:{21}", CertVersion, IsCert2018, IsCert2021, IsCert2025, IsCertEnabled, IsSecOcEnabled, IsSecOcMaster, IsSfaEnabled, IsIPSecEnabled, IsLcsServicePackSupported, IsLcsSystemTimeSwitchSupported, IsMirrorProtocolSupported, IsEcuAuthEnabled, IsIPsecBitmaskSupported, ProgrammingProtectionLevel, IsMACsecEnabled, AclEnabled, IsSmartActuatorMaster, UpdateSmartActuatorConfigurationSupported, LcsIntegrityProtectionOCSupported, LcsIukCluster, ServicePack);
         }
     }
 }
