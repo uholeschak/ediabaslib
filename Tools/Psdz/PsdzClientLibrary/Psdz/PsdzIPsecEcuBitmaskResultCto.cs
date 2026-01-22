d5bd75ed20d82394dae8f98b9e5bf88a1d1f4094
uholeschak <ulrich@holeschak.de>
2026-01-22 21:00:33 +0100

    Keep attributes

--- Tools/Psdz/PsdzClientLibrary/Psdz/PsdzIPsecEcuBitmaskResultCto.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/PsdzIPsecEcuBitmaskResultCto.cs
@@ -1,18 +1,22 @@
 ﻿using BMW.Rheingold.Psdz.Model.Ecu;
 using BMW.Rheingold.Psdz.Model.Sfa;
+using PsdzClient;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 
 namespace BMW.Rheingold.Psdz
 {
+    [PreserveSource(AttributesModified = true)]
     [DataContract]
     [KnownType(typeof(PsdzEcuIdentifier))]
     [KnownType(typeof(PsdzEcuFailureResponseCto))]
     public class PsdzIPsecEcuBitmaskResultCto : IPsdzIPsecEcuBitmaskResultCto
     {
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public IDictionary<IPsdzEcuIdentifier, byte[]> SuccessEcus { get; set; }
 
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public IEnumerable<IPsdzEcuFailureResponseCto> FailedEcus { get; set; }
     }
--- Tools/Psdz/PsdzClientLibrary/Psdz/PsdzIdRestoreTa.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/PsdzIdRestoreTa.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using PsdzClient;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BMW.Rheingold.Psdz.Model.Tal
 {
+    [PreserveSource(AttributesModified = true)]
     [DataContract]
     public class PsdzIdRestoreTa : PsdzTa
     {
+        [PreserveSource(KeepAttribute = true)]
         [DataMember]
         public string BackupFile { get; set; }
     }
