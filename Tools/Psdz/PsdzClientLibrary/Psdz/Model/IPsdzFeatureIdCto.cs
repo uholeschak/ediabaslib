33c21153de87f1c4112ff3232513b32adf9e3594
uholeschak <ulrich@holeschak.de>
2026-01-30 12:32:50 +0100

    Moved models

---
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Model/IPsdzFeatureIdCto.cs
@@ -0,0 +1,7 @@
+﻿namespace BMW.Rheingold.Psdz.Model.Sfa
+{
+    public interface IPsdzFeatureIdCto
+    {
+        long Value { get; set; }
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Model/IPsdzFeatureSpecificFieldCto.cs
@@ -0,0 +1,9 @@
+﻿namespace BMW.Rheingold.Psdz.Model.Sfa
+{
+    public interface IPsdzFeatureSpecificFieldCto
+    {
+        int FieldType { get; set; }
+
+        string FieldValue { get; set; }
+    }
+}
