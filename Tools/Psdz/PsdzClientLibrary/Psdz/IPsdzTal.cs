7d36dd1fbdc67edc2942ab9826120a993537f02a
uholeschak <ulrich@holeschak.de>
2026-01-30 17:31:37 +0100

    Fixed namespaces

--- Tools/Psdz/PsdzClientLibrary/Psdz/IPsdzTal.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/IPsdzTal.cs
@@ -1,26 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using BMW.Rheingold.Psdz.Model.Ecu;
 
 namespace BMW.Rheingold.Psdz.Model.Tal
 {
-    public enum PsdzTalExecutionState
-    {
-        AbortedByError,
-        AbortedByUser,
-        Executable,
-        Finished,
-        FinishedForHardwareTransactions,
-        FinishedForHardwareTransactionsWithError,
-        FinishedForHardwareTransactionsWithWarnings,
-        FinishedWithError,
-        FinishedWithWarnings,
-        Running
-    }
-
     public interface IPsdzTal : IPsdzTalElement
     {
         IEnumerable<IPsdzEcuIdentifier> AffectedEcus { get; }
---
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Model/PsdzTalExecutionState.cs
@@ -0,0 +1,16 @@
+﻿namespace BMW.Rheingold.Psdz.Model.Tal
+{
+    public enum PsdzTalExecutionState
+    {
+        AbortedByError,
+        AbortedByUser,
+        Executable,
+        Finished,
+        FinishedForHardwareTransactions,
+        FinishedForHardwareTransactionsWithError,
+        FinishedForHardwareTransactionsWithWarnings,
+        FinishedWithError,
+        FinishedWithWarnings,
+        Running
+    }
+}
