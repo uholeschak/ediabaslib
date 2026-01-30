8c0e45735e1a979414dfa2e6848fada86d9c31d1
uholeschak <ulrich@holeschak.de>
2026-01-30 12:20:57 +0100

    Moved models

---
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Model/IPsdzCalculatedNcdsEto.cs
@@ -0,0 +1,11 @@
+﻿namespace BMW.Rheingold.Psdz.Model.SecureCoding
+{
+    public interface IPsdzCalculatedNcdsEto
+    {
+        string Btld { get; }
+
+        IPsdzSgbmId CafdId { get; }
+
+        IPsdzNcd Ncd { get; }
+    }
+}
