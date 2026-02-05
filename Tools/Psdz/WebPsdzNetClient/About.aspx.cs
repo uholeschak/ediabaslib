2b12a45d41d51484c73b6aceb321093c4cbe3cf3
uholeschak <ulrich@holeschak.de>
2026-02-06 00:55:25 +0100

    Added updates

--- Tools/Psdz/WebPsdzNetClient/About.aspx.cs
+++ Tools/Psdz/WebPsdzNetClient/About.aspx.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using WebPsdzClient.App_Data;
 
 namespace WebPsdzClient
