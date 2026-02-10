83935039b61815ad08228c75e31fdc75868cbc5a
uholeschak <ulrich@holeschak.de>
2026-02-10 16:47:49 +0100

    Start server

--- Tools/Psdz/PsdzClientServer/Program.cs
+++ Tools/Psdz/PsdzClientServer/Program.cs
@@ -6,7 +6,30 @@ namespace PsdzClientServer
     {
         static int Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PsdzJsonRpcServer server = new PsdzJsonRpcServer();
+
+            try
+            {
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    server.Stop();
+                };
+
+
+                Console.WriteLine("Starting PsdzJsonRpcServer...");
+                server.StartAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error starting PsdzJsonRpcServer: {ex.Message}");
+            }
+            finally
+            {
+                server.Stop();
+            }
+
+            Console.WriteLine("PsdzJsonRpcServer stopped.");
             return 0;
         }
     }
