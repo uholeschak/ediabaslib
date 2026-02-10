3b683d69d906c8c8b22f11e0a10af75c2a9ddc69
uholeschak <ulrich@holeschak.de>
2026-02-10 12:53:34 +0100

    Frame for RPC server

---
+++ Tools/Psdz/PsdzClientServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PsdzClientServer
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            return 0;
+        }
+    }
+}
