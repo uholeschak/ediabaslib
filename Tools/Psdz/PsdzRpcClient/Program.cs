423d7fdd328fa122f272d3f2508bcf5deaa4052d
uholeschak <ulrich@holeschak.de>
2026-02-12 00:01:57 +0100

    Prepared client

--- Tools/Psdz/PsdzRpcClient/Program.cs
+++ Tools/Psdz/PsdzRpcClient/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace PsdzRpcClient
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            await using var client = new PsdzRpcClient();
+            await client.ConnectAsync();
+
+            if (client.RpcService != null)
+            {
+                bool resultConnect = await client.RpcService.Connect("Connect");
+                Console.WriteLine($"Connect = {resultConnect}");
+
+                bool resultDisconnect = await client.RpcService.Disconnect("Disconnect");
+                Console.WriteLine($"Disconnect = {resultDisconnect}");
+            }
+
+            return  0;
         }
     }
 }
---
+++ Tools/Psdz/PsdzRpcClient/PsdzRpcClient.cs
@@ -0,0 +1,45 @@
+﻿using PsdzRpcServer.Shared;
+using StreamJsonRpc;
+using System;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PsdzRpcClient;
+
+public class PsdzRpcClient : IAsyncDisposable
+{
+    private const string PipeName = "MyJsonRpcPipe";
+    private NamedPipeClientStream _pipeClient;
+    private JsonRpc _jsonRpc;
+
+    public IPsdzRpcService RpcService { get; private set; }
+
+    public async Task ConnectAsync(string serverName = ".", CancellationToken ct = default)
+    {
+        _pipeClient = new NamedPipeClientStream(
+            serverName,
+            PipeName,
+            PipeDirection.InOut,
+            PipeOptions.Asynchronous);
+
+        Console.WriteLine($"Connecting with server...");
+        await _pipeClient.ConnectAsync(ct);
+        Console.WriteLine("Connected!");
+
+        _jsonRpc = JsonRpc.Attach(_pipeClient);
+        RpcService = _jsonRpc.Attach<IPsdzRpcService>();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_jsonRpc != null)
+        {
+            _jsonRpc.Dispose();
+        }
+        if (_pipeClient != null)
+        {
+            await _pipeClient.DisposeAsync();
+        }
+    }
+}
