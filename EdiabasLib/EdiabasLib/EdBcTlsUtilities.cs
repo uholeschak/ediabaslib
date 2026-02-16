1f16c7f873144f6753d7d8702ed37220de488b7a
uholeschak <ulrich@holeschak.de>
2026-02-16 16:53:36 +0100

    Use central function

--- EdiabasLib/EdiabasLib/EdBcTlsUtilities.cs
+++ EdiabasLib/EdiabasLib/EdBcTlsUtilities.cs
@@ -22,7 +22,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace EdiabasLib
--- Tools/S29CertGenerator/MainForm.cs
+++ Tools/S29CertGenerator/MainForm.cs
@@ -1,7 +1,6 @@
 using EdiabasLib;
 using Microsoft.Win32;
 using Newtonsoft.Json;
-using Org.BouncyCastle.Asn1.Sec;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Pkcs;
 using Org.BouncyCastle.X509;
