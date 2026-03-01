3b7891fa16c6320215def78da4f4b330f3bd52bf
uholeschak <ulrich@holeschak.de>
2026-03-01 21:29:55 +0100

    Code patched

--- Tools/Psdz/PsdzClientLibrary/Utility/TimeMetricsUtility.cs
+++ Tools/Psdz/PsdzClientLibrary/Utility/TimeMetricsUtility.cs
@@ -3,20 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
-#pragma warning disable CS0169, CS0414
+#pragma warning disable CS0169, CS0414, CS0649
 namespace PsdzClient.Utility
 {
     public class TimeMetricsUtility
     {
         private static Lazy<TimeMetricsUtility> instance = new Lazy<TimeMetricsUtility>(() => new TimeMetricsUtility(), isThreadSafe: true);
         [PreserveSource(Hint = "TimeMetrics", Placeholder = true)]
-        private static PlaceholderType currentMetrics;
+        private static PlaceholderType? currentMetrics;
         private bool metricsEnabled;
         private bool apiEnabled;
         private int fastaApiDetailsCount;
         [PreserveSource(Hint = "List<TimeMetrics>", Placeholder = true)]
         private static PlaceholderType metrics = new PlaceholderType();
         public static TimeMetricsUtility Instance => instance.Value;
+        public static bool HasMetrics => currentMetrics != null;
 
         private TimeMetricsUtility()
         {
@@ -183,6 +184,13 @@ namespace PsdzClient.Utility
         public string GetCurrentlyRunningModuleName()
         {
             return "ISTA";
+        }
+
+        public TimeSpan GetVehicleIdentTestDuration(TimeMetricsStage stage)
+        {
+            //[-]return metrics.Find((TimeMetrics x) => x.Stage == stage)?.Watch.Elapsed ?? TimeSpan.Zero;
+            //[+]return TimeSpan.Zero;
+            return TimeSpan.Zero;
         }
     }
 }
