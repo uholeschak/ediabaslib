8ed9214fd7f85d281fe5503cd796028948438db7
uholeschak <ulrich@holeschak.de>
2026-03-01 19:08:18 +0100

    Automatic code update

--- Tools/Psdz/PsdzClientLibrary/Core/IInteractionService.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/IInteractionService.cs
@@ -7,28 +7,22 @@ namespace PsdzClient.Core
     public interface IInteractionService
     {
         void Deregister(InteractionModel model);
-
         [PreserveSource(Hint = "IInteractionProgressModel", Placeholder = true)]
         void DeregisterBackground();
-
         void Register(InteractionModel model);
-
-        Task<TResponse> RegisterAsync<TResponse>(InteractionRequestModel<TResponse> model) where TResponse : InteractionResponse;
-
+        Task<TResponse> RegisterAsync<TResponse>(InteractionRequestModel<TResponse> model)
+            where TResponse : InteractionResponse;
+        TResponse RegisterSync<TResponse>(InteractionRequestModel<TResponse> model)
+            where TResponse : InteractionResponse;
         [PreserveSource(Hint = "IInteractionProgressModel", Placeholder = true)]
         void RegisterBackground();
-
         [PreserveSource(Hint = "InteractionMessageModel", Placeholder = true)]
         PlaceholderType RegisterMessage(string title, string message, string details = "", int dialogSize = 0);
-
         [PreserveSource(Hint = "InteractionMessageModel", Placeholder = true)]
         PlaceholderType RegisterMessage(IList<LocalizedText> titleList, IList<LocalizedText> msgList, string details = "", int dialogSize = 0);
-
         Task<InteractionButtonResponse> RegisterMessageAsync(string title, string message, string details = "", int dialogSize = 0);
-
         [PreserveSource(Hint = "InteractionMessageModel", Placeholder = true)]
         PlaceholderType RegisterQuestion(string title, string message);
-
         [PreserveSource(Hint = "InteractionQuestionModel", Placeholder = true)]
         PlaceholderType RegisterQuestion(IList<LocalizedText> titleList, IList<LocalizedText> msgList);
     }
