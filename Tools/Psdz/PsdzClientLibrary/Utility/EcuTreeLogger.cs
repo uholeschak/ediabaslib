5efed23fe64fc1723c7aeb1161dde73a7307d155
uholeschak <ulrich@holeschak.de>
2026-03-02 21:13:04 +0100

    Added missing class

---
+++ Tools/Psdz/PsdzClientLibrary/Utility/EcuTreeLogger.cs
@@ -0,0 +1,16 @@
+﻿using PsdzClient.Core;
+
+namespace PsdzClient.Utility
+{
+    internal static class EcuTreeLogger
+    {
+        private static ILogger instance;
+
+        internal static ILogger Instance => instance;
+
+        internal static void Initialize(ILogger logger)
+        {
+            instance = logger;
+        }
+    }
+}
