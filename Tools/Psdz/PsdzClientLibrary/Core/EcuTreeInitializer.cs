2b51b30cb2315a6541a0b7a687d43d5b8777a728
uholeschak <ulrich@holeschak.de>
2026-03-02 21:59:53 +0100

    Added init class

---
+++ Tools/Psdz/PsdzClientLibrary/Core/EcuTreeInitializer.cs
@@ -0,0 +1,16 @@
+﻿using PsdzClient.Utility;
+using System;
+using System.Collections.Generic;
+using static PsdzClient.PsdzDatabase;
+
+namespace PsdzClient.Core
+{
+    public static class EcuTreeInitializer
+    {
+        public static void Initialize(Func<IEcuTreeVehicle, ICollection<BordnetsData>> getBordnetFromDatabase, ILogger logger, Action<string> logMissingBordnet = null)
+        {
+            EcuTreeLogger.Initialize(logger);
+            VehicleLogistics.Initialize(getBordnetFromDatabase, logMissingBordnet);
+        }
+    }
+}
