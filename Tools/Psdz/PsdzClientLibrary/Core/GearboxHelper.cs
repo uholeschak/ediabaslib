489f1b083b66c495a5fee8184baf23c487e7c237
uholeschak <ulrich@holeschak.de>
2026-03-02 21:02:47 +0100

    Added missing class

---
+++ Tools/Psdz/PsdzClientLibrary/Core/GearboxHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PsdzClient.Core
+{
+    public static class GearboxHelper
+    {
+        public const string Automatic = "AUT";
+
+        public static bool HasVehicleGearboxECU(string motor, string getriebe, Func<string, bool> hasSa)
+        {
+            if ("W10".Equals(motor, StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+            if ("AUT".Equals(getriebe, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            if (hasSa("205") || hasSa("206") || hasSa("2TB") || hasSa("2TC") || hasSa("2MK"))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
