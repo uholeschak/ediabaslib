48e7e7e6543509d652d6e6847c3c29e02d51afa9
uholeschak <ulrich@holeschak.de>
2026-03-02 01:39:03 +0100

    Patch update

--- Tools/Psdz/PsdzClientLibrary/Psdz/BaureiheReader.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/BaureiheReader.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using BMW.Rheingold.Psdz;
-using PsdzClient;
+﻿using PsdzClient;
 using PsdzClient.Core;
-using PsdzClient.Programming;
 using PsdzClient.Utility;
+using System;
 
 namespace BMW.Rheingold.Psdz
 {
     public class BaureiheReader
     {
-        private readonly IPsdz psdz;
-
-        [PreserveSource(Hint = "iPsdz added", SignatureModified = true)]
-        public BaureiheReader(IPsdz iPsdz)
-        {
-            //[-] psdz = StartPsdzWebService();
-            //[+] psdz = iPsdz;
-            psdz = iPsdz;
-        }
-
         public string GetBaureiheFormatted(string baureihe)
         {
             try
@@ -27,9 +15,12 @@ namespace BMW.Rheingold.Psdz
                 {
                     if (istaIcsServiceClient.IsAvailable() && istaIcsServiceClient.GetFeatureEnabledStatus("UsePsdzSeriesFormatter", istaIcsServiceClient.IsAvailable()).IsActive)
                     {
+                        //[-]return new PsdzWebServiceWrapper().BaureiheUtilityService?.GetBaureihe(baureihe) ?? FormatConverter.ConvertToBn2020ConformModelSeries(baureihe);
+                        //[+]return psdz?.BaureiheUtilityService?.GetBaureihe(baureihe) ?? FormatConverter.ConvertToBn2020ConformModelSeries(baureihe);
                         return psdz?.BaureiheUtilityService?.GetBaureihe(baureihe) ?? FormatConverter.ConvertToBn2020ConformModelSeries(baureihe);
                     }
                 }
+
                 return FormatConverter.ConvertToBn2020ConformModelSeries(baureihe);
             }
             catch (Exception exception)
@@ -37,6 +28,15 @@ namespace BMW.Rheingold.Psdz
                 Log.ErrorException("Baureihereader logged exception while reading baureihe from psdz.", exception);
                 return FormatConverter.ConvertToBn2020ConformModelSeries(baureihe);
             }
+        }
+
+        [PreserveSource(Hint = "psdz added")]
+        private readonly IPsdz psdz;
+        [PreserveSource(Hint = "iPsdz added", SignatureModified = true)]
+        public BaureiheReader(IPsdz iPsdz)
+        {
+            //[+] psdz = iPsdz;
+            psdz = iPsdz;
         }
 
         [PreserveSource(Cleaned = true, OriginalHash = "017EC1F95167348AF627A728304BD3E0")]
