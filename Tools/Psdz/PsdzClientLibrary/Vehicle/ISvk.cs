2857b41f0921466d857b314377d160a16b38a0df
uholeschak <ulrich@holeschak.de>
2026-03-03 00:21:40 +0100

    Interface update

--- Tools/Psdz/PsdzClientLibrary/Vehicle/ISvk.cs
+++ Tools/Psdz/PsdzClientLibrary/Vehicle/ISvk.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using PsdzClient.Core;
 
 namespace BMW.Rheingold.CoreFramework.Contracts.Vehicle
 {
     [AuthorAPI(SelectableTypeDeclaration = true)]
-    public interface ISvk : INotifyPropertyChanged
+    public interface ISvk : INotifyPropertyChanged, IEcuTreeSvk
     {
         string PROG_DATUM { get; }
 
         long? PROG_KM { get; }
 
         int? PROG_TEST { get; }
-
-        IEnumerable<string> XWE_SGBMID { get; }
+        new IEnumerable<string> XWE_SGBMID { get; }
 
         ICollection<int> XWE_PROZESSKLASSE_WERT { get; }
     }
