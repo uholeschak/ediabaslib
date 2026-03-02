8d621a73a80b2921d0853368a8db6efe113912d2
uholeschak <ulrich@holeschak.de>
2026-03-02 19:48:34 +0100

    Remove invalid get_ methods

--- Tools/Psdz/PsdzClientLibrary/Core/BasicFeaturesVci.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/BasicFeaturesVci.cs
@@ -3,8 +3,6 @@ using System.ComponentModel;
 
 namespace PsdzClient.Core
 {
-    // ToDo: Check on update
-    [PreserveSource(Hint = "Class incorrectly decompiled", SuppressWarning = true)]
     public class BasicFeaturesVci : typeBasicFeatures, IBasicFeatures, INotifyPropertyChanged
     {
     }
