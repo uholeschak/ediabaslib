86618c46eee422370624e27267c009de11fe23e9
uholeschak <ulrich@holeschak.de>
2026-03-02 01:51:45 +0100

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Service/IPsdzWebservicePreflightChecker.cs
@@ -0,0 +1,7 @@
+﻿namespace BMW.Rheingold.Psdz
+{
+    internal interface IPsdzWebservicePreflightChecker
+    {
+        void Execute(int sessionWebservicePort, string jarPath, string javaExePath);
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Service/PsdzWebserviceStartFailureReason.cs
@@ -0,0 +1,18 @@
+﻿namespace BMW.Rheingold.Psdz
+{
+    public enum PsdzWebserviceStartFailureReason
+    {
+        None,
+        PortInUse,
+        JarMissingOrCorrupt,
+        JavaRuntimeFaulty,
+        SpringBootStartupError,
+        Timeout,
+        AccessDenied,
+        MissingRights,
+        SdpFaulty,
+        JavaRuntimeInstallationFaulty,
+        JavaVersionError,
+        Unknown
+    }
+}
