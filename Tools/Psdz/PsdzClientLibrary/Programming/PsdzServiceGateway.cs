a1c0bc910df60013f61fad991a5b528f3be23c22
uholeschak <ulrich@holeschak.de>
2026-03-02 21:57:29 +0100

    Code update

--- Tools/Psdz/PsdzClientLibrary/Programming/PsdzServiceGateway.cs
+++ Tools/Psdz/PsdzClientLibrary/Programming/PsdzServiceGateway.cs
@@ -14,7 +14,7 @@ namespace PsdzClient.Programming
         private readonly PsdzWebServiceWrapper _psdzWebServiceWrapper;
         public static Type PsdzServiceType { get; set; }
 
-        [PreserveSource(Hint = "Added service host", SuppressWarning = true, OriginalHash = "C60FDDC78BCC55F8280E56E617864EA9")]
+        [PreserveSource(Hint = "Added service host", SuppressWarning = true)]
         public IPsdz Psdz
         {
             get
@@ -29,7 +29,7 @@ namespace PsdzClient.Programming
             }
         }
 
-        [PreserveSource(Hint = "Added service host", SuppressWarning = true, OriginalHash = "FAEBB11719E72CAFA7CE7407E9C005F5")]
+        [PreserveSource(Hint = "Added service host", SuppressWarning = true)]
         public string PsdzWebServiceLogFilePath
         {
             get
@@ -44,7 +44,7 @@ namespace PsdzClient.Programming
             }
         }
 
-        [PreserveSource(Hint = "Added service host", SuppressWarning = true, OriginalHash = "9FA19EABF011B967C0DB531E9CEA9906")]
+        [PreserveSource(Hint = "Added service host", SuppressWarning = true)]
         public string PsdzLogFilePath
         {
             get
