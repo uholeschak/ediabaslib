7343ace0b80431493b0f9f3fdc16f07313bab231
uholeschak <ulrich@holeschak.de>
2026-03-17 13:06:01 +0100

    Added option item

---
+++ Tools/Psdz/PsdzRpcServer/Shared/PsdzRpcOptionItem.cs
@@ -0,0 +1,23 @@
+﻿using PsdzClient;
+
+namespace PsdzRpcServer.Shared
+{
+    public class PsdzRpcOptionItem
+    {
+        public PsdzRpcOptionItem(PsdzDatabase.SwiRegisterEnum swiRegisterEnum, string caption, bool enabled, bool selected = false)
+        {
+            SwiRegisterEnum = swiRegisterEnum;
+            Caption = caption;
+            Enabled = enabled;
+            Selected = selected;
+        }
+
+        public PsdzDatabase.SwiRegisterEnum SwiRegisterEnum { get; private set; }
+
+        public string Caption { get; private set; }
+
+        public bool Enabled { get; set; }
+
+        public bool Selected { get; set; }
+    }
+}
