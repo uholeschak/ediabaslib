63f69b3d23c357fa53ddf8760406b5aaf59cfbe1
uholeschak <ulrich@holeschak.de>
2026-04-26 16:22:42 +0200

    Bind event

--- Tools/Psdz/PsdzRpcClient/Program.cs
+++ Tools/Psdz/PsdzRpcClient/Program.cs
@@ -298,6 +298,17 @@ namespace PsdzRpcClient
                     });
                 };
 
+                client.CallbackHandler.ReportError += (sender, s) => 
+                {
+                    syncContext.BeginInvoke(() =>
+                    {
+                        if (_verbosity >= Options.VerbosityOption.Error)
+                        {
+                            Console.WriteLine($"Error report: {s}");
+                        }
+                    });
+                };
+
                 client.CallbackHandler.TelSendQueueSize += (sender, queueArgs) =>
                 {
                     queueArgs.Result = -1; // Simulate no queue
