70cb377e519c6e17293d507153cce3090c0aec24
uholeschak <ulrich@holeschak.de>
2026-05-05 17:49:12 +0200

    Start server script

---
+++ Tools/Psdz/Scripts/StartJsonRpcTcp.bat
@@ -0,0 +1,16 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+set "BATPATH=%~dp0"
+set "RPC_SERVER_BIN=!BATPATH!..\PsdzRpcServer\artifacts\bin\PsdzRpcServer\debug_net10.0-windows10.0.26100.0\PsdzRpcServer.exe"
+
+echo PsdzRpcServer: "!RPC_SERVER_BIN!"
+if NOT EXIST "!RPC_SERVER_BIN!" (
+    echo Server not found: !RPC_SERVER_BIN!
+    exit /b 1
+)
+
+"!RPC_SERVER_BIN!" -p 0 || EXIT /b 1
+
+timeout /T 2
+exit /b 0
