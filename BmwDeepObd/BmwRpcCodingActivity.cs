f1778c8ef036e96b46c32e03cfefb37e83d12150
uholeschak <ulrich@holeschak.de>
2026-05-20 23:40:50 +0200

    Adapted remote info

--- BmwDeepObd/BmwRpcCodingActivity.cs
+++ BmwDeepObd/BmwRpcCodingActivity.cs
@@ -36,8 +36,8 @@ namespace BmwDeepObd
         {
             public InstanceData()
             {
-                CodingUrl = string.Empty;
-                CodingUrlTest = string.Empty;
+                CodingRpcUrl = string.Empty;
+                CodingRpcUrlTest = string.Empty;
                 DayString = string.Empty;
                 ValidSerial = string.Empty;
                 Url = string.Empty;
@@ -48,8 +48,8 @@ namespace BmwDeepObd
                 CommErrorsOccurred = false;
             }
 
-            public string CodingUrl { get; set; }
-            public string CodingUrlTest { get; set; }
+            public string CodingRpcUrl { get; set; }
+            public string CodingRpcUrlTest { get; set; }
             public string DayString { get; set; }
             public string ValidSerial { get; set; }
             public string Vin { get; set; }
@@ -373,6 +373,11 @@ namespace BmwDeepObd
                 if (pos >= 0 && pos < _spinnerOptionTypeAdapter.Items.Count)
                 {
                     selectedSwiRegister = _spinnerOptionTypeAdapter.Items[pos].Data as PsdzRpcSwiRegisterEnum?;
+                }
+
+                if (selectedSwiRegister == null)
+                {
+                    return;
                 }
 
                 lock (_statusLock)
@@ -741,7 +746,7 @@ namespace BmwDeepObd
                 return true;
             }
 
-            if (!string.IsNullOrEmpty(_instanceData.CodingUrl))
+            if (!string.IsNullOrEmpty(_instanceData.CodingRpcUrl))
             {
                 return true;
             }
@@ -777,8 +782,8 @@ namespace BmwDeepObd
                                 {
                                     lock (_instanceLock)
                                     {
-                                        _instanceData.CodingUrl = url;
-                                        _instanceData.CodingUrlTest = urlTest;
+                                        _instanceData.CodingRpcUrl = url;
+                                        _instanceData.CodingRpcUrlTest = urlTest;
                                         _instanceData.DayString = dayString;
                                         _instanceData.ValidSerial = validSerial;
                                     }
@@ -1015,14 +1020,14 @@ namespace BmwDeepObd
                 XElement infoNode = xmlDoc.Root.Element("info");
                 if (infoNode != null)
                 {
-                    XAttribute urlAttr = infoNode.Attribute("url");
+                    XAttribute urlAttr = infoNode.Attribute("rpc_url");
                     if (urlAttr != null && !string.IsNullOrEmpty(urlAttr.Value))
                     {
                         codingUrl = urlAttr.Value;
                         success = true;
                     }
 
-                    XAttribute urlTestAttr = infoNode.Attribute("url_test");
+                    XAttribute urlTestAttr = infoNode.Attribute("rpc_url_test");
                     if (urlTestAttr != null && !string.IsNullOrEmpty(urlTestAttr.Value))
                     {
                         codingUrlTest = urlTestAttr.Value;
@@ -1300,45 +1305,58 @@ namespace BmwDeepObd
 
             _layoutCodingOptions.Visibility = statusInfo.HasOptionsDict && statusOptionTypes != null && statusOptionTypes.Count > 0 ? ViewStates.Visible : ViewStates.Gone;
 
-            _ignoreItemSelection = true;
-            int optionSelPos = _spinnerOptionType.SelectedItemPosition;
-            _spinnerOptionTypeAdapter.Items.Clear();
-            if (statusOptionTypes != null)
+            try
             {
-                foreach (PsdzRpcOptionType optionType in statusOptionTypes)
+                _ignoreItemSelection = true;
+                int optionSelPos = _spinnerOptionType.SelectedItemPosition;
+                _spinnerOptionTypeAdapter.Items.Clear();
+                if (statusOptionTypes != null)
                 {
-                    _spinnerOptionTypeAdapter.Items.Add(new StringObjType(optionType.Caption, optionType.SwiRegisterEnum));
-                    if (selectedSwiRegister != null && selectedSwiRegister.Equals(optionType.SwiRegisterEnum))
+                    foreach (PsdzRpcOptionType optionType in statusOptionTypes)
                     {
-                        optionSelPos = _spinnerOptionTypeAdapter.Items.Count - 1;
+                        _spinnerOptionTypeAdapter.Items.Add(new StringObjType(optionType.Caption, optionType.SwiRegisterEnum));
+                        if (selectedSwiRegister != null && selectedSwiRegister.Equals(optionType.SwiRegisterEnum))
+                        {
+                            optionSelPos = _spinnerOptionTypeAdapter.Items.Count - 1;
+                        }
                     }
                 }
-            }
 
-            _spinnerOptionTypeAdapter.NotifyDataSetChanged();
-            if (optionSelPos < 0)
-            {
-                optionSelPos = 0;
-            }
-            _spinnerOptionType.SetSelection(optionSelPos);
-            _ignoreItemSelection = false;
-
-            _ignoreItemSelection = true;
-            _listViewOptionsAdapter.Items.Clear();
-            if (rpcListItems != null)
-            {
-                foreach (PsdzRpcOptionItem optionItem in rpcListItems)
+                _spinnerOptionTypeAdapter.NotifyDataSetChanged();
+                if (optionSelPos < 0)
                 {
-                    TableResultItem resultItem = new TableResultItem(optionItem.Caption, null, optionItem.Id, true, optionItem.Selected)
-                    {
-                        CheckEnable = optionItem.Enabled
-                    };
-                    _listViewOptionsAdapter.Items.Add(resultItem);
+                    optionSelPos = 0;
                 }
+                _spinnerOptionType.SetSelection(optionSelPos);
+            }
+            finally
+            {
+                _ignoreItemSelection = false;
             }
 
-            _listViewOptionsAdapter.NotifyDataSetChanged();
-            _ignoreItemSelection = false;
+            try
+            {
+                _ignoreItemSelection = true;
+                _listViewOptionsAdapter.Items.Clear();
+                if (rpcListItems != null)
+                {
+                    foreach (PsdzRpcOptionItem optionItem in rpcListItems)
+                    {
+                        TableResultItem resultItem = new TableResultItem(optionItem.Caption, null, optionItem.Id, true, optionItem.Selected)
+                        {
+                            CheckEnable = optionItem.Enabled
+                        };
+                        _listViewOptionsAdapter.Items.Add(resultItem);
+                    }
+                }
+
+                _listViewOptionsAdapter.NotifyDataSetChanged();
+                AndroidUtility.SetListViewHeightBasedOnChildren(_listViewOptions);
+            }
+            finally
+            {
+                _ignoreItemSelection = false;
+            }
 
             _textCodingStatus.Text = statusMessage ?? string.Empty;
 
@@ -1812,7 +1830,7 @@ namespace BmwDeepObd
             {
                 lock (_instanceLock)
                 {
-                    if (string.IsNullOrEmpty(_instanceData.CodingUrl))
+                    if (string.IsNullOrEmpty(_instanceData.CodingRpcUrl))
                     {
                         return false;
                     }
@@ -1888,21 +1906,20 @@ namespace BmwDeepObd
                         string url;
                         if (!string.IsNullOrEmpty(domains) && domains.Contains("local.holeschak.de", StringComparison.OrdinalIgnoreCase))
                         {
-                            if (!string.IsNullOrEmpty(_instanceData.CodingUrlTest))
+                            if (!string.IsNullOrEmpty(_instanceData.CodingRpcUrlTest))
                             {
-                                url = _instanceData.CodingUrlTest;
+                                url = _instanceData.CodingRpcUrlTest;
                             }
                             else
                             {
-                                url = @"http://vm-ista.local.holeschak.de:8000/";
+                                url = @"vm-ista.local.holeschak.de:" + PsdzRpcServiceConstants.DefaultTcpPort;
                             }
                         }
                         else
                         {
-                            url = _instanceData.CodingUrl;
+                            url = _instanceData.CodingRpcUrl;
                         }
 
-                        url = @"http://vm-ista.local.holeschak.de:8000/";
                         _instanceData.Url = url;
                     }
 
