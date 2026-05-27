7c8f2fb45e96349856b8e61b03c63674cf188e82
uholeschak <ulrich@holeschak.de>
2026-05-27 11:52:48 +0200

    Code moved

--- Tools/Psdz/PsdzRpcServer/Program.cs
+++ Tools/Psdz/PsdzRpcServer/Program.cs
@@ -11,26 +11,6 @@ namespace PsdzRpcServer
 {
     internal class Program
     {
-        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
-        static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
-
-        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
-        static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
-
-        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
-        static extern IntPtr GetStdHandle(int nStdHandle);
-
-        const uint ENABLE_QUICK_EDIT = 0x0040;
-        static void DisableQuickEdit()
-        {
-            IntPtr handle = GetStdHandle(-10); // STD_INPUT_HANDLE
-            if (GetConsoleMode(handle, out uint mode))
-            {
-                mode &= ~ENABLE_QUICK_EDIT; // ENABLE_QUICK_EDIT_MODE entfernen
-                SetConsoleMode(handle, mode);
-            }
-        }
-
         public class Options
         {
             public Options()
@@ -71,7 +51,7 @@ namespace PsdzRpcServer
 #if NET
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 #endif
-            DisableQuickEdit();
+            PsdzRpcTools.DisableQuickEdit();
 
             bool keepRunning = false;
             int? tcpPort = null;
---
+++ Tools/Psdz/PsdzRpcServer/Shared/PsdzRpcTools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PsdzRpcServer.Shared
+{
+    public static class PsdzRpcTools
+    {
+#if WINDOWS || NETFRAMEWORK
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
+
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetStdHandle(int nStdHandle);
+
+        const uint ENABLE_QUICK_EDIT = 0x0040;
+        public static void DisableQuickEdit()
+        {
+            IntPtr handle = GetStdHandle(-10); // STD_INPUT_HANDLE
+            if (GetConsoleMode(handle, out uint mode))
+            {
+                mode &= ~ENABLE_QUICK_EDIT; // ENABLE_QUICK_EDIT_MODE entfernen
+                SetConsoleMode(handle, mode);
+            }
+        }
+#endif
+    }
+}
