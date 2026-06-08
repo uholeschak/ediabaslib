1e517ca5f1332767f849329f4cf0c6449dd17cae
uholeschak <ulrich@holeschak.de>
2026-06-08 18:11:11 +0200

    Automatic code update

---
+++ Tools/Psdz/PsdzClientLibrary/Psdz/ISfaPerEcuOptions.cs
@@ -0,0 +1,14 @@
+﻿using PsdzClient.Core;
+
+namespace BMW.Rheingold.Psdz
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    public interface ISfaPerEcuOptions
+    {
+        TalFilterOptions CategoryAction { get; set; }
+
+        TalFilterOptions SfaWriteAction { get; set; }
+
+        TalFilterOptions SfaDeleteAction { get; set; }
+    }
+}
