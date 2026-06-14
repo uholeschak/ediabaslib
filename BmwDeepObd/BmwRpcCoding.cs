f90baa83bac0fa732e590e52d19d90259f3a46f1
uholeschak <ulrich@holeschak.de>
2026-06-14 16:14:26 +0200

    Moved some function code to extra class

--- BmwDeepObd/BmwRpcCoding.cs
+++ BmwDeepObd/BmwRpcCoding.cs
@@ -1,4 +1,4 @@
-﻿using Android.Nfc;
+﻿using Android.Content;
 using EdiabasLib;
 using PsdzRpcClient;
 using PsdzRpcServer.Shared;
@@ -10,14 +10,17 @@ using System.Threading.Tasks;
 
 namespace BmwDeepObd;
 
-public class BmwRpcCoding
+public class BmwRpcCoding : IDisposable
 {
+    private bool _disposed;
+    private ActivityCommon _activityCommon;
     private PsdzRpcClient.PsdzRpcClient _psdzRpcClient;
     private EdiabasProxyClient _ediabasProxyClient;
     private Task<bool> _startTask;
     private CancellationTokenSource _startCts;
     private object _startLock = new object();
     private object _statusLock = new object();
+    private object _dataLock = new object();
 
 #if DEBUG
     private static readonly string Tag = typeof(BmwRpcCoding).FullName;
@@ -46,18 +49,42 @@ public class BmwRpcCoding
     {
         get
         {
-            lock (_statusLock)
+            lock (_dataLock)
             {
                 return _psdzRpcClient;
             }
         }
         private set
         {
-            lock (_statusLock)
+            lock (_dataLock)
             {
                 _psdzRpcClient = value;
             }
         }
+    }
+
+    string _adapterSerialValid;
+    public string AdapterSerialValid
+    {
+        get
+        {
+            lock (_dataLock)
+            {
+                return _adapterSerialValid;
+            }
+        }
+        private set
+        {
+            lock (_dataLock)
+            {
+                _adapterSerialValid = value;
+            }
+        }
+    }
+
+    public BmwRpcCoding(Context context)
+    {
+        _activityCommon = new ActivityCommon(context);
     }
 
     private bool CreateRpcClient(ActivityCommon activityCommon, EdiabasNet ediabas)
@@ -72,54 +99,106 @@ public class BmwRpcCoding
                 PsdzRpcServiceConstants.CaCertFile, PsdzRpcServiceConstants.ClientPfxFile, Assembly.GetExecutingAssembly());
             _psdzRpcClient.ClientConnected += async (sender, connected) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
             };
 
             _psdzRpcClient.PingUpdated += (sender, pingDateTime) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
             };
 
             _psdzRpcClient.CallbackHandler.StartProgrammingCompleted += async (s, success) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
             };
 
             _psdzRpcClient.CallbackHandler.StopProgrammingCompleted += async (s, success) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
             };
 
             _psdzRpcClient.CallbackHandler.ConnectVehicleCompleted += async (s, connectArgs) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
             };
 
             _psdzRpcClient.CallbackHandler.DisconnectVehicleCompleted += async (s, success) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
             };
 
             _psdzRpcClient.CallbackHandler.VehicleFunctionsCompleted += async (s, vehicleArgs) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
             };
 
             _psdzRpcClient.CallbackHandler.UpdateStatus += async (s, message) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
             };
 
             _psdzRpcClient.CallbackHandler.UpdateProgress += (s, progressArgs) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
             };
 
             _psdzRpcClient.CallbackHandler.UpdateOptions += async (sender, optionArgs) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
             };
 
             _psdzRpcClient.CallbackHandler.UpdateOptionSelections += async (sender, swiRegisterEnum) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
             };
 
             _psdzRpcClient.CallbackHandler.ShowMessage += (sender, msgArgs) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
             };
 
             _psdzRpcClient.CallbackHandler.ShowMessageWait += (sender, msgArgs) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
             };
 
             _psdzRpcClient.CallbackHandler.TelSendQueueSize += (sender, queueArgs) =>
@@ -129,11 +208,36 @@ public class BmwRpcCoding
 
             _psdzRpcClient.CallbackHandler.ServiceInitialized += async (sender, serviceArgs) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
             };
 
 
             _psdzRpcClient.CallbackHandler.GetAppInfo += (sender, infoArgs) =>
             {
+                if (_disposed)
+                {
+                    return;
+                }
+
+                string adapterSerial = ActivityCommon.LastAdapterSerial ?? string.Empty;
+                string validSerial;
+                lock (_dataLock)
+                {
+                    validSerial = AdapterSerialValid ?? string.Empty;
+                }
+
+                bool adapterSerialValid = false;
+                if (!string.IsNullOrEmpty(validSerial) && string.Compare(validSerial, adapterSerial, StringComparison.Ordinal) == 0)
+                {
+                    adapterSerialValid = true;
+                }
+
+                infoArgs.AppId = ActivityCommon.AppId;
+                infoArgs.AdapterSerial = adapterSerial;
+                infoArgs.AdapterSerialValid = adapterSerialValid;
             };
 
             _ediabasProxyClient = new EdiabasProxyClient(ediabas);
@@ -209,5 +313,32 @@ public class BmwRpcCoding
         {
             Android.Util.Log.Info(_tag, value ?? string.Empty);
         }
+    }
+    public void Dispose()
+    {
+        Dispose(true);
+        // This object will be cleaned up by the Dispose method.
+        // Therefore, you should call GC.SupressFinalize to
+        // take this object off the finalization queue
+        // and prevent finalization code for this object
+        // from executing a second time.
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        // Check to see if Dispose has already been called.
+        if (!_disposed)
+        {
+            // If disposing equals true, dispose all managed
+            // and unmanaged resources.
+            if (disposing)
+            {
+                _activityCommon.Dispose();
+            }
+        }
+
+        // Note disposing has been done.
+        _disposed = true;
     }
 }
