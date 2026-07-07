e5282f5fd6fe04f1ae6c0bc40267c996f8046587
uholeschak <ulrich@holeschak.de>
2026-07-07 16:34:48 +0200

    Adapted patch range

--- Tools/Psdz/AssemblyPatcher/Program.cs
+++ Tools/Psdz/AssemblyPatcher/Program.cs
@@ -18,7 +18,8 @@ namespace AssemblyPatcher
     {
         public const long FileVersion450 = (4 << 24) + (50 << 16) + 0;
         public const long FileVersion456 = (4 << 24) + (56 << 16) + 0;
-        public const long FileVersion490 = (4 << 24) + (59 << 16) + 0;
+        public const long FileVersion459 = (4 << 24) + (59 << 16) + 0;
+        public const long FileVersion460 = (4 << 24) + (60 << 16) + 0;
 
         public class Options
         {
@@ -1453,7 +1454,7 @@ namespace AssemblyPatcher
                             Console.WriteLine("*** checkForPsdzInstancesLogFile Exception: {0}", ex.Message);
                         }
 
-                        if ((exeFileVersion & ~0xFF) == FileVersion490)
+                        if ((exeFileVersion & ~0xFF) >= FileVersion459 && (exeFileVersion & ~0xFF) <= FileVersion460)
                         {
                             try
                             {
