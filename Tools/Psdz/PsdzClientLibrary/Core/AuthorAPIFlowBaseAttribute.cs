237e91e7820ca8f0ffca47d49cd507e5be452007
uholeschak <ulrich@holeschak.de>
2026-07-22 16:19:00 +0200

    Added missing attribute

---
+++ Tools/Psdz/PsdzClientLibrary/Core/AuthorAPIFlowBaseAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PsdzClient.Core;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public class AuthorAPIFlowBaseAttribute : Attribute
+{
+}
