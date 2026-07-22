54b9f1f31e004955a8d5232d04914b83f38b5bfe
uholeschak <ulrich@holeschak.de>
2026-07-22 17:16:29 +0200

    Added missing interfaces

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/ISfaHandler.cs
@@ -0,0 +1,22 @@
+﻿using BMW.Authoring;
+using PsdzClient.Contracts;
+using PsdzClient.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using PsdzClient;
+
+namespace PsdzClientLibrary.Core.Module;
+
+[AuthorAPI(SelectableTypeDeclaration = true)]
+[EditorBrowsable(EditorBrowsableState.Advanced)]
+[PreserveSource(Hint = "No update", SuppressWarning = true)]
+public interface ISfaHandler : IHideObjectMembers
+{
+    [Obsolete("Use WriteSecureTokensAutomatic(ISfaFeature sfaFeature) instead.")]
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    IBoolResultObject WriteSecureTokensAutomatic(string vin, ISfaFeature sfaFeature);
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    IBoolResultObject WriteSecureTokensAutomatic(ISfaFeature sfaFeature);
+}
