557e32ddb2ba5737d15d9bff0ebd98b10063515e
uholeschak <ulrich@holeschak.de>
2026-07-22 17:12:33 +0200

    Added missing interfaces

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/IIPSDataProvider.cs
@@ -0,0 +1,20 @@
+﻿using PsdzClient.Core;
+using System;
+
+namespace BMW.Rheingold.CoreFramework
+{
+    [AuthorAPI(SelectableTypeDeclaration = false)]
+    [Obsolete("use new Method over the Authoring BackendCommunication GetServiceRideDataHandler")]
+    public interface IIPSDataProvider
+    {
+        DateTime? DueServiceDate { get; }
+
+        int DueServiceMileage { get; }
+
+        DateTime? TodaysServiceDate { get; }
+
+        int TodaysServiceMileage { get; }
+
+        void ImportMaintenanceScheduleInfo(string vin17);
+    }
+}
