b3817f81b3afd4e9fd008a15272c74ecb58253eb
uholeschak <ulrich@holeschak.de>
2026-07-22 15:33:22 +0200

    Source sync

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/ISwiActionReport.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace BMW.Rheingold.CoreFramework.Contracts.Programming
+{
+    public interface ISwiActionReport
+    {
+        string Name { get; }
+
+        string Category { get; }
+
+        bool IsVisible { get; }
+
+        IList<string> LinkedTo { get; }
+    }
+
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/InfoObjectExecutionCout.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+
+namespace BMW.Rheingold.CoreFramework.DatabaseProvider
+{
+    public class InfoObjectExecutionCout : INotifyPropertyChanged
+    {
+        private int allField;
+
+        private int canceledField;
+
+        public int All
+        {
+            get
+            {
+                return allField;
+            }
+            set
+            {
+                if (!allField.Equals(value))
+                {
+                    allField = value;
+                    OnPropertyChanged("All");
+                }
+            }
+        }
+
+        public int Canceled
+        {
+            get
+            {
+                return canceledField;
+            }
+            set
+            {
+                if (!canceledField.Equals(value))
+                {
+                    canceledField = value;
+                    OnPropertyChanged("Canceled");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
