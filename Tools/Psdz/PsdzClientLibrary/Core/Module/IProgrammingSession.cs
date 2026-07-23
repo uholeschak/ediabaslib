c6625b993c6998d02b77478060da4cf2c9437e1e
uholeschak <ulrich@holeschak.de>
2026-07-23 23:57:31 +0200

    Source sync

--- Tools/Psdz/PsdzClientLibrary/Core/Module/IProgrammingSession.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/IProgrammingSession.cs
@@ -14,9 +14,9 @@ namespace BMW.Rheingold.CoreFramework.Contracts.Programming
     [AuthorAPI(SelectableTypeDeclaration = true)]
     public interface IProgrammingSession : INotifyPropertyChanged, IDisposable
     {
-        BMW.Rheingold.CoreFramework.Contracts.Programming.IFa FaCurrent { get; }
+        IFa FaCurrent { get; }
 
-        BMW.Rheingold.CoreFramework.Contracts.Programming.IFa FaTarget { get; }
+        IFa FaTarget { get; }
 
         string IntegrationLevelTarget { get; }
 
@@ -65,117 +65,62 @@ namespace BMW.Rheingold.CoreFramework.Contracts.Programming
         ISet<ISmartActuatorMasterEcu> FailedProgrammingSmartActuatorMasters { get; }
 
         IList<string> GetServiceProgramsForSwiAction(string swiActionName);
-
         IBoolResultObject ImportSecureToken();
-
         bool GetProgrammingModeSwitchFromTALExecution();
-
         void SetProgrammingModeSwitchFromTALExecution(bool value);
-
         void SetECUsNotToSwitchToProgrammingMode(IList<string> ecus);
-
         void SetECUsToPreventUDSFallback(IList<string> ecus);
-
         bool? GetParallelFlashFromTALExecution();
-
         void SetParallelFlashFromTALExecution(bool valueToSet);
-
         void SetBackProgrammingModeSwitchFromTALExecution();
-
         void ClearTalFilter();
-
         void RestoreDefaultTalFilter();
-
-        void SetFaCurrent(BMW.Rheingold.CoreFramework.Contracts.Programming.IFa fa);
-
-        void SetFaTarget(BMW.Rheingold.CoreFramework.Contracts.Programming.IFa fa);
-
+        void SetFaCurrent(IFa fa);
+        void SetFaTarget(IFa fa);
         void SetSvtCurrent(ISvt svt);
-
         void SetVehicleUpdate(IVehicleUpdate vehicleUpdate);
-
         IVehicleUpdate SpecialPlanRequired(string swiActionName);
-
         void UpdateSFATalFilterForAllEcus(ISfaPerEcuOptions ecuOptions);
-
         void UpdateTalFilterForAllEcus(TaCategories[] taCategories, TalFilterOptions talFilterOptions);
-
         void UpdateSFATalFilterForSelectedEcus(IDictionary<int, ISfaPerEcuOptions> ecuOptions);
-
         void UpdateTalFilterForSelectedEcus(TaCategories[] taCategories, int[] diagAddress, TalFilterOptions talFilterOptions);
-
         [Obsolete("This API-Function is deprecated.")]
         void UpdateTalFilterForSelectedEcuOnSweLevel(int diagAddress, TaCategories taCategory, string processClass, TalFilterOptions talFilterOptions, IDictionary<string, TalFilterOptions> sweFilter);
-
         void UpdateTalFilterForSelectedEcuOnSweLevel(int diagAddress, TaCategories taCategory, string processClass, TalFilterOptions talFilterOptions, List<string> sgbmIds, List<TalFilterOptions> sweTalFilterOptions);
-
         void DisableCodingSelection(string da);
-
         void DisableProgrammingSelection(string da);
-
         void SetConnectionToDCan();
-
         void SetConnectionPort(int port);
-
         int GetConnectionPort();
-
         IHttpServerResponse RequestProgrammingHttpServer();
-
         bool IsHddUpdateUrlReachableByVehicle();
-
         bool SetTargetToBackupILevel();
-
         bool SetTargetToDefinedILevel(string targetILevel);
-
-        IBoolResultObject SetTargetContext(string newTargetILevel, BMW.Rheingold.CoreFramework.Contracts.Programming.IFa targetFa);
-
+        IBoolResultObject SetTargetContext(string newTargetILevel, IFa targetFa);
         IBoolResultObject PlanVehicleModifications(List<IPlannedSwiAction> plannedSwiActions);
-
         IBoolResultObject DeselectVehicleModifications(List<string> swiActionsToDeselect);
-
         IBoolResultObject SwtDeactivationWhiteListClear();
-
         IBoolResultObject SwtDeactivationWhiteListFill(string minDiagAdress, string maxDiagAdress, string minAppNumber, string maxAppNumber, string minUpgradeIndex, string maxUpgradeIndex);
-
         bool IsTargetILevelSetToBackIlevel();
-
         void SetPsdzPreferredFlashprotocolUDS(int diagAddress);
-
         bool SetPreExchangeSelectionForEcu(int diagAddress, bool activateSelection);
-
         bool SetPostExchangeSelectionForEcu(int diagAddress, bool activateSelection);
-
         IBoolResultObject AddTechnicalActionResultToProtocoll(string taNummer, string taBezeichnung, IList<string> mindestIStufens, bool abArbeitungsstatus, string diagnosisCodeTitle, string diagnoseCodes);
-
         IBoolResultObject AddRxSwinListToProtocol(List<IRxSwinObject> rxSwinList, bool updateActualContext);
-
         IBoolResultObject StartVehicleOrderImport();
-
         IBoolResultObject StartVehicleOrderImportOnlyOnlineOption();
-
         IBoolResultObject StartVehicleOrderImportOnlyManualOption();
-
         IBoolResultObject<ISdpPatchResult> SdpPatchAvailable();
-
         IList<ISdpPatchResult> GetAvailableSdpPatches();
-
         [Obsolete("This API-Function is deprecated, please use 'GetAvailableSdpPatches()' instead.")]
         IBoolResultObject<IList<ISdpPatchResult>> AvailableSdpPatches();
-
         IBoolResultObject SdpPatchDownload(string swiDataTarget);
-
         IBoolResultObject SdpPatchDownload(string swiDataTarget, string newTargetILevel);
-
         IBoolResultObject CheckAvailabilityOfPsdzConnection();
-
         IBoolResultObject<IEcuFailureResponseSet> ResetEcus(List<string> hexEcuAddress);
-
         IBoolResultObject CheckAvailabilityOfSdpPatchStorage();
-
         IBoolResultObject<long> GetDurationOfWenToken();
-
         bool? IsSoftwareUpToDate(string ecu);
-
         IEnumerable<ISgbmIdChange> GetDifferentSgbmIds(string ecu);
     }
 }
