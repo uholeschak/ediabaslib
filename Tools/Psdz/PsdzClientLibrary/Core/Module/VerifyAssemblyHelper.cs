bc77e25b41819963915b6ec49af4fe16939250fa
uholeschak <ulrich@holeschak.de>
2026-07-24 10:51:13 +0200

    Added helper class

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/VerifyAssemblyHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace BMW.Rheingold.CoreFramework.InteropHelper
+{
+    public static class VerifyAssemblyHelper
+    {
+        public static bool VerifyStrongName(string assemblyPath, bool force)
+        {
+            return true;
+        }
+
+        public static bool VerifyStrongName(Type t, bool force)
+        {
+            return VerifyStrongName(Assembly.GetAssembly(t).Location, force);
+        }
+    }
+}
