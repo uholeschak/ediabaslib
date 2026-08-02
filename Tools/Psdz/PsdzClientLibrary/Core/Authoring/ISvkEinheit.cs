8791e71e17626216a154a515a26df52135eb8180
uholeschak <ulrich@holeschak.de>
2026-08-03 00:23:51 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Authoring/ISvkEinheit.cs
@@ -0,0 +1,23 @@
+﻿using PsdzClient.Core;
+using System;
+using System.ComponentModel;
+
+namespace BMW.Authoring.Vehicle
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    public interface ISvkEinheit : IHideObjectMembers
+    {
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        string SGBMID { get; }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        Version VERSION { get; }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        string IDENTIFIER { get; }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        SvkProzessklasse Prozessklasse_Kurztext { get; }
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Core/Authoring/SvkProzessklasse.cs
@@ -0,0 +1,36 @@
+﻿using PsdzClient.Core;
+using System.ComponentModel;
+
+namespace BMW.Authoring.Vehicle
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    [EditorBrowsable(EditorBrowsableState.Always)]
+    public enum SvkProzessklasse
+    {
+        HWEL = 1,
+        HWAP = 2,
+        HWFR = 3,
+        GWTB = 4,
+        CAFD = 5,
+        BTLD = 6,
+        FLSL = 7,
+        SWFL = 8,
+        SWFF = 9,
+        SWPF = 10,
+        ONPS = 11,
+        IBAD = 12,
+        SWFK = 13,
+        FAFP = 15,
+        TLRT = 26,
+        TPRG = 27,
+        FCFA = 16,
+        BLUP = 28,
+        FLUP = 29,
+        SWUP = 192,
+        SWIP = 193,
+        ENTD = 160,
+        NAVD = 161,
+        FCFN = 162,
+        _ = 255
+    }
+}
