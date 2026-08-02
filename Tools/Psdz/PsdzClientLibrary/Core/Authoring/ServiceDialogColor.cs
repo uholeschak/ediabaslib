32bb34a642acea37c5d614e5513a139cc31a0e42
uholeschak <ulrich@holeschak.de>
2026-08-02 23:57:24 +0200

    Added missing class

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Authoring/ServiceDialogColor.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using PsdzClient.Core;
+
+namespace BMW.Authoring.Helper
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    [EditorBrowsable(EditorBrowsableState.Always)]
+    public enum ServiceDialogColor
+    {
+        Black,
+        White,
+        Lightgray,
+        Gray,
+        Green,
+        LightGreen,
+        Red,
+        Orange,
+        Yellow,
+        Blue
+    }
+}
