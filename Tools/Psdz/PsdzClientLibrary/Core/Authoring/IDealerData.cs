93fd0f7b86ff29b1a26e8b7c51f36fc33a60f117
uholeschak <ulrich@holeschak.de>
2026-08-03 00:52:48 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Authoring/IDealerData.cs
@@ -0,0 +1,46 @@
+﻿using PsdzClient.Core;
+using System.ComponentModel;
+
+namespace BMW.Authoring.Session
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    public interface IDealerData : IHideObjectMembers
+    {
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        string Vertriebspartnernummer { get; }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        string Betriebsnummer { get; }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        string Händlernummer { get; }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        Testerland Testerland { get; }
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        bool Vertriebspartnernummer_IsValue(string wert, params string[] wert_);
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        bool Vertriebspartnernummer_IsValue(string[] wert);
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        bool Betriebsnummer_IsValue(string wert, params string[] wert_);
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        bool Betriebsnummer_IsValue(string[] wert);
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        bool Händlernummer_IsValue(string wert, params string[] wert_);
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        bool Händlernummer_IsValue(string[] wert);
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        bool Testerland_IsValue(Testerland wert, params Testerland[] wert_);
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        bool Testerland_IsValue(Testerland[] wert);
+    }
+}
