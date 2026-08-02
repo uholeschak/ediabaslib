d437b5f952365108109b9a9fde9c5dae643b81cd
uholeschak <ulrich@holeschak.de>
2026-08-03 01:45:52 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/MeasurementPointPositions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Xml.Serialization;
+
+namespace BMW.Rheingold.Module.ISTA
+{
+    [Serializable]
+    [GeneratedCode("Xsd2Code", "3.4.0.26539")]
+    [XmlRoot(Namespace = "", IsNullable = false)]
+    public enum MeasurementPointPositions
+    {
+        BatteryPlus,
+        BatteryMinus,
+        CarBodyMinus,
+        MotorGround,
+        ECUGround,
+        AdapterPin,
+        UniversalAdapterPin,
+        BatteryMountingPointPlus,
+        OwnText
+    }
+}
