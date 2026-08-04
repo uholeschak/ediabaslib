f62094a495b99940c4289613ccd815734397ac4f
uholeschak <ulrich@holeschak.de>
2026-08-04 23:15:55 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/FeatureSpecificField.cs
@@ -0,0 +1,23 @@
+﻿using PsdzClient.Core;
+using PsdzClient.Programming;
+
+namespace BMW.Rheingold.CoreFramework.Contracts.Programming
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    public class FeatureSpecificField : IFeatureSpecificField
+    {
+        public int FieldType { get; set; }
+
+        public string FieldValue { get; set; }
+
+        public FeatureSpecificField(int fieldType, string fieldValue)
+        {
+            FieldType = fieldType;
+            FieldValue = fieldValue;
+        }
+
+        public FeatureSpecificField()
+        {
+        }
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/ValidityCondition.cs
@@ -0,0 +1,23 @@
+﻿using PsdzClient.Core;
+using PsdzClient.Programming;
+
+namespace BMW.Rheingold.CoreFramework.Contracts.Programming
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    public class ValidityCondition : IValidityCondition
+    {
+        public ConditionTypeEnum ConditionType { get; set; }
+
+        public string ValidityValue { get; set; }
+
+        public ValidityCondition()
+        {
+        }
+
+        public ValidityCondition(ConditionTypeEnum condition, string validityValue)
+        {
+            ConditionType = condition;
+            ValidityValue = validityValue;
+        }
+    }
+}
