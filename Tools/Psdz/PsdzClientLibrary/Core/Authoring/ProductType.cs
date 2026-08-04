59cd47decb2586dc8b1b375a7a47ba7ee50f4b39
uholeschak <ulrich@holeschak.de>
2026-08-04 14:42:24 +0200

    Namespace adapted

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Authoring/ProductType.cs
@@ -0,0 +1,13 @@
+﻿using PsdzClient.Core;
+using System.ComponentModel;
+
+namespace BMW.Authoring.Vehicle
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    [EditorBrowsable(EditorBrowsableState.Always)]
+    public enum ProductType
+    {
+        P,
+        M
+    }
+}
