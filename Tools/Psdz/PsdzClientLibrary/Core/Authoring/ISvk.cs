0cd11fc5cc6410d1c089ee5fb55f20bcfeb62354
uholeschak <ulrich@holeschak.de>
2026-08-04 23:18:22 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Authoring/ISvk.cs
@@ -0,0 +1,28 @@
+﻿using PsdzClient.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BMW.Authoring.Vehicle
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    [EditorBrowsable(EditorBrowsableState.Always)]
+    public interface ISvk : IHideObjectMembers
+    {
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        DateTime Programmierdatum { get; }
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        int Count { get; }
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        int ProgTest { get; }
+
+        [AuthorAPIHidden]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        IEnumerator<ISvkEinheit> GetEnumerator();
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        Version Svk_getVersion(SvkProzessklasse Prozessklasse, string Identifier);
+    }
+}
