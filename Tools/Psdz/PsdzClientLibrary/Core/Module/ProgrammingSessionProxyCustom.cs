d86c3f580666ade2e18cacbe0158b2a6967fe617
uholeschak <ulrich@holeschak.de>
2026-08-06 11:43:39 +0200

    Added custom proxy

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/ProgrammingSessionProxyCustom.cs
@@ -0,0 +1,451 @@
+﻿using BMW.Rheingold.CoreFramework;
+using BMW.Rheingold.CoreFramework.Contracts;
+using BMW.Rheingold.CoreFramework.Contracts.FASTA;
+using BMW.Rheingold.CoreFramework.Contracts.Programming;
+using BMW.Rheingold.CoreFramework.Contracts.Vehicle;
+using BMW.Rheingold.Psdz;
+using BMW.Rheingold.Psdz.Model.Ecu;
+using PsdzClient.Contracts;
+using PsdzClient.Core.Container;
+using PsdzClient.Programming;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using PsdzClient;
+
+#pragma warning disable CS0067, CS0618, CS0649
+namespace BMW.Rheingold.Module.ISTA
+{
+    [PreserveSource(Hint = "Adapted from ProgrammingSessionProxy", SuppressWarning = true)]
+    internal class ProgrammingSessionProxyCustom : IProgrammingSession, INotifyPropertyChanged, IDisposable
+    {
+        private readonly IProgrammingSessionExt programmingSession;
+
+        private readonly IProgrammingApi programmingApi;
+
+        private readonly IAPISecurity apiSecurity;
+
+        internal IProgrammingSession ProgrammingSession => programmingSession;
+
+        public BMW.Rheingold.CoreFramework.Contracts.Programming.IFa FaCurrent => programmingSession.FaCurrent;
+
+        public BMW.Rheingold.CoreFramework.Contracts.Programming.IFa FaTarget => programmingSession.FaTarget;
+
+        public string IntegrationLevelTarget => programmingSession.IntegrationLevelTarget;
+
+        public IProgrammingApi ProgrammingApi => programmingApi;
+
+        public IAPISecurity APISecurity => apiSecurity;
+
+        public IPsdzInfo Psdz => programmingSession.Psdz;
+
+        public IPsdzContext PsdzContext => programmingSession.PsdzContext;
+
+        public ISvt SvtCurrent => programmingSession.SvtCurrent;
+
+        public ISvt SvtTarget => programmingSession.SvtTarget;
+
+        public string TalAsXml => programmingSession.TalAsXml;
+
+        public string TalFilterAsXml => programmingSession.TalFilterAsXml;
+
+        public double TimeLeftSec => programmingSession.TimeLeftSec;
+
+        public bool UseReferenceSvtAsTarget
+        {
+            get
+            {
+                return programmingSession.UseReferenceSvtAsTarget;
+            }
+            set
+            {
+                programmingSession.UseReferenceSvtAsTarget = value;
+            }
+        }
+
+        public ITherapyPlanApi TherapyPlanApi => programmingSession.TherapyPlanApi;
+
+        public ISecureEcuModeService SecureEcuModeService => programmingSession.SecureEcuModeService;
+
+        public ISecManagementService SecurityManagementService => programmingSession.SecurityManagementService;
+
+        public IComponentTheftProtectionService ComponentTheftProtectionService => programmingSession.ComponentTheftProtectionService;
+
+        public IValidityCondition ValidityCondition => programmingSession.ValidityCondition;
+
+        public IFeatureSpecificField FeatureSpecificField => programmingSession.FeatureSpecificField;
+
+        public IDictionary<IEcu, ProgrammingActionType> FailedProgrammingEcus => programmingSession.FailedProgrammingEcus;
+
+        public IDictionary<IEcu, HashSet<ProgrammingActionType>> FailedProgrammingEcusActions => programmingSession.FailedProgrammingEcusActions;
+
+        public ISet<ISmartActuatorEcu> FailedProgrammingSmartActuators => programmingSession.FailedProgrammingSmartActuators;
+
+        public ISet<ISmartActuatorMasterEcu> FailedProgrammingSmartActuatorMasters => programmingSession.FailedProgrammingSmartActuatorMasters;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ProgrammingSessionProxyCustom(IProgrammingSessionExt programmingSession)
+        {
+            this.programmingSession = programmingSession;
+            if (programmingSession == null)
+            {
+                throw new ArgumentNullException("programmingSession");
+            }
+            if (programmingSession.ProgrammingApi != null)
+            {
+               //[-] programmingApi = new ProgrammingApiProxy(programmingSession.ProgrammingApi, fasta);
+            }
+            if (programmingSession.APISecurity != null)
+            {
+                //[-] apiSecurity = new APISecurityProxy(programmingSession.APISecurity, fasta);
+            }
+        }
+
+        public IList<string> GetServiceProgramsForSwiAction(string swiActionName)
+        {
+            IList<string> serviceProgramsForSwiAction = programmingSession.GetServiceProgramsForSwiAction(swiActionName);
+            return serviceProgramsForSwiAction;
+        }
+
+        public bool? IsSoftwareUpToDate(string ecu)
+        {
+            bool? result = programmingSession.IsSoftwareUpToDate(ecu);
+            return result;
+        }
+
+        public IEnumerable<ISgbmIdChange> GetDifferentSgbmIds(string ecu)
+        {
+            IEnumerable<ISgbmIdChange> differentSgbmIds = programmingSession.GetDifferentSgbmIds(ecu);
+            return differentSgbmIds;
+        }
+
+        public bool GetProgrammingModeSwitchFromTALExecution()
+        {
+            bool programmingModeSwitchFromTALExecution = programmingSession.GetProgrammingModeSwitchFromTALExecution();
+            return programmingModeSwitchFromTALExecution;
+        }
+
+        public void SetProgrammingModeSwitchFromTALExecution(bool value)
+        {
+            programmingSession.SetProgrammingModeSwitchFromTALExecution(value);
+        }
+
+        public void SetECUsNotToSwitchToProgrammingMode(IList<string> ecus)
+        {
+            programmingSession.SetECUsNotToSwitchToProgrammingMode(ecus);
+        }
+
+        public void SetECUsToPreventUDSFallback(IList<string> ecus)
+        {
+            programmingSession.SetECUsToPreventUDSFallback(ecus);
+        }
+
+        public bool? GetParallelFlashFromTALExecution()
+        {
+            return programmingSession.GetParallelFlashFromTALExecution();
+        }
+
+        public void SetParallelFlashFromTALExecution(bool valueToSet)
+        {
+            programmingSession.SetParallelFlashFromTALExecution(valueToSet);
+        }
+
+        public void SetBackProgrammingModeSwitchFromTALExecution()
+        {
+            programmingSession.SetBackProgrammingModeSwitchFromTALExecution();
+        }
+
+        public void ClearTalFilter()
+        {
+            programmingSession.ClearTalFilter();
+        }
+
+        public void RestoreDefaultTalFilter()
+        {
+            programmingSession.RestoreDefaultTalFilter();
+        }
+
+        public void SetFaCurrent(BMW.Rheingold.CoreFramework.Contracts.Programming.IFa fa)
+        {
+            programmingSession.SetFaCurrent(fa);
+        }
+
+        public void SetFaTarget(BMW.Rheingold.CoreFramework.Contracts.Programming.IFa fa)
+        {
+            programmingSession.SetFaTarget(fa, null);
+        }
+
+        public void SetSvtCurrent(ISvt svt)
+        {
+            programmingSession.SetSvtCurrent(svt);
+        }
+
+        public void SetVehicleUpdate(IVehicleUpdate vehicleUpdate)
+        {
+            programmingSession.SetVehicleUpdate(vehicleUpdate);
+        }
+
+        public IVehicleUpdate SpecialPlanRequired(string swiActionName)
+        {
+            IVehicleUpdate result = programmingSession.SpecialPlanRequired(swiActionName);
+            return result;
+        }
+
+        public void UpdateTalFilterForAllEcus(TaCategories[] taCategories, TalFilterOptions talFilterOptions)
+        {
+            programmingSession.UpdateTalFilterForAllEcus(taCategories, talFilterOptions);
+        }
+
+        public void UpdateSFATalFilterForAllEcus(ISfaPerEcuOptions ecuOptions)
+        {
+            programmingSession.UpdateSFATalFilterForAllEcus(ecuOptions);
+        }
+
+        public void UpdateSFATalFilterForSelectedEcus(IDictionary<int, ISfaPerEcuOptions> ecuOptions)
+        {
+            programmingSession.UpdateSFATalFilterForSelectedEcus(ecuOptions);
+        }
+
+        public void UpdateTalFilterForSelectedEcus(TaCategories[] taCategories, int[] diagAddress, TalFilterOptions talFilterOptions)
+        {
+            programmingSession.UpdateTalFilterForSelectedEcus(taCategories, diagAddress, talFilterOptions);
+        }
+
+        public void UpdateTalFilterForSelectedEcuOnSweLevel(int diagAddress, TaCategories taCategory, string processClass, TalFilterOptions talFilterOptions, IDictionary<string, TalFilterOptions> sweFilter)
+        {
+            programmingSession.UpdateTalFilterForSelectedEcuOnSweLevel(diagAddress, taCategory, processClass, talFilterOptions, sweFilter);
+        }
+
+        public void UpdateTalFilterForSelectedEcuOnSweLevel(int diagAddress, TaCategories taCategory, string processClass, TalFilterOptions talFilterOptions, List<string> sgbmIds, List<TalFilterOptions> sweTalFilterOptions)
+        {
+            programmingSession.UpdateTalFilterForSelectedEcuOnSweLevel(diagAddress, taCategory, processClass, talFilterOptions, sgbmIds, sweTalFilterOptions);
+        }
+
+        public void DisableCodingSelection(string da)
+        {
+            programmingSession.DisableCodingSelection(da);
+        }
+
+        public void DisableProgrammingSelection(string da)
+        {
+            programmingSession.DisableProgrammingSelection(da);
+        }
+
+        public void SetConnectionToDCan()
+        {
+            programmingSession.SetConnectionToDCan();
+        }
+
+        public void SetConnectionPort(int port)
+        {
+            programmingSession.SetConnectionPort(port);
+        }
+
+        public int GetConnectionPort()
+        {
+            int connectionPort = programmingSession.GetConnectionPort();
+            return connectionPort;
+        }
+
+        public IHttpServerResponse RequestProgrammingHttpServer()
+        {
+            IHttpServerResponse httpServerResponse = programmingSession.RequestProgrammingHttpServer();
+            return httpServerResponse;
+        }
+
+        public bool IsHddUpdateUrlReachableByVehicle()
+        {
+            bool result = programmingSession.IsHddUpdateUrlReachableByVehicle();
+            return result;
+        }
+
+        public void SetPsdzPreferredFlashprotocolUDS(int diagAddress)
+        {
+            programmingSession.SetPsdzPreferredFlashprotocolUDS(diagAddress);
+        }
+
+        public bool SetPreExchangeSelectionForEcu(int diagAddress, bool activateSelection)
+        {
+            bool result = programmingSession.SetPreExchangeSelectionForEcu(diagAddress, activateSelection);
+            return result;
+        }
+
+        public bool SetPostExchangeSelectionForEcu(int diagAddress, bool activateSelection)
+        {
+            bool result = programmingSession.SetPostExchangeSelectionForEcu(diagAddress, activateSelection);
+            return result;
+        }
+
+        public bool SetTargetToBackupILevel()
+        {
+            bool result = programmingSession.SetTargetToBackupILevel(null);
+            return result;
+        }
+
+        public bool SetTargetToDefinedILevel(string targetILevel)
+        {
+            bool result = programmingSession.SetTargetToDefinedILevel(null, targetILevel);
+            return result;
+        }
+
+        public IBoolResultObject SetTargetContext(string newTargetILevel, BMW.Rheingold.CoreFramework.Contracts.Programming.IFa targetFa)
+        {
+            IBoolResultObject boolResultObject = programmingSession.SetTargetContext(newTargetILevel, targetFa);
+            return boolResultObject;
+        }
+
+        public IBoolResultObject PlanVehicleModifications(List<IPlannedSwiAction> plannedSwiActions)
+        {
+            IBoolResultObject boolResultObject = programmingSession.PlanVehicleModifications(plannedSwiActions);
+            return boolResultObject;
+        }
+
+        public IBoolResultObject DeselectVehicleModifications(List<string> swiActionsToDeselect)
+        {
+            IBoolResultObject boolResultObject = programmingSession.DeselectVehicleModifications(swiActionsToDeselect);
+            return boolResultObject;
+        }
+
+        public bool IsTargetILevelSetToBackIlevel()
+        {
+            bool result = programmingSession.IsTargetILevelSetToBackIlevel(null);
+            return result;
+        }
+
+        public IBoolResultObject AddTechnicalActionResultToProtocoll(string taNummer, string taBezeichnung, IList<string> mindestIStufens, bool abArbeitungsstatus, string diagnosisCodeTitle, string diagnoseCodes)
+        {
+            IBoolResultObject boolResultObject = programmingSession.AddTechnicalActionResultToProtocoll(taNummer, taBezeichnung, mindestIStufens, abArbeitungsstatus, diagnosisCodeTitle, diagnoseCodes);
+            return boolResultObject;
+        }
+
+        public IBoolResultObject SwtDeactivationWhiteListFill(string minDiagAdress, string maxDiagAdress, string minAppNumber, string maxAppNumber, string minUpgradeIndex, string maxUpgradeIndex)
+        {
+            IBoolResultObject boolResultObject = programmingSession.SwtDeactivationWhiteListFill(minDiagAdress, maxDiagAdress, minAppNumber, maxAppNumber, minUpgradeIndex, maxUpgradeIndex);
+            return boolResultObject;
+        }
+
+        public IBoolResultObject SwtDeactivationWhiteListClear()
+        {
+            IBoolResultObject boolResultObject = programmingSession.SwtDeactivationWhiteListClear();
+            return boolResultObject;
+        }
+
+        public IBoolResultObject AddRxSwinListToProtocol(List<IRxSwinObject> rxSwinList, bool updateActualContext)
+        {
+            IBoolResultObject boolResultObject = programmingSession.AddRxSwinListToProtocol(rxSwinList, updateActualContext);
+            return boolResultObject;
+        }
+
+        public IBoolResultObject StartVehicleOrderImport()
+        {
+            IBoolResultObject result = programmingSession.StartVehicleOrderImport();
+            return result;
+        }
+
+        public IBoolResultObject StartVehicleOrderImportOnlyOnlineOption()
+        {
+            IBoolResultObject result = programmingSession.StartVehicleOrderImportOnlyOnlineOption();
+            return result;
+        }
+
+        public IBoolResultObject StartVehicleOrderImportOnlyManualOption()
+        {
+            IBoolResultObject result = programmingSession.StartVehicleOrderImportOnlyManualOption();
+            return result;
+        }
+
+        public IBoolResultObject ImportSecureToken()
+        {
+            IBoolResultObject result = programmingSession.ImportSecureToken();
+            return result;
+        }
+
+        public IBoolResultObject<ISdpPatchResult> SdpPatchAvailable()
+        {
+            IBoolResultObject<ISdpPatchResult> result = programmingSession.SdpPatchAvailable();
+            return result;
+        }
+
+        public IList<ISdpPatchResult> GetAvailableSdpPatches()
+        {
+            IList<ISdpPatchResult> availableSdpPatches = programmingSession.GetAvailableSdpPatches();
+            return availableSdpPatches;
+        }
+
+        public IBoolResultObject<IList<ISdpPatchResult>> AvailableSdpPatches()
+        {
+            IBoolResultObject<IList<ISdpPatchResult>> result = programmingSession.AvailableSdpPatches();
+            return result;
+        }
+
+        public IBoolResultObject SdpPatchDownload(string swiDataTarget)
+        {
+            IBoolResultObject result = programmingSession.SdpPatchDownload(swiDataTarget);
+            return result;
+        }
+
+        public IBoolResultObject SdpPatchDownload(string swiDataTarget, string newTargetILevel)
+        {
+            IBoolResultObject result = programmingSession.SdpPatchDownload(swiDataTarget, newTargetILevel);
+            return result;
+        }
+
+        public IBoolResultObject CheckAvailabilityOfPsdzConnection()
+        {
+            IBoolResultObject result = programmingSession.CheckAvailabilityOfPsdzConnection();
+            return result;
+        }
+
+        public IBoolResultObject<IEcuFailureResponseSet> ResetEcus(List<string> hexEcuAddress)
+        {
+            IBoolResultObject<IEcuFailureResponseSet> result = programmingSession.ResetEcus(hexEcuAddress);
+            return result;
+        }
+
+        public IBoolResultObject CheckAvailabilityOfSdpPatchStorage()
+        {
+            IBoolResultObject result = programmingSession.CheckAvailabilityOfSdpPatchStorage();
+            return result;
+        }
+
+        public IBoolResultObject<long> GetDurationOfWenToken()
+        {
+            IBoolResultObject<long> durationOfWenToken = programmingSession.GetDurationOfWenToken();
+            return durationOfWenToken;
+        }
+
+        private string LogArray<T>(T[] array)
+        {
+            if (array == null)
+            {
+                return "null";
+            }
+            StringBuilder stringBuilder = new StringBuilder("[");
+            foreach (object obj in array)
+            {
+                if (obj == null)
+                {
+                    stringBuilder.Append("null");
+                }
+                else
+                {
+                    stringBuilder.Append(obj.ToString());
+                }
+                stringBuilder.Append(",");
+            }
+            stringBuilder.Replace(',', ']', stringBuilder.Length - 1, 1);
+            if (stringBuilder[stringBuilder.Length - 1] != ']')
+            {
+                stringBuilder.Append("]");
+            }
+            return stringBuilder.ToString();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
