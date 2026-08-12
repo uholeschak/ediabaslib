51a0b4f249eed0a4323649584b5c6833193430a9
uholeschak <ulrich@holeschak.de>
2026-08-12 17:47:08 +0200

    Added missing class

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Base64.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PsdzClient.Core
+{
+    [AuthorAPI]
+    public static class Base64
+    {
+        public static byte[] FromBase64String(string input)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(input))
+                {
+                    return Convert.FromBase64String(input);
+                }
+            }
+            catch (Exception exception)
+            {
+                Log.WarningException("Base64.FromBase64String()", exception);
+            }
+            return null;
+        }
+
+        public static string ToBase64(byte[] buf)
+        {
+            try
+            {
+                if (buf != null)
+                {
+                    return Convert.ToBase64String(buf);
+                }
+            }
+            catch (Exception exception)
+            {
+                Log.WarningException("Base64.ToBase64()", exception);
+            }
+            return null;
+        }
+    }
+}
