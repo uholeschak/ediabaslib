5bed8954282de7eee6c6d96f8f0e5f286f6f159c
uholeschak <ulrich@holeschak.de>
2026-08-17 01:02:38 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/ILcSwitch.cs
@@ -0,0 +1,16 @@
+﻿using PsdzClient.Core;
+
+namespace BMW.Rheingold.CoreFramework.Contracts.Vehicle
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    public interface ILcSwitch
+    {
+        string Number { get; set; }
+
+        string NumberText { get; set; }
+
+        string Value { get; set; }
+
+        string ValueText { get; set; }
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/ILcSwitchList.cs
@@ -0,0 +1,12 @@
+﻿using PsdzClient.Core;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BMW.Rheingold.CoreFramework.Contracts.Vehicle
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    public interface ILcSwitchList : IList<ILcSwitch>, ICollection<ILcSwitch>, IEnumerable<ILcSwitch>, IEnumerable
+    {
+        new int Count { get; }
+    }
+}
