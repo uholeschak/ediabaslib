ff36fafd50c7dc75a22e2570e868ac322086fb95
uholeschak <ulrich@holeschak.de>
2026-08-17 13:04:06 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/LcSwitch.cs
@@ -0,0 +1,15 @@
+﻿using BMW.Rheingold.CoreFramework.Contracts.Vehicle;
+
+namespace BMW.Rheingold.CoreFramework.DatabaseProvider
+{
+    public class LcSwitch : ILcSwitch
+    {
+        public string Number { get; set; }
+
+        public string NumberText { get; set; }
+
+        public string Value { get; set; }
+
+        public string ValueText { get; set; }
+    }
+}
