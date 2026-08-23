bc2f1994040a3d101069458480151e5e48d097b9
uholeschak <ulrich@holeschak.de>
2026-08-23 09:56:29 +0200

    Added missing classes

--- Tools/Psdz/PsdzClientLibrary/Core/Authoring/IBackendCommunication.cs
+++ Tools/Psdz/PsdzClientLibrary/Core/Authoring/IBackendCommunication.cs
@@ -1,7 +1,8 @@
-﻿using PsdzClient;
+﻿using BMW.Authoring.API.ServiceDemand;
+using BMW.Authoring.API.ServiceRide;
+using PsdzClient;
 using PsdzClient.Core;
 using System.ComponentModel;
-using BMW.Authoring.API.ServiceRide;
 
 namespace BMW.Authoring.API
 {
@@ -11,6 +12,9 @@ namespace BMW.Authoring.API
     {
         [EditorBrowsable(EditorBrowsableState.Advanced)]
         IApiResult SendCustomerSimDataToBackend(string eid, string imei, string euicc);
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        IServiceDemandDataHandler GetServiceDemandDataHandler();
 
         [EditorBrowsable(EditorBrowsableState.Advanced)]
         IServiceRideDataHandler GetServiceRideDataHandler();
---
+++ Tools/Psdz/PsdzClientLibrary/Core/Authoring/IServiceDemandDataHandler.cs
@@ -0,0 +1,19 @@
+﻿using BMW.Authoring;
+using BMW.Authoring.API;
+using PsdzClient.Core;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BMW.Authoring.API.ServiceDemand
+{
+    [AuthorAPI(SelectableTypeDeclaration = false)]
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    public interface IServiceDemandDataHandler : IHideObjectMembers
+    {
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        IApiResult SendServiceDemandDataToBackend(Dictionary<string, string> pathValuePair);
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        void SendSpeedlinkDataToBackend();
+    }
+}
