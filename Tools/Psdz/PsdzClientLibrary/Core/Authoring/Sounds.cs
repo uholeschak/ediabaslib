1bc1de62e3622aec24740475f977b224cefa6f4a
uholeschak <ulrich@holeschak.de>
2026-08-23 22:04:02 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Authoring/Sounds.cs
@@ -0,0 +1,13 @@
+﻿using PsdzClient.Core;
+using System.ComponentModel;
+
+namespace BMW.Authoring.Helper
+{
+    [AuthorAPI(SelectableTypeDeclaration = true)]
+    [EditorBrowsable(EditorBrowsableState.Always)]
+    public enum Sounds
+    {
+        PositiveFeedback,
+        NegativeFeedback
+    }
+}
