fdb914356b2f162b9028b6d1e433a6ab4cd4b7c8
uholeschak <ulrich@holeschak.de>
2026-08-23 22:12:23 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/BATDemandDetailBATBO.cs
@@ -0,0 +1,27 @@
+﻿using BMW.ISPI.TRIC.ISTA.Contracts.Models.SeamLM2.Enums;
+
+namespace BMW.ISPI.TRIC.ISTA.Contracts.Models.SeamLM2
+{
+    public sealed class BATDemandDetailBATBO
+    {
+        public BatteryType? batteryType { get; set; }
+
+        public double? wearAgeComponent { get; set; }
+
+        public int? wearLifeTimePercentage { get; set; }
+
+        public int? travelledDistanceAtDetection { get; set; }
+
+        public int? capacity { get; set; }
+
+        public int? healthClass { get; set; }
+
+        public int? vdaHealthClass { get; set; }
+
+        public int? customerHealthClass { get; set; }
+
+        public int? startHealthClass { get; set; }
+
+        public int? stateOfChargeHealthClass { get; set; }
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/BatteryType.cs
@@ -0,0 +1,12 @@
+﻿namespace BMW.ISPI.TRIC.ISTA.Contracts.Models.SeamLM2.Enums
+{
+    public enum BatteryType
+    {
+        AGM,
+        EFB,
+        LFP,
+        LTO,
+        PbCa,
+        UNKNOWN
+    }
+}
