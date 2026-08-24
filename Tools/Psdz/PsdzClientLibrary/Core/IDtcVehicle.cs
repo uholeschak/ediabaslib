309462808689cba5b100c32fba9aa66766e06f35
uholeschak <ulrich@holeschak.de>
2026-08-24 16:41:27 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/IDtcVehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace BMW.ISPI.TRIC.ISTA.Contracts.Interfaces
+{
+    public interface IDtcVehicle
+    {
+        string ecuAddress { get; set; }
+
+        string dtcId { get; set; }
+
+        bool? Relevance { get; set; }
+
+        decimal? fOrt { get; set; }
+
+        string EcuDTCType { get; set; }
+    }
+}
