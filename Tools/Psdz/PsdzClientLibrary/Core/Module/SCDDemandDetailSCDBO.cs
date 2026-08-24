71159a9954c824f6723ca92f4658eabadfb5a199
uholeschak <ulrich@holeschak.de>
2026-08-24 02:18:08 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/SCDDemandDetailSCDBO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using BMW.ISPI.TRIC.ISTA.Contracts.Models.SeamLM2.Enums;
+
+namespace BMW.ISPI.TRIC.ISTA.Contracts.Models.SeamLM2
+{
+    public class SCDDemandDetailSCDBO
+    {
+        public string filterId { get; set; }
+
+        public double? filterVersion { get; set; }
+
+        public List<int> causingCcms { get; set; }
+
+        public double? accuracy { get; set; }
+
+        public string isarDocumentId { get; set; }
+
+        public SCDDataSource? dataSource { get; set; }
+    }
+}
