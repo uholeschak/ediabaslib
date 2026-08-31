4fe7e564238a6999ad965dc38c27ed153767f0d1
uholeschak <ulrich@holeschak.de>
2026-09-01 01:19:13 +0200

    Dummy button

--- Tools/Psdz/PsdzClientLibrary/Psdz/Service/ServiceProgramController.cs
+++ Tools/Psdz/PsdzClientLibrary/Psdz/Service/ServiceProgramController.cs
@@ -3,13 +3,14 @@ using BMW.ISPI.IstaOperation.Contract.ServiceProgram;
 using BMW.Rheingold.CoreFramework;
 using BMW.Rheingold.CoreFramework.DatabaseProvider;
 using BMW.Rheingold.CoreFramework.ServiceProgram;
+using PsdzClient;
+using PsdzClient.Core;
 using PsdzClient.Core.Container;
 using System;
 using System.Collections.Generic;
 using System.Threading;
-using PsdzClient;
 
-#pragma warning disable CS0169
+#pragma warning disable CS0169, CS0649
 namespace BMW.ISPI.IstaOperation.Impl
 {
     [PreserveSource(Hint="No update", SuppressWarning = true)]
@@ -39,7 +40,7 @@ namespace BMW.ISPI.IstaOperation.Impl
 
         //private ServiceProgramDocumentItem currenTestModuleDocument;
 
-        //private ModuleData currentModule;
+        private ModuleData currentModule;
 
         private CancellationTokenSource automaticForwardNavigationCTS;
 
@@ -62,12 +63,41 @@ namespace BMW.ISPI.IstaOperation.Impl
 
         public bool IsNextButtonPressedWithinTimePeriod()
         {
-            return DateTime.Now < lastTimeNextButtonPressed.AddMilliseconds(10000.0);
+            //[-] return DateTime.Now < lastTimeNextButtonPressed.AddMilliseconds(10000.0);
+            //[+] return true;
+            return true;
         }
 
         public ServiceProgramAction AwaitUserAction(int millisecondsTimeout)
         {
-            return null;
+            try
+            {
+                Log.Info("ServiceProgramController.AwaitUserAction()", "Wait on user input. Timeout: {0} ms", millisecondsTimeout);
+                if (IsNextButtonEnabled() && IsNextButtonPressedWithinTimePeriod())
+                {
+                    Log.Info("ServiceProgramController.AwaitUserAction()", "Next button was pressed within the defined time period ({0} ms). Continue immediately.", 10000);
+                    userAction = new ServiceProgramNavigationAction(NavigationAction.Next);
+                    return userAction;
+                }
+                if (millisecondsTimeout < 0)
+                {
+                    currentModule.ModuleState = ModuleExecutionStateType.idle;
+                }
+                userAction = null;
+                resumeEvent.Reset();
+                if ((millisecondsTimeout < 0) ? resumeEvent.WaitOne() : resumeEvent.WaitOne(millisecondsTimeout, exitContext: true))
+                {
+                    Log.Info("ServiceProgramController.AwaitUserAction()", "User input received.");
+                    return userAction;
+                }
+                Log.Info("ServiceProgramController.AwaitUserAction()", "Timeout reached.");
+                return null;
+            }
+            finally
+            {
+                lastTimeNextButtonPressed = DateTime.MinValue;
+                currentModule.ModuleState = ((currentModule.ModuleState != ModuleExecutionStateType.aborted && currentModule.ModuleState != ModuleExecutionStateType.error && currentModule.ModuleState != ModuleExecutionStateType.finished) ? ModuleExecutionStateType.running : currentModule.ModuleState);
+            }
         }
 
         public void ResetLastTimeNextButtonPressed()
@@ -105,7 +135,7 @@ namespace BMW.ISPI.IstaOperation.Impl
 
         public bool IsNextButtonEnabled()
         {
-            return false;
+            return true;
         }
 
         public void NavigateForward(int milliseconds = 0)
