c5e4a36b0da74118f059ee3dc36af66d627e5ce3
uholeschak <ulrich@holeschak.de>
2026-09-05 02:31:33 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/ISTA_Zeit.cs
@@ -0,0 +1,41 @@
+﻿using BMW.Rheingold.Module.ISTA;
+using PsdzClient.Core.Container;
+using System;
+
+namespace BMW.Rheingold.Module.ISTA
+{
+    public class ISTA_Zeit : ISTAModule
+    {
+        public ISTA_Zeit(ParameterContainer InParameter)
+        {
+            if (InParameter != null)
+            {
+                _globalModuleInParameter = InParameter;
+            }
+            __handleInParameter();
+        }
+
+        public virtual void Prepare()
+        {
+        }
+
+        public virtual void Reset()
+        {
+        }
+
+        public virtual void Datum(ref string Datum_String, ref int Datum_Jahr, ref int Datum_Monat, ref int Datum_Tag, ref int Datum_JJJJMMTT)
+        {
+            int num = 0;
+            Logger.WriteInformation("Datumcalled");
+            int year = DateTime.UtcNow.Year;
+            int month = DateTime.UtcNow.Month;
+            int day = DateTime.UtcNow.Day;
+            Datum_String = $"{year:0000}" + "/" + $"{month:00}" + "/" + $"{day:00}";
+            Datum_Jahr = year;
+            Datum_Monat = month;
+            Datum_Tag = day;
+            Datum_JJJJMMTT = Convert.ToInt32($"{year:0000}" + $"{month:00}" + $"{day:00}");
+            Logger.WriteInformation("_ExitIndex is: {0}", num);
+        }
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/IstaZeitCmd.cs
@@ -0,0 +1,50 @@
+﻿using BMW.Rheingold.CoreFramework;
+using BMW.Rheingold.Module.ISTA;
+using PsdzClient.Core;
+using PsdzClient.Core.Container;
+
+namespace BMW.Rheingold.Module.ISTA
+{
+    internal class IstaZeitCmd : ServiceDialogCmdBase
+    {
+        public IstaZeitCmd(ISTAModule callingModule, string methodName, string path, IModuleExecutionParent globalTabModuleISTA, int elementNo)
+            : base(callingModule, methodName, path, globalTabModuleISTA, elementNo)
+        {
+        }
+
+        public override void CreateDialog(ParameterContainer inParam, ParameterContainer inoutParam)
+        {
+            Log.Info("IstaZeitCmd.CreateDialog()", "ISTA_Zeit init started.");
+            base.Display = false;
+        }
+
+        public override void DoInvoke(string method, ParameterContainer inParam, ParameterContainer outParam, ParameterContainer inoutParam)
+        {
+            if (base.CallingModule == null)
+            {
+                Log.Error("IstaZeitCmd.Invoke()", "Failed to invoke method {0}, because calling module is null.", method);
+                return;
+            }
+            ModuleParameter value = base.CallingModule.__RheinGoldCoreModuleParameters__.Clone();
+            inParam.Parameter.Add("__RheinGoldCoreModuleParameters__", value);
+            inParam.Parameter.Add("__RheinGoldTabModuleISTA__", base.CallingModule.GlobalTabModuleISTA);
+            inParam.Parameter.Add("__RheinGoldSOCAccessor__", base.CallingModule.SOCAccessor);
+            if ("Datum".Equals(method))
+            {
+                string Datum_String = string.Empty;
+                int Datum_Jahr = 0;
+                int Datum_Monat = 0;
+                int Datum_Tag = 0;
+                int Datum_JJJJMMTT = 0;
+                new ISTA_Zeit(inParam).Datum(ref Datum_String, ref Datum_Jahr, ref Datum_Monat, ref Datum_Tag, ref Datum_JJJJMMTT);
+                outParam.setParameter("Datum_String", Datum_String);
+                outParam.setParameter("Datum_Jahr", Datum_Jahr);
+                outParam.setParameter("Datum_Monat", Datum_Monat);
+                outParam.setParameter("Datum_Tag", Datum_Tag);
+                outParam.setParameter("Datum_JJJJMMTT", Datum_JJJJMMTT);
+                return;
+            }
+            throw new ServiceDialogMethodUnsupportedException();
+        }
+    }
+}
