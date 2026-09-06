4a0929d3c6342dae5ea5ca9c04b10cf0f5909024
uholeschak <ulrich@holeschak.de>
2026-09-06 18:23:32 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Database/FaultCodeIdDtcFOrtEcuVariantKey.cs
@@ -0,0 +1,48 @@
+﻿using BMW.Rheingold.CoreFramework.DatabaseProvider.DatabaseProviderHelper;
+using System;
+
+namespace BMW.Rheingold.CoreFramework.DatabaseProvider
+{
+    public sealed class FaultCodeIdDtcFOrtEcuVariantKey : DtcFOrtEcuVariantKey, IEquatable<FaultCodeIdDtcFOrtEcuVariantKey>
+    {
+        public decimal FaultId { get; private set; }
+
+        public FaultCodeIdDtcFOrtEcuVariantKey(decimal faultId, string dtcFOrt, string ecuVariant)
+            : base(dtcFOrt, ecuVariant)
+        {
+            FaultId = faultId;
+        }
+
+        public DtcFOrtEcuVariantKey GetDtcFOrtEcuVariantKey()
+        {
+            return new DtcFOrtEcuVariantKey(base.DtcF_Ort, base.EcuVariant);
+        }
+
+        public bool Equals(FaultCodeIdDtcFOrtEcuVariantKey other)
+        {
+            if (other != null && FaultId == other.FaultId && string.Equals(base.DtcF_Ort, other.DtcF_Ort, StringComparison.OrdinalIgnoreCase))
+            {
+                return string.Equals(base.EcuVariant, other.EcuVariant, StringComparison.OrdinalIgnoreCase);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return ((17 * 23 + FaultId.GetHashCode()) * 23 + base.DtcF_Ort.GetHashCode()) * 23 + base.EcuVariant.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is FaultCodeIdDtcFOrtEcuVariantKey))
+            {
+                return false;
+            }
+            if (this == obj)
+            {
+                return true;
+            }
+            return Equals(obj as FaultCodeIdDtcFOrtEcuVariantKey);
+        }
+    }
+}
