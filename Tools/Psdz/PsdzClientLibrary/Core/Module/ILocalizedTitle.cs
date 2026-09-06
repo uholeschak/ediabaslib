d196990d30e2b6c7ad54e94ada21a5dd196d9c06
uholeschak <ulrich@holeschak.de>
2026-09-06 11:19:17 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/ILocalizedTitle.cs
@@ -0,0 +1,9 @@
+﻿namespace BMW.Rheingold.CoreFramework.DatabaseProvider
+{
+    public interface ILocalizedTitle
+    {
+        string Title { get; }
+
+        string GetLocalizedTitle(string language);
+    }
+}
