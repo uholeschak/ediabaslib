b6d265f3857774baf63b7286ce4ff8befbfd5ac0
uholeschak <ulrich@holeschak.de>
2026-09-06 18:10:57 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Database/IXepEcuGroups.cs
@@ -0,0 +1,11 @@
+﻿namespace BMW.Rheingold.CoreFramework.DatabaseProvider
+{
+    public interface IXepEcuGroups
+    {
+        string Name { get; }
+
+        decimal Id { get; }
+
+        decimal DiagnosticAddress { get; }
+    }
+}
