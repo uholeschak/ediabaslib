6fb0044c82afbc9ee8cf63beffcf034438db0230
uholeschak <ulrich@holeschak.de>
2026-09-07 18:40:59 +0200

    Source sync

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/IXepSaLaPasRuleEvaluation.cs
@@ -0,0 +1,9 @@
+﻿namespace BMW.ISPI.TRIC.ISTA.Contracts.Interfaces
+{
+    public interface IXepSaLaPasRuleEvaluation
+    {
+        string Name { get; }
+
+        string ProductType { get; }
+    }
+}
