1dda85a4d1d7a9ce256af290640fc2bb6431fb1d
uholeschak <ulrich@holeschak.de>
2026-09-08 15:14:03 +0200

    Added missing classes

---
+++ Tools/Psdz/PsdzClientLibrary/Core/Database/XEP_ECUPROGRAMMINGVARIANT.cs
@@ -0,0 +1,122 @@
+﻿using System.ComponentModel;
+using BMW.ISPI.TRIC.ISTA.Contracts.Interfaces;
+
+namespace BMW.Rheingold.CoreFramework.DatabaseProvider
+{
+    public class XEP_ECUPROGRAMMINGVARIANT : INotifyPropertyChanged, IXepEcuProgrammingVariant
+    {
+        private decimal idField;
+
+        private string nameField;
+
+        private decimal? flashLimitField;
+
+        private decimal ecuVariantIdField;
+
+        public decimal Id
+        {
+            get
+            {
+                return idField;
+            }
+            set
+            {
+                if (!idField.Equals(value))
+                {
+                    idField = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return nameField;
+            }
+            set
+            {
+                if (nameField == null || !nameField.Equals(value))
+                {
+                    nameField = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public decimal? FlashLimit
+        {
+            get
+            {
+                return flashLimitField;
+            }
+            set
+            {
+                if (!flashLimitField.HasValue || !flashLimitField.Equals(value))
+                {
+                    flashLimitField = value;
+                    OnPropertyChanged("FlashLimit");
+                }
+            }
+        }
+
+        public decimal EcuVariantId
+        {
+            get
+            {
+                return ecuVariantIdField;
+            }
+            set
+            {
+                if (!ecuVariantIdField.Equals(value))
+                {
+                    ecuVariantIdField = value;
+                    OnPropertyChanged("EcuVariantId");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public virtual XEP_ECUPROGRAMMINGVARIANT Clone()
+        {
+            return (XEP_ECUPROGRAMMINGVARIANT)MemberwiseClone();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is XEP_ECUPROGRAMMINGVARIANT xEP_ECUPROGRAMMINGVARIANT))
+            {
+                return false;
+            }
+            if (Id != xEP_ECUPROGRAMMINGVARIANT.Id)
+            {
+                return false;
+            }
+            if (string.CompareOrdinal(Name, xEP_ECUPROGRAMMINGVARIANT.Name) != 0)
+            {
+                return false;
+            }
+            if (!(FlashLimit == xEP_ECUPROGRAMMINGVARIANT.FlashLimit))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ Tools/Psdz/PsdzClientLibrary/Core/Module/IXepEcuProgrammingVariant.cs
@@ -0,0 +1,13 @@
+﻿namespace BMW.ISPI.TRIC.ISTA.Contracts.Interfaces
+{
+    public interface IXepEcuProgrammingVariant
+    {
+        decimal EcuVariantId { get; }
+
+        string Name { get; }
+
+        decimal Id { get; }
+
+        decimal? FlashLimit { get; }
+    }
+}
